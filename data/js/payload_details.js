fa983b8f9bf3bf8d0d9462b8d2735044f8e4592f
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-03-20 17:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62448,
-	"created_at": "Mon Mar 20 11:21:07 +0000 2017",
+	"tweets": 62449,
+	"created_at": "Mon Mar 20 16:21:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,298 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Mon Mar 20 16:09:58 +0000 2017",
+		"id": 843857153140101100,
+		"id_str": "843857153140101120",
+		"text": "Het begint ineens te rieken naar een fraai staaltje influencer marketing gedoe. Vanavond DWDD en RTLLN itempje? https://t.co/oOewEbcmJf",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oOewEbcmJf",
+					"expanded_url": "https://twitter.com/upcoming/status/843844832292409345",
+					"display_url": "twitter.com/upcoming/statu…",
+					"indices": [
+						112,
+						135
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! |",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9219,
+			"friends_count": 1838,
+			"listed_count": 625,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3155,
+			"utc_offset": 3600,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62260,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1487681446",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 843844832292409300,
+		"quoted_status_id_str": "843844832292409345",
+		"quoted_status": {
+			"created_at": "Mon Mar 20 15:21:00 +0000 2017",
+			"id": 843844832292409300,
+			"id_str": "843844832292409345",
+			"text": "Iemand Photoshopt naakte mannen in Google Map-foto's en het is hilarisch https://t.co/pyUdD5lnWD https://t.co/PH3bUFoNnc",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/pyUdD5lnWD",
+						"expanded_url": "http://www.upcoming.nl/rogerbloem/20481/iemand-photoshopt-naakte-mannen-in-google-map-fotos-en-het-is-hilarisch",
+						"display_url": "upcoming.nl/rogerbloem/204…",
+						"indices": [
+							73,
+							96
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 843844830568599600,
+						"id_str": "843844830568599552",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/C7XwZwuXUAASDgd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C7XwZwuXUAASDgd.jpg",
+						"url": "https://t.co/PH3bUFoNnc",
+						"display_url": "pic.twitter.com/PH3bUFoNnc",
+						"expanded_url": "https://twitter.com/upcoming/status/843844832292409345/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 874,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 280,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 874,
+								"h": 360,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 843844830568599600,
+						"id_str": "843844830568599552",
+						"indices": [
+							97,
+							120
+						],
+						"media_url": "http://pbs.twimg.com/media/C7XwZwuXUAASDgd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C7XwZwuXUAASDgd.jpg",
+						"url": "https://t.co/PH3bUFoNnc",
+						"display_url": "pic.twitter.com/PH3bUFoNnc",
+						"expanded_url": "https://twitter.com/upcoming/status/843844832292409345/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 874,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 280,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 874,
+								"h": 360,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.upcoming.nl\" rel=\"nofollow\">Upcoming</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1074155208,
+				"id_str": "1074155208",
+				"name": "Upcoming",
+				"screen_name": "upcoming",
+				"location": "Nederland",
+				"description": "Upcoming is de plek waar je de populairste internet-trends van Nederland kunt ontdekken en delen met je vrienden.   \nCheck https://t.co/313tWBmpwQ",
+				"url": "http://t.co/WNmpqpbDbM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/WNmpqpbDbM",
+								"expanded_url": "http://www.upcoming.nl",
+								"display_url": "upcoming.nl",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/313tWBmpwQ",
+								"expanded_url": "http://Upcoming.nl/app",
+								"display_url": "Upcoming.nl/app",
+								"indices": [
+									123,
+									146
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 9338,
+				"friends_count": 489,
+				"listed_count": 96,
+				"created_at": "Wed Jan 09 15:59:33 +0000 2013",
+				"favourites_count": 176,
+				"utc_offset": 3600,
+				"time_zone": "Amsterdam",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 9432,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "E84A3E",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000171462392/HTi-JiMS.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000171462392/HTi-JiMS.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/425296540362485760/76ks5vor_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/425296540362485760/76ks5vor_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1074155208/1390223598",
+				"profile_link_color": "E84A3E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Mon Mar 20 11:18:34 +0000 2017",
 		"id": 843783820042289200,
 		"id_str": "843783820042289152",
