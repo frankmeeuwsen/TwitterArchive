000a2d8913497d37a2d970cc697dec77d5784db8
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-03-20 09:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62446,
-	"created_at": "Sun Mar 19 15:21:05 +0000 2017",
+	"tweets": 62447,
+	"created_at": "Mon Mar 20 08:21:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,242 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Mon Mar 20 08:01:45 +0000 2017",
+		"id": 843734291943166000,
+		"id_str": "843734291943165952",
+		"text": "RT @brechtjedeleij: De toekomst van media in de wereld van smart homes, connected cars en smart cities https://t.co/Ar79Q6EDKw #mobile #med…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "mobile",
+					"indices": [
+						127,
+						134
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "brechtjedeleij",
+					"name": "brechtjedeleij",
+					"id": 9880322,
+					"id_str": "9880322",
+					"indices": [
+						3,
+						18
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ar79Q6EDKw",
+					"expanded_url": "http://www.b-digitized.com/2017/03/17/de-toekomst-van-media-in-de-wereld-van-smart-homes-connected-cars-en-smart-cities/",
+					"display_url": "b-digitized.com/2017/03/17/de-…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! |",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9219,
+			"friends_count": 1838,
+			"listed_count": 625,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3155,
+			"utc_offset": 3600,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62258,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1487681446",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Mar 20 07:09:41 +0000 2017",
+			"id": 843721189260509200,
+			"id_str": "843721189260509184",
+			"text": "De toekomst van media in de wereld van smart homes, connected cars en smart cities https://t.co/Ar79Q6EDKw #mobile #media",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "mobile",
+						"indices": [
+							107,
+							114
+						]
+					},
+					{
+						"text": "media",
+						"indices": [
+							115,
+							121
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Ar79Q6EDKw",
+						"expanded_url": "http://www.b-digitized.com/2017/03/17/de-toekomst-van-media-in-de-wereld-van-smart-homes-connected-cars-en-smart-cities/",
+						"display_url": "b-digitized.com/2017/03/17/de-…",
+						"indices": [
+							83,
+							106
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 9880322,
+				"id_str": "9880322",
+				"name": "brechtjedeleij",
+				"screen_name": "brechtjedeleij",
+				"location": "Amsterdam",
+				"description": "Sr Productmanager | Mobile Expert | Shares stuff on mobile, digital & (social) media | Jokes around | Lady Geek | Quote's Heersende Twitteraar | Views my own.",
+				"url": "https://t.co/nPWjn2uJ9A",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/nPWjn2uJ9A",
+								"expanded_url": "https://www.managementboek.nl/boek/9789492221339/mobiele-eenheid-brechtje-de-leij",
+								"display_url": "managementboek.nl/boek/978949222…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 15721,
+				"friends_count": 4536,
+				"listed_count": 631,
+				"created_at": "Fri Nov 02 10:09:10 +0000 2007",
+				"favourites_count": 7868,
+				"utc_offset": 3600,
+				"time_zone": "Amsterdam",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 133660,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000057634328/bab720d8e92fc155f18148daedf18b91.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000057634328/bab720d8e92fc155f18148daedf18b91.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/735919343024930818/xZY11f58_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/735919343024930818/xZY11f58_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/9880322/1393751741",
+				"profile_link_color": "1A1A18",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Sun Mar 19 15:11:56 +0000 2017",
 		"id": 843480163849592800,
 		"id_str": "843480163849592832",
