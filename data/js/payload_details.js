d3bb4182422c4ad3b23fab01926da495c6f085c3
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-04-02 00:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62602,
-	"created_at": "Sat Apr 01 15:21:03 +0000 2017",
+	"tweets": 62603,
+	"created_at": "Sat Apr 01 22:21:04 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_04.js
+++ data/js/tweets/2017_04.js
@@ -1,6 +1,265 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Sat Apr 01 21:35:11 +0000 2017",
+		"id": 848287651279110100,
+		"id_str": "848287651279110144",
+		"text": "Een 1 april grap rondom fake news... erg meta maar zou wel mooi zijn. https://t.co/Oczdi8jC6K",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Oczdi8jC6K",
+					"expanded_url": "https://twitter.com/socialmedia411/status/848238295217844224",
+					"display_url": "twitter.com/socialmedia411…",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! |",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9210,
+			"friends_count": 1843,
+			"listed_count": 624,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3177,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62233,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1487681446",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 848238295217844200,
+		"quoted_status_id_str": "848238295217844224",
+		"quoted_status": {
+			"created_at": "Sat Apr 01 18:19:03 +0000 2017",
+			"id": 848238295217844200,
+			"id_str": "848238295217844224",
+			"text": "Sad but true ....... https://t.co/oC7X05eXkC",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 848238292537626600,
+						"id_str": "848238292537626625",
+						"indices": [
+							21,
+							44
+						],
+						"media_url": "http://pbs.twimg.com/media/C8WMO5-W0AEuLiV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C8WMO5-W0AEuLiV.jpg",
+						"url": "https://t.co/oC7X05eXkC",
+						"display_url": "pic.twitter.com/oC7X05eXkC",
+						"expanded_url": "https://twitter.com/SocialMedia411/status/848238295217844224/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 742,
+								"h": 409,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 742,
+								"h": 409,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 375,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 848238292537626600,
+						"id_str": "848238292537626625",
+						"indices": [
+							21,
+							44
+						],
+						"media_url": "http://pbs.twimg.com/media/C8WMO5-W0AEuLiV.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C8WMO5-W0AEuLiV.jpg",
+						"url": "https://t.co/oC7X05eXkC",
+						"display_url": "pic.twitter.com/oC7X05eXkC",
+						"expanded_url": "https://twitter.com/SocialMedia411/status/848238295217844224/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 742,
+								"h": 409,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 742,
+								"h": 409,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 375,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.hootsuite.com\" rel=\"nofollow\">Hootsuite</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15859039,
+				"id_str": "15859039",
+				"name": "Social Media Insider",
+				"screen_name": "SocialMedia411",
+				"location": "San Francisco, CA",
+				"description": "Highly recommended by people who like things that don't suck.  Created/Curated by @UniqueVisitor",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 253957,
+				"friends_count": 34903,
+				"listed_count": 12653,
+				"created_at": "Fri Aug 15 02:43:55 +0000 2008",
+				"favourites_count": 231,
+				"utc_offset": -18000,
+				"time_zone": "Central Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 35315,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/884903361/4fb0a2ea197f53eece2c27c77c35f96c.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/884903361/4fb0a2ea197f53eece2c27c77c35f96c.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/673030519899164672/BQiUi8Em_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/673030519899164672/BQiUi8Em_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15859039/1399522295",
+				"profile_link_color": "0000FF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E9E9E7",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 33,
+			"favorite_count": 29,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Sat Apr 01 14:24:57 +0000 2017",
 		"id": 848179380992045000,
 		"id_str": "848179380992045056",
