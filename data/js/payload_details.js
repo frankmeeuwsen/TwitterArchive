68716a7ff714a7a1df80c29ddceb1da4065673c8
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-04-13 10:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62722,
-	"created_at": "Wed Apr 12 20:21:05 +0000 2017",
+	"tweets": 62723,
+	"created_at": "Thu Apr 13 08:21:05 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_04.js",
 		"year": 2017,
 		"var_name": "tweets_2017_04",
-		"tweet_count": 127,
+		"tweet_count": 128,
 		"month": 4
 	},
 	{
