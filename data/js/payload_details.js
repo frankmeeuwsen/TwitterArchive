29d711077221aeb28cd5f7743409c57539b8fb90
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-05-21 17:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63018,
-	"created_at": "Sat May 20 22:21:08 +0000 2017",
+	"tweets": 63019,
+	"created_at": "Sun May 21 15:21:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_05.js
+++ data/js/tweets/2017_05.js
@@ -1,6 +1,229 @@
 Grailbird.data.tweets_2017_05 = 
 [
 	{
+		"created_at": "Sun May 21 15:13:49 +0000 2017",
+		"id": 866311070872469500,
+		"id_str": "866311070872469504",
+		"text": "RT @DANIELLEBKNYC: Super excited to be finishing my new Beastie Boys Pauls Boutique wall today starting at 1pm at… https://t.co/AZLa1EXuT2",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DANIELLEBKNYC",
+					"name": "DANIELLE MASTRION",
+					"id": 20578222,
+					"id_str": "20578222",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AZLa1EXuT2",
+					"expanded_url": "https://www.instagram.com/p/BUWkxKPgBve/",
+					"display_url": "instagram.com/p/BUWkxKPgBve/",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9256,
+			"friends_count": 1821,
+			"listed_count": 619,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3261,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62040,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/838040225628762112/WeV3XjHa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun May 21 11:28:45 +0000 2017",
+			"id": 866254430013935600,
+			"id_str": "866254430013935616",
+			"text": "Super excited to be finishing my new Beastie Boys Pauls Boutique wall today starting at 1pm at… https://t.co/AZLa1EXuT2",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/AZLa1EXuT2",
+						"expanded_url": "https://www.instagram.com/p/BUWkxKPgBve/",
+						"display_url": "instagram.com/p/BUWkxKPgBve/",
+						"indices": [
+							96,
+							119
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20578222,
+				"id_str": "20578222",
+				"name": "DANIELLE MASTRION",
+				"screen_name": "DANIELLEBKNYC",
+				"location": "BROOKLYN NYC",
+				"description": "ARTIST . PAINTER . MURALIST . TRAVELLER . YOGI . BEACH BUM .",
+				"url": "https://t.co/JTYuO9GYFB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JTYuO9GYFB",
+								"expanded_url": "http://www.daniellemastrion.com/",
+								"display_url": "daniellemastrion.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 954,
+				"friends_count": 631,
+				"listed_count": 33,
+				"created_at": "Wed Feb 11 08:15:08 +0000 2009",
+				"favourites_count": 1086,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 17915,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0D0C0C",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/448289059840856064/djB4XiBe.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/448289059840856064/djB4XiBe.jpeg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/843926715437137922/AsPi3HJn_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/843926715437137922/AsPi3HJn_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/20578222/1490042783",
+				"profile_link_color": "9266CC",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "0F0F0F",
+				"profile_text_color": "FF0894",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat May 20 22:16:33 +0000 2017",
 		"id": 866055067828453400,
 		"id_str": "866055067828453380",
