43a66159ad33de1836a0265ea17289b2004d8457
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-07-08 18:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63402,
-	"created_at": "Sat Jul 08 07:21:04 +0000 2017",
+	"tweets": 63403,
+	"created_at": "Sat Jul 08 16:21:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,373 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Sat Jul 08 15:54:15 +0000 2017",
+		"id": 883715864301899800,
+		"id_str": "883715864301899777",
+		"text": "RT @metalband_exe: Fun of thë Dragon https://t.co/4VMgUhUuUv",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "metalband_exe",
+					"name": "MetalBand.exe",
+					"id": 4406413835,
+					"id_str": "4406413835",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 883702012721991700,
+					"id_str": "883702012721991680",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+					"media_url_https": "https://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+					"url": "https://t.co/4VMgUhUuUv",
+					"display_url": "pic.twitter.com/4VMgUhUuUv",
+					"expanded_url": "https://twitter.com/metalband_exe/status/883702016224178176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 883702016224178200,
+					"source_status_id_str": "883702016224178176",
+					"source_user_id": 4406413835,
+					"source_user_id_str": "4406413835"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 883702012721991700,
+					"id_str": "883702012721991680",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+					"media_url_https": "https://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+					"url": "https://t.co/4VMgUhUuUv",
+					"display_url": "pic.twitter.com/4VMgUhUuUv",
+					"expanded_url": "https://twitter.com/metalband_exe/status/883702016224178176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 883702016224178200,
+					"source_status_id_str": "883702016224178176",
+					"source_user_id": 4406413835,
+					"source_user_id_str": "4406413835"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9235,
+			"friends_count": 1837,
+			"listed_count": 616,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3350,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61666,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Jul 08 14:59:13 +0000 2017",
+			"id": 883702016224178200,
+			"id_str": "883702016224178176",
+			"text": "Fun of thë Dragon https://t.co/4VMgUhUuUv",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 883702012721991700,
+						"id_str": "883702012721991680",
+						"indices": [
+							18,
+							41
+						],
+						"media_url": "http://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+						"media_url_https": "https://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+						"url": "https://t.co/4VMgUhUuUv",
+						"display_url": "pic.twitter.com/4VMgUhUuUv",
+						"expanded_url": "https://twitter.com/metalband_exe/status/883702016224178176/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 600,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 883702012721991700,
+						"id_str": "883702012721991680",
+						"indices": [
+							18,
+							41
+						],
+						"media_url": "http://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+						"media_url_https": "https://pbs.twimg.com/media/DEOKTGuXkAACqOo.png",
+						"url": "https://t.co/4VMgUhUuUv",
+						"display_url": "pic.twitter.com/4VMgUhUuUv",
+						"expanded_url": "https://twitter.com/metalband_exe/status/883702016224178176/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 600,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://mikewatson.me/bots/metalband_exe\" rel=\"nofollow\">metalband.exe</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4406413835,
+				"id_str": "4406413835",
+				"name": "MetalBand.exe",
+				"screen_name": "metalband_exe",
+				"location": "",
+				"description": "Brutally generating metal band names and logos every four hours. By @mike_watson",
+				"url": "https://t.co/dQeynO0i7Q",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/dQeynO0i7Q",
+								"expanded_url": "http://mikewatson.me/bots/metalband_exe",
+								"display_url": "mikewatson.me/bots/metalband…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3078,
+				"friends_count": 2,
+				"listed_count": 94,
+				"created_at": "Mon Dec 07 16:09:38 +0000 2015",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 3478,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/674808771005169664/joYOHt22_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/674808771005169664/joYOHt22_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4406413835/1449721830",
+				"profile_link_color": "1A1B1F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 24,
+			"favorite_count": 39,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "cy"
+		},
+		"is_quote_status": false,
+		"retweet_count": 24,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "cy"
+	},
+	{
 		"created_at": "Sat Jul 08 06:48:34 +0000 2017",
 		"id": 883578537311121400,
 		"id_str": "883578537311121408",
