1908158e70d2a0188017ad7699f8a869d5ff3263
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-07-27 21:21:11 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63515,
-	"created_at": "Thu Jul 27 06:21:06 +0000 2017",
+	"tweets": 63516,
+	"created_at": "Thu Jul 27 19:21:10 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -1,6 +1,355 @@
 Grailbird.data.tweets_2017_07 = 
 [
 	{
+		"created_at": "Thu Jul 27 19:05:22 +0000 2017",
+		"id": 890649331795152900,
+		"id_str": "890649331795152896",
+		"text": "Metarickroll https://t.co/BGnwVyz87N",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BGnwVyz87N",
+					"expanded_url": "https://twitter.com/rickastley/status/890617797956456448",
+					"display_url": "twitter.com/rickastley/sta…",
+					"indices": [
+						13,
+						36
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9218,
+			"friends_count": 1852,
+			"listed_count": 616,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3379,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61665,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "cd661902b07eb657",
+			"url": "https://api.twitter.com/1.1/geo/id/cd661902b07eb657.json",
+			"place_type": "city",
+			"name": "Como",
+			"full_name": "Como, Lombardy",
+			"country_code": "IT",
+			"country": "Italy",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							9.0308616,
+							45.7586916
+						],
+						[
+							9.1367725,
+							45.7586916
+						],
+						[
+							9.1367725,
+							45.8420502
+						],
+						[
+							9.0308616,
+							45.8420502
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 890617797956456400,
+		"quoted_status_id_str": "890617797956456448",
+		"quoted_status": {
+			"created_at": "Thu Jul 27 17:00:04 +0000 2017",
+			"id": 890617797956456400,
+			"id_str": "890617797956456448",
+			"text": "30 years ago today I said I was Never Gonna Give You Up. I am a man of my word - Rick x https://t.co/VmbMQA6tQB",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 890578640697733100,
+						"id_str": "890578640697733121",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/890578640697733121/img/oDnKxUvvPnAZnowZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/890578640697733121/img/oDnKxUvvPnAZnowZ.jpg",
+						"url": "https://t.co/VmbMQA6tQB",
+						"display_url": "pic.twitter.com/VmbMQA6tQB",
+						"expanded_url": "https://twitter.com/rickastley/status/890617797956456448/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 600,
+								"h": 338,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 340,
+								"h": 191,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 576,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 890578640697733100,
+						"id_str": "890578640697733121",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/890578640697733121/img/oDnKxUvvPnAZnowZ.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/890578640697733121/img/oDnKxUvvPnAZnowZ.jpg",
+						"url": "https://t.co/VmbMQA6tQB",
+						"display_url": "pic.twitter.com/VmbMQA6tQB",
+						"expanded_url": "https://twitter.com/rickastley/status/890617797956456448/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 600,
+								"h": 338,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 340,
+								"h": 191,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 576,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 24640,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/890578640697733121/vid/640x360/oxajJC_9Lrje5B7N.mp4"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/890578640697733121/pl/p3hqvxj-dJXfhmoZ.m3u8"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/890578640697733121/vid/1280x720/cFKJ1TWLciveWFzP.mp4"
+								},
+								{
+									"bitrate": 320000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/890578640697733121/vid/320x180/8pMTQvczrgm2bB8Q.mp4"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "",
+							"description": "",
+							"embeddable": true,
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 148137271,
+				"id_str": "148137271",
+				"name": "Rick Astley",
+				"screen_name": "rickastley",
+				"location": "London",
+				"description": "Official Twitter page for Rick Astley. Doing gigs, writing songs, hosting my own radio show and drumming for a band. Taking time to enjoy it this time round.",
+				"url": "https://t.co/TAX5m1i3cu",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/TAX5m1i3cu",
+								"expanded_url": "http://www.rickastley.co.uk",
+								"display_url": "rickastley.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 81545,
+				"friends_count": 1835,
+				"listed_count": 504,
+				"created_at": "Tue May 25 23:17:00 +0000 2010",
+				"favourites_count": 9285,
+				"utc_offset": 3600,
+				"time_zone": "London",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 1823,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/818895112533147649/lHxlMRi3_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/818895112533147649/lHxlMRi3_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/148137271/1499706489",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 11352,
+			"favorite_count": 15848,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Jul 27 06:01:04 +0000 2017",
 		"id": 890451953666445300,
 		"id_str": "890451953666445314",
