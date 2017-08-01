bf072cfa386cef676a13a850fc52cf0c85be64d7
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-08-01 21:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63547,
-	"created_at": "Tue Aug 01 15:21:03 +0000 2017",
+	"tweets": 63548,
+	"created_at": "Tue Aug 01 19:21:05 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Tue Aug 01 19:00:30 +0000 2017",
+		"id": 892460046508068900,
+		"id_str": "892460046508068865",
+		"text": "RT @Bronneke: Lees hier het verhaal over mijn zoon die  ongeveer 48229 artsen zag voor er een diagnose werd gesteld. RT mag!\n\nhttps://t.co/…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Bronneke",
+					"name": "Bronja Prazdny",
+					"id": 101541823,
+					"id_str": "101541823",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9211,
+			"friends_count": 1857,
+			"listed_count": 616,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3385,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61623,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Aug 01 12:25:34 +0000 2017",
+			"id": 892360655436644400,
+			"id_str": "892360655436644352",
+			"text": "Lees hier het verhaal over mijn zoon die  ongeveer 48229 artsen zag voor er een diagnose werd gesteld. RT mag!\n\nhttps://t.co/WxA8rAVv87",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/WxA8rAVv87",
+						"expanded_url": "http://bronjaprazdny.nl/2017/07/31/witte-muur-eerste-levensjaar-zoon-anusatresie/",
+						"display_url": "bronjaprazdny.nl/2017/07/31/wit…",
+						"indices": [
+							112,
+							135
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": 892359634316668900,
+			"in_reply_to_status_id_str": "892359634316668930",
+			"in_reply_to_user_id": 101541823,
+			"in_reply_to_user_id_str": "101541823",
+			"in_reply_to_screen_name": "Bronneke",
+			"user": {
+				"id": 101541823,
+				"id_str": "101541823",
+				"name": "Bronja Prazdny",
+				"screen_name": "Bronneke",
+				"location": "Groningen, Nederland",
+				"description": "Journalist en schrijver. Tweede generatie allochtoon zonder identiteitsproblemen. 'Verloren taal' (@NWADAM '16) en 'Vrouwen met autisme' (Brevier '15).",
+				"url": "https://t.co/bQojUT2Nhh",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bQojUT2Nhh",
+								"expanded_url": "http://bronjaprazdny.nl",
+								"display_url": "bronjaprazdny.nl",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 922,
+				"friends_count": 646,
+				"listed_count": 38,
+				"created_at": "Sun Jan 03 18:34:20 +0000 2010",
+				"favourites_count": 3376,
+				"utc_offset": 7200,
+				"time_zone": "Amsterdam",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 29155,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "642D8B",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/411551003/380.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/411551003/380.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/889542140979818496/VzeQGUqB_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/889542140979818496/VzeQGUqB_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/101541823/1495907258",
+				"profile_link_color": "FF0000",
+				"profile_sidebar_border_color": "65B0DA",
+				"profile_sidebar_fill_color": "7AC3EE",
+				"profile_text_color": "3D1957",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Tue Aug 01 15:18:36 +0000 2017",
 		"id": 892404203511062500,
 		"id_str": "892404203511062528",
