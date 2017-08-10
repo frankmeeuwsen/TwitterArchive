ac7d363987fbdb06317e9e9273dbc6e72d411012
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-08-10 18:21:09 +0200

    Added 1 tweets

--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,195 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Thu Aug 10 15:28:31 +0000 2017",
+		"id": 895668190226862100,
+		"id_str": "895668190226862080",
+		"text": "I can't stand this snow white b*tch • r/youseeingthisshit https://t.co/NKPTRM3nQh https://t.co/tMoR69AqUc",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NKPTRM3nQh",
+					"expanded_url": "https://www.reddit.com/r/youseeingthisshit/comments/6srz93/i_cant_stand_this_snow_white_btch/",
+					"display_url": "reddit.com/r/youseeingthi…",
+					"indices": [
+						58,
+						81
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 895668187773251600,
+					"id_str": "895668187773251584",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/DG4NeKjXsAAOfDG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DG4NeKjXsAAOfDG.jpg",
+					"url": "https://t.co/tMoR69AqUc",
+					"display_url": "pic.twitter.com/tMoR69AqUc",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/895668190226862080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 140,
+							"h": 140,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 140,
+							"h": 140,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 140,
+							"h": 140,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 140,
+							"h": 140,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 895668187773251600,
+					"id_str": "895668187773251584",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/DG4NeKjXsAAOfDG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DG4NeKjXsAAOfDG.jpg",
+					"url": "https://t.co/tMoR69AqUc",
+					"display_url": "pic.twitter.com/tMoR69AqUc",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/895668190226862080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 140,
+							"h": 140,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 140,
+							"h": 140,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 140,
+							"h": 140,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 140,
+							"h": 140,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9219,
+			"friends_count": 1862,
+			"listed_count": 616,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3404,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61618,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Aug 10 11:36:11 +0000 2017",
 		"id": 895609721083772900,
 		"id_str": "895609721083772928",
