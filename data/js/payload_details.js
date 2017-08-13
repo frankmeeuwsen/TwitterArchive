d9b088fe06128aadd5c413cdb70b702880f2c9d2
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-08-13 16:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63645,
-	"created_at": "Sun Aug 13 08:21:05 +0000 2017",
+	"tweets": 63646,
+	"created_at": "Sun Aug 13 14:21:06 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,389 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Sun Aug 13 13:44:22 +0000 2017",
+		"id": 896729141860606000,
+		"id_str": "896729141860605952",
+		"text": "RT @OldSchool80s: Aug 13, 1982, 35 years ago, Fast Times at Ridgemont High was released in theaters. #80s https://t.co/JLIU6AxBql",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "80s",
+					"indices": [
+						101,
+						105
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OldSchool80s",
+					"name": "Old School 80s",
+					"id": 31015297,
+					"id_str": "31015297",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 896728075219685400,
+					"id_str": "896728075219685376",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+					"url": "https://t.co/JLIU6AxBql",
+					"display_url": "pic.twitter.com/JLIU6AxBql",
+					"expanded_url": "https://twitter.com/OldSchool80s/status/896728077539065856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 535,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 535,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 535,
+							"h": 400,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 896728077539065900,
+					"source_status_id_str": "896728077539065856",
+					"source_user_id": 31015297,
+					"source_user_id_str": "31015297"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 896728075219685400,
+					"id_str": "896728075219685376",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+					"url": "https://t.co/JLIU6AxBql",
+					"display_url": "pic.twitter.com/JLIU6AxBql",
+					"expanded_url": "https://twitter.com/OldSchool80s/status/896728077539065856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 535,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 535,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 535,
+							"h": 400,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 896728077539065900,
+					"source_status_id_str": "896728077539065856",
+					"source_user_id": 31015297,
+					"source_user_id_str": "31015297"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9216,
+			"friends_count": 1863,
+			"listed_count": 616,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3406,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61614,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Aug 13 13:40:08 +0000 2017",
+			"id": 896728077539065900,
+			"id_str": "896728077539065856",
+			"text": "Aug 13, 1982, 35 years ago, Fast Times at Ridgemont High was released in theaters. #80s https://t.co/JLIU6AxBql",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "80s",
+						"indices": [
+							83,
+							87
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 896728075219685400,
+						"id_str": "896728075219685376",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+						"url": "https://t.co/JLIU6AxBql",
+						"display_url": "pic.twitter.com/JLIU6AxBql",
+						"expanded_url": "https://twitter.com/OldSchool80s/status/896728077539065856/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 535,
+								"h": 400,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 535,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 535,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 896728075219685400,
+						"id_str": "896728075219685376",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DHHRbu-W0AAlzD6.jpg",
+						"url": "https://t.co/JLIU6AxBql",
+						"display_url": "pic.twitter.com/JLIU6AxBql",
+						"expanded_url": "https://twitter.com/OldSchool80s/status/896728077539065856/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 535,
+								"h": 400,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 535,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 535,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.hootsuite.com\" rel=\"nofollow\">Hootsuite</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 31015297,
+				"id_str": "31015297",
+				"name": "Old School 80s",
+				"screen_name": "OldSchool80s",
+				"location": "Shermer, Illinois circa '80s",
+				"description": "One dude's perspective on all things pop culture w/ special attention paid to the awesome 80s decade  including exclusive interviews w/ some who made it so",
+				"url": "https://t.co/bAzV4JJ7ML",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bAzV4JJ7ML",
+								"expanded_url": "http://www.Facebook.com/KickinItOldSchool80s",
+								"display_url": "Facebook.com/KickinItOldSch…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 69839,
+				"friends_count": 367,
+				"listed_count": 553,
+				"created_at": "Tue Apr 14 01:13:52 +0000 2009",
+				"favourites_count": 487,
+				"utc_offset": -18000,
+				"time_zone": "Central Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 15779,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/8871817/i_love_the_80s.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/8871817/i_love_the_80s.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/499733959148711936/KFV83fHA_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/499733959148711936/KFV83fHA_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/31015297/1415467401",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 97,
+			"favorite_count": 140,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 97,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sun Aug 13 08:00:12 +0000 2017",
 		"id": 896642530527383600,
 		"id_str": "896642530527383552",
