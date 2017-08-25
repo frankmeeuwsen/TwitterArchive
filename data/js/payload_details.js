bfb132cbc67c4c53490b44d7df16c4bfda68a0b8
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-08-25 09:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63732,
-	"created_at": "Fri Aug 25 06:21:07 +0000 2017",
+	"tweets": 63733,
+	"created_at": "Fri Aug 25 07:21:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,402 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Fri Aug 25 06:56:54 +0000 2017",
+		"id": 900975253681315800,
+		"id_str": "900975253681315840",
+		"text": "RT @SensemakersA: Url's truncated on mobile😮 https://t.co/mr1bNc8K1E",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SensemakersA",
+					"name": "IoT Sensemakers AMS",
+					"id": 747697406603575300,
+					"id_str": "747697406603575296",
+					"indices": [
+						3,
+						16
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mr1bNc8K1E",
+					"expanded_url": "https://twitter.com/ericlaw/status/900429796240277504",
+					"display_url": "twitter.com/ericlaw/status…",
+					"indices": [
+						45,
+						68
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9220,
+			"friends_count": 1874,
+			"listed_count": 613,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3423,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61533,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Aug 25 06:04:38 +0000 2017",
+			"id": 900962099941093400,
+			"id_str": "900962099941093376",
+			"text": "Url's truncated on mobile😮 https://t.co/mr1bNc8K1E",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mr1bNc8K1E",
+						"expanded_url": "https://twitter.com/ericlaw/status/900429796240277504",
+						"display_url": "twitter.com/ericlaw/status…",
+						"indices": [
+							27,
+							50
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 747697406603575300,
+				"id_str": "747697406603575296",
+				"name": "IoT Sensemakers AMS",
+				"screen_name": "SensemakersA",
+				"location": "Amsterdam",
+				"description": "A community driven learning platform for meaningful creation of IoT, where people share their knowledge and network. Kindly hosted @ MakerversityAMS #IoT",
+				"url": "https://t.co/scf3QPGhz2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/scf3QPGhz2",
+								"expanded_url": "http://www.meetup.com/sensemakersams/",
+								"display_url": "meetup.com/sensemakersams/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 695,
+				"friends_count": 649,
+				"listed_count": 120,
+				"created_at": "Tue Jun 28 07:45:28 +0000 2016",
+				"favourites_count": 761,
+				"utc_offset": 7200,
+				"time_zone": "Amsterdam",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1630,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/755872078629404672/yVDHm8fo_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/755872078629404672/yVDHm8fo_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/747697406603575296/1480699981",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 900429796240277500,
+			"quoted_status_id_str": "900429796240277504",
+			"quoted_status": {
+				"created_at": "Wed Aug 23 18:49:27 +0000 2017",
+				"id": 900429796240277500,
+				"id_str": "900429796240277504",
+				"text": "Okay, we have a winner. https://t.co/deZ0Ol1eiB",
+				"truncated": false,
+				"entities": {
+					"hashtags": [],
+					"symbols": [],
+					"user_mentions": [],
+					"urls": [],
+					"media": [
+						{
+							"id": 900429789449797600,
+							"id_str": "900429789449797632",
+							"indices": [
+								24,
+								47
+							],
+							"media_url": "http://pbs.twimg.com/media/DH74H3FVwAAZFud.jpg",
+							"media_url_https": "https://pbs.twimg.com/media/DH74H3FVwAAZFud.jpg",
+							"url": "https://t.co/deZ0Ol1eiB",
+							"display_url": "pic.twitter.com/deZ0Ol1eiB",
+							"expanded_url": "https://twitter.com/ericlaw/status/900429796240277504/photo/1",
+							"type": "photo",
+							"sizes": {
+								"medium": {
+									"w": 675,
+									"h": 1200,
+									"resize": "fit"
+								},
+								"small": {
+									"w": 383,
+									"h": 680,
+									"resize": "fit"
+								},
+								"thumb": {
+									"w": 150,
+									"h": 150,
+									"resize": "crop"
+								},
+								"large": {
+									"w": 1080,
+									"h": 1920,
+									"resize": "fit"
+								}
+							}
+						}
+					]
+				},
+				"extended_entities": {
+					"media": [
+						{
+							"id": 900429789449797600,
+							"id_str": "900429789449797632",
+							"indices": [
+								24,
+								47
+							],
+							"media_url": "http://pbs.twimg.com/media/DH74H3FVwAAZFud.jpg",
+							"media_url_https": "https://pbs.twimg.com/media/DH74H3FVwAAZFud.jpg",
+							"url": "https://t.co/deZ0Ol1eiB",
+							"display_url": "pic.twitter.com/deZ0Ol1eiB",
+							"expanded_url": "https://twitter.com/ericlaw/status/900429796240277504/photo/1",
+							"type": "photo",
+							"sizes": {
+								"medium": {
+									"w": 675,
+									"h": 1200,
+									"resize": "fit"
+								},
+								"small": {
+									"w": 383,
+									"h": 680,
+									"resize": "fit"
+								},
+								"thumb": {
+									"w": 150,
+									"h": 150,
+									"resize": "crop"
+								},
+								"large": {
+									"w": 1080,
+									"h": 1920,
+									"resize": "fit"
+								}
+							}
+						}
+					]
+				},
+				"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 5725652,
+					"id_str": "5725652",
+					"name": "Eric Lawrence",
+					"screen_name": "ericlaw",
+					"location": "Texas, USA",
+					"description": "Grumpy but hopeful. Dad. Author/speaker. Created Fiddler & SlickRun. PM @ MSFT '01-'12. Now @googlechrome. My words are my own.",
+					"url": "https://t.co/CTvglKdx9z",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "https://t.co/CTvglKdx9z",
+									"expanded_url": "https://textslashplain.com",
+									"display_url": "textslashplain.com",
+									"indices": [
+										0,
+										23
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": []
+						}
+					},
+					"protected": false,
+					"followers_count": 7845,
+					"friends_count": 1088,
+					"listed_count": 499,
+					"created_at": "Wed May 02 22:43:15 +0000 2007",
+					"favourites_count": 48697,
+					"utc_offset": -18000,
+					"time_zone": "Central Time (US & Canada)",
+					"geo_enabled": true,
+					"verified": false,
+					"statuses_count": 56347,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "000000",
+					"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+					"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/719190209934405632/2s2ogDPw_normal.jpg",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/719190209934405632/2s2ogDPw_normal.jpg",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/5725652/1402602299",
+					"profile_link_color": "1B95E0",
+					"profile_sidebar_border_color": "000000",
+					"profile_sidebar_fill_color": "000000",
+					"profile_text_color": "000000",
+					"profile_use_background_image": false,
+					"has_extended_profile": true,
+					"default_profile": false,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false,
+					"translator_type": "none"
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 1185,
+				"favorite_count": 1571,
+				"favorited": false,
+				"retweeted": false,
+				"possibly_sensitive": false,
+				"lang": "en"
+			},
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 900429796240277500,
+		"quoted_status_id_str": "900429796240277504",
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Aug 25 05:42:01 +0000 2017",
 		"id": 900956410757890000,
 		"id_str": "900956410757890053",
