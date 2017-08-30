1f26589515d187b188f29516af77496b44f6b648
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-08-30 09:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63753,
-	"created_at": "Tue Aug 29 18:21:07 +0000 2017",
+	"tweets": 63754,
+	"created_at": "Wed Aug 30 07:21:07 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,277 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Wed Aug 30 07:09:23 +0000 2017",
+		"id": 902790333964197900,
+		"id_str": "902790333964197892",
+		"text": "Grootste timewaster met mijn eerste koffie. Maar de moeite waard! https://t.co/uDNBH79jdB",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uDNBH79jdB",
+					"expanded_url": "https://twitter.com/boredpanda/status/902771858495471616",
+					"display_url": "twitter.com/boredpanda/sta…",
+					"indices": [
+						66,
+						89
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9213,
+			"friends_count": 1877,
+			"listed_count": 612,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3427,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61520,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "871685aa82f8a489",
+			"url": "https://api.twitter.com/1.1/geo/id/871685aa82f8a489.json",
+			"place_type": "city",
+			"name": "Utrecht",
+			"full_name": "Utrecht, The Netherlands",
+			"country_code": "NL",
+			"country": "The Netherlands",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							4.9702867,
+							52.0262429
+						],
+						[
+							5.1952468,
+							52.0262429
+						],
+						[
+							5.1952468,
+							52.142137
+						],
+						[
+							4.9702867,
+							52.142137
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 902771858495471600,
+		"quoted_status_id_str": "902771858495471616",
+		"quoted_status": {
+			"created_at": "Wed Aug 30 05:55:58 +0000 2017",
+			"id": 902771858495471600,
+			"id_str": "902771858495471616",
+			"text": "10+ Hilarious Reactions To The ‘Game Of Thrones’ Season Finale: https://t.co/5mgz6EhJAx #GoTS7 #GameOfThronesFinale… https://t.co/zkRrEE8A3l",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GoTS7",
+						"indices": [
+							88,
+							94
+						]
+					},
+					{
+						"text": "GameOfThronesFinale",
+						"indices": [
+							95,
+							115
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/5mgz6EhJAx",
+						"expanded_url": "http://www.boredpanda.com/game-of-thrones-season-7-finale-funny-reactions/",
+						"display_url": "boredpanda.com/game-of-throne…",
+						"indices": [
+							64,
+							87
+						]
+					},
+					{
+						"url": "https://t.co/zkRrEE8A3l",
+						"expanded_url": "https://twitter.com/i/web/status/902771858495471616",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 59627487,
+				"id_str": "59627487",
+				"name": "Bored Panda",
+				"screen_name": "boredpanda",
+				"location": "Bamboo Forest",
+				"description": "The only magazine for pandas.",
+				"url": "http://t.co/bIYDVUFO0m",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/bIYDVUFO0m",
+								"expanded_url": "http://www.boredpanda.com",
+								"display_url": "boredpanda.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 125637,
+				"friends_count": 9,
+				"listed_count": 2450,
+				"created_at": "Thu Jul 23 23:43:16 +0000 2009",
+				"favourites_count": 0,
+				"utc_offset": -10800,
+				"time_zone": "Santiago",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4070,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "969696",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/64172763/for-twitter-bg2.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/64172763/for-twitter-bg2.jpg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/485712694964518912/KFezSCjG_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/485712694964518912/KFezSCjG_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/59627487/1404640641",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "CCCCCC",
+				"profile_sidebar_fill_color": "F2F2F2",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 13,
+			"favorite_count": 36,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Tue Aug 29 18:03:56 +0000 2017",
 		"id": 902592667950952400,
 		"id_str": "902592667950952449",
