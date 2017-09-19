7af873647b6591a41b4c1c86d6df9a6fc9d82ab3
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-09-19 19:21:09 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63882,
-	"created_at": "Tue Sep 19 09:21:07 +0000 2017",
+	"tweets": 63883,
+	"created_at": "Tue Sep 19 17:21:08 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_09.js
+++ data/js/tweets/2017_09.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2017_09 = 
 [
 	{
+		"created_at": "Tue Sep 19 17:08:56 +0000 2017",
+		"id": 910188971875020800,
+		"id_str": "910188971875020800",
+		"text": "Always playing with @glitcheapp #glitche https://t.co/d5Q9I0ylvP",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "glitche",
+					"indices": [
+						32,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "glitcheapp",
+					"name": "Glitché",
+					"id": 1337534774,
+					"id_str": "1337534774",
+					"indices": [
+						20,
+						31
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 910188938945425400,
+					"id_str": "910188938945425408",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/910188938945425408/pu/img/CGSVgq_CprJv94ab.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/910188938945425408/pu/img/CGSVgq_CprJv94ab.jpg",
+					"url": "https://t.co/d5Q9I0ylvP",
+					"display_url": "pic.twitter.com/d5Q9I0ylvP",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/910188971875020800/video/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 340,
+							"h": 604,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 404,
+							"h": 718,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 404,
+							"h": 718,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 910188938945425400,
+					"id_str": "910188938945425408",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/910188938945425408/pu/img/CGSVgq_CprJv94ab.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/910188938945425408/pu/img/CGSVgq_CprJv94ab.jpg",
+					"url": "https://t.co/d5Q9I0ylvP",
+					"display_url": "pic.twitter.com/d5Q9I0ylvP",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/910188971875020800/video/1",
+					"type": "video",
+					"sizes": {
+						"small": {
+							"w": 340,
+							"h": 604,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 404,
+							"h": 718,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 404,
+							"h": 718,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							202,
+							359
+						],
+						"duration_millis": 4034,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/910188938945425408/pu/vid/360x640/PQJ7Yk6t5uTETVZW.mp4"
+							},
+							{
+								"bitrate": 320000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/910188938945425408/pu/vid/180x320/zbYWGYA7vFytxpk3.mp4"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/910188938945425408/pu/pl/pge61hItF5GsVdnC.m3u8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://bufferapp.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9213,
+			"friends_count": 1890,
+			"listed_count": 613,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3459,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61391,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Sep 19 08:42:37 +0000 2017",
 		"id": 910061555428950000,
 		"id_str": "910061555428950016",
