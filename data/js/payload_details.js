ad0d91ed96edb70eb31e3def9d4a9028a6ae43c3
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-10-18 10:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64059,
-	"created_at": "Wed Oct 18 07:21:05 +0000 2017",
+	"tweets": 64060,
+	"created_at": "Wed Oct 18 08:21:05 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_10.js
+++ data/js/tweets/2017_10.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2017_10 = 
 [
 	{
+		"created_at": "Wed Oct 18 07:31:10 +0000 2017",
+		"id": 920552822004994000,
+		"id_str": "920552822004994050",
+		"text": "RT @030Caro: \"Verdriet om de dood van een jonge vrouw, maar ik teken de petitie niet.\" Bijzondere brief in @Trouw. #GGZ #justitie #AnneFabe…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GGZ",
+					"indices": [
+						115,
+						119
+					]
+				},
+				{
+					"text": "justitie",
+					"indices": [
+						120,
+						129
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "030Caro",
+					"name": "030Caro",
+					"id": 173073796,
+					"id_str": "173073796",
+					"indices": [
+						3,
+						11
+					]
+				},
+				{
+					"screen_name": "trouw",
+					"name": "Trouw",
+					"id": 3103511,
+					"id_str": "3103511",
+					"indices": [
+						107,
+						113
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/Hm80gCQ0VF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Hm80gCQ0VF",
+							"expanded_url": "http://frankmeeuwsen.com",
+							"display_url": "frankmeeuwsen.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								88,
+								111
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9211,
+			"friends_count": 1938,
+			"listed_count": 613,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3479,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61263,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Oct 18 06:59:30 +0000 2017",
+			"id": 920544852919705600,
+			"id_str": "920544852919705600",
+			"text": "\"Verdriet om de dood van een jonge vrouw, maar ik teken de petitie niet.\" Bijzondere brief in @Trouw. #GGZ… https://t.co/DsCZ0YqyMP",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "GGZ",
+						"indices": [
+							102,
+							106
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "trouw",
+						"name": "Trouw",
+						"id": 3103511,
+						"id_str": "3103511",
+						"indices": [
+							94,
+							100
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/DsCZ0YqyMP",
+						"expanded_url": "https://twitter.com/i/web/status/920544852919705600",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							108,
+							131
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 173073796,
+				"id_str": "173073796",
+				"name": "030Caro",
+				"screen_name": "030Caro",
+				"location": "Utrecht NL",
+				"description": "oud-journalist, nu (weer) hulpverlener | politiek, zorg, privacy, dieren, films, boeken | republikein | winter ❄ | schrijft voor Old Passioned |",
+				"url": "https://t.co/7s9SARbFZA",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7s9SARbFZA",
+								"expanded_url": "http://oldpassioned.nl/",
+								"display_url": "oldpassioned.nl",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2694,
+				"friends_count": 2213,
+				"listed_count": 111,
+				"created_at": "Sat Jul 31 10:26:13 +0000 2010",
+				"favourites_count": 6979,
+				"utc_offset": -7200,
+				"time_zone": "Greenland",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1073,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "07090B",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/606183402790731776/aob8SnCG.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/606183402790731776/aob8SnCG.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/916648848864444416/3SrZcXx-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/916648848864444416/3SrZcXx-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/173073796/1503152102",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C9C9C9",
+				"profile_text_color": "1C1F23",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 88,
+			"favorite_count": 72,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"is_quote_status": false,
+		"retweet_count": 88,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Wed Oct 18 07:16:58 +0000 2017",
 		"id": 920549247178723300,
 		"id_str": "920549247178723328",
