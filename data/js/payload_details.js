533e34b3219456b7ca0c171e768f557ae8501dd6
Frank Meeuwsen <frank@incredibleadventure.nl>
2017-11-27 00:21:10 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64434,
-	"created_at": "Sun Nov 26 22:21:07 +0000 2017",
+	"tweets": 64436,
+	"created_at": "Sun Nov 26 23:21:09 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_11.js
+++ data/js/tweets/2017_11.js
@@ -1,6 +1,546 @@
 Grailbird.data.tweets_2017_11 = 
 [
 	{
+		"created_at": "Sun Nov 26 23:12:33 +0000 2017",
+		"id": 934922855745314800,
+		"id_str": "934922855745314816",
+		"text": "RT @associatesmind: This is an under appreciated bit of interaction that is only possible on Twitter. https://t.co/HRRoxduE0M",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "associatesmind",
+					"name": "Keith Lee",
+					"id": 161656516,
+					"id_str": "161656516",
+					"indices": [
+						3,
+						18
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 934423527234551800,
+					"id_str": "934423527234551808",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+					"url": "https://t.co/HRRoxduE0M",
+					"display_url": "pic.twitter.com/HRRoxduE0M",
+					"expanded_url": "https://twitter.com/associatesmind/status/934423541327564801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 751,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 751,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"source_status_id": 934423541327564800,
+					"source_status_id_str": "934423541327564801",
+					"source_user_id": 161656516,
+					"source_user_id_str": "161656516"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 934423527234551800,
+					"id_str": "934423527234551808",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+					"url": "https://t.co/HRRoxduE0M",
+					"display_url": "pic.twitter.com/HRRoxduE0M",
+					"expanded_url": "https://twitter.com/associatesmind/status/934423541327564801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 751,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 751,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"source_status_id": 934423541327564800,
+					"source_status_id_str": "934423541327564801",
+					"source_user_id": 161656516,
+					"source_user_id_str": "161656516"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | Autonomous Biological Machine | https://t.co/bys0LKyw9p",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKyw9p",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								120,
+								143
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9209,
+			"friends_count": 1958,
+			"listed_count": 613,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3607,
+			"utc_offset": 3600,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61348,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Nov 25 14:08:27 +0000 2017",
+			"id": 934423541327564800,
+			"id_str": "934423541327564801",
+			"text": "This is an under appreciated bit of interaction that is only possible on Twitter. https://t.co/HRRoxduE0M",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 934423527234551800,
+						"id_str": "934423527234551808",
+						"indices": [
+							82,
+							105
+						],
+						"media_url": "http://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+						"url": "https://t.co/HRRoxduE0M",
+						"display_url": "pic.twitter.com/HRRoxduE0M",
+						"expanded_url": "https://twitter.com/associatesmind/status/934423541327564801/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 750,
+								"h": 751,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 751,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 679,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 934423527234551800,
+						"id_str": "934423527234551808",
+						"indices": [
+							82,
+							105
+						],
+						"media_url": "http://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DPe9P0qVQAAUIsg.jpg",
+						"url": "https://t.co/HRRoxduE0M",
+						"display_url": "pic.twitter.com/HRRoxduE0M",
+						"expanded_url": "https://twitter.com/associatesmind/status/934423541327564801/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 750,
+								"h": 751,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 751,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 679,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 161656516,
+				"id_str": "161656516",
+				"name": "Keith Lee",
+				"screen_name": "associatesmind",
+				"location": "Birmingham, Alabama",
+				"description": "Lawyer/author. Law. Tech. Snark. (I also run https://t.co/8GAHcGmdgp)",
+				"url": "https://t.co/rP6N5IN01Z",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/rP6N5IN01Z",
+								"expanded_url": "http://www.associatesmind.com",
+								"display_url": "associatesmind.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/8GAHcGmdgp",
+								"expanded_url": "http://lawyerslack.com",
+								"display_url": "lawyerslack.com",
+								"indices": [
+									45,
+									68
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 4477,
+				"friends_count": 215,
+				"listed_count": 236,
+				"created_at": "Thu Jul 01 12:08:47 +0000 2010",
+				"favourites_count": 2408,
+				"utc_offset": -21600,
+				"time_zone": "Central Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 18868,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "6BB9D6",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/344918034410490439/651d850e10de9d9a1d19b17bfb890231.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/344918034410490439/651d850e10de9d9a1d19b17bfb890231.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/830056326474690566/ZqQCTaey_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/830056326474690566/ZqQCTaey_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/161656516/1390329226",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFDDF",
+				"profile_text_color": "D5A539",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "7142eb97ae21e839",
+				"url": "https://api.twitter.com/1.1/geo/id/7142eb97ae21e839.json",
+				"place_type": "admin",
+				"name": "Georgia",
+				"full_name": "Georgia, USA",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-85.605166,
+								30.355644
+							],
+							[
+								-80.742567,
+								30.355644
+							],
+							[
+								-80.742567,
+								35.000771
+							],
+							[
+								-85.605166,
+								35.000771
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2767,
+			"favorite_count": 7279,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2767,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sun Nov 26 23:08:18 +0000 2017",
+		"id": 934921785224593400,
+		"id_str": "934921785224593408",
+		"text": "Nog even voor slapen gaan. Over #zml en @arjenlubach https://t.co/yAukGiwL7F",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "zml",
+					"indices": [
+						32,
+						36
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "arjenlubach",
+					"name": "Arjen Lubach",
+					"id": 14210251,
+					"id_str": "14210251",
+					"indices": [
+						40,
+						52
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/yAukGiwL7F",
+					"expanded_url": "http://diggingthedigital.com/tolerantie/",
+					"display_url": "diggingthedigital.com/tolerantie/",
+					"indices": [
+						53,
+						76
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamtriggi | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | Autonomous Biological Machine | https://t.co/bys0LKyw9p",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKyw9p",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								120,
+								143
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9209,
+			"friends_count": 1958,
+			"listed_count": 613,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3607,
+			"utc_offset": 3600,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61348,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Sun Nov 26 22:17:48 +0000 2017",
 		"id": 934909076831719400,
 		"id_str": "934909076831719424",
