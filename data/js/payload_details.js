7843dc03308747095d30c41323a8e9cdd1ff4c53
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-03-01 20:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64858,
-	"created_at": "Thu Mar 01 14:21:06 +0000 2018",
+	"tweets": 64859,
+	"created_at": "Thu Mar 01 19:21:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Thu Mar 01 19:16:04 +0000 2018",
+		"id": 969290189523873800,
+		"id_str": "969290189523873793",
+		"text": "Wat zij zegt 👇. Als je \"druk druk druk\" antwoord op de vraag \"hoe gaat het\" ben je heulnie druk want je kunt wel dr… https://t.co/g0vMG0HcEl",
+		"truncated": true,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/g0vMG0HcEl",
+					"expanded_url": "https://twitter.com/i/web/status/969290189523873793",
+					"display_url": "twitter.com/i/web/status/9…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | Autonomous Biological Machine | https://t.co/bys0LKgUKP",
+			"url": "https://t.co/4cv62Bjx8G",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62Bjx8G",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKgUKP",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								120,
+								143
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9186,
+			"friends_count": 1976,
+			"listed_count": 608,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3812,
+			"utc_offset": 3600,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61062,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1493186756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 969173874461040600,
+		"quoted_status_id_str": "969173874461040640",
+		"quoted_status": {
+			"created_at": "Thu Mar 01 11:33:52 +0000 2018",
+			"id": 969173874461040600,
+			"id_str": "969173874461040640",
+			"text": "Ik hoor veel mensen klagen over druk zijn, altijd bereikbaar (moeten) zijn en nooit uit staan. Allemaal dingen waar… https://t.co/Kh7QljBVtJ",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Kh7QljBVtJ",
+						"expanded_url": "https://twitter.com/i/web/status/969173874461040640",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 6850052,
+				"id_str": "6850052",
+				"name": "Nelleke Poorthuis",
+				"screen_name": "Nelleke",
+				"location": "Utrecht, The Netherlands",
+				"description": "Blogt op @hartjeutrecht - werkt bij @Omdenken - maakt @TrustNobodyCast  - speelt bij @IntraMintra - loopt best hard - nieuwsgierig - fotograaf - geocacher",
+				"url": "https://t.co/vbH6jjg3ps",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/vbH6jjg3ps",
+								"expanded_url": "http://hartjeutrecht.com",
+								"display_url": "hartjeutrecht.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1836,
+				"friends_count": 293,
+				"listed_count": 62,
+				"created_at": "Sat Jun 16 09:43:10 +0000 2007",
+				"favourites_count": 1508,
+				"utc_offset": 3600,
+				"time_zone": "Amsterdam",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 54686,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B4FAD8",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/435324859137400832/eBzGEW72.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/435324859137400832/eBzGEW72.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/956945171945533446/7yZ9nkGZ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/956945171945533446/7yZ9nkGZ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/6850052/1469042130",
+				"profile_link_color": "0BB884",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Thu Mar 01 14:17:16 +0000 2018",
 		"id": 969214992930271200,
 		"id_str": "969214992930271236",
