c2d42a52b961d0fbc074d93a55185e40cf552fd7
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-03-13 18:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64932,
-	"created_at": "Tue Mar 13 16:21:05 +0000 2018",
+	"tweets": 64933,
+	"created_at": "Tue Mar 13 17:21:06 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,480 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Tue Mar 13 16:40:10 +0000 2018",
+		"id": 973599612522192900,
+		"id_str": "973599612522192896",
+		"text": "RT @PearlJamHolland: Tonight’s poster by Brad Klausen! #pearljam #PJChile #PJChile2018 https://t.co/McZKBWrmYx",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "pearljam",
+					"indices": [
+						55,
+						64
+					]
+				},
+				{
+					"text": "PJChile",
+					"indices": [
+						65,
+						73
+					]
+				},
+				{
+					"text": "PJChile2018",
+					"indices": [
+						74,
+						86
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PearlJamHolland",
+					"name": "Pearl Jam Holland",
+					"id": 2933941318,
+					"id_str": "2933941318",
+					"indices": [
+						3,
+						19
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 973596852376531000,
+					"id_str": "973596852376530944",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+					"url": "https://t.co/McZKBWrmYx",
+					"display_url": "pic.twitter.com/McZKBWrmYx",
+					"expanded_url": "https://twitter.com/PearlJamHolland/status/973596859666255872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 419,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 419,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 356,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 973596859666255900,
+					"source_status_id_str": "973596859666255872",
+					"source_user_id": 2933941318,
+					"source_user_id_str": "2933941318"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 973596852376531000,
+					"id_str": "973596852376530944",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+					"url": "https://t.co/McZKBWrmYx",
+					"display_url": "pic.twitter.com/McZKBWrmYx",
+					"expanded_url": "https://twitter.com/PearlJamHolland/status/973596859666255872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 419,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 419,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 356,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 973596859666255900,
+					"source_status_id_str": "973596859666255872",
+					"source_user_id": 2933941318,
+					"source_user_id_str": "2933941318"
+				},
+				{
+					"id": 973596852368093200,
+					"id_str": "973596852368093185",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/DYLpLcvWsAEP5QI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYLpLcvWsAEP5QI.jpg",
+					"url": "https://t.co/McZKBWrmYx",
+					"display_url": "pic.twitter.com/McZKBWrmYx",
+					"expanded_url": "https://twitter.com/PearlJamHolland/status/973596859666255872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 842,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 559,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 842,
+							"h": 1024,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 973596859666255900,
+					"source_status_id_str": "973596859666255872",
+					"source_user_id": 2933941318,
+					"source_user_id_str": "2933941318"
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | =W= | .gif 4 life yo! | Autonomous Biological Machine | https://t.co/bys0LKgUKP",
+			"url": "https://t.co/4cv62Bjx8G",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62Bjx8G",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKgUKP",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								120,
+								143
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9169,
+			"friends_count": 1982,
+			"listed_count": 608,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3827,
+			"utc_offset": 3600,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61071,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1520723563",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Mar 13 16:29:14 +0000 2018",
+			"id": 973596859666255900,
+			"id_str": "973596859666255872",
+			"text": "Tonight’s poster by Brad Klausen! #pearljam #PJChile #PJChile2018 https://t.co/McZKBWrmYx",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "pearljam",
+						"indices": [
+							34,
+							43
+						]
+					},
+					{
+						"text": "PJChile",
+						"indices": [
+							44,
+							52
+						]
+					},
+					{
+						"text": "PJChile2018",
+						"indices": [
+							53,
+							65
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 973596852376531000,
+						"id_str": "973596852376530944",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+						"url": "https://t.co/McZKBWrmYx",
+						"display_url": "pic.twitter.com/McZKBWrmYx",
+						"expanded_url": "https://twitter.com/PearlJamHolland/status/973596859666255872/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 419,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 419,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 356,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 973596852376531000,
+						"id_str": "973596852376530944",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYLpLcxXcAAGo6v.jpg",
+						"url": "https://t.co/McZKBWrmYx",
+						"display_url": "pic.twitter.com/McZKBWrmYx",
+						"expanded_url": "https://twitter.com/PearlJamHolland/status/973596859666255872/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 419,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 419,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 356,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 973596852368093200,
+						"id_str": "973596852368093185",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/DYLpLcvWsAEP5QI.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYLpLcvWsAEP5QI.jpg",
+						"url": "https://t.co/McZKBWrmYx",
+						"display_url": "pic.twitter.com/McZKBWrmYx",
+						"expanded_url": "https://twitter.com/PearlJamHolland/status/973596859666255872/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 842,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 559,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 842,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2933941318,
+				"id_str": "2933941318",
+				"name": "Pearl Jam Holland",
+				"screen_name": "PearlJamHolland",
+				"location": "",
+				"description": "Troubled souls unite! Dutch PJ-fanaccount. Follow for Pearl Jam news, videos, lyrics, facts & photos. Instagram: pearljamholland.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1196,
+				"friends_count": 229,
+				"listed_count": 17,
+				"created_at": "Sat Dec 20 17:16:58 +0000 2014",
+				"favourites_count": 5272,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 2306,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/968918956860985346/u_FxXkMa_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/968918956860985346/u_FxXkMa_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2933941318/1419097641",
+				"profile_link_color": "DD2E44",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 2,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Mar 13 15:59:21 +0000 2018",
 		"id": 973589338675085300,
 		"id_str": "973589338675085317",
