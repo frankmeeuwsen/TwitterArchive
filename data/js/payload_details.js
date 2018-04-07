b9bc77327cecf36b0eadca02c84d13db5fb45062
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-04-07 20:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65112,
-	"created_at": "Sat Apr 07 17:21:06 +0000 2018",
+	"tweets": 65113,
+	"created_at": "Sat Apr 07 18:21:04 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,299 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Sat Apr 07 17:27:39 +0000 2018",
+		"id": 982671256561561600,
+		"id_str": "982671256561561601",
+		"text": "RT @AaronSwartzDay: LIVESTREAM For TODAY'S John Perry Barlow Symposium here: https://t.co/nbeX2BjJh8   w/ @snowden @doctorow @joi @brewster…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AaronSwartzDay",
+					"name": "Aaron Swartz Day",
+					"id": 2565971588,
+					"id_str": "2565971588",
+					"indices": [
+						3,
+						18
+					]
+				},
+				{
+					"screen_name": "Snowden",
+					"name": "Edward Snowden",
+					"id": 2916305152,
+					"id_str": "2916305152",
+					"indices": [
+						106,
+						114
+					]
+				},
+				{
+					"screen_name": "doctorow",
+					"name": "Cory Doctorow",
+					"id": 2729061,
+					"id_str": "2729061",
+					"indices": [
+						115,
+						124
+					]
+				},
+				{
+					"screen_name": "Joi",
+					"name": "Joi Ito",
+					"id": 691353,
+					"id_str": "691353",
+					"indices": [
+						125,
+						129
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nbeX2BjJh8",
+					"expanded_url": "https://eff.org/ArchiveYouTube",
+					"display_url": "eff.org/ArchiveYouTube",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Autonomous Biological Machine | Power user of the internet | https://t.co/bys0LKgUKP",
+			"url": "https://t.co/4cv62Bjx8G",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62Bjx8G",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKgUKP",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9177,
+			"friends_count": 1996,
+			"listed_count": 606,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 3898,
+			"utc_offset": 7200,
+			"time_zone": "Amsterdam",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60898,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/132273940/3dlife.jpg",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/875434894905810944/EzchHrtM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1520723563",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Apr 07 17:15:00 +0000 2018",
+			"id": 982668073449730000,
+			"id_str": "982668073449730048",
+			"text": "LIVESTREAM For TODAY'S John Perry Barlow Symposium here: https://t.co/nbeX2BjJh8   w/ @snowden @doctorow @joi… https://t.co/bp3SxEKohx",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Snowden",
+						"name": "Edward Snowden",
+						"id": 2916305152,
+						"id_str": "2916305152",
+						"indices": [
+							86,
+							94
+						]
+					},
+					{
+						"screen_name": "doctorow",
+						"name": "Cory Doctorow",
+						"id": 2729061,
+						"id_str": "2729061",
+						"indices": [
+							95,
+							104
+						]
+					},
+					{
+						"screen_name": "Joi",
+						"name": "Joi Ito",
+						"id": 691353,
+						"id_str": "691353",
+						"indices": [
+							105,
+							109
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/nbeX2BjJh8",
+						"expanded_url": "https://eff.org/ArchiveYouTube",
+						"display_url": "eff.org/ArchiveYouTube",
+						"indices": [
+							57,
+							80
+						]
+					},
+					{
+						"url": "https://t.co/bp3SxEKohx",
+						"expanded_url": "https://twitter.com/i/web/status/982668073449730048",
+						"display_url": "twitter.com/i/web/status/9…",
+						"indices": [
+							111,
+							134
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2565971588,
+				"id_str": "2565971588",
+				"name": "Aaron Swartz Day",
+				"screen_name": "AaronSwartzDay",
+				"location": "San Francisco and everywhere!",
+				"description": "Aaron Swartz-inspired Hackathons are now going on all year, all over the world. Joining us is easy; just by helping those around you, however you can. #love",
+				"url": "https://t.co/WzLIDX1dQD",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/WzLIDX1dQD",
+								"expanded_url": "http://www.aaronswartzday.org",
+								"display_url": "aaronswartzday.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4124,
+				"friends_count": 470,
+				"listed_count": 71,
+				"created_at": "Fri Jun 13 21:57:11 +0000 2014",
+				"favourites_count": 1242,
+				"utc_offset": -25200,
+				"time_zone": "Arizona",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5370,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/953379722125131776/6lyScwKo_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/953379722125131776/6lyScwKo_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2565971588/1445140488",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Apr 07 17:10:48 +0000 2018",
 		"id": 982667017777016800,
 		"id_str": "982667017777016833",
