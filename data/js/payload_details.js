4ab71de75f5f19045bd9af3d88b96f01604c8b85
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-04-29 14:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65320,
-	"created_at": "Sun Apr 29 09:21:08 +0000 2018",
+	"tweets": 65321,
+	"created_at": "Sun Apr 29 12:21:06 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_04.js",
 		"year": 2018,
 		"var_name": "tweets_2018_04",
-		"tweet_count": 254,
+		"tweet_count": 255,
 		"month": 4
 	},
 	{
