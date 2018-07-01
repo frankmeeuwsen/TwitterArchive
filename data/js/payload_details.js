29f7917c8db3ef2f259225ef23b183f23dc0feb4
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-07-01 13:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66073,
-	"created_at": "Sat Jun 30 19:21:10 +0000 2018",
+	"tweets": 66074,
+	"created_at": "Sun Jul 01 11:21:07 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_07.js
@@ -0,0 +1,390 @@
+Grailbird.data.tweets_2018_07 = 
+[
+	{
+		"created_at": "Sun Jul 01 10:29:25 +0000 2018",
+		"id": 1013368973092687900,
+		"id_str": "1013368973092687872",
+		"text": "RT @PearlJam: The official artwork for tonight's sold out show at @o2ArenaPraha.\n\nArtwork: @MattTaylorDraws. #PJLIVE2018 #PearlJam https://…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PJLIVE2018",
+					"indices": [
+						109,
+						120
+					]
+				},
+				{
+					"text": "PearlJam",
+					"indices": [
+						121,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PearlJam",
+					"name": "Pearl Jam",
+					"id": 15155074,
+					"id_str": "15155074",
+					"indices": [
+						3,
+						12
+					]
+				},
+				{
+					"screen_name": "o2arenapraha",
+					"name": "O2 arena Praha",
+					"id": 533251336,
+					"id_str": "533251336",
+					"indices": [
+						66,
+						79
+					]
+				},
+				{
+					"screen_name": "MattTaylorDraws",
+					"name": "Matt Taylor",
+					"id": 24553095,
+					"id_str": "24553095",
+					"indices": [
+						91,
+						107
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Autonomous Biological Machine | Power user of the internet | https://t.co/bys0LKgUKP",
+			"url": "https://t.co/4cv62Bjx8G",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62Bjx8G",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKgUKP",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9152,
+			"friends_count": 2053,
+			"listed_count": 604,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4410,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60455,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1528570148",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Jul 01 07:00:00 +0000 2018",
+			"id": 1013316271985872900,
+			"id_str": "1013316271985872896",
+			"text": "The official artwork for tonight's sold out show at @o2ArenaPraha.\n\nArtwork: @MattTaylorDraws. #PJLIVE2018 #PearlJam https://t.co/pWeIuo7Q6l",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "PJLIVE2018",
+						"indices": [
+							95,
+							106
+						]
+					},
+					{
+						"text": "PearlJam",
+						"indices": [
+							107,
+							116
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "o2arenapraha",
+						"name": "O2 arena Praha",
+						"id": 533251336,
+						"id_str": "533251336",
+						"indices": [
+							52,
+							65
+						]
+					},
+					{
+						"screen_name": "MattTaylorDraws",
+						"name": "Matt Taylor",
+						"id": 24553095,
+						"id_str": "24553095",
+						"indices": [
+							77,
+							93
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1011283033314480100,
+						"id_str": "1011283033314480128",
+						"indices": [
+							117,
+							140
+						],
+						"media_url": "http://pbs.twimg.com/media/DgjMj5AW0AA3VMh.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DgjMj5AW0AA3VMh.jpg",
+						"url": "https://t.co/pWeIuo7Q6l",
+						"display_url": "pic.twitter.com/pWeIuo7Q6l",
+						"expanded_url": "https://twitter.com/PearlJam/status/1013316271985872896/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 600,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 600,
+								"h": 800,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1011283033314480100,
+						"id_str": "1011283033314480128",
+						"indices": [
+							117,
+							140
+						],
+						"media_url": "http://pbs.twimg.com/media/DgjMj5AW0AA3VMh.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DgjMj5AW0AA3VMh.jpg",
+						"url": "https://t.co/pWeIuo7Q6l",
+						"display_url": "pic.twitter.com/pWeIuo7Q6l",
+						"expanded_url": "https://twitter.com/PearlJam/status/1013316271985872896/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 600,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 600,
+								"h": 800,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1011283060795600900,
+						"id_str": "1011283060795600896",
+						"indices": [
+							117,
+							140
+						],
+						"media_url": "http://pbs.twimg.com/media/DgjMlfYXcAA4527.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DgjMlfYXcAA4527.jpg",
+						"url": "https://t.co/pWeIuo7Q6l",
+						"display_url": "pic.twitter.com/pWeIuo7Q6l",
+						"expanded_url": "https://twitter.com/PearlJam/status/1013316271985872896/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 600,
+								"h": 800,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 600,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://ads-api.twitter.com\" rel=\"nofollow\">Twitter Ads Composer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15155074,
+				"id_str": "15155074",
+				"name": "Pearl Jam",
+				"screen_name": "PearlJam",
+				"location": "Seattle, WA",
+				"description": "Pearl Jam is coming home - August 8 & 10 in Seattle. #TheHomeShows",
+				"url": "https://t.co/J95aoJ5FKF",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/J95aoJ5FKF",
+								"expanded_url": "http://www.PearlJam.com",
+								"display_url": "PearlJam.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3753991,
+				"friends_count": 874,
+				"listed_count": 18287,
+				"created_at": "Wed Jun 18 06:59:14 +0000 2008",
+				"favourites_count": 3213,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 6756,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/973604653475692544/c9IX6SI__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/973604653475692544/c9IX6SI__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15155074/1520960500",
+				"profile_link_color": "A17F59",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 59,
+			"favorite_count": 505,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 59,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en"
+	}
+]
