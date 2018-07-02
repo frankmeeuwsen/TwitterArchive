0f942b14306c256fc815c1a10323731ba34ebe0d
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-07-03 00:21:05 +0200

    Added 1 tweets

--- data/js/tweets/2018_07.js
+++ data/js/tweets/2018_07.js
@@ -1,6 +1,496 @@
 Grailbird.data.tweets_2018_07 = 
 [
 	{
+		"created_at": "Mon Jul 02 21:48:29 +0000 2018",
+		"id": 1013902253202333700,
+		"id_str": "1013902253202333697",
+		"text": "RT @johnbcrist: If golf and soccer switched announcers... https://t.co/W0qr4n3GWx",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "johnbcrist",
+					"name": "John Crist",
+					"id": 379415450,
+					"id_str": "379415450",
+					"indices": [
+						3,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1013602017091055600,
+					"id_str": "1013602017091055617",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+					"url": "https://t.co/W0qr4n3GWx",
+					"display_url": "pic.twitter.com/W0qr4n3GWx",
+					"expanded_url": "https://twitter.com/johnbcrist/status/1013602082421698567/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1013602082421698600,
+					"source_status_id_str": "1013602082421698567",
+					"source_user_id": 379415450,
+					"source_user_id_str": "379415450"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1013602017091055600,
+					"id_str": "1013602017091055617",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+					"url": "https://t.co/W0qr4n3GWx",
+					"display_url": "pic.twitter.com/W0qr4n3GWx",
+					"expanded_url": "https://twitter.com/johnbcrist/status/1013602082421698567/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1013602082421698600,
+					"source_status_id_str": "1013602082421698567",
+					"source_user_id": 379415450,
+					"source_user_id_str": "379415450",
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 27867,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/vid/640x360/ZSHuYrTK4bEUOyZT.mp4?tag=3"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/vid/320x180/oPPx8xlhwX3YZVVu.mp4?tag=3"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/pl/RKe-nI6rAtCX88E7.m3u8?tag=3"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/vid/1280x720/SSqmqhJN69NIYBIM.mp4?tag=3"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 379415450,
+							"id_str": "379415450",
+							"name": "John Crist",
+							"screen_name": "johnbcrist",
+							"location": "",
+							"description": "\"Look at this photograph, every time I do it makes me laugh.\"",
+							"url": "https://t.co/DI4jsk8mNd",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/DI4jsk8mNd",
+											"expanded_url": "http://www.johncristcomedy.com",
+											"display_url": "johncristcomedy.com",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": []
+								}
+							},
+							"protected": false,
+							"followers_count": 109699,
+							"friends_count": 202,
+							"listed_count": 373,
+							"created_at": "Sat Sep 24 22:49:02 +0000 2011",
+							"favourites_count": 10618,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": false,
+							"verified": true,
+							"statuses_count": 22180,
+							"lang": "en",
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "131516",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+							"profile_background_tile": true,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/868552047271886848/rxS-5nry_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/868552047271886848/rxS-5nry_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/379415450/1520395846",
+							"profile_link_color": "009999",
+							"profile_sidebar_border_color": "EEEEEE",
+							"profile_sidebar_fill_color": "EFEFEF",
+							"profile_text_color": "333333",
+							"profile_use_background_image": true,
+							"has_extended_profile": false,
+							"default_profile": false,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Autonomous Biological Machine | Power user of the internet | https://t.co/bys0LKgUKP",
+			"url": "https://t.co/4cv62Bjx8G",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62Bjx8G",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKgUKP",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9149,
+			"friends_count": 2054,
+			"listed_count": 604,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4421,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60455,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1528570148",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Jul 02 01:55:42 +0000 2018",
+			"id": 1013602082421698600,
+			"id_str": "1013602082421698567",
+			"text": "If golf and soccer switched announcers... https://t.co/W0qr4n3GWx",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1013602017091055600,
+						"id_str": "1013602017091055617",
+						"indices": [
+							42,
+							65
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+						"url": "https://t.co/W0qr4n3GWx",
+						"display_url": "pic.twitter.com/W0qr4n3GWx",
+						"expanded_url": "https://twitter.com/johnbcrist/status/1013602082421698567/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1013602017091055600,
+						"id_str": "1013602017091055617",
+						"indices": [
+							42,
+							65
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1013602017091055617/pu/img/VjuViQWWNrLFRwSl.jpg",
+						"url": "https://t.co/W0qr4n3GWx",
+						"display_url": "pic.twitter.com/W0qr4n3GWx",
+						"expanded_url": "https://twitter.com/johnbcrist/status/1013602082421698567/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 27867,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/vid/640x360/ZSHuYrTK4bEUOyZT.mp4?tag=3"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/vid/320x180/oPPx8xlhwX3YZVVu.mp4?tag=3"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/pl/RKe-nI6rAtCX88E7.m3u8?tag=3"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1013602017091055617/pu/vid/1280x720/SSqmqhJN69NIYBIM.mp4?tag=3"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 379415450,
+				"id_str": "379415450",
+				"name": "John Crist",
+				"screen_name": "johnbcrist",
+				"location": "",
+				"description": "\"Look at this photograph, every time I do it makes me laugh.\"",
+				"url": "https://t.co/DI4jsk8mNd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/DI4jsk8mNd",
+								"expanded_url": "http://www.johncristcomedy.com",
+								"display_url": "johncristcomedy.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 109699,
+				"friends_count": 202,
+				"listed_count": 373,
+				"created_at": "Sat Sep 24 22:49:02 +0000 2011",
+				"favourites_count": 10618,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 22180,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/868552047271886848/rxS-5nry_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/868552047271886848/rxS-5nry_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/379415450/1520395846",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 104978,
+			"favorite_count": 206754,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 104978,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Mon Jul 02 20:59:13 +0000 2018",
 		"id": 1013889856639062000,
 		"id_str": "1013889856639062016",
