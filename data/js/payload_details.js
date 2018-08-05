63becc536347d2ce3b55181171e6d72ecf14ca54
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-08-05 09:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66382,
-	"created_at": "Sun Aug 05 05:21:05 +0000 2018",
+	"tweets": 66383,
+	"created_at": "Sun Aug 05 07:21:04 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,290 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Sun Aug 05 07:04:42 +0000 2018",
+		"id": 1026001029672960000,
+		"id_str": "1026001029672960001",
+		"text": "Zo dan. Rondje langs de Linge zit er weer op. 8 kilometer. Best een end om hard te lopen al zeg ik het zelf. https://t.co/AcWEfSqrpp",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1026001001495625700,
+					"id_str": "1026001001495625728",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/Dj0WeZFXgAAhPTX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dj0WeZFXgAAhPTX.jpg",
+					"url": "https://t.co/AcWEfSqrpp",
+					"display_url": "pic.twitter.com/AcWEfSqrpp",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1026001029672960001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1538,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1026001001495625700,
+					"id_str": "1026001001495625728",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/Dj0WeZFXgAAhPTX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dj0WeZFXgAAhPTX.jpg",
+					"url": "https://t.co/AcWEfSqrpp",
+					"display_url": "pic.twitter.com/AcWEfSqrpp",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1026001029672960001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1538,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1026001001462091800,
+					"id_str": "1026001001462091777",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/Dj0WeY9X0AEYns4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dj0WeY9X0AEYns4.jpg",
+					"url": "https://t.co/AcWEfSqrpp",
+					"display_url": "pic.twitter.com/AcWEfSqrpp",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1026001029672960001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1089,
+							"h": 1089,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1089,
+							"h": 1089,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1026001001495646200,
+					"id_str": "1026001001495646208",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/Dj0WeZFX0AAfANk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dj0WeZFX0AAfANk.jpg",
+					"url": "https://t.co/AcWEfSqrpp",
+					"display_url": "pic.twitter.com/AcWEfSqrpp",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1026001029672960001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 901,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1538,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Autonomous Biological Machine | Power user of the internet | https://t.co/bys0LKgUKP",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKgUKP",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								125,
+								148
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8934,
+			"friends_count": 120,
+			"listed_count": 603,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4515,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60659,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1528570148",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9f51978080001",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9f51978080001.json",
+			"place_type": "poi",
+			"name": "De Lievelinge",
+			"full_name": "De Lievelinge",
+			"country_code": "NL",
+			"country": "The Netherlands",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							5.039811779123441,
+							51.845388134753485
+						],
+						[
+							5.039811779123441,
+							51.845388134753485
+						],
+						[
+							5.039811779123441,
+							51.845388134753485
+						],
+						[
+							5.039811779123441,
+							51.845388134753485
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Sun Aug 05 04:36:08 +0000 2018",
 		"id": 1025963644171866100,
 		"id_str": "1025963644171866113",
