a5ae9d54fa1e10ced799a173416878bbba97084c
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-08-11 00:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66429,
-	"created_at": "Fri Aug 10 18:21:05 +0000 2018",
+	"tweets": 66430,
+	"created_at": "Fri Aug 10 22:21:04 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_08.js
+++ data/js/tweets/2018_08.js
@@ -1,6 +1,507 @@
 Grailbird.data.tweets_2018_08 = 
 [
 	{
+		"created_at": "Fri Aug 10 21:21:08 +0000 2018",
+		"id": 1028028498752036900,
+		"id_str": "1028028498752036864",
+		"text": "RT @bdsams: AI has peaked. https://t.co/xYynF7azUt",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bdsams",
+					"name": "Brad Sams",
+					"id": 102800191,
+					"id_str": "102800191",
+					"indices": [
+						3,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1027879283975495700,
+					"id_str": "1027879283975495680",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+					"url": "https://t.co/xYynF7azUt",
+					"display_url": "pic.twitter.com/xYynF7azUt",
+					"expanded_url": "https://twitter.com/bdsams/status/1027879335515107329/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 381,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 728,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 728,
+							"h": 408,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1027879335515107300,
+					"source_status_id_str": "1027879335515107329",
+					"source_user_id": 102800191,
+					"source_user_id_str": "102800191"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1027879283975495700,
+					"id_str": "1027879283975495680",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+					"url": "https://t.co/xYynF7azUt",
+					"display_url": "pic.twitter.com/xYynF7azUt",
+					"expanded_url": "https://twitter.com/bdsams/status/1027879335515107329/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 381,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 728,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 728,
+							"h": 408,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1027879335515107300,
+					"source_status_id_str": "1027879335515107329",
+					"source_user_id": 102800191,
+					"source_user_id_str": "102800191",
+					"video_info": {
+						"aspect_ratio": [
+							91,
+							51
+						],
+						"duration_millis": 15010,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1027879283975495680/pu/pl/hkCcmOn6gbCFsaPt.m3u8?tag=5"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1027879283975495680/pu/vid/728x408/BdMJqOpOM4JQX8br.mp4?tag=5"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1027879283975495680/pu/vid/320x180/RHwYxC3UkGpeMEAn.mp4?tag=5"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false,
+						"source_user": {
+							"id": 102800191,
+							"id_str": "102800191",
+							"name": "Brad Sams",
+							"screen_name": "bdsams",
+							"location": "Cincinnati, Ohio",
+							"description": "Executive Editor of https://t.co/bz3vwkuRFp & https://t.co/DpuqUuy2Te | Writer, Speaker, Podcaster, Human.",
+							"url": null,
+							"entities": {
+								"description": {
+									"urls": [
+										{
+											"url": "https://t.co/bz3vwkuRFp",
+											"expanded_url": "http://Petri.com",
+											"display_url": "Petri.com",
+											"indices": [
+												20,
+												43
+											]
+										},
+										{
+											"url": "https://t.co/DpuqUuy2Te",
+											"expanded_url": "http://Thurrott.com",
+											"display_url": "Thurrott.com",
+											"indices": [
+												46,
+												69
+											]
+										}
+									]
+								}
+							},
+							"protected": false,
+							"followers_count": 23559,
+							"friends_count": 456,
+							"listed_count": 587,
+							"created_at": "Thu Jan 07 21:47:50 +0000 2010",
+							"favourites_count": 28611,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": true,
+							"verified": true,
+							"statuses_count": 75241,
+							"lang": "en",
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": false,
+							"profile_background_color": "F5F5F5",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/1021398199871975424/B1No4U2w_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021398199871975424/B1No4U2w_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/102800191/1406509372",
+							"profile_link_color": "4F0DA6",
+							"profile_sidebar_border_color": "FFFFFF",
+							"profile_sidebar_fill_color": "EFEFEF",
+							"profile_text_color": "333333",
+							"profile_use_background_image": false,
+							"has_extended_profile": true,
+							"default_profile": false,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "none"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | https://t.co/bys0LKyw9p and https://t.co/7xQ0m9WQVH",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKyw9p",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								93,
+								116
+							]
+						},
+						{
+							"url": "https://t.co/7xQ0m9WQVH",
+							"expanded_url": "https://mastodon.social/@frankmeeuwsen",
+							"display_url": "mastodon.social/@frankmeeuwsen",
+							"indices": [
+								121,
+								144
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8927,
+			"friends_count": 126,
+			"listed_count": 601,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4537,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60675,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1528570148",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Aug 10 11:28:25 +0000 2018",
+			"id": 1027879335515107300,
+			"id_str": "1027879335515107329",
+			"text": "AI has peaked. https://t.co/xYynF7azUt",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1027879283975495700,
+						"id_str": "1027879283975495680",
+						"indices": [
+							15,
+							38
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+						"url": "https://t.co/xYynF7azUt",
+						"display_url": "pic.twitter.com/xYynF7azUt",
+						"expanded_url": "https://twitter.com/bdsams/status/1027879335515107329/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 381,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 728,
+								"h": 408,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 728,
+								"h": 408,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1027879283975495700,
+						"id_str": "1027879283975495680",
+						"indices": [
+							15,
+							38
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1027879283975495680/pu/img/fGKwAiZXkrsjMeqg.jpg",
+						"url": "https://t.co/xYynF7azUt",
+						"display_url": "pic.twitter.com/xYynF7azUt",
+						"expanded_url": "https://twitter.com/bdsams/status/1027879335515107329/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 381,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 728,
+								"h": 408,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 728,
+								"h": 408,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								91,
+								51
+							],
+							"duration_millis": 15010,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1027879283975495680/pu/pl/hkCcmOn6gbCFsaPt.m3u8?tag=5"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1027879283975495680/pu/vid/728x408/BdMJqOpOM4JQX8br.mp4?tag=5"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1027879283975495680/pu/vid/320x180/RHwYxC3UkGpeMEAn.mp4?tag=5"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 102800191,
+				"id_str": "102800191",
+				"name": "Brad Sams",
+				"screen_name": "bdsams",
+				"location": "Cincinnati, Ohio",
+				"description": "Executive Editor of https://t.co/bz3vwkuRFp & https://t.co/DpuqUuy2Te | Writer, Speaker, Podcaster, Human.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/bz3vwkuRFp",
+								"expanded_url": "http://Petri.com",
+								"display_url": "Petri.com",
+								"indices": [
+									20,
+									43
+								]
+							},
+							{
+								"url": "https://t.co/DpuqUuy2Te",
+								"expanded_url": "http://Thurrott.com",
+								"display_url": "Thurrott.com",
+								"indices": [
+									46,
+									69
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 23559,
+				"friends_count": 456,
+				"listed_count": 587,
+				"created_at": "Thu Jan 07 21:47:50 +0000 2010",
+				"favourites_count": 28611,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 75241,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F5F5",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1021398199871975424/B1No4U2w_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021398199871975424/B1No4U2w_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/102800191/1406509372",
+				"profile_link_color": "4F0DA6",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5082,
+			"favorite_count": 12488,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 5082,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Fri Aug 10 18:09:43 +0000 2018",
 		"id": 1027980326826586100,
 		"id_str": "1027980326826586120",
