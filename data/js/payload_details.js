a5b83df84c64f8887ca90677909cabd2ef0cdc55
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-10-03 22:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66798,
-	"created_at": "Wed Oct 03 08:21:03 +0000 2018",
+	"tweets": 66799,
+	"created_at": "Wed Oct 03 20:21:04 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Wed Oct 03 19:32:43 +0000 2018",
+		"id": 1047570159194595300,
+		"id_str": "1047570159194595329",
+		"text": "Ma! Pak de nootjes! Me boek is op een thumbnail op de internets! https://t.co/0IjyHeP47R",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0IjyHeP47R",
+					"expanded_url": "https://twitter.com/MichaelMinneboo/status/1047468718513684480",
+					"display_url": "twitter.com/MichaelMinnebo…",
+					"indices": [
+						65,
+						88
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | https://t.co/bys0LKyw9p and https://t.co/4q8wDtyw6y",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/bys0LKyw9p",
+							"expanded_url": "http://micro.blog/frank",
+							"display_url": "micro.blog/frank",
+							"indices": [
+								93,
+								116
+							]
+						},
+						{
+							"url": "https://t.co/4q8wDtyw6y",
+							"expanded_url": "https://me.frankmeeuwsen.xyz/@frank",
+							"display_url": "me.frankmeeuwsen.xyz/@frank",
+							"indices": [
+								121,
+								144
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8938,
+			"friends_count": 154,
+			"listed_count": 600,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4669,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60719,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1047468718513684500,
+		"quoted_status_id_str": "1047468718513684480",
+		"quoted_status": {
+			"created_at": "Wed Oct 03 12:49:38 +0000 2018",
+			"id": 1047468718513684500,
+			"id_str": "1047468718513684480",
+			"text": "Zo krijgen we het Web weer terug https://t.co/pskAgnlOCR\n\n@peterdekock @emijnsbergen @frankmeeuwsen @waag",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "peterdekock",
+						"name": "Peter de Kock",
+						"id": 19243661,
+						"id_str": "19243661",
+						"indices": [
+							58,
+							70
+						]
+					},
+					{
+						"screen_name": "emijnsbergen",
+						"name": "Edwin Mijnsbergen",
+						"id": 3494941,
+						"id_str": "3494941",
+						"indices": [
+							71,
+							84
+						]
+					},
+					{
+						"screen_name": "frankmeeuwsen",
+						"name": "Frank Meeuwsen",
+						"id": 816203,
+						"id_str": "816203",
+						"indices": [
+							85,
+							99
+						]
+					},
+					{
+						"screen_name": "waag",
+						"name": "Waag",
+						"id": 26712390,
+						"id_str": "26712390",
+						"indices": [
+							100,
+							105
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/pskAgnlOCR",
+						"expanded_url": "https://youtu.be/9HIX3cHfHXI",
+						"display_url": "youtu.be/9HIX3cHfHXI",
+						"indices": [
+							33,
+							56
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16735358,
+				"id_str": "16735358",
+				"name": "Michael Minneboo",
+				"screen_name": "MichaelMinneboo",
+				"location": "The Netherlands",
+				"description": "Journalist & videomaker. Gespecialiseerd in film, strips en beeldcultuur. \nAuteur Mijn vriend Spider-Man: Superhelden, geeks en fancultuur.",
+				"url": "http://t.co/atJDeN9Dzo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/atJDeN9Dzo",
+								"expanded_url": "http://michaelminneboo.nl/",
+								"display_url": "michaelminneboo.nl",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1245,
+				"friends_count": 709,
+				"listed_count": 129,
+				"created_at": "Tue Oct 14 08:16:56 +0000 2008",
+				"favourites_count": 1083,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 35760,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/901062095894183938/Z6K6u5y7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/901062095894183938/Z6K6u5y7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16735358/1444923442",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "DDFFCC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl"
+	},
+	{
 		"created_at": "Wed Oct 03 07:41:30 +0000 2018",
 		"id": 1047391174317813800,
 		"id_str": "1047391174317813760",
