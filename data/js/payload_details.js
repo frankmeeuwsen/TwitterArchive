e7ce373a9233bbfb20fb5c4aba02659b0a6d8283
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-10-20 17:21:10 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67322,
-	"created_at": "Sat Oct 20 13:21:09 +0000 2018",
+	"tweets": 67323,
+	"created_at": "Sat Oct 20 15:21:09 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Sat Oct 20 14:56:34 +0000 2018",
+		"id": 1053661255557353500,
+		"id_str": "1053661255557353472",
+		"full_text": "Tijdens de IndieWebCamp heb...: https://t.co/kgDKTepCeq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kgDKTepCeq",
+					"expanded_url": "https://diggingthedigital.com/46160/",
+					"display_url": "diggingthedigital.com/46160/",
+					"indices": [
+						32,
+						55
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8982,
+			"friends_count": 166,
+			"listed_count": 598,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4760,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60676,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Tijdens de IndieWebCamp heb...: https://t.co/kgDKTepCeq"
+	},
+	{
 		"created_at": "Sat Oct 20 12:53:46 +0000 2018",
 		"id": 1053630354056249300,
 		"id_str": "1053630354056249344",
@@ -118,7 +224,7 @@ Grailbird.data.tweets_2018_10 =
 		"created_at": "Sat Oct 20 08:51:21 +0000 2018",
 		"id": 1053569347942301700,
 		"id_str": "1053569347942301696",
-		"full_text": "As of today Dutch Design Week with a.o. Anke Haspers and #TAG a tangible ‘to-do list’ for people that can benefit from a reminder now and then (🙋‍♂️). Come experience TAG at #mindthestep expo at #Klokgebouw #Eindhoven \nPowered by @utwenteEN \n@DesignLabUT @dutchdesignweek https://t.co/qHaJ7Vnnkq",
+		"full_text": "As of today Dutch Design Week with a.o. Anke Haspers and #TAG a tangible ‘to-do list’ for people that can benefit from a reminder now and then (🙋♂️). Come experience TAG at #mindthestep expo at #Klokgebouw #Eindhoven \nPowered by @utwenteEN \n@DesignLabUT @dutchdesignweek https://t.co/qHaJ7Vnnkq",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -346,7 +452,7 @@ Grailbird.data.tweets_2018_10 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "As of today Dutch Design Week with a.o. Anke Haspers and #TAG a tangible ‘to-do list’ for people that can benefit from a reminder now and then (🙋‍♂️). Come experience TAG at #mindthestep expo at #Klokgebouw #Eindhoven \nPowered by @utwenteEN \n@DesignLabUT @dutchdesignweek https://t.co/qHaJ7Vnnkq"
+		"text": "As of today Dutch Design Week with a.o. Anke Haspers and #TAG a tangible ‘to-do list’ for people that can benefit from a reminder now and then (🙋♂️). Come experience TAG at #mindthestep expo at #Klokgebouw #Eindhoven \nPowered by @utwenteEN \n@DesignLabUT @dutchdesignweek https://t.co/qHaJ7Vnnkq"
 	},
 	{
 		"created_at": "Sat Oct 20 09:37:55 +0000 2018",
