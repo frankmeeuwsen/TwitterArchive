dd79f9953ebbcf43d1c9f4716033fda297ee4397
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-10-23 04:21:11 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67380,
-	"created_at": "Tue Oct 23 01:21:11 +0000 2018",
+	"tweets": 67381,
+	"created_at": "Tue Oct 23 02:21:10 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -59,6 +59,317 @@ Grailbird.data.tweets_2018_10 =
 				}
 			},
 			"protected": false,
+			"followers_count": 147866,
+			"friends_count": 491,
+			"listed_count": 1372,
+			"created_at": "Tue Mar 22 04:53:16 +0000 2011",
+			"favourites_count": 693,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2100,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1050093389423628288/oT3kdDul_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1050093389423628288/oT3kdDul_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270193608/1539629829",
+			"profile_link_color": "0F1111",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1054445451498405900,
+		"quoted_status_id_str": "1054445451498405889",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ETlF9iLvHA",
+			"expanded": "https://twitter.com/PearlJam/status/1054445451498405889",
+			"display": "twitter.com/PearlJam/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Oct 22 18:52:40 +0000 2018",
+			"id": 1054445451498405900,
+			"id_str": "1054445451498405889",
+			"full_text": "The first-ever Mookie Blaylock show. #PearlJam #PJHistory https://t.co/xaqDGO058n",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				57
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "PearlJam",
+						"indices": [
+							37,
+							46
+						]
+					},
+					{
+						"text": "PJHistory",
+						"indices": [
+							47,
+							57
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1054445189933105200,
+						"id_str": "1054445189933105152",
+						"indices": [
+							58,
+							81
+						],
+						"media_url": "http://pbs.twimg.com/media/DqIkUEUVAAAOwh7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DqIkUEUVAAAOwh7.jpg",
+						"url": "https://t.co/xaqDGO058n",
+						"display_url": "pic.twitter.com/xaqDGO058n",
+						"expanded_url": "https://twitter.com/PearlJam/status/1054445451498405889/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 741,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 790,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 420,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1054445189933105200,
+						"id_str": "1054445189933105152",
+						"indices": [
+							58,
+							81
+						],
+						"media_url": "http://pbs.twimg.com/media/DqIkUEUVAAAOwh7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DqIkUEUVAAAOwh7.jpg",
+						"url": "https://t.co/xaqDGO058n",
+						"display_url": "pic.twitter.com/xaqDGO058n",
+						"expanded_url": "https://twitter.com/PearlJam/status/1054445451498405889/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 741,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 790,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 420,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15155074,
+				"id_str": "15155074",
+				"name": "Pearl Jam",
+				"screen_name": "PearlJam",
+				"location": "Seattle, WA",
+				"description": "Check out the latest news, music, and merch at the official PJ site and become a member of Pearl Jam's official Fan Club! https://t.co/m7RuxVW4F1",
+				"url": "https://t.co/J95aoJ5FKF",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/J95aoJ5FKF",
+								"expanded_url": "http://www.PearlJam.com",
+								"display_url": "PearlJam.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/m7RuxVW4F1",
+								"expanded_url": "https://pj.lnk.to/10clubTw",
+								"display_url": "pj.lnk.to/10clubTw",
+								"indices": [
+									122,
+									145
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 3637071,
+				"friends_count": 880,
+				"listed_count": 18280,
+				"created_at": "Wed Jun 18 06:59:14 +0000 2008",
+				"favourites_count": 3606,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 7142,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1050824444321693696/_aF043gq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1050824444321693696/_aF043gq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15155074/1539371082",
+				"profile_link_color": "A17F59",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 536,
+			"favorite_count": 2566,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 217,
+		"favorite_count": 1615,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I remember it like it was....28 years ago. https://t.co/ETlF9iLvHA"
+	},
+	{
+		"created_at": "Mon Oct 22 19:11:12 +0000 2018",
+		"id": 1054450114775834600,
+		"id_str": "1054450114775834624",
+		"full_text": "I remember it like it was....28 years ago. https://t.co/ETlF9iLvHA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			42
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ETlF9iLvHA",
+					"expanded_url": "https://twitter.com/PearlJam/status/1054445451498405889",
+					"display_url": "twitter.com/PearlJam/statu…",
+					"indices": [
+						43,
+						66
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270193608,
+			"id_str": "270193608",
+			"name": "Mike McCready",
+			"screen_name": "MikeMcCreadyPJ",
+			"location": "Seattle, WA",
+			"description": "I play guitar in @pearljam.",
+			"url": "https://t.co/KiOFV9fJfm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KiOFV9fJfm",
+							"expanded_url": "http://www.pearljam.com",
+							"display_url": "pearljam.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 147858,
 			"friends_count": 491,
 			"listed_count": 1371,
