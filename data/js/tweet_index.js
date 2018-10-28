2f7381ff7f50caf494c68d1e664995f39004cfdf
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-10-28 20:21:13 +0100

    Added 6 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_10.js",
 		"year": 2018,
 		"var_name": "tweets_2018_10",
-		"tweet_count": 639,
+		"tweet_count": 645,
 		"month": 10
 	},
 	{
