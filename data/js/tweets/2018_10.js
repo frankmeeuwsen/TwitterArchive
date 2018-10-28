6d18f7c99f9126a74551e7edd76cd38c8b097b41
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-10-28 13:21:13 +0100

    Added 1 tweets

--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -127,6 +127,213 @@ Grailbird.data.tweets_2018_10 =
 				}
 			},
 			"protected": false,
+			"followers_count": 67567,
+			"friends_count": 3807,
+			"listed_count": 5579,
+			"created_at": "Sat Aug 05 23:04:08 +0000 2006",
+			"favourites_count": 10268,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 152878,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/923372777670938625/Wjzo_hWE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/923372777670938625/Wjzo_hWE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3839/1485462576",
+			"profile_link_color": "7F7F90",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E1E1DF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "01a9a39529b27f36",
+			"url": "https://api.twitter.com/1.1/geo/id/01a9a39529b27f36.json",
+			"place_type": "city",
+			"name": "Manhattan",
+			"full_name": "Manhattan, NY",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-74.026675,
+							40.683935
+						],
+						[
+							-73.910408,
+							40.683935
+						],
+						[
+							-73.910408,
+							40.877483
+						],
+						[
+							-74.026675,
+							40.877483
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/PlHZWNhIKK"
+	},
+	{
+		"created_at": "Sun Oct 28 02:48:07 +0000 2018",
+		"id": 1056377041606578200,
+		"id_str": "1056377041606578176",
+		"full_text": "https://t.co/PlHZWNhIKK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1056376952758693900,
+					"id_str": "1056376952758693888",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/DqkBPdoXcAAg5uc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DqkBPdoXcAAg5uc.jpg",
+					"url": "https://t.co/PlHZWNhIKK",
+					"display_url": "pic.twitter.com/PlHZWNhIKK",
+					"expanded_url": "https://twitter.com/davewiner/status/1056377041606578176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 654,
+							"h": 554,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 654,
+							"h": 554,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 654,
+							"h": 554,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1056376952758693900,
+					"id_str": "1056376952758693888",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/DqkBPdoXcAAg5uc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DqkBPdoXcAAg5uc.jpg",
+					"url": "https://t.co/PlHZWNhIKK",
+					"display_url": "pic.twitter.com/PlHZWNhIKK",
+					"expanded_url": "https://twitter.com/davewiner/status/1056377041606578176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 654,
+							"h": 554,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 654,
+							"h": 554,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 654,
+							"h": 554,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3839,
+			"id_str": "3839",
+			"name": "scripting.com",
+			"screen_name": "davewiner",
+			"location": "New York",
+			"description": "American software developer, blogger, inventor of new media types.",
+			"url": "https://t.co/ztgzDGiyOj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ztgzDGiyOj",
+							"expanded_url": "http://scripting.com/",
+							"display_url": "scripting.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 67568,
 			"friends_count": 3807,
 			"listed_count": 5579,
