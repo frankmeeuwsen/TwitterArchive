74eabc7b1f4fac30f47908b62d444fc5f760c0a5
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-11-10 17:21:09 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68047,
-	"created_at": "Fri Nov 09 12:21:08 +0000 2018",
+	"tweets": 68048,
+	"created_at": "Sat Nov 10 16:21:08 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sat Nov 10 15:54:04 +0000 2018",
+		"id": 1061285872954433500,
+		"id_str": "1061285872954433536",
+		"full_text": "I breath a sigh of relief when I come across pages like Hrefhunt. Those curated, handmade, well-thought little explorations and collections of personal websites. Free of any businessmodel,  Producthunt-format or growthhack-tactics. \nhttps://t.co/GdHTymNfJW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			256
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GdHTymNfJW",
+					"expanded_url": "https://diggingthedigital.com/a-nice-find-href-hunt/",
+					"display_url": "diggingthedigital.com/a-nice-find-hr…",
+					"indices": [
+						233,
+						256
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8925,
+			"friends_count": 184,
+			"listed_count": 596,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4818,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60658,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I breath a sigh of relief when I come across pages like Hrefhunt. Those curated, handmade, well-thought little explorations and collections of personal websites. Free of any businessmodel,  Producthunt-format or growthhack-tactics. \nhttps://t.co/GdHTymNfJW"
+	},
+	{
 		"created_at": "Fri Nov 09 11:43:44 +0000 2018",
 		"id": 1060860484990181400,
 		"id_str": "1060860484990181377",
@@ -230,7 +336,7 @@ Grailbird.data.tweets_2018_11 =
 		"user": {
 			"id": 21161380,
 			"id_str": "21161380",
-			"name": "Matt Linton 🐦👨‍💻⚕️⚒️🥋🎻",
+			"name": "Matt Linton 🐦👨💻⚕️⚒️🥋🎻",
 			"screen_name": "0xMatt",
 			"location": "",
 			"description": "Heavy Rescue Specialist, Cellist, Space Nut, Parrot lover. Incident Wrangler @ Google.\n\nMy opinions & Tweets are mine, not work's.",
