2df995b88a401868c49b59e9f7a144304d54cca3
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-11-18 21:21:09 +0100

    Added 1 tweets

--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sun Nov 18 19:45:31 +0000 2018",
+		"id": 1064243220731895800,
+		"id_str": "1064243220731895809",
+		"full_text": "Boem! Greef is terug. \n\nWake the sleeping dragon! https://t.co/wD0Hodkd18 https://t.co/4uRsNn0gaP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			73
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wD0Hodkd18",
+					"expanded_url": "https://open.spotify.com/track/6PSlCVx0ct6q5dAtUziMqi?si=D2juY9KhS02_8GXEhLL9EA",
+					"display_url": "open.spotify.com/track/6PSlCVx0…",
+					"indices": [
+						50,
+						73
+					]
+				},
+				{
+					"url": "https://t.co/4uRsNn0gaP",
+					"expanded_url": "https://twitter.com/MENEERDEGREEF/status/1064233232219152389",
+					"display_url": "twitter.com/MENEERDEGREEF/…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8928,
+			"friends_count": 192,
+			"listed_count": 598,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4834,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60603,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1064233232219152400,
+		"quoted_status_id_str": "1064233232219152389",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4uRsNn0gaP",
+			"expanded": "https://twitter.com/MENEERDEGREEF/status/1064233232219152389",
+			"display": "twitter.com/MENEERDEGREEF/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Nov 18 19:05:49 +0000 2018",
+			"id": 1064233232219152400,
+			"id_str": "1064233232219152389",
+			"full_text": "Oh, wacht. \n  \nHALLO SCHATJES!",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				30
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+			"in_reply_to_status_id": 1064232231454031900,
+			"in_reply_to_status_id_str": "1064232231454031872",
+			"in_reply_to_user_id": 260245776,
+			"in_reply_to_user_id_str": "260245776",
+			"in_reply_to_screen_name": "MENEERDEGREEF",
+			"user": {
+				"id": 260245776,
+				"id_str": "260245776",
+				"name": "GREEFBEEST DE SMAADSMID",
+				"screen_name": "MENEERDEGREEF",
+				"location": "Cunter, Svizra",
+				"description": "HARD SCHREEUWENDE VERTICAAL UITGEDAAGDE. GOONIE. KINKSTER. NARE WOESTELING. SUPPORT VIOLENT DANCING. GEEN PIJN GEEN PRET. #JEUGDBENDE REDACTIE @ https://t.co/jAPR19PCWD",
+				"url": "https://t.co/fGvuR8CYuG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/fGvuR8CYuG",
+								"expanded_url": "http://www.retecool.com/author/meneer-de-greef/",
+								"display_url": "retecool.com/author/meneer-…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/jAPR19PCWD",
+								"expanded_url": "http://RETECOOL.COM",
+								"display_url": "RETECOOL.COM",
+								"indices": [
+									145,
+									168
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 346,
+				"friends_count": 1,
+				"listed_count": 5,
+				"created_at": "Thu Mar 03 14:04:17 +0000 2011",
+				"favourites_count": 42,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 81830,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/974299058956316672/Hlxp2X9u_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/974299058956316672/Hlxp2X9u_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/260245776/1519049066",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 25,
+			"favorite_count": 215,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Boem! Greef is terug. \n\nWake the sleeping dragon! https://t.co/wD0Hodkd18 https://t.co/4uRsNn0gaP"
+	},
+	{
 		"created_at": "Sun Nov 18 15:14:39 +0000 2018",
 		"id": 1064175058053140500,
 		"id_str": "1064175058053140481",
