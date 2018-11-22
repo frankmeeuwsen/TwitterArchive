fc40c828c456d83c9eb11ce00b7544f48f9c9f54
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-11-22 12:29:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68172,
-	"created_at": "Wed Nov 21 18:21:10 +0000 2018",
+	"tweets": 68173,
+	"created_at": "Thu Nov 22 11:29:07 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,303 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Thu Nov 22 11:28:39 +0000 2018",
+		"id": 1065567733495017500,
+		"id_str": "1065567733495017472",
+		"full_text": "Als @bol_com nu eens een fout in hun bezorging doet bij ons (doen ze nooit) dan kan ik ook klagen in de chat en gebbetjes voor op Twitter krijgen.... https://t.co/4U67kvBZmt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bol_com",
+					"name": "bol.com",
+					"id": 18894593,
+					"id_str": "18894593",
+					"indices": [
+						4,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/4U67kvBZmt",
+					"expanded_url": "https://twitter.com/bol_com/status/1065565417815621632",
+					"display_url": "twitter.com/bol_com/status…",
+					"indices": [
+						150,
+						173
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8919,
+			"friends_count": 195,
+			"listed_count": 596,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4858,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60640,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1065565417815621600,
+		"quoted_status_id_str": "1065565417815621632",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4U67kvBZmt",
+			"expanded": "https://twitter.com/bol_com/status/1065565417815621632",
+			"display": "twitter.com/bol_com/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 22 11:19:27 +0000 2018",
+			"id": 1065565417815621600,
+			"id_str": "1065565417815621632",
+			"full_text": "Ondertussen niet op de klantenservice. https://t.co/M3DEj9H0g3",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				38
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1065565374601674800,
+						"id_str": "1065565374601674752",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/DsmmEAYWwAADpeQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DsmmEAYWwAADpeQ.jpg",
+						"url": "https://t.co/M3DEj9H0g3",
+						"display_url": "pic.twitter.com/M3DEj9H0g3",
+						"expanded_url": "https://twitter.com/bol_com/status/1065565417815621632/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 266,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1065565374601674800,
+						"id_str": "1065565374601674752",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/media/DsmmEAYWwAADpeQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DsmmEAYWwAADpeQ.jpg",
+						"url": "https://t.co/M3DEj9H0g3",
+						"display_url": "pic.twitter.com/M3DEj9H0g3",
+						"expanded_url": "https://twitter.com/bol_com/status/1065565417815621632/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 400,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 266,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 400,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://ads-api.twitter.com\" rel=\"nofollow\">Twitter Ads Composer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18894593,
+				"id_str": "18894593",
+				"name": "bol.com",
+				"screen_name": "bol_com",
+				"location": "Utrecht",
+				"description": "Welkom op ons Twitter-kanaal. Boordevol inspiratie, winacties en antwoord op al je vragen. Tussen 08:00 en 23:00 reageren wij binnen één uur. #speciaalvoorjou",
+				"url": "https://t.co/fNLoBombr8",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/fNLoBombr8",
+								"expanded_url": "http://www.bol.com",
+								"display_url": "bol.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 62171,
+				"friends_count": 18308,
+				"listed_count": 386,
+				"created_at": "Mon Jan 12 07:23:41 +0000 2009",
+				"favourites_count": 125,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 200727,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/2622406003/qhirc530zlnl7oog4wvb_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/2622406003/qhirc530zlnl7oog4wvb_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18894593/1463149832",
+				"profile_link_color": "3366CC",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 50,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Als @bol_com nu eens een fout in hun bezorging doet bij ons (doen ze nooit) dan kan ik ook klagen in de chat en gebbetjes voor op Twitter krijgen.... https://t.co/4U67kvBZmt"
+	},
+	{
 		"created_at": "Wed Nov 21 18:06:15 +0000 2018",
 		"id": 1065305402936574000,
 		"id_str": "1065305402936573952",
