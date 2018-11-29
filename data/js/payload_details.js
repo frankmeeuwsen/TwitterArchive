452c285d7f62e5eec515519e8f724953c9dc9f86
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-11-29 08:32:31 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68572,
-	"created_at": "Wed Nov 28 20:26:21 +0000 2018",
+	"tweets": 68573,
+	"created_at": "Thu Nov 29 07:32:30 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Thu Nov 29 06:26:46 +0000 2018",
+		"id": 1068028475091664900,
+		"id_str": "1068028475091664896",
+		"full_text": "Vrolijk Sint Pannenkoek!\n\nDe eerste feestdag in tijden waar je (volgens mij) niemand mee kwetst en waar tradities niet krampachtig worden verdedigd. \n\nHoop ik. https://t.co/uNquMPDr1m",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1068028467181232100,
+					"id_str": "1068028467181232129",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/DtJmO3RXoAEbQ1V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtJmO3RXoAEbQ1V.jpg",
+					"url": "https://t.co/uNquMPDr1m",
+					"display_url": "pic.twitter.com/uNquMPDr1m",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1068028475091664896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 315,
+							"h": 437,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 315,
+							"h": 437,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 315,
+							"h": 437,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1068028467181232100,
+					"id_str": "1068028467181232129",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/DtJmO3RXoAEbQ1V.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtJmO3RXoAEbQ1V.jpg",
+					"url": "https://t.co/uNquMPDr1m",
+					"display_url": "pic.twitter.com/uNquMPDr1m",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1068028475091664896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 315,
+							"h": 437,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 315,
+							"h": 437,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 315,
+							"h": 437,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8914,
+			"friends_count": 199,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4859,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60547,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Vrolijk Sint Pannenkoek!\n\nDe eerste feestdag in tijden waar je (volgens mij) niemand mee kwetst en waar tradities niet krampachtig worden verdedigd. \n\nHoop ik. https://t.co/uNquMPDr1m"
+	},
+	{
 		"created_at": "Wed Nov 28 20:18:01 +0000 2018",
 		"id": 1067875280319057900,
 		"id_str": "1067875280319057920",
