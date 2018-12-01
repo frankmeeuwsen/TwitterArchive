a60f9147c63f8f2a26c890ba9991bcd46a9ed323
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-12-01 09:23:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68580,
-	"created_at": "Fri Nov 30 21:21:12 +0000 2018",
+	"tweets": 68581,
+	"created_at": "Sat Dec 01 08:23:18 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_12.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2018_12 = 
+[
+	{
+		"created_at": "Sat Dec 01 08:17:25 +0000 2018",
+		"id": 1068781096639189000,
+		"id_str": "1068781096639188992",
+		"full_text": "Nou @PostNL. Ik probeerde de chatbot op jullie site. Dat was geen overweldigend succes. Wat nu? En waar is mijn pakketje? De chatpostbode vroeg alle gegevens waar ik op te bereiken ben dus ik verwacht binnen 2 werkdagen een mail, SMS, Whatsapp of tweet. OK? Doei! https://t.co/uXe0WvpQYS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			263
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PostNL",
+					"name": "PostNL",
+					"id": 138062686,
+					"id_str": "138062686",
+					"indices": [
+						4,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1068780875440107500,
+					"id_str": "1068780875440107520",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/DtUSiydWsAAa-71.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtUSiydWsAAa-71.jpg",
+					"url": "https://t.co/uXe0WvpQYS",
+					"display_url": "pic.twitter.com/uXe0WvpQYS",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1068781096639188992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 244,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 734,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1068780875440107500,
+					"id_str": "1068780875440107520",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/DtUSiydWsAAa-71.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtUSiydWsAAa-71.jpg",
+					"url": "https://t.co/uXe0WvpQYS",
+					"display_url": "pic.twitter.com/uXe0WvpQYS",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1068781096639188992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 244,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 734,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 430,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8913,
+			"friends_count": 199,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4864,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60554,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Nou @PostNL. Ik probeerde de chatbot op jullie site. Dat was geen overweldigend succes. Wat nu? En waar is mijn pakketje? De chatpostbode vroeg alle gegevens waar ik op te bereiken ben dus ik verwacht binnen 2 werkdagen een mail, SMS, Whatsapp of tweet. OK? Doei! https://t.co/uXe0WvpQYS"
+	}
+]
