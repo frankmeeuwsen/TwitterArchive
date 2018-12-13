8cd646a73a3d19c1dda4c4e2519df4a4af7977ea
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-12-13 07:21:05 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68690,
-	"created_at": "Thu Dec 13 00:24:28 +0000 2018",
+	"tweets": 68692,
+	"created_at": "Thu Dec 13 06:21:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Thu Dec 13 05:41:06 +0000 2018",
+		"id": 1073090413664313300,
+		"id_str": "1073090413664313346",
+		"full_text": "Good bot. https://t.co/1VEsDjbOMX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1VEsDjbOMX",
+					"expanded_url": "https://twitter.com/RCTGuests/status/1071996245378514946",
+					"display_url": "twitter.com/RCTGuests/stat…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8912,
+			"friends_count": 204,
+			"listed_count": 595,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4875,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60411,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1071996245378515000,
+		"quoted_status_id_str": "1071996245378514946",
+		"quoted_status_permalink": {
+			"url": "https://t.co/1VEsDjbOMX",
+			"expanded": "https://twitter.com/RCTGuests/status/1071996245378514946",
+			"display": "twitter.com/RCTGuests/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Dec 10 05:13:16 +0000 2018",
+			"id": 1071996245378515000,
+			"id_str": "1071996245378514946",
+			"full_text": "Guest 1486 has died",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				19
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://www.rollercoastertycoon.com\" rel=\"nofollow\">RCTGuests</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 809179237609521200,
+				"id_str": "809179237609521152",
+				"name": "RollerCoaster Tycoon",
+				"screen_name": "RCTGuests",
+				"location": "",
+				"description": "Nausea tolerance: Low",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6769,
+				"friends_count": 2,
+				"listed_count": 5,
+				"created_at": "Wed Dec 14 23:32:18 +0000 2016",
+				"favourites_count": 11,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2427,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/809185004630769665/4vUu7E67_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/809185004630769665/4vUu7E67_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/809179237609521152/1481759759",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3613,
+			"favorite_count": 19964,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Good bot. https://t.co/1VEsDjbOMX"
+	},
+	{
+		"created_at": "Thu Dec 13 02:18:37 +0000 2018",
+		"id": 1073039456393805800,
+		"id_str": "1073039456393805825",
+		"full_text": "Yep: Facebook Sells Data to Advertisers   via @NYTimes https://t.co/iNbnUrgOjz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nytimes",
+					"name": "The New York Times",
+					"id": 807095,
+					"id_str": "807095",
+					"indices": [
+						46,
+						54
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/iNbnUrgOjz",
+					"expanded_url": "https://nyti.ms/2zV3ih0?smid=nytcore-ios-share",
+					"display_url": "nyti.ms/2zV3ih0?smid=n…",
+					"indices": [
+						55,
+						78
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5763262,
+			"id_str": "5763262",
+			"name": "Kara Swisher",
+			"screen_name": "karaswisher",
+			"location": "San Francisco",
+			"description": "Grumpy lady of tech",
+			"url": "https://t.co/trFjw6yxoC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/trFjw6yxoC",
+							"expanded_url": "http://recode.net",
+							"display_url": "recode.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1273978,
+			"friends_count": 1668,
+			"listed_count": 14931,
+			"created_at": "Fri May 04 10:32:22 +0000 2007",
+			"favourites_count": 40177,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 112108,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F4FCFC",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/517197689746755584/795gibU7_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/517197689746755584/795gibU7_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5763262/1411788872",
+			"profile_link_color": "01010A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 52,
+		"favorite_count": 152,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Yep: Facebook Sells Data to Advertisers   via @NYTimes https://t.co/iNbnUrgOjz"
+	},
+	{
 		"created_at": "Wed Dec 12 22:54:58 +0000 2018",
 		"id": 1072988207527153700,
 		"id_str": "1072988207527153664",
