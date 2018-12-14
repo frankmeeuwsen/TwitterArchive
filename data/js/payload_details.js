8f5d51b1435e94370f30b4bceaa2373837329061
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-12-14 09:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68704,
-	"created_at": "Thu Dec 13 11:21:06 +0000 2018",
+	"tweets": 68705,
+	"created_at": "Fri Dec 14 08:21:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Fri Dec 14 08:03:04 +0000 2018",
+		"id": 1073488530050048000,
+		"id_str": "1073488530050048000",
+		"full_text": "“Mega-internet-influencer”... wacht, even mijn LinkedIn aanpassen... https://t.co/3jVirnoKq5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3jVirnoKq5",
+					"expanded_url": "https://twitter.com/djzwartveld/status/1073478786988036096",
+					"display_url": "twitter.com/djzwartveld/st…",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Lite</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Community Architect @iamolisto | Utrecht | Father | Pearl Jam | Power user of the internet | \"je moet je lengte in je Twitterbio zetten\" aldus @brechtjedeleij",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8909,
+			"friends_count": 204,
+			"listed_count": 595,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4877,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60417,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1073478786988036100,
+		"quoted_status_id_str": "1073478786988036096",
+		"quoted_status_permalink": {
+			"url": "https://t.co/3jVirnoKq5",
+			"expanded": "https://twitter.com/djzwartveld/status/1073478786988036096",
+			"display": "twitter.com/djzwartveld/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Dec 14 07:24:21 +0000 2018",
+			"id": 1073478786988036100,
+			"id_str": "1073478786988036096",
+			"full_text": "Dagje inspiratie op doen/kennis delen in het midden van het land. Eerst een  kop koffie met mega-internet-influencer @frankmeeuwsen en daarna langs bij mede-@CustomerFirstNL-blogger en directeur Tevreden Klanten Remko Amnesz van de AMP Groep. Go-Go-Go",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				251
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "frankmeeuwsen",
+						"name": "Frank Meeuwsen",
+						"id": 816203,
+						"id_str": "816203",
+						"indices": [
+							117,
+							131
+						]
+					},
+					{
+						"screen_name": "CustomerFirstNL",
+						"name": "CustomerFirst Tweets",
+						"id": 102760849,
+						"id_str": "102760849",
+						"indices": [
+							157,
+							173
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2220361328,
+				"id_str": "2220361328",
+				"name": "Dirk Jan Zwartveld",
+				"screen_name": "djzwartveld",
+				"location": "Wateringen",
+				"description": "Vinex vader en afkickende smartphone verslaafde. Houdt van muziek, lezen, hardlopen en bloggen. Helpt organisaties om medewerker én klanten blij te maken.",
+				"url": "https://t.co/Cw9zPnWiKh",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Cw9zPnWiKh",
+								"expanded_url": "http://www.djzwartveld.nl",
+								"display_url": "djzwartveld.nl",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 440,
+				"friends_count": 80,
+				"listed_count": 29,
+				"created_at": "Thu Nov 28 21:13:41 +0000 2013",
+				"favourites_count": 1729,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6908,
+				"lang": "nl",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1062089180975636484/YOEWpNm-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1062089180975636484/YOEWpNm-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2220361328/1497866546",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "“Mega-internet-influencer”... wacht, even mijn LinkedIn aanpassen... https://t.co/3jVirnoKq5"
+	},
+	{
 		"created_at": "Thu Dec 13 10:31:12 +0000 2018",
 		"id": 1073163420269244400,
 		"id_str": "1073163420269244416",
