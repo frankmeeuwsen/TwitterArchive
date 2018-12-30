9179bc5320a5e5d44da7d06cfdaf62cfb83c6de6
Frank Meeuwsen <frank@incredibleadventure.nl>
2018-12-30 21:21:09 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68886,
-	"created_at": "Sun Dec 30 13:21:07 +0000 2018",
+	"tweets": 68887,
+	"created_at": "Sun Dec 30 20:21:08 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sun Dec 30 20:01:31 +0000 2018",
+		"id": 1079467540974309400,
+		"id_str": "1079467540974309377",
+		"full_text": "@aljo @BasBoerman Je kunt mijn stappenplan lezen op https://t.co/MpMSOQmRS5",
+		"truncated": false,
+		"display_text_range": [
+			18,
+			75
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "aljo",
+					"name": "Aljo Hartgers ⚡️🚗",
+					"id": 9993492,
+					"id_str": "9993492",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "BasBoerman",
+					"name": "Bas Boerman",
+					"id": 17084172,
+					"id_str": "17084172",
+					"indices": [
+						6,
+						17
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MpMSOQmRS5",
+					"expanded_url": "https://diggingthedigital.com/Afscheid-Google-Apps/",
+					"display_url": "diggingthedigital.com/Afscheid-Googl…",
+					"indices": [
+						52,
+						75
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1079451855153971200,
+		"in_reply_to_status_id_str": "1079451855153971201",
+		"in_reply_to_user_id": 9993492,
+		"in_reply_to_user_id_str": "9993492",
+		"in_reply_to_screen_name": "aljo",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Utrecht | Father | Pearl Jam | Power user of the internet |",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8893,
+			"friends_count": 206,
+			"listed_count": 597,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4904,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60293,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@aljo @BasBoerman Je kunt mijn stappenplan lezen op https://t.co/MpMSOQmRS5"
+	},
+	{
 		"created_at": "Sun Dec 30 12:53:31 +0000 2018",
 		"id": 1079359827292373000,
 		"id_str": "1079359827292372992",
