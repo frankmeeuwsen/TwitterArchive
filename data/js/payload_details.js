4329de16c2a18e79c84cf79ba0ea65b9e4cdfe2c
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-01 02:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68902,
-	"created_at": "Tue Jan 01 00:21:07 +0000 2019",
+	"tweets": 68903,
+	"created_at": "Tue Jan 01 01:21:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,313 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 01 00:36:38 +0000 2019",
+		"id": 1079899163582754800,
+		"id_str": "1079899163582754816",
+		"full_text": "GawdDAMN that was good! The Record Company mense... Check dat uit. https://t.co/XUR8FmVBY6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XUR8FmVBY6",
+					"expanded_url": "https://twitter.com/therecordcomp/status/1079823758263693312",
+					"display_url": "twitter.com/therecordcomp/…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Utrecht | Father | Pearl Jam | Power user of the internet |",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8892,
+			"friends_count": 207,
+			"listed_count": 596,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4909,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60300,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1079823758263693300,
+		"quoted_status_id_str": "1079823758263693312",
+		"quoted_status_permalink": {
+			"url": "https://t.co/XUR8FmVBY6",
+			"expanded": "https://twitter.com/therecordcomp/status/1079823758263693312",
+			"display": "twitter.com/therecordcomp/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Dec 31 19:37:00 +0000 2018",
+			"id": 1079823758263693300,
+			"id_str": "1079823758263693312",
+			"full_text": "HAPPY NEW YEAR! Excited to be a part of Jools Holland's 26th Annual Hootenanny to ring in the new year! Catch all the great performances on @BBCTwo or stream here https://t.co/njEfC6jrTK (at 3:15 PST if in the US) https://t.co/dNLhSpLZap",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				213
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "BBCTwo",
+						"name": "BBC Two",
+						"id": 1586183960,
+						"id_str": "1586183960",
+						"indices": [
+							140,
+							147
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/njEfC6jrTK",
+						"expanded_url": "https://bit.ly/2AnAgZf",
+						"display_url": "bit.ly/2AnAgZf",
+						"indices": [
+							163,
+							186
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1079821992793071600,
+						"id_str": "1079821992793071616",
+						"indices": [
+							214,
+							237
+						],
+						"media_url": "http://pbs.twimg.com/media/DvxMYZ0V4AACe7b.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DvxMYZ0V4AACe7b.jpg",
+						"url": "https://t.co/dNLhSpLZap",
+						"display_url": "pic.twitter.com/dNLhSpLZap",
+						"expanded_url": "https://twitter.com/therecordcomp/status/1079823758263693312/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 300,
+								"h": 168,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 300,
+								"h": 168,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 300,
+								"h": 168,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1079821992793071600,
+						"id_str": "1079821992793071616",
+						"indices": [
+							214,
+							237
+						],
+						"media_url": "http://pbs.twimg.com/media/DvxMYZ0V4AACe7b.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DvxMYZ0V4AACe7b.jpg",
+						"url": "https://t.co/dNLhSpLZap",
+						"display_url": "pic.twitter.com/dNLhSpLZap",
+						"expanded_url": "https://twitter.com/therecordcomp/status/1079823758263693312/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 300,
+								"h": 168,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 300,
+								"h": 168,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 300,
+								"h": 168,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 416782606,
+				"id_str": "416782606",
+				"name": "The Record Company",
+				"screen_name": "therecordcomp",
+				"location": "Los Angeles, CA",
+				"description": "All Of This Life out now!",
+				"url": "https://t.co/o5mwalQusy",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/o5mwalQusy",
+								"expanded_url": "http://found.ee/TRC_AOTLpivot-r",
+								"display_url": "found.ee/TRC_AOTLpivot-r",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 7953,
+				"friends_count": 269,
+				"listed_count": 129,
+				"created_at": "Sun Nov 20 04:23:35 +0000 2011",
+				"favourites_count": 6239,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 4396,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/982964393834147840/OznM73R0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/982964393834147840/OznM73R0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/416782606/1543807648",
+				"profile_link_color": "009999",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18,
+			"favorite_count": 125,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "GawdDAMN that was good! The Record Company mense... Check dat uit. https://t.co/XUR8FmVBY6"
+	},
+	{
 		"created_at": "Tue Jan 01 00:09:36 +0000 2019",
 		"id": 1079892360950173700,
 		"id_str": "1079892360950173696",
