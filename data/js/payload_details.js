ad141e68496ef8a6dda4e8738882da75c86a839a
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-04 19:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68980,
-	"created_at": "Fri Jan 04 10:21:04 +0000 2019",
+	"tweets": 68981,
+	"created_at": "Fri Jan 04 18:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,331 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Fri Jan 04 17:37:24 +0000 2019",
+		"id": 1081243212193452000,
+		"id_str": "1081243212193452035",
+		"full_text": "Zo'n Twitter meme account die je eigenlijk zelf wilt bedenken en doen. Geweldig! Check de Twitter search op #aocdancing ofzo voor de achtergrond als je het nog niet kent. https://t.co/qUrz6QkAjG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aocdancing",
+					"indices": [
+						108,
+						119
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qUrz6QkAjG",
+					"expanded_url": "https://twitter.com/aoc_dances/status/1081083811952087040",
+					"display_url": "twitter.com/aoc_dances/sta…",
+					"indices": [
+						171,
+						194
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Utrecht | Father | Pearl Jam | Power user of the internet |",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8889,
+			"friends_count": 217,
+			"listed_count": 596,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4922,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60264,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1081083811952087000,
+		"quoted_status_id_str": "1081083811952087040",
+		"quoted_status_permalink": {
+			"url": "https://t.co/qUrz6QkAjG",
+			"expanded": "https://twitter.com/aoc_dances/status/1081083811952087040",
+			"display": "twitter.com/aoc_dances/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jan 04 07:04:00 +0000 2019",
+			"id": 1081083811952087000,
+			"id_str": "1081083811952087040",
+			"full_text": "Wham! - Wake Me Up Before You Go Go https://t.co/zrfjHLWJuj",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				35
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1081083597296005100,
+						"id_str": "1081083597296005122",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1081083597296005122/pu/img/Y4h6vQc8gAjhvXtA.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1081083597296005122/pu/img/Y4h6vQc8gAjhvXtA.jpg",
+						"url": "https://t.co/zrfjHLWJuj",
+						"display_url": "pic.twitter.com/zrfjHLWJuj",
+						"expanded_url": "https://twitter.com/aoc_dances/status/1081083811952087040/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1081083597296005100,
+						"id_str": "1081083597296005122",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1081083597296005122/pu/img/Y4h6vQc8gAjhvXtA.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1081083597296005122/pu/img/Y4h6vQc8gAjhvXtA.jpg",
+						"url": "https://t.co/zrfjHLWJuj",
+						"display_url": "pic.twitter.com/zrfjHLWJuj",
+						"expanded_url": "https://twitter.com/aoc_dances/status/1081083811952087040/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 29720,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1081083597296005122/pu/vid/320x180/l7zDH_pkrxOS9AwT.mp4?tag=6"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1081083597296005122/pu/vid/640x360/EIIHjry9LZmy0CbJ.mp4?tag=6"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1081083597296005122/pu/pl/ksBMSZrctxD9rH1i.m3u8?tag=6"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1081083597296005122/pu/vid/1280x720/S6laSk35O-DrFZ9M.mp4?tag=6"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1081050713277395000,
+				"id_str": "1081050713277394944",
+				"name": "AOC Dances To Every Song",
+				"screen_name": "aoc_dances",
+				"location": "",
+				"description": "edits of AOC dancing to every song!\nrun by @leftistthot420",
+				"url": "https://t.co/TzrVpKnyWn",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/TzrVpKnyWn",
+								"expanded_url": "http://cash.me/leftistthot",
+								"display_url": "cash.me/leftistthot",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 13069,
+				"friends_count": 2,
+				"listed_count": 47,
+				"created_at": "Fri Jan 04 04:52:29 +0000 2019",
+				"favourites_count": 5,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 49,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1081051604676108288/mCXw_MuR_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081051604676108288/mCXw_MuR_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1081050713277394944/1546586601",
+				"profile_link_color": "F58EA8",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 814,
+			"favorite_count": 3674,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Zo'n Twitter meme account die je eigenlijk zelf wilt bedenken en doen. Geweldig! Check de Twitter search op #aocdancing ofzo voor de achtergrond als je het nog niet kent. https://t.co/qUrz6QkAjG"
+	},
+	{
 		"created_at": "Fri Jan 04 09:40:59 +0000 2019",
 		"id": 1081123316847558700,
 		"id_str": "1081123316847558656",
