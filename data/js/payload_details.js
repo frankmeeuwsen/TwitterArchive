27f1125015275f34b2a8d6f77c97d15f8ecc64a3
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-05 14:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68997,
-	"created_at": "Sat Jan 05 11:21:04 +0000 2019",
+	"tweets": 68998,
+	"created_at": "Sat Jan 05 13:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 05 12:54:15 +0000 2019",
+		"id": 1081534342730600400,
+		"id_str": "1081534342730600448",
+		"full_text": "@erwblo Mark zit niet op Twitter of zo denk ik? Hij heeft echt wel wat mooie ontmoetingen met de band gehad.",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			108
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "erwblo",
+					"name": "erwin blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1081513813889949700,
+		"in_reply_to_status_id_str": "1081513813889949696",
+		"in_reply_to_user_id": 713333,
+		"in_reply_to_user_id_str": "713333",
+		"in_reply_to_screen_name": "erwblo",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Utrecht | Father | Pearl Jam | Power user of the internet |",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8890,
+			"friends_count": 220,
+			"listed_count": 596,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4925,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60269,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1535224303",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@erwblo Mark zit niet op Twitter of zo denk ik? Hij heeft echt wel wat mooie ontmoetingen met de band gehad."
+	},
+	{
 		"created_at": "Sat Jan 05 10:21:05 +0000 2019",
 		"id": 1081495794312773600,
 		"id_str": "1081495794312773632",
@@ -720,7 +826,7 @@ Grailbird.data.tweets_2019_01 =
 				"name": "Tim Fitzsimons",
 				"screen_name": "tfitzsimons",
 				"location": "New York, NY",
-				"description": "🏳️‍🌈 Reporter @NBCOut. Alum: 📻 @npr, @marketplace, @HRW, @columbiajourn. Ex-Beiruti. Tuna's human. Unarmed. Trilingual: EN/ES/عر Tips: tim.fitzsimons@nbcuni.com",
+				"description": "🏳️🌈 Reporter @NBCOut. Alum: 📻 @npr, @marketplace, @HRW, @columbiajourn. Ex-Beiruti. Tuna's human. Unarmed. Trilingual: EN/ES/عر Tips: tim.fitzsimons@nbcuni.com",
 				"url": "https://t.co/g0hP8uxNBg",
 				"entities": {
 					"url": {
