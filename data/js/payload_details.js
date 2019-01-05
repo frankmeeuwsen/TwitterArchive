dd1170a395215acb366a7e91e82042d9d0601478
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-05 09:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68983,
-	"created_at": "Fri Jan 04 23:21:04 +0000 2019",
+	"tweets": 68984,
+	"created_at": "Sat Jan 05 08:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Fri Jan 04 13:08:41 +0000 2019",
+		"id": 1081175584540115000,
+		"id_str": "1081175584540114944",
+		"full_text": "hey @britishmuseum give us your best duck",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "britishmuseum",
+					"name": "British Museum",
+					"id": 19066345,
+					"id_str": "19066345",
+					"indices": [
+						4,
+						18
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19907840,
+			"id_str": "19907840",
+			"name": "The Museum of English Rural Life",
+			"screen_name": "TheMERL",
+			"location": "Reading, Berkshire, UK",
+			"description": "The MERL, @UniofReading. We explore the English countryside, its history and its people. Lawful Good.",
+			"url": "https://t.co/I9v2bFDB1U",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/I9v2bFDB1U",
+							"expanded_url": "https://merl.reading.ac.uk/",
+							"display_url": "merl.reading.ac.uk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 85886,
+			"friends_count": 2973,
+			"listed_count": 778,
+			"created_at": "Mon Feb 02 14:07:07 +0000 2009",
+			"favourites_count": 11123,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 18099,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1025403744639504384/UjSwRuBR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1025403744639504384/UjSwRuBR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19907840/1542801392",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1336,
+		"favorite_count": 5226,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "hey @britishmuseum give us your best duck"
+	},
+	{
 		"created_at": "Fri Jan 04 22:21:40 +0000 2019",
 		"id": 1081314747050532900,
 		"id_str": "1081314747050532864",
