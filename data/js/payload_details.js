5149f9cf32d58e7fbdf9afc6e5085d2b0348a9a9
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-10 20:21:11 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69101,
-	"created_at": "Thu Jan 10 16:21:07 +0000 2019",
+	"tweets": 69102,
+	"created_at": "Thu Jan 10 19:21:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,116 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Jan 10 10:17:40 +0000 2019",
+		"id": 1083306876534775800,
+		"id_str": "1083306876534775809",
+		"full_text": "Tijd om op te stappen bij #xs4all nu #kpn heeft besloten het 'merk' op te heffen. Sorry, maar xs4all is geen 'merk'. Laten we opnieuw beginnen en de historische fout van 1998 herstellen.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			186
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "xs4all",
+					"indices": [
+						26,
+						33
+					]
+				},
+				{
+					"text": "kpn",
+					"indices": [
+						37,
+						41
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.echofon.com/\" rel=\"nofollow\">Echofon</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8352352,
+			"id_str": "8352352",
+			"name": "Geert Lovink",
+			"screen_name": "glovink",
+			"location": "Amsterdam, The Netherlands",
+			"description": "media theorist/net critic",
+			"url": "http://t.co/N1zSmN7jPI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/N1zSmN7jPI",
+							"expanded_url": "http://www.networkcultures.org/geert",
+							"display_url": "networkcultures.org/geert",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7374,
+			"friends_count": 539,
+			"listed_count": 330,
+			"created_at": "Wed Aug 22 08:15:28 +0000 2007",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3409,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1061901364148817920/EmiBjMpd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1061901364148817920/EmiBjMpd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8352352/1541956265",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 21,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "nl",
+		"text": "Tijd om op te stappen bij #xs4all nu #kpn heeft besloten het 'merk' op te heffen. Sorry, maar xs4all is geen 'merk'. Laten we opnieuw beginnen en de historische fout van 1998 herstellen."
+	},
+	{
 		"created_at": "Thu Jan 10 16:19:52 +0000 2019",
 		"id": 1083398027317035000,
 		"id_str": "1083398027317035008",
