8b25f6093223f1b6a848aa267cc3bc6c1211cad0
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-15 09:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69155,
-	"created_at": "Mon Jan 14 15:21:10 +0000 2019",
+	"tweets": 69156,
+	"created_at": "Tue Jan 15 08:21:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 15 06:54:51 +0000 2019",
+		"id": 1085067774030958600,
+		"id_str": "1085081770352087040",
+		"full_text": "Smart TV 15.01.94 - 6 uur live tv met o.a. de officiële opening van De Digitale Stad door @FrankdeGrave Interviews met Rop, Felipe, Marleen en vele vanderen. Tamelijk hilarisch hoe iedereen probeert uit te leggen wat het internet is.  https://t.co/Ib6D0LQLLH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FrankdeGrave",
+					"name": "Frank de Grave",
+					"id": 216313893,
+					"id_str": "216313893",
+					"indices": [
+						90,
+						103
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ib6D0LQLLH",
+					"expanded_url": "https://youtu.be/nCx887IW_wA",
+					"display_url": "youtu.be/nCx887IW_wA",
+					"indices": [
+						235,
+						258
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16408747,
+			"id_str": "16408747",
+			"name": "marleenstikker",
+			"screen_name": "marleenstikker",
+			"location": "Amsterdam",
+			"description": "Technology is not Neutral @Waag, Fablab Amsterdam, Fairphone ‘The ego is nothing more than a structure in the whole which can come and go’ David Bohm",
+			"url": "https://t.co/0aywcFOPDn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0aywcFOPDn",
+							"expanded_url": "http://www.waag.org",
+							"display_url": "waag.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9832,
+			"friends_count": 3384,
+			"listed_count": 295,
+			"created_at": "Mon Sep 22 19:53:07 +0000 2008",
+			"favourites_count": 7169,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8662,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "090606",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1059542037228339203/WuaOvoz__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1059542037228339203/WuaOvoz__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16408747/1547218674",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "141315",
+			"profile_sidebar_fill_color": "E6E0E0",
+			"profile_text_color": "342D30",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 23,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Smart TV 15.01.94 - 6 uur live tv met o.a. de officiële opening van De Digitale Stad door @FrankdeGrave Interviews met Rop, Felipe, Marleen en vele vanderen. Tamelijk hilarisch hoe iedereen probeert uit te leggen wat het internet is.  https://t.co/Ib6D0LQLLH"
+	},
+	{
 		"created_at": "Mon Jan 14 15:13:52 +0000 2019",
 		"id": 1084830969184567300,
 		"id_str": "1084830969184567296",
