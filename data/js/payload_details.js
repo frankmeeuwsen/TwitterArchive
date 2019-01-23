82217b1be3adbc35b65fd210cab37601ef41c5c5
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-23 23:21:09 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69212,
-	"created_at": "Wed Jan 23 19:21:12 +0000 2019",
+	"tweets": 69213,
+	"created_at": "Wed Jan 23 22:21:08 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 23 22:12:22 +0000 2019",
+		"id": 1088197777581060100,
+		"id_str": "1088197777581060097",
+		"full_text": "@remkusdevries @KeesRomkes @contentful @Netlify @statamic Sinds het redesign van de Olisto site zie ik weer de mogelijkheden van het WP platform. Nu ik 20 jaar bloggen wil verzamelen op 1 platform kom ik wel de grenzen van static site builders tegen.",
+		"truncated": false,
+		"display_text_range": [
+			58,
+			250
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "remkusdevries",
+					"name": "Remkus de Vries",
+					"id": 14264386,
+					"id_str": "14264386",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "KeesRomkes",
+					"name": "Kees",
+					"id": 7842092,
+					"id_str": "7842092",
+					"indices": [
+						15,
+						26
+					]
+				},
+				{
+					"screen_name": "contentful",
+					"name": "Contentful",
+					"id": 164208193,
+					"id_str": "164208193",
+					"indices": [
+						27,
+						38
+					]
+				},
+				{
+					"screen_name": "Netlify",
+					"name": "Netlify",
+					"id": 2571501973,
+					"id_str": "2571501973",
+					"indices": [
+						39,
+						47
+					]
+				},
+				{
+					"screen_name": "statamic",
+					"name": "Statamic",
+					"id": 546114119,
+					"id_str": "546114119",
+					"indices": [
+						48,
+						57
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1088136355044053000,
+		"in_reply_to_status_id_str": "1088136355044052992",
+		"in_reply_to_user_id": 14264386,
+		"in_reply_to_user_id_str": "14264386",
+		"in_reply_to_screen_name": "remkusdevries",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Doet toffe dingen met content en communities.",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8883,
+			"friends_count": 233,
+			"listed_count": 593,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4985,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59943,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@remkusdevries @KeesRomkes @contentful @Netlify @statamic Sinds het redesign van de Olisto site zie ik weer de mogelijkheden van het WP platform. Nu ik 20 jaar bloggen wil verzamelen op 1 platform kom ik wel de grenzen van static site builders tegen."
+	},
+	{
 		"created_at": "Wed Jan 23 18:34:54 +0000 2019",
 		"id": 1088143049895694300,
 		"id_str": "1088143049895694336",
