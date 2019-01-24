da8d87ab635a109356b1d87898aa7d1a04b0c3d5
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-24 20:21:10 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69216,
-	"created_at": "Thu Jan 24 17:21:10 +0000 2019",
+	"tweets": 69217,
+	"created_at": "Thu Jan 24 19:21:09 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,130 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Thu Jan 24 19:18:52 +0000 2019",
+		"id": 1088516501810958300,
+		"id_str": "1088516501810958336",
+		"full_text": "@TrustNobodycast @WieIsDeMol Idem. Al is die foutcode 42 wel erg verdacht natuurlijk. Qua Hitchhiker's Guide To The Galaxy. En we weten allemaal wiens favoriete boek dat is! Lees alle Wikipedia en Youtube en Instagram er maar op na! #onbruikbarewidmhints",
+		"truncated": false,
+		"display_text_range": [
+			29,
+			254
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "onbruikbarewidmhints",
+					"indices": [
+						233,
+						254
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TrustNobodycast",
+					"name": "Trust Nobody",
+					"id": 943435303846662100,
+					"id_str": "943435303846662144",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "WieIsDeMol",
+					"name": "Wie is de Mol?",
+					"id": 18267154,
+					"id_str": "18267154",
+					"indices": [
+						17,
+						28
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1088504024473706500,
+		"in_reply_to_status_id_str": "1088504024473706496",
+		"in_reply_to_user_id": 943435303846662100,
+		"in_reply_to_user_id_str": "943435303846662144",
+		"in_reply_to_screen_name": "TrustNobodycast",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Doet toffe dingen met content en communities.",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8878,
+			"friends_count": 233,
+			"listed_count": 593,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 4989,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59948,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@TrustNobodycast @WieIsDeMol Idem. Al is die foutcode 42 wel erg verdacht natuurlijk. Qua Hitchhiker's Guide To The Galaxy. En we weten allemaal wiens favoriete boek dat is! Lees alle Wikipedia en Youtube en Instagram er maar op na! #onbruikbarewidmhints"
+	},
+	{
 		"created_at": "Sun Jan 20 08:56:54 +0000 2019",
 		"id": 1086910428976795600,
 		"id_str": "1088486552056070146",
