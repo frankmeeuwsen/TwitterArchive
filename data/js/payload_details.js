9412d9bbb6780479ec1ebd56b168100996c9a21d
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-01-29 18:21:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69251,
-	"created_at": "Tue Jan 29 16:21:08 +0000 2019",
+	"tweets": 69252,
+	"created_at": "Tue Jan 29 17:21:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,137 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 29 16:33:21 +0000 2019",
+		"id": 1090286786167877600,
+		"id_str": "1090286786167877633",
+		"full_text": "@TrustNobodycast ik luister naar de aluhoedjes van @Nelleke (beste deel vind ik). Komtie: check de tijdscodes van opdracht 1 met het album van Eddie en de hotrods! Nee maar echt! Ik weet zeker dat je dan een songtekst over meteoriet krijgt. En Sinan. #widm2019 #widm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			266
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "widm2019",
+					"indices": [
+						251,
+						260
+					]
+				},
+				{
+					"text": "widm",
+					"indices": [
+						261,
+						266
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TrustNobodycast",
+					"name": "Trust Nobody",
+					"id": 943435303846662100,
+					"id_str": "943435303846662144",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "Nelleke",
+					"name": "Nelleke Poorthuis",
+					"id": 6850052,
+					"id_str": "6850052",
+					"indices": [
+						51,
+						59
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 943435303846662100,
+		"in_reply_to_user_id_str": "943435303846662144",
+		"in_reply_to_screen_name": "TrustNobodycast",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Doet toffe dingen met content en communities.",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8872,
+			"friends_count": 236,
+			"listed_count": 593,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5011,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59945,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@TrustNobodycast ik luister naar de aluhoedjes van @Nelleke (beste deel vind ik). Komtie: check de tijdscodes van opdracht 1 met het album van Eddie en de hotrods! Nee maar echt! Ik weet zeker dat je dan een songtekst over meteoriet krijgt. En Sinan. #widm2019 #widm"
+	},
+	{
 		"created_at": "Tue Jan 29 15:26:01 +0000 2019",
 		"id": 1090269843218022400,
 		"id_str": "1090269843218022401",
