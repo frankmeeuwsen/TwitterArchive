cfa07820655d6a1dbbd8fc2504ba1bed44025e97
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-02-03 11:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69289,
-	"created_at": "Sat Feb 02 08:21:03 +0000 2019",
+	"tweets": 69290,
+	"created_at": "Sun Feb 03 10:21:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 03 10:09:43 +0000 2019",
+		"id": 1092002184928157700,
+		"id_str": "1092002184928157696",
+		"full_text": "Net weer lekker gehardgelopen door Utrecht Oost en Park Bloeyendaal. Fijn rondje, wel wat langzamer dan de laatste maanden. (zo beter @reneevh?) https://t.co/ntpWXeuJgz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "reneevh",
+					"name": "Renée van Holsteijn",
+					"id": 25069966,
+					"id_str": "25069966",
+					"indices": [
+						134,
+						142
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1092002177944555500,
+					"id_str": "1092002177944555520",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/DyeSMfZWkAA94Ql.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyeSMfZWkAA94Ql.jpg",
+					"url": "https://t.co/ntpWXeuJgz",
+					"display_url": "pic.twitter.com/ntpWXeuJgz",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1092002184928157696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1089,
+							"h": 1089,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1089,
+							"h": 1089,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092002177944555500,
+					"id_str": "1092002177944555520",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/DyeSMfZWkAA94Ql.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyeSMfZWkAA94Ql.jpg",
+					"url": "https://t.co/ntpWXeuJgz",
+					"display_url": "pic.twitter.com/ntpWXeuJgz",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1092002184928157696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1089,
+							"h": 1089,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1089,
+							"h": 1089,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Doet toffe dingen met content en communities.",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8867,
+			"friends_count": 237,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5039,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59974,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Net weer lekker gehardgelopen door Utrecht Oost en Park Bloeyendaal. Fijn rondje, wel wat langzamer dan de laatste maanden. (zo beter @reneevh?) https://t.co/ntpWXeuJgz"
+	},
+	{
 		"created_at": "Sat Feb 02 07:35:49 +0000 2019",
 		"id": 1091601062891528200,
 		"id_str": "1091601062891528192",
