91868e3b31b841960ce368b9f2892c95dfce28ed
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-02-04 11:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69294,
-	"created_at": "Mon Feb 04 08:21:03 +0000 2019",
+	"tweets": 69295,
+	"created_at": "Mon Feb 04 10:21:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 04 09:24:04 +0000 2019",
+		"id": 1092353081210220500,
+		"id_str": "1092353081210220545",
+		"full_text": "@arnehulstein @erwblo @indirareynaert @marcoderksen @AlexOsterwalder Door deze tweets ben ik wel van plan dat boek weer eens door te bladeren. Dat is dan een pluspunt!",
+		"truncated": false,
+		"display_text_range": [
+			69,
+			167
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "arnehulstein",
+					"name": "Arne Hulstein",
+					"id": 6281472,
+					"id_str": "6281472",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "erwblo",
+					"name": "erwin blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						14,
+						21
+					]
+				},
+				{
+					"screen_name": "indirareynaert",
+					"name": "G. Indira Reynaert",
+					"id": 3408191,
+					"id_str": "3408191",
+					"indices": [
+						22,
+						37
+					]
+				},
+				{
+					"screen_name": "marcoderksen",
+					"name": "Marco Derksen",
+					"id": 1252681,
+					"id_str": "1252681",
+					"indices": [
+						38,
+						51
+					]
+				},
+				{
+					"screen_name": "AlexOsterwalder",
+					"name": "Alex Osterwalder🇨🇭",
+					"id": 15320887,
+					"id_str": "15320887",
+					"indices": [
+						52,
+						68
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1092352018210009100,
+		"in_reply_to_status_id_str": "1092352018210009089",
+		"in_reply_to_user_id": 6281472,
+		"in_reply_to_user_id_str": "6281472",
+		"in_reply_to_screen_name": "arnehulstein",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Doet toffe dingen met content en communities.",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8868,
+			"friends_count": 237,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5043,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59982,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@arnehulstein @erwblo @indirareynaert @marcoderksen @AlexOsterwalder Door deze tweets ben ik wel van plan dat boek weer eens door te bladeren. Dat is dan een pluspunt!"
+	},
+	{
 		"created_at": "Mon Feb 04 07:25:04 +0000 2019",
 		"id": 1092323136383664100,
 		"id_str": "1092323136383664128",
