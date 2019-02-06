1180e4b1486e2a3f9f171a1164b619e15edc9d97
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-02-06 15:21:10 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69317,
-	"created_at": "Wed Feb 06 13:21:08 +0000 2019",
+	"tweets": 69318,
+	"created_at": "Wed Feb 06 14:21:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,166 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 04 21:35:03 +0000 2019",
+		"id": 1092537042636963800,
+		"id_str": "1093141009406586882",
+		"full_text": "This is the most brilliant iPhone app grouping I’ve ever seen... https://t.co/3zyKpFkJr3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1092537037767401500,
+					"id_str": "1092537037767401473",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyl4pbgVYAEDxZA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyl4pbgVYAEDxZA.jpg",
+					"url": "https://t.co/3zyKpFkJr3",
+					"display_url": "pic.twitter.com/3zyKpFkJr3",
+					"expanded_url": "https://twitter.com/arampell/status/1092537042636963840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 599,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 661,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 661,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092537037767401500,
+					"id_str": "1092537037767401473",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyl4pbgVYAEDxZA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyl4pbgVYAEDxZA.jpg",
+					"url": "https://t.co/3zyKpFkJr3",
+					"display_url": "pic.twitter.com/3zyKpFkJr3",
+					"expanded_url": "https://twitter.com/arampell/status/1092537042636963840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 599,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 661,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 661,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 206938213,
+			"id_str": "206938213",
+			"name": "Alex Rampell",
+			"screen_name": "arampell",
+			"location": "Palo Alto, CA",
+			"description": "Silicon Valley entrepreneur (TrialPay, Yub, Affirm, Point, TXN, etc), investor (General Partner @a16z), husband, father, and sarcast (one who is sarcastic)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15475,
+			"friends_count": 532,
+			"listed_count": 537,
+			"created_at": "Sun Oct 24 03:12:40 +0000 2010",
+			"favourites_count": 6525,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4577,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/633413191624736768/fXTjtZKT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/633413191624736768/fXTjtZKT_normal.jpg",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7869,
+		"favorite_count": 31119,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is the most brilliant iPhone app grouping I’ve ever seen... https://t.co/3zyKpFkJr3"
+	},
+	{
 		"created_at": "Wed Feb 06 13:15:54 +0000 2019",
 		"id": 1093136200242790400,
 		"id_str": "1093136200242790400",
