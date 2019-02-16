b9c125c8437e730698f2c9c1e9eebc2ebb86d539
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-02-16 12:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69373,
-	"created_at": "Sat Feb 16 09:21:05 +0000 2019",
+	"tweets": 69374,
+	"created_at": "Sat Feb 16 11:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,314 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sat Feb 16 10:22:40 +0000 2019",
+		"id": 1096716484729077800,
+		"id_str": "1096716484729077760",
+		"full_text": "Who needs Fortnite en Apex als je Lolo hebt via @openemu! #retrogame #NESClassic https://t.co/Gtvx1IB8oZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "retrogame",
+					"indices": [
+						58,
+						68
+					]
+				},
+				{
+					"text": "NESClassic",
+					"indices": [
+						69,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "openemu",
+					"name": "OpenEmu Team",
+					"id": 50421081,
+					"id_str": "50421081",
+					"indices": [
+						48,
+						56
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1096716119015080000,
+					"id_str": "1096716119015079936",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/DzhRf-VWwAA37EK.png",
+					"media_url_https": "https://pbs.twimg.com/media/DzhRf-VWwAA37EK.png",
+					"url": "https://t.co/Gtvx1IB8oZ",
+					"display_url": "pic.twitter.com/Gtvx1IB8oZ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1096716484729077760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 987,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1684,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1096716119015080000,
+					"id_str": "1096716119015079936",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/DzhRf-VWwAA37EK.png",
+					"media_url_https": "https://pbs.twimg.com/media/DzhRf-VWwAA37EK.png",
+					"url": "https://t.co/Gtvx1IB8oZ",
+					"display_url": "pic.twitter.com/Gtvx1IB8oZ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1096716484729077760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 987,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1684,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1096716241195143200,
+					"id_str": "1096716241195143168",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/DzhRnFfWkAA9GUb.png",
+					"media_url_https": "https://pbs.twimg.com/media/DzhRnFfWkAA9GUb.png",
+					"url": "https://t.co/Gtvx1IB8oZ",
+					"display_url": "pic.twitter.com/Gtvx1IB8oZ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1096716484729077760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 987,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1684,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1096716241186836500,
+					"id_str": "1096716241186836480",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/DzhRnFdX0AAMjN7.png",
+					"media_url_https": "https://pbs.twimg.com/media/DzhRnFdX0AAMjN7.png",
+					"url": "https://t.co/Gtvx1IB8oZ",
+					"display_url": "pic.twitter.com/Gtvx1IB8oZ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1096716484729077760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1684,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 987,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1096716306609524700,
+					"id_str": "1096716306609524736",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/DzhRq5LW0AAgNpc.png",
+					"media_url_https": "https://pbs.twimg.com/media/DzhRq5LW0AAgNpc.png",
+					"url": "https://t.co/Gtvx1IB8oZ",
+					"display_url": "pic.twitter.com/Gtvx1IB8oZ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1096716484729077760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 559,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 987,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1684,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Doet toffe dingen met content en communities.",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8859,
+			"friends_count": 245,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5063,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59970,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Who needs Fortnite en Apex als je Lolo hebt via @openemu! #retrogame #NESClassic https://t.co/Gtvx1IB8oZ"
+	},
+	{
 		"created_at": "Sat Feb 16 08:41:40 +0000 2019",
 		"id": 1096691066248589300,
 		"id_str": "1096691066248589313",
