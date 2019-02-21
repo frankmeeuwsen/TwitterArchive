78e6a38284f170904be2cbff073c1804831fb8ec
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-02-21 14:21:07 +0100

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Thu Feb 21 12:40:43 +0000 2019",
+		"id": 1098563165904867300,
+		"id_str": "1098563165904867328",
+		"full_text": "@Jojanneke @robertwinkel526 @2525 Ha mooi! Eigenlijk verandert er weinig in dat gevoel en gedrag. Het is als mijn dochter die nu een bekende Youtuber op straat zou tegen komen. Maar tóch voelde het toen anders omdat we onderdeel waren van \"iets nieuws\"",
+		"truncated": false,
+		"display_text_range": [
+			34,
+			252
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Jojanneke",
+					"name": "Jojanneke van den Bosch",
+					"id": 1689191,
+					"id_str": "1689191",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "robertwinkel526",
+					"name": "robert winkel",
+					"id": 39958660,
+					"id_str": "39958660",
+					"indices": [
+						11,
+						27
+					]
+				},
+				{
+					"screen_name": "2525",
+					"name": "Francisco van Jole",
+					"id": 798003,
+					"id_str": "798003",
+					"indices": [
+						28,
+						33
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1098557908621684700,
+		"in_reply_to_status_id_str": "1098557908621684737",
+		"in_reply_to_user_id": 1689191,
+		"in_reply_to_user_id_str": "1689191",
+		"in_reply_to_screen_name": "Jojanneke",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019).",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8854,
+			"friends_count": 247,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5070,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59939,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@Jojanneke @robertwinkel526 @2525 Ha mooi! Eigenlijk verandert er weinig in dat gevoel en gedrag. Het is als mijn dochter die nu een bekende Youtuber op straat zou tegen komen. Maar tóch voelde het toen anders omdat we onderdeel waren van \"iets nieuws\""
+	},
+	{
 		"created_at": "Thu Feb 21 12:16:32 +0000 2019",
 		"id": 1098557078325678100,
 		"id_str": "1098557078325678081",
