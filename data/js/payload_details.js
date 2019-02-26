cfb5caed242a0a98212085dc2a744391b31ff0b7
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-02-27 00:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69408,
-	"created_at": "Tue Feb 26 19:21:06 +0000 2019",
+	"tweets": 69409,
+	"created_at": "Tue Feb 26 23:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,305 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Tue Feb 26 22:52:43 +0000 2019",
+		"id": 1100529119673094100,
+		"id_str": "1100529119673094151",
+		"full_text": "Vraag 1: waarom doe je dit sowieso?\nVraag 2: waarom wordt dit gefilmd? (Eternal /r/whyWhereTheyFilming fame?)\nVraag 3: waarom is dit na het filmen online gezet?\nMet subvraag 2-3b: is dit een selfievideo of was er een cameramens? https://t.co/HJYDomIITM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			228
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HJYDomIITM",
+					"expanded_url": "https://twitter.com/holdmyale/status/1100519955685093376",
+					"display_url": "twitter.com/holdmyale/stat…",
+					"indices": [
+						229,
+						252
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019).",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8884,
+			"friends_count": 249,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5077,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59904,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1100519955685093400,
+		"quoted_status_id_str": "1100519955685093376",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HJYDomIITM",
+			"expanded": "https://twitter.com/holdmyale/status/1100519955685093376",
+			"display": "twitter.com/holdmyale/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Feb 26 22:16:18 +0000 2019",
+			"id": 1100519955685093400,
+			"id_str": "1100519955685093376",
+			"full_text": "Hold my beer while I set fire to my pants. https://t.co/dGctxhlWum",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				42
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1100519893437501400,
+						"id_str": "1100519893437501440",
+						"indices": [
+							43,
+							66
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1100519893437501440/pu/img/jstigK8CN8JtbDue.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1100519893437501440/pu/img/jstigK8CN8JtbDue.jpg",
+						"url": "https://t.co/dGctxhlWum",
+						"display_url": "pic.twitter.com/dGctxhlWum",
+						"expanded_url": "https://twitter.com/holdmyale/status/1100519955685093376/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 352,
+								"h": 264,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 352,
+								"h": 264,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 352,
+								"h": 264,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1100519893437501400,
+						"id_str": "1100519893437501440",
+						"indices": [
+							43,
+							66
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1100519893437501440/pu/img/jstigK8CN8JtbDue.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1100519893437501440/pu/img/jstigK8CN8JtbDue.jpg",
+						"url": "https://t.co/dGctxhlWum",
+						"display_url": "pic.twitter.com/dGctxhlWum",
+						"expanded_url": "https://twitter.com/holdmyale/status/1100519955685093376/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 352,
+								"h": 264,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 352,
+								"h": 264,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 352,
+								"h": 264,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								4,
+								3
+							],
+							"duration_millis": 10980,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1100519893437501440/pu/vid/352x264/iOuoa08jfYvL3TJY.mp4?tag=6"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1100519893437501440/pu/pl/ALWXSini_EpVVN47.m3u8?tag=6"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1100519893437501440/pu/vid/240x180/c6bSIK2qt53dYUdj.mp4?tag=6"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3105214361,
+				"id_str": "3105214361",
+				"name": "Hold My Beer",
+				"screen_name": "holdmyale",
+				"location": "",
+				"description": "Hold My Beer while I do something stupid. We are a FAN / parody * of the posts/videos and do not claim any ownership or copywrites. Account started 26/4/17",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 197952,
+				"friends_count": 77,
+				"listed_count": 789,
+				"created_at": "Sun Mar 22 17:00:09 +0000 2015",
+				"favourites_count": 187,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 222,
+				"lang": "en-GB",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/857590815878766593/ZBW76yfF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/857590815878766593/ZBW76yfF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3105214361/1493300560",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 45,
+			"favorite_count": 194,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Vraag 1: waarom doe je dit sowieso?\nVraag 2: waarom wordt dit gefilmd? (Eternal /r/whyWhereTheyFilming fame?)\nVraag 3: waarom is dit na het filmen online gezet?\nMet subvraag 2-3b: is dit een selfievideo of was er een cameramens? https://t.co/HJYDomIITM"
+	},
+	{
 		"created_at": "Tue Feb 26 18:37:17 +0000 2019",
 		"id": 1100464837073821700,
 		"id_str": "1100464837073821698",
