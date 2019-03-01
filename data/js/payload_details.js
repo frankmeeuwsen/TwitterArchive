bfe13141199becc30e0cff3ee6b2bd41d77c6800
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-03-01 19:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69419,
-	"created_at": "Fri Mar 01 13:21:04 +0000 2019",
+	"tweets": 69420,
+	"created_at": "Fri Mar 01 18:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 01 18:04:09 +0000 2019",
+		"id": 1101543661307871200,
+		"id_str": "1101543661307871233",
+		"full_text": "Het wordt dit weekend weer de lokale omroepen kijken uit Breedje Da aldaar en @roh screenshots sturen van de fabuleuze Wordart commercials die er te zien zijn rondom de carnavalsoptochten. #fijnfisjenie",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			202
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "fijnfisjenie",
+					"indices": [
+						189,
+						202
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Roh",
+					"name": "Ronald van Schaik",
+					"id": 1447551,
+					"id_str": "1447551",
+					"indices": [
+						78,
+						82
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019).",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8887,
+			"friends_count": 250,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5088,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59921,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "Het wordt dit weekend weer de lokale omroepen kijken uit Breedje Da aldaar en @roh screenshots sturen van de fabuleuze Wordart commercials die er te zien zijn rondom de carnavalsoptochten. #fijnfisjenie"
+	},
+	{
 		"created_at": "Fri Mar 01 12:38:21 +0000 2019",
 		"id": 1101461673574051800,
 		"id_str": "1101461673574051841",
@@ -319,7 +433,7 @@ Grailbird.data.tweets_2019_03 =
 				"name": "Daniël Verlaan",
 				"screen_name": "danielverlaan",
 				"location": "🚀",
-				"description": "techjournalist @RTLnieuws: hackers & cybercrime. tips? ☎️ +316 2089 8142 📫 daniel.verlaan@rtl.nl 👨🏻‍💻 auteur cybersecuritygids https://t.co/PqsPCI7jbA",
+				"description": "techjournalist @RTLnieuws: hackers & cybercrime. tips? ☎️ +316 2089 8142 📫 daniel.verlaan@rtl.nl 👨🏻💻 auteur cybersecuritygids https://t.co/PqsPCI7jbA",
 				"url": "https://t.co/CmkHfPB2GP",
 				"entities": {
 					"url": {
