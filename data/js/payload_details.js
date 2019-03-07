fb63dadb038058b9d3497fa54412fe97287e8633
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-03-07 09:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69445,
-	"created_at": "Wed Mar 06 18:21:07 +0000 2019",
+	"tweets": 69446,
+	"created_at": "Thu Mar 07 08:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 07 07:28:08 +0000 2019",
+		"id": 1103557931386187800,
+		"id_str": "1103557931386187777",
+		"full_text": "@gonniespijkstra Wanneer ga je een blog beginnen over meta-dingen? Waar een boek uit voortkomt en je een meta-influencer wordt? Met Meta de Vries als beschermheilige natuurlijk. 😂😂😂 https://t.co/089Swp6yp8",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gonniespijkstra",
+					"name": "Gonnie Spijkstra",
+					"id": 7677082,
+					"id_str": "7677082",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1103557922854969300,
+					"id_str": "1103557922854969344",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D1CgFXzWoAAi0Wt.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D1CgFXzWoAAi0Wt.jpg",
+					"url": "https://t.co/089Swp6yp8",
+					"display_url": "pic.twitter.com/089Swp6yp8",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1103557931386187777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 498,
+							"h": 436,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 436,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 436,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103557922854969300,
+					"id_str": "1103557922854969344",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D1CgFXzWoAAi0Wt.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D1CgFXzWoAAi0Wt.jpg",
+					"url": "https://t.co/089Swp6yp8",
+					"display_url": "pic.twitter.com/089Swp6yp8",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1103557931386187777/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 498,
+							"h": 436,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 436,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 436,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							249,
+							218
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D1CgFXzWoAAi0Wt.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1103550745813704700,
+		"in_reply_to_status_id_str": "1103550745813704704",
+		"in_reply_to_user_id": 7677082,
+		"in_reply_to_user_id_str": "7677082",
+		"in_reply_to_screen_name": "gonniespijkstra",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Per 1/3 beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019).",
+			"url": "https://t.co/4cv62B1Wh8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4cv62B1Wh8",
+							"expanded_url": "http://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8890,
+			"friends_count": 250,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5105,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59894,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@gonniespijkstra Wanneer ga je een blog beginnen over meta-dingen? Waar een boek uit voortkomt en je een meta-influencer wordt? Met Meta de Vries als beschermheilige natuurlijk. 😂😂😂 https://t.co/089Swp6yp8"
+	},
+	{
 		"created_at": "Wed Mar 06 17:42:11 +0000 2019",
 		"id": 1103350072690004000,
 		"id_str": "1103350072690003969",
