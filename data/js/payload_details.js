c3a46ab46552d08b604db2c04ee001c22e67b2cb
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-04-05 16:21:09 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69693,
-	"created_at": "Fri Apr 05 10:21:04 +0000 2019",
+	"tweets": 69694,
+	"created_at": "Fri Apr 05 14:21:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,193 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 05 13:45:46 +0000 2019",
+		"id": 1114162213277794300,
+		"id_str": "1114162213277794305",
+		"full_text": "Dus je koopt Super Mario Odyssee, Super Smash Brothers, Kirby Star Allies voor de Nintendo Switch....is zoon van 7 vooral hooked aan de retrogames van Nintendo Switch Online zoals Zelda, Metroid en Bomber Jack. https://t.co/QLYuRMQtZr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1114162185960398800,
+					"id_str": "1114162185960398848",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D3ZMmuRXoAANwQT.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D3ZMmuRXoAANwQT.jpg",
+					"url": "https://t.co/QLYuRMQtZr",
+					"display_url": "pic.twitter.com/QLYuRMQtZr",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1114162213277794305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 498,
+							"h": 440,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 498,
+							"h": 440,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1114162185960398800,
+					"id_str": "1114162185960398848",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D3ZMmuRXoAANwQT.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D3ZMmuRXoAANwQT.jpg",
+					"url": "https://t.co/QLYuRMQtZr",
+					"display_url": "pic.twitter.com/QLYuRMQtZr",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1114162213277794305/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 498,
+							"h": 440,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 498,
+							"h": 440,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							249,
+							220
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D3ZMmuRXoAANwQT.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8885,
+			"friends_count": 273,
+			"listed_count": 594,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5226,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60033,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1547153586",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Dus je koopt Super Mario Odyssee, Super Smash Brothers, Kirby Star Allies voor de Nintendo Switch....is zoon van 7 vooral hooked aan de retrogames van Nintendo Switch Online zoals Zelda, Metroid en Bomber Jack. https://t.co/QLYuRMQtZr"
+	},
+	{
 		"created_at": "Fri Apr 05 09:42:06 +0000 2019",
 		"id": 1114100892863750100,
 		"id_str": "1114100892863750144",
