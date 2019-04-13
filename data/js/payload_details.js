75eb45a6da5cd97cb326aa5823fe9ec2db80bb9d
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-04-13 07:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69733,
-	"created_at": "Fri Apr 12 18:21:06 +0000 2019",
+	"tweets": 69734,
+	"created_at": "Sat Apr 13 05:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 13 04:23:57 +0000 2019",
+		"id": 1116843870841712600,
+		"id_str": "1116919929314451456",
+		"full_text": "So strange I never saw people count the lines of code Steve Jobs or Elon Musk have written 🤔",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 813333008,
+			"id_str": "813333008",
+			"name": "Sarah Drasner @jsheroes",
+			"screen_name": "sarah_edo",
+			"location": "",
+			"description": "stupid like a fox. @vuejs Core Team, staff writer @css, co-organizer @ConcatenateConf https://t.co/1cManf7L7S https://t.co/vhOERxzOOl she/her",
+			"url": "https://t.co/pr1NhYseRK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pr1NhYseRK",
+							"expanded_url": "http://sarahdrasnerdesign.com",
+							"display_url": "sarahdrasnerdesign.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/1cManf7L7S",
+							"expanded_url": "http://codepen.io/sdras",
+							"display_url": "codepen.io/sdras",
+							"indices": [
+								86,
+								109
+							]
+						},
+						{
+							"url": "https://t.co/vhOERxzOOl",
+							"expanded_url": "http://github.com/sdras",
+							"display_url": "github.com/sdras",
+							"indices": [
+								110,
+								133
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 116795,
+			"friends_count": 2384,
+			"listed_count": 2523,
+			"created_at": "Sun Sep 09 15:24:34 +0000 2012",
+			"favourites_count": 138842,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 45358,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099384716170452992/Z_2ELEQp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099384716170452992/Z_2ELEQp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/813333008/1434927146",
+			"profile_link_color": "068D9C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 953,
+		"favorite_count": 4749,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "So strange I never saw people count the lines of code Steve Jobs or Elon Musk have written 🤔"
+	},
+	{
 		"created_at": "Fri Apr 12 17:41:00 +0000 2019",
 		"id": 1116758127494324200,
 		"id_str": "1116758127494324224",
