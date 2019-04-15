8865f77ccc488dbb8898862b73423fef6b2370af
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-04-15 20:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69742,
-	"created_at": "Mon Apr 15 17:21:07 +0000 2019",
+	"tweets": 69743,
+	"created_at": "Mon Apr 15 18:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 15 18:02:29 +0000 2019",
+		"id": 1117850698559836200,
+		"id_str": "1117850698559836160",
+		"full_text": "En ge-algoritmiseerde halve zolen. https://t.co/zczlZnzbRh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zczlZnzbRh",
+					"expanded_url": "https://twitter.com/broderick/status/1117849663036481538",
+					"display_url": "twitter.com/broderick/stat…",
+					"indices": [
+						35,
+						58
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8878,
+			"friends_count": 282,
+			"listed_count": 591,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5252,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60059,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1117849663036481500,
+		"quoted_status_id_str": "1117849663036481538",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zczlZnzbRh",
+			"expanded": "https://twitter.com/broderick/status/1117849663036481538",
+			"display": "twitter.com/broderick/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 15 17:58:22 +0000 2019",
+			"id": 1117849663036481500,
+			"id_str": "1117849663036481538",
+			"full_text": "I'm not sure there's a better metaphor for the state of the tech industry right now than an American social platform slapping an algorithmically-generated related article about an American terror attack on to a live stream about a completely unrelated French news story. https://t.co/KLWGlg5NEc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				270
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1117849123494010900,
+						"id_str": "1117849123494010880",
+						"indices": [
+							271,
+							294
+						],
+						"media_url": "http://pbs.twimg.com/media/D4Nl2u5XoAAXb6-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D4Nl2u5XoAAXb6-.jpg",
+						"url": "https://t.co/KLWGlg5NEc",
+						"display_url": "pic.twitter.com/KLWGlg5NEc",
+						"expanded_url": "https://twitter.com/broderick/status/1117849663036481538/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 361,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 964,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 964,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1117849123494010900,
+						"id_str": "1117849123494010880",
+						"indices": [
+							271,
+							294
+						],
+						"media_url": "http://pbs.twimg.com/media/D4Nl2u5XoAAXb6-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D4Nl2u5XoAAXb6-.jpg",
+						"url": "https://t.co/KLWGlg5NEc",
+						"display_url": "pic.twitter.com/KLWGlg5NEc",
+						"expanded_url": "https://twitter.com/broderick/status/1117849663036481538/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 361,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 964,
+								"h": 512,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 964,
+								"h": 512,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": 1117848277225857000,
+			"in_reply_to_status_id_str": "1117848277225857024",
+			"in_reply_to_user_id": 18205531,
+			"in_reply_to_user_id_str": "18205531",
+			"in_reply_to_screen_name": "broderick",
+			"user": {
+				"id": 18205531,
+				"id_str": "18205531",
+				"name": "Ryan Broderick",
+				"screen_name": "broderick",
+				"location": "New York, I guess",
+				"description": "Senior reporter @BuzzFeedNews covering globalism and tech platforms. @iexplorer podcast. Lots of typos. Open DMs. Send tips. Email: ryan@buzzfeed.com",
+				"url": "https://t.co/z9Bjj1EEut",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/z9Bjj1EEut",
+								"expanded_url": "http://Instagram.com/ryanhatesthis",
+								"display_url": "Instagram.com/ryanhatesthis",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 55170,
+				"friends_count": 1824,
+				"listed_count": 1589,
+				"created_at": "Thu Dec 18 01:29:08 +0000 2008",
+				"favourites_count": 17577,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 73686,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/928934118439649280/n2IdfvoR_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/928934118439649280/n2IdfvoR_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18205531/1422647849",
+				"profile_link_color": "1F98C7",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 91,
+			"favorite_count": 174,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "es",
+		"text": "En ge-algoritmiseerde halve zolen. https://t.co/zczlZnzbRh"
+	},
+	{
 		"created_at": "Mon Apr 15 16:54:21 +0000 2019",
 		"id": 1117833551154643000,
 		"id_str": "1117833551154642946",
