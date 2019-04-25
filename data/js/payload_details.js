66b74a9d6cf033fce5b8a6ee5e2325b66774f8af
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-04-25 21:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69789,
-	"created_at": "Thu Apr 25 17:21:07 +0000 2019",
+	"tweets": 69790,
+	"created_at": "Thu Apr 25 19:21:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 25 18:40:04 +0000 2019",
+		"id": 1121484032938737700,
+		"id_str": "1121484032938737664",
+		"full_text": "@vaneikenhorst @Fashiorexia En ik begreep van dezelfde werkcoach dat ik als ZZP-er alleen de uren hoef door te geven bij UWV, niet de inkomsten. Die worden later wel gecheckt met inkomstenbelasting van BD maar het gaat hen om de uren waarmee ze op basis van fictief inkomen een aanvullende WW berekenen",
+		"truncated": false,
+		"display_text_range": [
+			28,
+			302
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "vaneikenhorst",
+					"name": "Lot",
+					"id": 1938743876,
+					"id_str": "1938743876",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "Fashiorexia",
+					"name": "Michelle 🗻",
+					"id": 47913982,
+					"id_str": "47913982",
+					"indices": [
+						15,
+						27
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1121324483216908300,
+		"in_reply_to_status_id_str": "1121324483216908288",
+		"in_reply_to_user_id": 1938743876,
+		"in_reply_to_user_id_str": "1938743876",
+		"in_reply_to_screen_name": "vaneikenhorst",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8884,
+			"friends_count": 280,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5279,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59983,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@vaneikenhorst @Fashiorexia En ik begreep van dezelfde werkcoach dat ik als ZZP-er alleen de uren hoef door te geven bij UWV, niet de inkomsten. Die worden later wel gecheckt met inkomstenbelasting van BD maar het gaat hen om de uren waarmee ze op basis van fictief inkomen een aanvullende WW berekenen"
+	},
+	{
 		"created_at": "Thu Apr 25 17:00:47 +0000 2019",
 		"id": 1121459046471688200,
 		"id_str": "1121459046471688192",
