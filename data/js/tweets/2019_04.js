f98ffbdf6b4e110c713bc5244e40b861292a8f4d
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-04-26 08:21:06 +0200

    Added 2 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,347 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 26 06:18:00 +0000 2019",
+		"id": 1121659673222844400,
+		"id_str": "1121659673222844418",
+		"full_text": "Een Amerikaans bedrijf sluit een van de mediakanalen van een Europese politieke partij. Ik ben benieuwd naar het \"waarom\". https://t.co/pU2kBNQ3wB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pU2kBNQ3wB",
+					"expanded_url": "https://twitter.com/BenjaminNorton/status/1121083347785195522",
+					"display_url": "twitter.com/BenjaminNorton…",
+					"indices": [
+						123,
+						146
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8884,
+			"friends_count": 280,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5281,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59987,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1121083347785195500,
+		"quoted_status_id_str": "1121083347785195522",
+		"quoted_status_permalink": {
+			"url": "https://t.co/pU2kBNQ3wB",
+			"expanded": "https://twitter.com/BenjaminNorton/status/1121083347785195522",
+			"display": "twitter.com/BenjaminNorton…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Apr 24 16:07:53 +0000 2019",
+			"id": 1121083347785195500,
+			"id_str": "1121083347785195522",
+			"full_text": "WhatsApp, which is owned by Facebook, just shut down the massive channel used by Spain's independent leftist political party Podemos.\n\nAnd it did this just days before the general election\n\nThis is actual, extreme election meddling by a foreign corporation\nhttps://t.co/PxzZ722Mk8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/PxzZ722Mk8",
+						"expanded_url": "https://twitter.com/juanmalpr/status/1120459372994420736?s=19",
+						"display_url": "twitter.com/juanmalpr/stat…",
+						"indices": [
+							257,
+							280
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 852269288,
+				"id_str": "852269288",
+				"name": "Ben Norton",
+				"screen_name": "BenjaminNorton",
+				"location": "NYC",
+				"description": "Journalist | @GrayzoneProject  https://t.co/ytp7yxvPDh | @Moderate_Rebels producer/co-host",
+				"url": "https://t.co/w8DKDKXuD1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/w8DKDKXuD1",
+								"expanded_url": "http://BenNorton.com",
+								"display_url": "BenNorton.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/ytp7yxvPDh",
+								"expanded_url": "https://thegrayzone.com/author/ben-norton/",
+								"display_url": "thegrayzone.com/author/ben-nor…",
+								"indices": [
+									31,
+									54
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 75846,
+				"friends_count": 501,
+				"listed_count": 1695,
+				"created_at": "Sat Sep 29 05:05:50 +0000 2012",
+				"favourites_count": 30371,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 68203,
+				"lang": "es",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "053166",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1104135015372386305/wc8p3Rzj_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1104135015372386305/wc8p3Rzj_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/852269288/1495412836",
+				"profile_link_color": "08088A",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1120459372994420700,
+			"quoted_status_id_str": "1120459372994420736",
+			"quoted_status_permalink": {
+				"url": "https://t.co/PxzZ722Mk8",
+				"expanded": "https://twitter.com/juanmalpr/status/1120459372994420736?s=19",
+				"display": "twitter.com/juanmalpr/stat…"
+			},
+			"retweet_count": 3805,
+			"favorite_count": 4939,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Een Amerikaans bedrijf sluit een van de mediakanalen van een Europese politieke partij. Ik ben benieuwd naar het \"waarom\". https://t.co/pU2kBNQ3wB"
+	},
+	{
+		"created_at": "Fri Apr 26 05:39:26 +0000 2019",
+		"id": 1121649970296152000,
+		"id_str": "1121649970296152064",
+		"full_text": "@bertboerland In elk geval stevige overwoestbare verlichting... Ze krijgen het voor elkaar om die na een week in de fietsenstalling op school al kapot te hebben.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			161
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bertboerland",
+					"name": "bert boerland",
+					"id": 3218781,
+					"id_str": "3218781",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1121332276288282600,
+		"in_reply_to_status_id_str": "1121332276288282624",
+		"in_reply_to_user_id": 3218781,
+		"in_reply_to_user_id_str": "3218781",
+		"in_reply_to_screen_name": "bertboerland",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8884,
+			"friends_count": 280,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5281,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59987,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@bertboerland In elk geval stevige overwoestbare verlichting... Ze krijgen het voor elkaar om die na een week in de fietsenstalling op school al kapot te hebben."
+	},
+	{
 		"created_at": "Thu Apr 25 20:45:19 +0000 2019",
 		"id": 1121515554936905700,
 		"id_str": "1121515554936905728",
