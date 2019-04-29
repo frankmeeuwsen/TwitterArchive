0ab1d10c02d8cf6cd28c6791fc17de4171d35917
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-04-29 16:21:09 +0200

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,130 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 29 13:48:24 +0000 2019",
+		"id": 1122860184756527100,
+		"id_str": "1122860184756527105",
+		"full_text": "Terug van mini-verkaansie, dus even bloggen naar demensetoe. Over wat ik lees: https://t.co/0DwrRGuWE5, zie: https://t.co/RavyQFc23e en overpeins: https://t.co/hXLpPxmGW8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0DwrRGuWE5",
+					"expanded_url": "https://diggingthedigital.com/sourdough-robin-sloan/",
+					"display_url": "diggingthedigital.com/sourdough-robi…",
+					"indices": [
+						79,
+						102
+					]
+				},
+				{
+					"url": "https://t.co/RavyQFc23e",
+					"expanded_url": "https://diggingthedigital.com/avengersinfinity-wars-gezien/",
+					"display_url": "diggingthedigital.com/avengersinfini…",
+					"indices": [
+						109,
+						132
+					]
+				},
+				{
+					"url": "https://t.co/hXLpPxmGW8",
+					"expanded_url": "https://diggingthedigital.com/hoe-nostalgisch-is-het-indieweb/",
+					"display_url": "diggingthedigital.com/hoe-nostalgisc…",
+					"indices": [
+						147,
+						170
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8884,
+			"friends_count": 282,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5275,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59969,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Terug van mini-verkaansie, dus even bloggen naar demensetoe. Over wat ik lees: https://t.co/0DwrRGuWE5, zie: https://t.co/RavyQFc23e en overpeins: https://t.co/hXLpPxmGW8"
+	},
+	{
 		"created_at": "Mon Apr 29 11:57:14 +0000 2019",
 		"id": 1122827045585739800,
 		"id_str": "1122832210850140160",
