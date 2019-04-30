bd135a2982e4b554680c7ea45bca98e40951b2eb
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-04-30 19:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69812,
-	"created_at": "Tue Apr 30 13:21:07 +0000 2019",
+	"tweets": 69813,
+	"created_at": "Tue Apr 30 17:21:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,127 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 30 17:17:11 +0000 2019",
+		"id": 1123025338567659500,
+		"id_str": "1123275115918188544",
+		"full_text": "Here's footage of the #GameOfThrones battle of Winterfell that was too intense for the test audience. #Spoilers \n\nhttps://t.co/395prNgLeq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			137
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GameOfThrones",
+					"indices": [
+						22,
+						36
+					]
+				},
+				{
+					"text": "Spoilers",
+					"indices": [
+						102,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/395prNgLeq",
+					"expanded_url": "https://youtu.be/qRxwBb7ev1Y",
+					"display_url": "youtu.be/qRxwBb7ev1Y",
+					"indices": [
+						114,
+						137
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 28932876,
+			"id_str": "28932876",
+			"name": "Monty Python",
+			"screen_name": "pythonquotes",
+			"location": "The Fish Tank",
+			"description": "I tweet and re-tweet Monty Python quotes. (This is an account by a fan for fans. Not an official Monty Python account!)",
+			"url": "https://t.co/zcyCT8Nh1i",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zcyCT8Nh1i",
+							"expanded_url": "http://www.montypython.com/",
+							"display_url": "montypython.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21714,
+			"friends_count": 391,
+			"listed_count": 340,
+			"created_at": "Sun Apr 05 03:33:59 +0000 2009",
+			"favourites_count": 992,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27139,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/974875387/PYTHONquotes_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/974875387/PYTHONquotes_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/28932876/1456609874",
+			"profile_link_color": "0C1BF0",
+			"profile_sidebar_border_color": "080808",
+			"profile_sidebar_fill_color": "8C96A1",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 65,
+		"favorite_count": 191,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Here's footage of the #GameOfThrones battle of Winterfell that was too intense for the test audience. #Spoilers \n\nhttps://t.co/395prNgLeq"
+	},
+	{
 		"created_at": "Tue Apr 30 12:31:09 +0000 2019",
 		"id": 1123203133780451300,
 		"id_str": "1123203133780451328",
