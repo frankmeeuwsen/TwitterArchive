d9cb5f73fd92a8e2c46a2edb3e2412c8993184d9
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-05-01 11:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69821,
-	"created_at": "Wed May 01 08:21:06 +0000 2019",
+	"tweets": 69822,
+	"created_at": "Wed May 01 09:21:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 01 09:14:59 +0000 2019",
+		"id": 1123516152322379800,
+		"id_str": "1123516152322379776",
+		"full_text": "@SabinedeVos @wimrampen @TristanLavender Hahaha. Helemaal eens. Mijn oudste van 12 is behoorlijk mediawijs. Die van 7 heeft wel wat begeleiding nodig. Dus ga ik nu weer lekker een middagje gamen met hem in @NaVGMuseum 😉",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			219
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SabinedeVos",
+					"name": "𝓢𝓪𝓫𝓲𝓷𝓮 🦊",
+					"id": 17703983,
+					"id_str": "17703983",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "wimrampen",
+					"name": "Wim Rampen ♻️",
+					"id": 34012344,
+					"id_str": "34012344",
+					"indices": [
+						13,
+						23
+					]
+				},
+				{
+					"screen_name": "TristanLavender",
+					"name": "Tristan Lavender",
+					"id": 27412577,
+					"id_str": "27412577",
+					"indices": [
+						24,
+						40
+					]
+				},
+				{
+					"screen_name": "NaVGMuseum",
+					"name": "Nationaal Videogame Museum",
+					"id": 899402523827732500,
+					"id_str": "899402523827732481",
+					"indices": [
+						206,
+						217
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1123511191937146900,
+		"in_reply_to_status_id_str": "1123511191937146880",
+		"in_reply_to_user_id": 17703983,
+		"in_reply_to_user_id_str": "17703983",
+		"in_reply_to_screen_name": "SabinedeVos",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8880,
+			"friends_count": 282,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5280,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59987,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@SabinedeVos @wimrampen @TristanLavender Hahaha. Helemaal eens. Mijn oudste van 12 is behoorlijk mediawijs. Die van 7 heeft wel wat begeleiding nodig. Dus ga ik nu weer lekker een middagje gamen met hem in @NaVGMuseum 😉"
+	},
+	{
 		"created_at": "Wed May 01 07:54:34 +0000 2019",
 		"id": 1123495914260324400,
 		"id_str": "1123495914260324352",
