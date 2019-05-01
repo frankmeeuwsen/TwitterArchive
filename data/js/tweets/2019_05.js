6c44bb9c3fabc890d5bf1ac55ee72227f1cfac15
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-05-01 10:21:07 +0200

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 01 07:54:34 +0000 2019",
+		"id": 1123495914260324400,
+		"id_str": "1123495914260324352",
+		"full_text": "@wimrampen @SabinedeVos @TristanLavender Oh zeker, daar is niets nieuws aan. Maar de schaal, snelheid en impact lijken mij wel heftiger nu. Ik kan het mis hebben en overdadig het verleden romantiseren...",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			203
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wimrampen",
+					"name": "Wim Rampen ♻️",
+					"id": 34012344,
+					"id_str": "34012344",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "SabinedeVos",
+					"name": "𝓢𝓪𝓫𝓲𝓷𝓮 🦊",
+					"id": 17703983,
+					"id_str": "17703983",
+					"indices": [
+						11,
+						23
+					]
+				},
+				{
+					"screen_name": "TristanLavender",
+					"name": "Tristan Lavender",
+					"id": 27412577,
+					"id_str": "27412577",
+					"indices": [
+						24,
+						40
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1123494548288811000,
+		"in_reply_to_status_id_str": "1123494548288811009",
+		"in_reply_to_user_id": 34012344,
+		"in_reply_to_user_id_str": "34012344",
+		"in_reply_to_screen_name": "wimrampen",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8881,
+			"friends_count": 282,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5279,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59986,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@wimrampen @SabinedeVos @TristanLavender Oh zeker, daar is niets nieuws aan. Maar de schaal, snelheid en impact lijken mij wel heftiger nu. Ik kan het mis hebben en overdadig het verleden romantiseren..."
+	},
+	{
 		"created_at": "Wed May 01 06:37:15 +0000 2019",
 		"id": 1123476459094511600,
 		"id_str": "1123476459094511616",
