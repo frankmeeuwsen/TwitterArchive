3c9e28d3ffb99ef83d3db9525e7484e6022ee46d
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-05-03 16:21:06 +0200

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 14:01:14 +0000 2019",
+		"id": 1124312964620746800,
+		"id_str": "1124312964620746752",
+		"full_text": "@rutger_ @Kzzas @aafkeromeijn @KVK_NL Ik voel een nieuw side-project aankomen...",
+		"truncated": false,
+		"display_text_range": [
+			38,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rutger_",
+					"name": "Rutger de Quay",
+					"id": 14250415,
+					"id_str": "14250415",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "Kzzas",
+					"name": "Kasper Hermans",
+					"id": 91305882,
+					"id_str": "91305882",
+					"indices": [
+						9,
+						15
+					]
+				},
+				{
+					"screen_name": "aafkeromeijn",
+					"name": "Aafke Romeijn",
+					"id": 234295296,
+					"id_str": "234295296",
+					"indices": [
+						16,
+						29
+					]
+				},
+				{
+					"screen_name": "KVK_NL",
+					"name": "Kamer van Koophandel",
+					"id": 158672328,
+					"id_str": "158672328",
+					"indices": [
+						30,
+						37
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1124309275034161200,
+		"in_reply_to_status_id_str": "1124309275034161156",
+		"in_reply_to_user_id": 14250415,
+		"in_reply_to_user_id_str": "14250415",
+		"in_reply_to_screen_name": "rutger_",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. Organiseert IndieWebCamp Utrecht (18-19 mei 2019). #TeamHuman",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8877,
+			"friends_count": 281,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5285,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 59997,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@rutger_ @Kzzas @aafkeromeijn @KVK_NL Ik voel een nieuw side-project aankomen..."
+	},
+	{
 		"created_at": "Fri May 03 09:28:17 +0000 2019",
 		"id": 1124244275418554400,
 		"id_str": "1124244275418554368",
