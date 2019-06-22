d43b1a0c6132e102ba2c89cf8f3e7397aa1663fd
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-06-22 11:21:06 +0200

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sat Jun 22 08:57:35 +0000 2019",
+		"id": 1142355941398143000,
+		"id_str": "1142355941398142977",
+		"full_text": "@tomashanedoes @puur Heb je ooit ergens bewijs gezien als adverteerder of admanager specialist, dat online ads worden gepresenteerd als gevolg van een uitgesproken woord of een gesprek wat rondom een thema plaatsvindt?",
+		"truncated": false,
+		"display_text_range": [
+			21,
+			218
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tomashanedoes",
+					"name": "Tomas Hanedoes",
+					"id": 15351436,
+					"id_str": "15351436",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "puur",
+					"name": "puur",
+					"id": 5776422,
+					"id_str": "5776422",
+					"indices": [
+						15,
+						20
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1142344517095448600,
+		"in_reply_to_status_id_str": "1142344517095448578",
+		"in_reply_to_user_id": 15351436,
+		"in_reply_to_user_id_str": "15351436",
+		"in_reply_to_screen_name": "tomashanedoes",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8855,
+			"friends_count": 260,
+			"listed_count": 587,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5417,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60064,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@tomashanedoes @puur Heb je ooit ergens bewijs gezien als adverteerder of admanager specialist, dat online ads worden gepresenteerd als gevolg van een uitgesproken woord of een gesprek wat rondom een thema plaatsvindt?"
+	},
+	{
 		"created_at": "Sat Jun 22 07:40:56 +0000 2019",
 		"id": 1142336655627554800,
 		"id_str": "1142336655627554816",
