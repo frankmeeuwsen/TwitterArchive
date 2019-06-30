63dd3e170400473aab482bc8a642a302075dff9e
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-07-01 00:21:06 +0200

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,229 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sun Jun 30 21:59:21 +0000 2019",
+		"id": 1144857827019640800,
+		"id_str": "1145451783642370048",
+		"full_text": "#NT金沢\n波動拳ティッシュ、無限に引っ張りたいw https://t.co/B58GTnVFbC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			25
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NT金沢",
+					"indices": [
+						0,
+						5
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1144857749567619100,
+					"id_str": "1144857749567619074",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1144857749567619074/pu/img/KwLaLGT0MhTGSvhX.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1144857749567619074/pu/img/KwLaLGT0MhTGSvhX.jpg",
+					"url": "https://t.co/B58GTnVFbC",
+					"display_url": "pic.twitter.com/B58GTnVFbC",
+					"expanded_url": "https://twitter.com/taniguchi36993/status/1144857827019640832/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1144857749567619100,
+					"id_str": "1144857749567619074",
+					"indices": [
+						26,
+						49
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1144857749567619074/pu/img/KwLaLGT0MhTGSvhX.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1144857749567619074/pu/img/KwLaLGT0MhTGSvhX.jpg",
+					"url": "https://t.co/B58GTnVFbC",
+					"display_url": "pic.twitter.com/B58GTnVFbC",
+					"expanded_url": "https://twitter.com/taniguchi36993/status/1144857827019640832/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 9942,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1144857749567619074/pu/pl/fXodlms41cE364sk.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1144857749567619074/pu/vid/360x640/M0H0Gh6VFz1_A4XK.mp4?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1144857749567619074/pu/vid/320x568/eZkgk6N2LV63DMyH.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1144857749567619074/pu/vid/720x1280/Dc-Nzp7kNxtaeWbJ.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 778943278641131500,
+			"id_str": "778943278641131520",
+			"name": "Taniharu",
+			"screen_name": "taniguchi36993",
+			"location": "岐阜 飛騨高山、石川 津幡、群馬 館林",
+			"description": "石川高専3i MakerFaire/Fab/メディアアートに興味あり。たまに作ったものツイートします。ポートフォリオ→https://t.co/qbvXkxi3AH",
+			"url": "https://t.co/qbvXkxi3AH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbvXkxi3AH",
+							"expanded_url": "https://scrapbox.io/taniharuPortfolio/",
+							"display_url": "scrapbox.io/taniharuPortfo…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/qbvXkxi3AH",
+							"expanded_url": "https://scrapbox.io/taniharuPortfolio/",
+							"display_url": "scrapbox.io/taniharuPortfo…",
+							"indices": [
+								59,
+								82
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 238,
+			"friends_count": 414,
+			"listed_count": 3,
+			"created_at": "Thu Sep 22 13:05:24 +0000 2016",
+			"favourites_count": 6778,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 343,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/939662509652426752/DTyDWv28_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/939662509652426752/DTyDWv28_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/778943278641131520/1532568728",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 475,
+		"favorite_count": 867,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "#NT金沢\n波動拳ティッシュ、無限に引っ張りたいw https://t.co/B58GTnVFbC"
+	},
+	{
 		"created_at": "Sun Jun 30 18:39:28 +0000 2019",
 		"id": 1145401480192696300,
 		"id_str": "1145401480192696320",
