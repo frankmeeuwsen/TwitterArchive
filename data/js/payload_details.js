1cebef24b2db5d81bc0b8268c55fcab7b69b97b0
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-07-04 00:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70109,
-	"created_at": "Wed Jul 03 21:21:04 +0000 2019",
+	"tweets": 70110,
+	"created_at": "Wed Jul 03 22:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,429 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Wed Jul 03 21:23:08 +0000 2019",
+		"id": 1146529833343684600,
+		"id_str": "1146529833343684608",
+		"full_text": "#pand @Kaliberinteract https://t.co/zqN4omhqyj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "pand",
+					"indices": [
+						0,
+						5
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Kaliberinteract",
+					"name": "Kaliber",
+					"id": 10932162,
+					"id_str": "10932162",
+					"indices": [
+						6,
+						22
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zqN4omhqyj",
+					"expanded_url": "https://twitter.com/41strange/status/1146528710171844608",
+					"display_url": "twitter.com/41strange/stat…",
+					"indices": [
+						23,
+						46
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8860,
+			"friends_count": 261,
+			"listed_count": 588,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5451,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60137,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1146528710171844600,
+		"quoted_status_id_str": "1146528710171844608",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zqN4omhqyj",
+			"expanded": "https://twitter.com/41strange/status/1146528710171844608",
+			"display": "twitter.com/41strange/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jul 03 21:18:40 +0000 2019",
+			"id": 1146528710171844600,
+			"id_str": "1146528710171844608",
+			"full_text": "Octopus Chandeliers by Adam Wallacavage\n(https://t.co/lXWBbS3LIs) https://t.co/9qB6ilG0m8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				65
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/lXWBbS3LIs",
+						"expanded_url": "https://www.instagram.com/adam_wallacavage/?hl=en",
+						"display_url": "instagram.com/adam_wallacava…",
+						"indices": [
+							41,
+							64
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1146528292402421800,
+						"id_str": "1146528292402421762",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/D-lJaEmU4AIKD9H.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D-lJaEmU4AIKD9H.jpg",
+						"url": "https://t.co/9qB6ilG0m8",
+						"display_url": "pic.twitter.com/9qB6ilG0m8",
+						"expanded_url": "https://twitter.com/41Strange/status/1146528710171844608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1067,
+								"h": 1114,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1067,
+								"h": 1114,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 651,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1146528292402421800,
+						"id_str": "1146528292402421762",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/D-lJaEmU4AIKD9H.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D-lJaEmU4AIKD9H.jpg",
+						"url": "https://t.co/9qB6ilG0m8",
+						"display_url": "pic.twitter.com/9qB6ilG0m8",
+						"expanded_url": "https://twitter.com/41Strange/status/1146528710171844608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1067,
+								"h": 1114,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1067,
+								"h": 1114,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 651,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1146528316670636000,
+						"id_str": "1146528316670636032",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/D-lJbfAUcAAfuo2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D-lJbfAUcAAfuo2.jpg",
+						"url": "https://t.co/9qB6ilG0m8",
+						"display_url": "pic.twitter.com/9qB6ilG0m8",
+						"expanded_url": "https://twitter.com/41Strange/status/1146528710171844608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 653,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1153,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 1249,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1146528433066803200,
+						"id_str": "1146528433066803200",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/D-lJiQnVAAAWM8U.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D-lJiQnVAAAWM8U.jpg",
+						"url": "https://t.co/9qB6ilG0m8",
+						"display_url": "pic.twitter.com/9qB6ilG0m8",
+						"expanded_url": "https://twitter.com/41Strange/status/1146528710171844608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 580,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 874,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 874,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1146528457465090000,
+						"id_str": "1146528457465090048",
+						"indices": [
+							66,
+							89
+						],
+						"media_url": "http://pbs.twimg.com/media/D-lJjrgVUAABeSi.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D-lJjrgVUAABeSi.jpg",
+						"url": "https://t.co/9qB6ilG0m8",
+						"display_url": "pic.twitter.com/9qB6ilG0m8",
+						"expanded_url": "https://twitter.com/41Strange/status/1146528710171844608/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 600,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 600,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 453,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2355808260,
+				"id_str": "2355808260",
+				"name": "41 Strange",
+				"screen_name": "41Strange",
+				"location": "Hollywood, CA",
+				"description": "Horror short stories bestseller out now",
+				"url": "https://t.co/lApirIcxFC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/lApirIcxFC",
+								"expanded_url": "https://41strange.wordpress.com",
+								"display_url": "41strange.wordpress.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 553213,
+				"friends_count": 6119,
+				"listed_count": 3787,
+				"created_at": "Sat Feb 22 05:04:53 +0000 2014",
+				"favourites_count": 174584,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 29546,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "080403",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/453318225720123393/zbjvCWzf_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/453318225720123393/zbjvCWzf_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2355808260/1405535968",
+				"profile_link_color": "CC3366",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "99CC33",
+				"profile_text_color": "3E4415",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 301,
+			"favorite_count": 1195,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#pand @Kaliberinteract https://t.co/zqN4omhqyj"
+	},
+	{
 		"created_at": "Wed Jul 03 21:14:33 +0000 2019",
 		"id": 1146527672752910300,
 		"id_str": "1146527672752910336",
