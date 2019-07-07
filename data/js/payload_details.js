644f0d2bc3bd7b6a4b132211e83ceea85657d810
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-07-07 16:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70118,
-	"created_at": "Sat Jul 06 20:21:03 +0000 2019",
+	"tweets": 70119,
+	"created_at": "Sun Jul 07 14:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 07 13:43:38 +0000 2019",
+		"id": 1147863746997604400,
+		"id_str": "1147863746997604352",
+		"full_text": "@TristanLavender @jorritkr @thomaswensma Wie weet komt er een pre-paid giftcard systeem voor specifieke series. Ipv een generieke voor Netflix, alleen voor serie x of filmgenre y. En dat weer digitaal.",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			201
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TristanLavender",
+					"name": "Tristan Lavender",
+					"id": 27412577,
+					"id_str": "27412577",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "jorritkr",
+					"name": "Jorrit",
+					"id": 14351759,
+					"id_str": "14351759",
+					"indices": [
+						17,
+						26
+					]
+				},
+				{
+					"screen_name": "thomaswensma",
+					"name": "Thomas Wensma",
+					"id": 111623095,
+					"id_str": "111623095",
+					"indices": [
+						27,
+						40
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1147857654917869600,
+		"in_reply_to_status_id_str": "1147857654917869569",
+		"in_reply_to_user_id": 27412577,
+		"in_reply_to_user_id_str": "27412577",
+		"in_reply_to_screen_name": "TristanLavender",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8859,
+			"friends_count": 263,
+			"listed_count": 588,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5463,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60148,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@TristanLavender @jorritkr @thomaswensma Wie weet komt er een pre-paid giftcard systeem voor specifieke series. Ipv een generieke voor Netflix, alleen voor serie x of filmgenre y. En dat weer digitaal."
+	},
+	{
 		"created_at": "Sat Jul 06 19:46:19 +0000 2019",
 		"id": 1147479700236845000,
 		"id_str": "1147592634208313344",
