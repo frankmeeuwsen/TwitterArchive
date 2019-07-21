328c1aa30943561a7c37d9809af354cc3df34330
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-07-21 11:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70169,
-	"created_at": "Sun Jul 21 08:21:04 +0000 2019",
+	"tweets": 70170,
+	"created_at": "Sun Jul 21 09:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 21 08:47:31 +0000 2019",
+		"id": 1152862657147543600,
+		"id_str": "1152862657147543552",
+		"full_text": "@JanPieter Het zou je verbazen hoe moeilijk mensen het nog hebben met een langharige jongen. In 2019 in Nederland. Kinderen in de speeltuin zijn al geindoctrineerd dat jongens kort haar moeten hebben. Ouders die mij verbaasd aanspreken dat Finn een jongen is. https://t.co/FOP40AyDrq",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JanPieter",
+					"name": "JanPieter 🇪🇺",
+					"id": 7501502,
+					"id_str": "7501502",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1152862647928447000,
+					"id_str": "1152862647928446976",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/D__KeFyXYAA9Qe8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D__KeFyXYAA9Qe8.jpg",
+					"url": "https://t.co/FOP40AyDrq",
+					"display_url": "pic.twitter.com/FOP40AyDrq",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1152862657147543552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1127,
+							"h": 1745,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 775,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 439,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1152862647928447000,
+					"id_str": "1152862647928446976",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/D__KeFyXYAA9Qe8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D__KeFyXYAA9Qe8.jpg",
+					"url": "https://t.co/FOP40AyDrq",
+					"display_url": "pic.twitter.com/FOP40AyDrq",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1152862657147543552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1127,
+							"h": 1745,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 775,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 439,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1152854970921881600,
+		"in_reply_to_status_id_str": "1152854970921881600",
+		"in_reply_to_user_id": 7501502,
+		"in_reply_to_user_id_str": "7501502",
+		"in_reply_to_screen_name": "JanPieter",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8858,
+			"friends_count": 267,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5492,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60212,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@JanPieter Het zou je verbazen hoe moeilijk mensen het nog hebben met een langharige jongen. In 2019 in Nederland. Kinderen in de speeltuin zijn al geindoctrineerd dat jongens kort haar moeten hebben. Ouders die mij verbaasd aanspreken dat Finn een jongen is. https://t.co/FOP40AyDrq"
+	},
+	{
 		"created_at": "Sun Jul 21 07:23:59 +0000 2019",
 		"id": 1152841637460095000,
 		"id_str": "1152841637460094978",
