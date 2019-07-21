70ff4ff350317033dba5fb53dc064070c7ee7cb7
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-07-21 10:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70168,
-	"created_at": "Sat Jul 20 17:21:04 +0000 2019",
+	"tweets": 70169,
+	"created_at": "Sun Jul 21 08:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,193 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 21 07:23:59 +0000 2019",
+		"id": 1152841637460095000,
+		"id_str": "1152841637460094978",
+		"full_text": "Gelukkig gaat het nu weer een stuk beter. Ik ben in elk geval fit genoeg voor de BBQ van Finn’s nieuwe club, De Club Van Jongens Met Lang Haar. Die bestaat echt. DuckDuckGo het. https://t.co/wDBS0YCZ6V",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1152841625321783300,
+					"id_str": "1152841625321783296",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D_-3WaeWwAAqsEc.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D_-3WaeWwAAqsEc.jpg",
+					"url": "https://t.co/wDBS0YCZ6V",
+					"display_url": "pic.twitter.com/wDBS0YCZ6V",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1152841637460094978/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 300,
+							"h": 180,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 300,
+							"h": 180,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 300,
+							"h": 180,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1152841625321783300,
+					"id_str": "1152841625321783296",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D_-3WaeWwAAqsEc.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D_-3WaeWwAAqsEc.jpg",
+					"url": "https://t.co/wDBS0YCZ6V",
+					"display_url": "pic.twitter.com/wDBS0YCZ6V",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1152841637460094978/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 300,
+							"h": 180,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 300,
+							"h": 180,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 300,
+							"h": 180,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							5,
+							3
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D_-3WaeWwAAqsEc.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1152621860468736000,
+		"in_reply_to_status_id_str": "1152621860468736002",
+		"in_reply_to_user_id": 816203,
+		"in_reply_to_user_id_str": "816203",
+		"in_reply_to_screen_name": "frankmeeuwsen",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Beschikbaar voor werk rondom de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8858,
+			"friends_count": 267,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5492,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60211,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1555103121",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Gelukkig gaat het nu weer een stuk beter. Ik ben in elk geval fit genoeg voor de BBQ van Finn’s nieuwe club, De Club Van Jongens Met Lang Haar. Die bestaat echt. DuckDuckGo het. https://t.co/wDBS0YCZ6V"
+	},
+	{
 		"created_at": "Sat Jul 20 16:50:40 +0000 2019",
 		"id": 1152621860468736000,
 		"id_str": "1152621860468736002",
