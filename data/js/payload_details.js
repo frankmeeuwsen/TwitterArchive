bb01a1300fa05a8a3fbc8a13acc2d7896961450a
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-08-03 08:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70206,
-	"created_at": "Fri Aug 02 14:21:04 +0000 2019",
+	"tweets": 70207,
+	"created_at": "Sat Aug 03 06:21:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 03 05:22:31 +0000 2019",
+		"id": 1157522109301760000,
+		"id_str": "1157522109301760000",
+		"full_text": "Ha! Dat is een logische stap natuurlijk. Een playlist bij het top-culinaire programma De Smaak van Joel. https://t.co/OtBa7uhTfP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OtBa7uhTfP",
+					"expanded_url": "https://twitter.com/joelbroekaert/status/1157386857036038144",
+					"display_url": "twitter.com/joelbroekaert/…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8857,
+			"friends_count": 268,
+			"listed_count": 589,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5512,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60258,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1157386857036038100,
+		"quoted_status_id_str": "1157386857036038144",
+		"quoted_status_permalink": {
+			"url": "https://t.co/OtBa7uhTfP",
+			"expanded": "https://twitter.com/joelbroekaert/status/1157386857036038144",
+			"display": "twitter.com/joelbroekaert/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Aug 02 20:25:04 +0000 2019",
+			"id": 1157386857036038100,
+			"id_str": "1157386857036038144",
+			"full_text": "@frankmeeuwsen https://t.co/wi06m2KYun",
+			"truncated": false,
+			"display_text_range": [
+				15,
+				38
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "frankmeeuwsen",
+						"name": "Frank Meeuwsen",
+						"id": 816203,
+						"id_str": "816203",
+						"indices": [
+							0,
+							14
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/wi06m2KYun",
+						"expanded_url": "https://open.spotify.com/user/francescoindaco/playlist/64prgppibzkYfWLJOa7s0s?si=J8Z-uxx_R02YqE_v3q2WFQ",
+						"display_url": "open.spotify.com/user/francesco…",
+						"indices": [
+							15,
+							38
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1154836333480874000,
+			"in_reply_to_status_id_str": "1154836333480873985",
+			"in_reply_to_user_id": 816203,
+			"in_reply_to_user_id_str": "816203",
+			"in_reply_to_screen_name": "frankmeeuwsen",
+			"user": {
+				"id": 140024646,
+				"id_str": "140024646",
+				"name": "Joël Broekaert",
+				"screen_name": "joelbroekaert",
+				"location": "",
+				"description": "Restaurant critic NRC | culinary journalist | Amsterdam based | food traveller | tv host",
+				"url": "https://t.co/5ABnSVZF0C",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5ABnSVZF0C",
+								"expanded_url": "http://talenttalks.nl/spreker/joel-broekaert/",
+								"display_url": "talenttalks.nl/spreker/joel-b…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5338,
+				"friends_count": 633,
+				"listed_count": 74,
+				"created_at": "Tue May 04 10:26:51 +0000 2010",
+				"favourites_count": 2242,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4761,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "ACDED6",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1157360344395898880/defZmDf3_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1157360344395898880/defZmDf3_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/140024646/1563562687",
+				"profile_link_color": "038543",
+				"profile_sidebar_border_color": "EEEEEE",
+				"profile_sidebar_fill_color": "F6F6F6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ha! Dat is een logische stap natuurlijk. Een playlist bij het top-culinaire programma De Smaak van Joel. https://t.co/OtBa7uhTfP"
+	},
+	{
 		"created_at": "Fri Aug 02 13:38:19 +0000 2019",
 		"id": 1157284494350672000,
 		"id_str": "1157284494350671872",
