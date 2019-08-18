3fe5af38e938848cf2a03d3181bbf8224889c0f5
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-08-18 10:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70216,
-	"created_at": "Thu Aug 08 13:21:05 +0000 2019",
+	"tweets": 70217,
+	"created_at": "Sun Aug 18 08:21:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sun Aug 18 07:47:43 +0000 2019",
+		"id": 1162994469765038000,
+		"id_str": "1162994469765038080",
+		"full_text": "King Kirby tentoonstelling: https://t.co/x7YHYsI7qa https://t.co/xldjXAlZhV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/x7YHYsI7qa",
+					"expanded_url": "https://diggingthedigital.com/?p=3979",
+					"display_url": "diggingthedigital.com/?p=3979",
+					"indices": [
+						28,
+						51
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1162994468439634000,
+					"id_str": "1162994468439633921",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/ECPJTqcWsAEnudT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECPJTqcWsAEnudT.jpg",
+					"url": "https://t.co/xldjXAlZhV",
+					"display_url": "pic.twitter.com/xldjXAlZhV",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1162994469765038080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 100,
+							"h": 100,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 100,
+							"h": 100,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 100,
+							"h": 100,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 100,
+							"h": 100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1162994468439634000,
+					"id_str": "1162994468439633921",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/ECPJTqcWsAEnudT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECPJTqcWsAEnudT.jpg",
+					"url": "https://t.co/xldjXAlZhV",
+					"display_url": "pic.twitter.com/xldjXAlZhV",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1162994469765038080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 100,
+							"h": 100,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 100,
+							"h": 100,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 100,
+							"h": 100,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 100,
+							"h": 100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8855,
+			"friends_count": 260,
+			"listed_count": 588,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5310,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60268,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "King Kirby tentoonstelling: https://t.co/x7YHYsI7qa https://t.co/xldjXAlZhV"
+	},
+	{
 		"created_at": "Thu Aug 08 12:49:30 +0000 2019",
 		"id": 1159446537392533500,
 		"id_str": "1159446537392533504",
@@ -143,7 +327,7 @@ Grailbird.data.tweets_2019_08 =
 				"name": "Daniël Verlaan",
 				"screen_name": "danielverlaan",
 				"location": "🚀",
-				"description": "techjournalist @RTLnieuws ☎️ +316 2089 8142 📫 daniel.verlaan@rtl.nl 🏆 Tegel & Loep '18 📕 boek @DasMag 2020 👨🏻‍💻 auteur cybersecuritygids https://t.co/PqsPCI7jbA",
+				"description": "techjournalist @RTLnieuws ☎️ +316 2089 8142 📫 daniel.verlaan@rtl.nl 🏆 Tegel & Loep '18 📕 boek @DasMag 2020 👨🏻💻 auteur cybersecuritygids https://t.co/PqsPCI7jbA",
 				"url": "https://t.co/CmkHfPB2GP",
 				"entities": {
 					"url": {
