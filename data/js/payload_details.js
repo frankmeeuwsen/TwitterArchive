e84b7780549483d6e64d23ab26213f4bb2d8f19c
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-08-28 10:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70219,
-	"created_at": "Tue Aug 27 18:21:06 +0000 2019",
+	"tweets": 70220,
+	"created_at": "Wed Aug 28 08:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 28 07:25:16 +0000 2019",
+		"id": 1166612696873283600,
+		"id_str": "1166612696873283591",
+		"full_text": "Vreemd. Ik krijg een melding dat iemand een mailaccount op mijn domein wil resetten bij een dienst van @microsoftnl. Maar ik kan het zelf met geen mogelijkheid ongedaan maken of overnemen. Hoe nu verder? https://t.co/hlSoqzynch /cc @MicrosoftHelpt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			247
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "microsoftnl",
+					"name": "Microsoft Nederland",
+					"id": 19016519,
+					"id_str": "19016519",
+					"indices": [
+						103,
+						115
+					]
+				},
+				{
+					"screen_name": "MicrosoftHelpt",
+					"name": "Microsoft Helpt",
+					"id": 34569543,
+					"id_str": "34569543",
+					"indices": [
+						232,
+						247
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/hlSoqzynch",
+					"expanded_url": "https://diggingthedigital.com/hoe-kan-ik-het-wachtwoord-wijzigen-van-een-microsoft-account/",
+					"display_url": "diggingthedigital.com/hoe-kan-ik-het…",
+					"indices": [
+						204,
+						227
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8853,
+			"friends_count": 260,
+			"listed_count": 586,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5310,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60272,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Vreemd. Ik krijg een melding dat iemand een mailaccount op mijn domein wil resetten bij een dienst van @microsoftnl. Maar ik kan het zelf met geen mogelijkheid ongedaan maken of overnemen. Hoe nu verder? https://t.co/hlSoqzynch /cc @MicrosoftHelpt"
+	},
+	{
 		"created_at": "Tue Aug 27 18:07:40 +0000 2019",
 		"id": 1166411975309561900,
 		"id_str": "1166411975309561859",
