8e271e4b1339da20a2d047faeb097ef201007ddc
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-08-30 19:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70228,
-	"created_at": "Fri Aug 30 10:21:04 +0000 2019",
+	"tweets": 70229,
+	"created_at": "Fri Aug 30 17:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 30 17:19:02 +0000 2019",
+		"id": 1167262276207857700,
+		"id_str": "1167486901420511233",
+		"full_text": "May RSS rise again. Stronger because we are wiser. Independent publishers sharing on open platforms with no surveillance capitalism in the middle ❤️ https://t.co/SLqvMo7YUa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SLqvMo7YUa",
+					"expanded_url": "https://twitter.com/daringfireball/status/1167258616774254592",
+					"display_url": "twitter.com/daringfireball…",
+					"indices": [
+						149,
+						172
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14561327,
+			"id_str": "14561327",
+			"name": "DHH",
+			"screen_name": "dhh",
+			"location": "",
+			"description": "Creator of Ruby on Rails, Founder & CTO at Basecamp, NYT best-selling author, and Le Mans 24h class-winning racing driver.",
+			"url": "https://t.co/472m0ihZkH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/472m0ihZkH",
+							"expanded_url": "https://dhh.dk",
+							"display_url": "dhh.dk",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 341994,
+			"friends_count": 123,
+			"listed_count": 9563,
+			"created_at": "Sun Apr 27 20:19:25 +0000 2008",
+			"favourites_count": 8827,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 43505,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/975876868455809024/eK7mDppU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/975876868455809024/eK7mDppU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14561327/1539031099",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1167258616774254600,
+		"quoted_status_id_str": "1167258616774254592",
+		"quoted_status_permalink": {
+			"url": "https://t.co/SLqvMo7YUa",
+			"expanded": "https://twitter.com/daringfireball/status/1167258616774254592",
+			"display": "twitter.com/daringfireball…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Aug 30 02:11:55 +0000 2019",
+			"id": 1167258616774254600,
+			"id_str": "1167258616774254592",
+			"full_text": "NetNewsWire 5.0: https://t.co/ETvrkR7mCe",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				40
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ETvrkR7mCe",
+						"expanded_url": "https://daringfireball.net/linked/2019/08/29/netnewswire-5",
+						"display_url": "daringfireball.net/linked/2019/08…",
+						"indices": [
+							17,
+							40
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://daringfireball.net/\" rel=\"nofollow\">DF Tootbot</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 10760422,
+				"id_str": "10760422",
+				"name": "Daring Fireball",
+				"screen_name": "daringfireball",
+				"location": "",
+				"description": "Entries from Daring Fireball. Sort of like RSS but via Twitter.",
+				"url": "https://t.co/iipNKeMOWB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/iipNKeMOWB",
+								"expanded_url": "https://daringfireball.net/",
+								"display_url": "daringfireball.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 92932,
+				"friends_count": 0,
+				"listed_count": 6562,
+				"created_at": "Sat Dec 01 05:55:06 +0000 2007",
+				"favourites_count": 336,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 19353,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "4A525A",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1068658191041536001/q1bHVBDc_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068658191041536001/q1bHVBDc_normal.jpg",
+				"profile_link_color": "323232",
+				"profile_sidebar_border_color": "4A525A",
+				"profile_sidebar_fill_color": "EEEEEE",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 21,
+			"favorite_count": 130,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 275,
+		"favorite_count": 1244,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "May RSS rise again. Stronger because we are wiser. Independent publishers sharing on open platforms with no surveillance capitalism in the middle ❤️ https://t.co/SLqvMo7YUa"
+	},
+	{
 		"created_at": "Fri Aug 30 10:16:27 +0000 2019",
 		"id": 1167380554318274600,
 		"id_str": "1167380554318274560",
