20d3a9fe0ca8654712477d7d833fa9eee57e5acc
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-09-06 09:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70244,
-	"created_at": "Thu Sep 05 10:21:04 +0000 2019",
+	"tweets": 70245,
+	"created_at": "Fri Sep 06 07:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 06 06:39:52 +0000 2019",
+		"id": 1169862763386032000,
+		"id_str": "1169862763386032128",
+		"full_text": "@_loveallthis @xoxo Can I use your photo for my annual \"I'm sad I can't go to XOXO\"-blogpost?",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_loveallthis",
+					"name": "Sarah Emerson @ XOXO",
+					"id": 64853040,
+					"id_str": "64853040",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "xoxo",
+					"name": "XOXO",
+					"id": 516875194,
+					"id_str": "516875194",
+					"indices": [
+						14,
+						19
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1169835018098598000,
+		"in_reply_to_status_id_str": "1169835018098597890",
+		"in_reply_to_user_id": 64853040,
+		"in_reply_to_user_id_str": "64853040",
+		"in_reply_to_screen_name": "_loveallthis",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8860,
+			"friends_count": 262,
+			"listed_count": 585,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5330,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60302,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@_loveallthis @xoxo Can I use your photo for my annual \"I'm sad I can't go to XOXO\"-blogpost?"
+	},
+	{
 		"created_at": "Thu Sep 05 09:35:18 +0000 2019",
 		"id": 1169539426097254400,
 		"id_str": "1169544527075598336",
