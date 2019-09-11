c38a87ed878e629d7340e0cb1a4a9fb1eba29a28
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-09-11 21:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70258,
-	"created_at": "Wed Sep 11 15:21:05 +0000 2019",
+	"tweets": 70259,
+	"created_at": "Wed Sep 11 19:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Wed Sep 11 19:02:28 +0000 2019",
+		"id": 1171861583179501600,
+		"id_str": "1171861583179501569",
+		"full_text": "Dankzij het boek Brat Pack America en de @GeekyDingen podcast ga ik zo eens de highschool cultklassieker \"The Legend of Billie Jean\" kijken. Met een piepjonge Christian Slater. https://t.co/KwJBZWKXBM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GeekyDingen",
+					"name": "Geeky Dingen",
+					"id": 955185741633605600,
+					"id_str": "955185741633605633",
+					"indices": [
+						41,
+						53
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1171861563780796400,
+					"id_str": "1171861563780796416",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/EENJ4isWkAAgul7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EENJ4isWkAAgul7.jpg",
+					"url": "https://t.co/KwJBZWKXBM",
+					"display_url": "pic.twitter.com/KwJBZWKXBM",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1171861583179501569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 329,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 329,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 329,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1171861563780796400,
+					"id_str": "1171861563780796416",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/EENJ4isWkAAgul7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EENJ4isWkAAgul7.jpg",
+					"url": "https://t.co/KwJBZWKXBM",
+					"display_url": "pic.twitter.com/KwJBZWKXBM",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1171861583179501569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 329,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 329,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 329,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8856,
+			"friends_count": 264,
+			"listed_count": 585,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5339,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60317,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Dankzij het boek Brat Pack America en de @GeekyDingen podcast ga ik zo eens de highschool cultklassieker \"The Legend of Billie Jean\" kijken. Met een piepjonge Christian Slater. https://t.co/KwJBZWKXBM"
+	},
+	{
 		"created_at": "Wed Sep 11 14:51:13 +0000 2019",
 		"id": 1171798354088607700,
 		"id_str": "1171798354088607745",
