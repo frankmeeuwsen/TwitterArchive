f4bb3bde56e0f7255ed5c2d8038e67ee63482e56
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-09-19 16:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70282,
-	"created_at": "Thu Sep 19 07:21:04 +0000 2019",
+	"tweets": 70283,
+	"created_at": "Thu Sep 19 14:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Thu Sep 19 14:21:00 +0000 2019",
+		"id": 1174689854040526800,
+		"id_str": "1174689854040526848",
+		"full_text": "@OpenDictator @rushkoff Dat is een goede vraag. Zijn podcasts zijn soms een live opname en daar zit wel wat Q&amp;A met de gast in. Ik hoop dat het zaterdag ook zo is, maar ik weet dat niet zeker. We kunnen sowieso bitterballen doen aan de bar daar! ;-)",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			253
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OpenDictator",
+					"name": "Stephan Verveen",
+					"id": 6719712,
+					"id_str": "6719712",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "rushkoff",
+					"name": "douglas rushkoff",
+					"id": 15085196,
+					"id_str": "15085196",
+					"indices": [
+						14,
+						23
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1174614659569213400,
+		"in_reply_to_status_id_str": "1174614659569213443",
+		"in_reply_to_user_id": 6719712,
+		"in_reply_to_user_id_str": "6719712",
+		"in_reply_to_screen_name": "OpenDictator",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8857,
+			"friends_count": 265,
+			"listed_count": 584,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5360,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60345,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@OpenDictator @rushkoff Dat is een goede vraag. Zijn podcasts zijn soms een live opname en daar zit wel wat Q&amp;A met de gast in. Ik hoop dat het zaterdag ook zo is, maar ik weet dat niet zeker. We kunnen sowieso bitterballen doen aan de bar daar! ;-)"
+	},
+	{
 		"created_at": "Thu Sep 19 07:00:13 +0000 2019",
 		"id": 1174578928863842300,
 		"id_str": "1174578928863842304",
