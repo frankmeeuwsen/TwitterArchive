d8b02b8039ab407047ecadf0a35a0ef3c5663ded
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-09-20 20:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70296,
-	"created_at": "Fri Sep 20 13:21:04 +0000 2019",
+	"tweets": 70297,
+	"created_at": "Fri Sep 20 18:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Fri Sep 20 18:20:47 +0000 2019",
+		"id": 1175067889331769300,
+		"id_str": "1175112584191778821",
+		"full_text": "Own your content.\n\n“YouTubers said that the most disappointing thing about losing their checkmark was that they’d suddenly be second-class citizens on a platform that some had dedicated years of their lives to.”\n\nhttps://t.co/ijXx21QoH2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			236
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ijXx21QoH2",
+					"expanded_url": "https://www.nytimes.com/2019/09/19/style/youtube-unverified-creators.html",
+					"display_url": "nytimes.com/2019/09/19/sty…",
+					"indices": [
+						213,
+						236
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 61133,
+			"id_str": "61133",
+			"name": "zeldman",
+			"screen_name": "zeldman",
+			"location": "New York, NY",
+			"description": "Principal Designer, Creative Director @Automattic. Cofounder @AnEventApart. Publisher @AListApart & @ABookApart. Faculty @svaixd. Host @TheBigWebShow.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 339653,
+			"friends_count": 4678,
+			"listed_count": 11314,
+			"created_at": "Tue Dec 12 17:11:41 +0000 2006",
+			"favourites_count": 63473,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 59893,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1134893582232502273/SzNreZ93_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134893582232502273/SzNreZ93_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/61133/1559414691",
+			"profile_link_color": "CC3300",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FF6600",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Own your content.\n\n“YouTubers said that the most disappointing thing about losing their checkmark was that they’d suddenly be second-class citizens on a platform that some had dedicated years of their lives to.”\n\nhttps://t.co/ijXx21QoH2"
+	},
+	{
 		"created_at": "Fri Sep 20 13:05:51 +0000 2019",
 		"id": 1175033330426962000,
 		"id_str": "1175033330426961921",
