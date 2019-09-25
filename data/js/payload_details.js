135ea9e748a3cfb2f5b556b99c39c558e311011d
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-09-25 18:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70307,
-	"created_at": "Wed Sep 25 10:21:05 +0000 2019",
+	"tweets": 70308,
+	"created_at": "Wed Sep 25 16:21:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Wed Sep 25 15:50:24 +0000 2019",
+		"id": 1176886679854047200,
+		"id_str": "1176886679854047232",
+		"full_text": "Jij een idee @elger of @DagenNachtMedia? Europese podcasts in de stijl van This American Life (en vooruit, Radiolab) https://t.co/oX9PiUraCK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "elger",
+					"name": "Elger van der Wel",
+					"id": 14142375,
+					"id_str": "14142375",
+					"indices": [
+						13,
+						19
+					]
+				},
+				{
+					"screen_name": "DagenNachtMedia",
+					"name": "Dag en Nacht Podcasts",
+					"id": 803561324039049200,
+					"id_str": "803561324039049216",
+					"indices": [
+						23,
+						39
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oX9PiUraCK",
+					"expanded_url": "https://twitter.com/ton_zylstra/status/1176877689988210688",
+					"display_url": "twitter.com/ton_zylstra/st…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8851,
+			"friends_count": 267,
+			"listed_count": 583,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5363,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60375,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1176877689988210700,
+		"quoted_status_id_str": "1176877689988210688",
+		"quoted_status_permalink": {
+			"url": "https://t.co/oX9PiUraCK",
+			"expanded": "https://twitter.com/ton_zylstra/status/1176877689988210688",
+			"display": "twitter.com/ton_zylstra/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 25 15:14:41 +0000 2019",
+			"id": 1176877689988210700,
+			"id_str": "1176877689988210688",
+			"full_text": "Wie helpt @elmine aan goede podcasts van Europese bodem; https://t.co/CPXDyJ1HvY",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				80
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "elmine",
+						"name": "Elmine Wijnia",
+						"id": 5922092,
+						"id_str": "5922092",
+						"indices": [
+							10,
+							17
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/CPXDyJ1HvY",
+						"expanded_url": "https://infullflow.net/2019/09/op-zoek-naar-europese-verhalen/",
+						"display_url": "infullflow.net/2019/09/op-zoe…",
+						"indices": [
+							57,
+							80
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 25049203,
+				"id_str": "25049203",
+				"name": "Ton Zijlstra",
+				"screen_name": "ton_zylstra",
+				"location": "The Netherlands",
+				"description": "https://t.co/MRVMotNfWT\n\nComplex change, #opendata, #opengov, FabLab, ethics, IoT, learning, communities, philosophy of technology. .eu Signed https://t.co/4ltlqZnngQ",
+				"url": "https://t.co/mxPIeEaDtM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/mxPIeEaDtM",
+								"expanded_url": "https://www.zylstra.org/blog",
+								"display_url": "zylstra.org/blog",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/MRVMotNfWT",
+								"expanded_url": "http://m.tzyl.nl/@ton",
+								"display_url": "m.tzyl.nl/@ton",
+								"indices": [
+									0,
+									23
+								]
+							},
+							{
+								"url": "https://t.co/4ltlqZnngQ",
+								"expanded_url": "http://techpledge.org",
+								"display_url": "techpledge.org",
+								"indices": [
+									143,
+									166
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1588,
+				"friends_count": 497,
+				"listed_count": 193,
+				"created_at": "Wed Mar 18 09:24:58 +0000 2009",
+				"favourites_count": 240,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4857,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/659398307081379840/pyAVq5hk_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/659398307081379840/pyAVq5hk_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/25049203/1446047749",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Jij een idee @elger of @DagenNachtMedia? Europese podcasts in de stijl van This American Life (en vooruit, Radiolab) https://t.co/oX9PiUraCK"
+	},
+	{
 		"created_at": "Wed Sep 25 09:34:27 +0000 2019",
 		"id": 1176792069060403200,
 		"id_str": "1176792069060403200",
