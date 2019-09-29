2480513f22eee343c3e7be04a3893fe8dcf3d3e0
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-09-29 17:21:06 +0200

    Added 1 tweets

--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 29 14:47:19 +0000 2019",
+		"id": 1178320355611558000,
+		"id_str": "1178320355611557888",
+		"full_text": "\"Today we celebrate the first glorious anniversary of the Information Purification Directives. \" [...] \"And you'll see why 1984 won't be like '1984.'\" ;-) https://t.co/VEAAaSCQ73",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VEAAaSCQ73",
+					"expanded_url": "https://twitter.com/JamieTanna/status/1178318671707549696",
+					"display_url": "twitter.com/JamieTanna/sta…",
+					"indices": [
+						155,
+						178
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8855,
+			"friends_count": 272,
+			"listed_count": 583,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5376,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60413,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1178318671707549700,
+		"quoted_status_id_str": "1178318671707549696",
+		"quoted_status_permalink": {
+			"url": "https://t.co/VEAAaSCQ73",
+			"expanded": "https://twitter.com/JamieTanna/status/1178318671707549696",
+			"display": "twitter.com/JamieTanna/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Sep 29 14:40:38 +0000 2019",
+			"id": 1178318671707549700,
+			"id_str": "1178318671707549696",
+			"full_text": "#IndieWebCamp Amsterdam co-organiser @frankmeeuwsen is remote and telling us about his #IndieWeb blogging for the day https://t.co/6voX1NHFHd",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				117
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "IndieWebCamp",
+						"indices": [
+							0,
+							13
+						]
+					},
+					{
+						"text": "IndieWeb",
+						"indices": [
+							87,
+							96
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "frankmeeuwsen",
+						"name": "Frank Meeuwsen",
+						"id": 816203,
+						"id_str": "816203",
+						"indices": [
+							37,
+							51
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1178318664463986700,
+						"id_str": "1178318664463986688",
+						"indices": [
+							118,
+							141
+						],
+						"media_url": "http://pbs.twimg.com/media/EFo6lROWsAAZIxe.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EFo6lROWsAAZIxe.jpg",
+						"url": "https://t.co/6voX1NHFHd",
+						"display_url": "pic.twitter.com/6voX1NHFHd",
+						"expanded_url": "https://twitter.com/JamieTanna/status/1178318671707549696/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1178318664463986700,
+						"id_str": "1178318664463986688",
+						"indices": [
+							118,
+							141
+						],
+						"media_url": "http://pbs.twimg.com/media/EFo6lROWsAAZIxe.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EFo6lROWsAAZIxe.jpg",
+						"url": "https://t.co/6voX1NHFHd",
+						"display_url": "pic.twitter.com/6voX1NHFHd",
+						"expanded_url": "https://twitter.com/JamieTanna/status/1178318671707549696/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2292531098,
+				"id_str": "2292531098",
+				"name": "Jamie Tanna @ #IndieWebCamp AMS | www.jvt.me",
+				"screen_name": "JamieTanna",
+				"location": "127.0.0.1",
+				"description": "Self-documenting Software Quality Engineer @CapitalOneUK, Savagely Mediocre Gamer, GNU/Linux, Free Software advocate, #IndieWeb, #Chef infra. He/him. Emoji:🙌🏼",
+				"url": "https://t.co/sKHwNA0pPn",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/sKHwNA0pPn",
+								"expanded_url": "https://www.jvt.me",
+								"display_url": "jvt.me",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 874,
+				"friends_count": 1816,
+				"listed_count": 44,
+				"created_at": "Wed Jan 15 10:55:11 +0000 2014",
+				"favourites_count": 10040,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5870,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1140379955223834626/OnFy3-rl_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1140379955223834626/OnFy3-rl_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2292531098/1541619449",
+				"profile_link_color": "8B16A8",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Today we celebrate the first glorious anniversary of the Information Purification Directives. \" [...] \"And you'll see why 1984 won't be like '1984.'\" ;-) https://t.co/VEAAaSCQ73"
+	},
+	{
 		"created_at": "Sun Sep 29 14:16:03 +0000 2019",
 		"id": 1178312223288152000,
 		"id_str": "1178312486132617216",
