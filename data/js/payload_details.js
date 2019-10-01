03c8c4d917d8d0bca9888e8a14ecd0bd00a5f439
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-10-01 19:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70342,
-	"created_at": "Mon Sep 30 18:21:05 +0000 2019",
+	"tweets": 70343,
+	"created_at": "Tue Oct 01 17:21:05 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_10.js
@@ -0,0 +1,298 @@
+Grailbird.data.tweets_2019_10 = 
+[
+	{
+		"created_at": "Tue Oct 01 16:51:36 +0000 2019",
+		"id": 1178693703621398500,
+		"id_str": "1179076406506131457",
+		"full_text": "Dit is doodeng. Alle surveillance technologieën die je maar kunt bedenken worden al gebruikt in Chinese klaslokalen. Wakker worden collega's #AI https://t.co/73KWjXTTXk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AI",
+					"indices": [
+						141,
+						144
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1177331921879277600,
+					"id_str": "1177331921879277568",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EFa6MuUWkAEei3x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFa6MuUWkAEei3x.jpg",
+					"url": "https://t.co/73KWjXTTXk",
+					"display_url": "pic.twitter.com/73KWjXTTXk",
+					"expanded_url": "https://twitter.com/WSJ/status/1177357178975457285/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1177357178975457300,
+					"source_status_id_str": "1177357178975457285",
+					"source_user_id": 3108351,
+					"source_user_id_str": "3108351"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1177331921879277600,
+					"id_str": "1177331921879277568",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EFa6MuUWkAEei3x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFa6MuUWkAEei3x.jpg",
+					"url": "https://t.co/73KWjXTTXk",
+					"display_url": "pic.twitter.com/73KWjXTTXk",
+					"expanded_url": "https://twitter.com/WSJ/status/1177357178975457285/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1177357178975457300,
+					"source_status_id_str": "1177357178975457285",
+					"source_user_id": 3108351,
+					"source_user_id_str": "3108351",
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 343385,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1177331921879277568/vid/720x720/18dWB-gwR9jaP5wL.mp4?tag=13"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1177331921879277568/pl/o6biH6JTgstUX-UD.m3u8?tag=13"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1177331921879277568/vid/320x320/dyYddV6XxXyYpczy.mp4?tag=13"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1177331921879277568/vid/480x480/dJkuP3iCbRou3uxB.mp4?tag=13"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "Under AI’s Watchful Eye, China Wants to Raise Smarter Students",
+						"description": "",
+						"embeddable": true,
+						"monetizable": true,
+						"source_user": {
+							"id": 3108351,
+							"id_str": "3108351",
+							"name": "The Wall Street Journal",
+							"screen_name": "WSJ",
+							"location": "New York, NY",
+							"description": "Breaking news and features from https://t.co/GhhR6PLfem.",
+							"url": "https://t.co/GhhR6PLfem",
+							"entities": {
+								"url": {
+									"urls": [
+										{
+											"url": "https://t.co/GhhR6PLfem",
+											"expanded_url": "http://wsj.com",
+											"display_url": "wsj.com",
+											"indices": [
+												0,
+												23
+											]
+										}
+									]
+								},
+								"description": {
+									"urls": [
+										{
+											"url": "https://t.co/GhhR6PLfem",
+											"expanded_url": "http://wsj.com",
+											"display_url": "wsj.com",
+											"indices": [
+												32,
+												55
+											]
+										}
+									]
+								}
+							},
+							"protected": false,
+							"followers_count": 16927573,
+							"friends_count": 1114,
+							"listed_count": 113210,
+							"created_at": "Sun Apr 01 06:22:13 +0000 2007",
+							"favourites_count": 1228,
+							"utc_offset": null,
+							"time_zone": null,
+							"geo_enabled": true,
+							"verified": true,
+							"statuses_count": 287978,
+							"lang": null,
+							"contributors_enabled": false,
+							"is_translator": false,
+							"is_translation_enabled": true,
+							"profile_background_color": "EDF2F5",
+							"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+							"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+							"profile_background_tile": false,
+							"profile_image_url": "http://pbs.twimg.com/profile_images/971415515754266624/zCX0q9d5_normal.jpg",
+							"profile_image_url_https": "https://pbs.twimg.com/profile_images/971415515754266624/zCX0q9d5_normal.jpg",
+							"profile_banner_url": "https://pbs.twimg.com/profile_banners/3108351/1562333209",
+							"profile_link_color": "1164B8",
+							"profile_sidebar_border_color": "FFFFFF",
+							"profile_sidebar_fill_color": "EFEFEF",
+							"profile_text_color": "333333",
+							"profile_use_background_image": false,
+							"has_extended_profile": false,
+							"default_profile": false,
+							"default_profile_image": false,
+							"following": false,
+							"follow_request_sent": false,
+							"notifications": false,
+							"translator_type": "regular"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 137451762,
+			"id_str": "137451762",
+			"name": "Jelmer Evers",
+			"screen_name": "jelmerevers",
+			"location": "Utrecht",
+			"description": "History teacher | author | union activist| executive board @aobtweets | @eduint | #flipthesystem| #HetAlternatief",
+			"url": "https://t.co/MHf8mfcXs4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MHf8mfcXs4",
+							"expanded_url": "http://www.jelmerevers.nl",
+							"display_url": "jelmerevers.nl",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13095,
+			"friends_count": 2110,
+			"listed_count": 364,
+			"created_at": "Mon Apr 26 20:02:37 +0000 2010",
+			"favourites_count": 34419,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 84666,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099741455088869381/SdvV2gQf_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099741455088869381/SdvV2gQf_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/137451762/1450207601",
+			"profile_link_color": "D1A104",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 179,
+		"favorite_count": 163,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Dit is doodeng. Alle surveillance technologieën die je maar kunt bedenken worden al gebruikt in Chinese klaslokalen. Wakker worden collega's #AI https://t.co/73KWjXTTXk"
+	}
+]
