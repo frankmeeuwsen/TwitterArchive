1024d69aa536cf63da73cb88aa61e84d47ca6f88
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-10-04 16:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70361,
-	"created_at": "Thu Oct 03 20:21:05 +0000 2019",
+	"tweets": 70362,
+	"created_at": "Fri Oct 04 14:21:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 04 13:26:14 +0000 2019",
+		"id": 1180111891282182100,
+		"id_str": "1180111891282182146",
+		"full_text": "@FZelders Ik gebruik hem vrijwel wekelijks om allerlei redenen en ik heb nooit een probleem. Wat vreemd. Het werkt gewoon.",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			122
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "FZelders",
+					"name": "FredZ",
+					"id": 8511492,
+					"id_str": "8511492",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1180094732241948700,
+		"in_reply_to_status_id_str": "1180094732241948673",
+		"in_reply_to_user_id": 8511492,
+		"in_reply_to_user_id_str": "8511492",
+		"in_reply_to_screen_name": "FZelders",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8857,
+			"friends_count": 274,
+			"listed_count": 583,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60441,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@FZelders Ik gebruik hem vrijwel wekelijks om allerlei redenen en ik heb nooit een probleem. Wat vreemd. Het werkt gewoon."
+	},
+	{
 		"created_at": "Thu Oct 03 19:46:46 +0000 2019",
 		"id": 1179845264741458000,
 		"id_str": "1179845264741457920",
@@ -380,7 +486,7 @@ Grailbird.data.tweets_2019_10 =
 			"name": "Charlie Don't Surf",
 			"screen_name": "sonniesedge",
 			"location": "Sitting on the Berlin wall",
-			"description": "People before code. Kindness before capitalism. \"One of those #a11y cultists\". #Indieweb lover. #Simplicity aficionado. She/her. 🏳️‍🌈 Pisces as fuck. ♓",
+			"description": "People before code. Kindness before capitalism. \"One of those #a11y cultists\". #Indieweb lover. #Simplicity aficionado. She/her. 🏳️🌈 Pisces as fuck. ♓",
 			"url": "https://t.co/L4TaNjGmbv",
 			"entities": {
 				"url": {
