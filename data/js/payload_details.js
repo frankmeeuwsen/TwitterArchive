6e608e8d46566fffc3b1c2d614f0058df9f00318
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-10-16 11:21:10 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70426,
-	"created_at": "Wed Oct 16 08:21:05 +0000 2019",
+	"tweets": 70427,
+	"created_at": "Wed Oct 16 09:21:09 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,172 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Wed Oct 16 08:52:04 +0000 2019",
+		"id": 1184391546961121300,
+		"id_str": "1184391546961121281",
+		"full_text": "@NickKivits @Artoek @mackaaij @arjanbroere @resourcerer @NielsGouman @jplattel Dank je voor de uitleg. Ik wacht vooralsnog een bericht van Reshift af over de gang van zaken voor ik een mogelijke volgende actie onderneem.",
+		"truncated": false,
+		"display_text_range": [
+			79,
+			220
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NickKivits",
+					"name": "Nick Kivits 🚀",
+					"id": 83644622,
+					"id_str": "83644622",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "Artoek",
+					"name": "Martine Bakx",
+					"id": 127502172,
+					"id_str": "127502172",
+					"indices": [
+						12,
+						19
+					]
+				},
+				{
+					"screen_name": "mackaaij",
+					"name": "Patrick Mackaaij",
+					"id": 16212528,
+					"id_str": "16212528",
+					"indices": [
+						20,
+						29
+					]
+				},
+				{
+					"screen_name": "arjanbroere",
+					"name": "arjan broere",
+					"id": 2087771,
+					"id_str": "2087771",
+					"indices": [
+						30,
+						42
+					]
+				},
+				{
+					"screen_name": "resourcerer",
+					"name": " martijn aslander",
+					"id": 3051761,
+					"id_str": "3051761",
+					"indices": [
+						43,
+						55
+					]
+				},
+				{
+					"screen_name": "NielsGouman",
+					"name": "Niels Gouman",
+					"id": 25689874,
+					"id_str": "25689874",
+					"indices": [
+						56,
+						68
+					]
+				},
+				{
+					"screen_name": "jplattel",
+					"name": "Joost Plattel",
+					"id": 16376925,
+					"id_str": "16376925",
+					"indices": [
+						69,
+						78
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1184384619413753900,
+		"in_reply_to_status_id_str": "1184384619413753856",
+		"in_reply_to_user_id": 83644622,
+		"in_reply_to_user_id_str": "83644622",
+		"in_reply_to_screen_name": "NickKivits",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8869,
+			"friends_count": 273,
+			"listed_count": 585,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5442,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60520,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@NickKivits @Artoek @mackaaij @arjanbroere @resourcerer @NielsGouman @jplattel Dank je voor de uitleg. Ik wacht vooralsnog een bericht van Reshift af over de gang van zaken voor ik een mogelijke volgende actie onderneem."
+	},
+	{
 		"created_at": "Wed Oct 16 07:54:27 +0000 2019",
 		"id": 1184377045746368500,
 		"id_str": "1184377045746368513",
