1c51667e7a7b0c657fde97a1fe7b3ab88d4df8c5
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-10-20 17:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70448,
-	"created_at": "Sun Oct 20 06:21:05 +0000 2019",
+	"tweets": 70449,
+	"created_at": "Sun Oct 20 15:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sun Oct 20 14:24:58 +0000 2019",
+		"id": 909056611687288800,
+		"id_str": "1185924876588212225",
+		"full_text": "If Great Britain was located next to Japan https://t.co/bAtWI9afSr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			42
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 909056185457872900,
+					"id_str": "909056185457872896",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/media/DJ2dyMcWkAA4lMn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DJ2dyMcWkAA4lMn.jpg",
+					"url": "https://t.co/bAtWI9afSr",
+					"display_url": "pic.twitter.com/bAtWI9afSr",
+					"expanded_url": "https://twitter.com/TerribleMaps/status/909056611687288832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1198,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1198,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 581,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 909056185457872900,
+					"id_str": "909056185457872896",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/media/DJ2dyMcWkAA4lMn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DJ2dyMcWkAA4lMn.jpg",
+					"url": "https://t.co/bAtWI9afSr",
+					"display_url": "pic.twitter.com/bAtWI9afSr",
+					"expanded_url": "https://twitter.com/TerribleMaps/status/909056611687288832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1198,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1198,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 581,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1663172653,
+			"id_str": "1663172653",
+			"name": "Terrible Maps",
+			"screen_name": "TerribleMaps",
+			"location": "",
+			"description": "The home of terrible maps with a pinch of humour  https://t.co/XP2FfGZkLt  Business: terriblemaps@gmail.com",
+			"url": "https://t.co/y51V7HkfS8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/y51V7HkfS8",
+							"expanded_url": "https://ko-fi.com/TerribleMaps",
+							"display_url": "ko-fi.com/TerribleMaps",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/XP2FfGZkLt",
+							"expanded_url": "https://www.instagram.com/terriblemap/",
+							"display_url": "instagram.com/terriblemap/",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 251904,
+			"friends_count": 1486,
+			"listed_count": 1366,
+			"created_at": "Sun Aug 11 18:15:23 +0000 2013",
+			"favourites_count": 346,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 601,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "FCF7FC",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/695741339326160900/CalT3T7Y_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/695741339326160900/CalT3T7Y_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1663172653/1454712717",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2331,
+		"favorite_count": 7131,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If Great Britain was located next to Japan https://t.co/bAtWI9afSr"
+	},
+	{
 		"created_at": "Sun Oct 20 05:51:42 +0000 2019",
 		"id": 1185596493232607200,
 		"id_str": "1185795706075975680",
