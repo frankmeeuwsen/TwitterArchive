c9b45b5242f20eadf836691ff4ff49f310136711
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-10-28 11:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70473,
-	"created_at": "Sun Oct 27 08:21:05 +0000 2019",
+	"tweets": 70474,
+	"created_at": "Mon Oct 28 10:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Mon Oct 28 09:36:46 +0000 2019",
+		"id": 1187754016278638600,
+		"id_str": "1188751451348230144",
+		"full_text": "New study just released: \"Automation Might End Most Unskilled Jobs in 10 Years\"\n\nJust kidding it's from... 1961\n\nhttps://t.co/99x0nFNTlX https://t.co/ILqVQF8Tmg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/99x0nFNTlX",
+					"expanded_url": "https://news.google.com/newspapers?id=anxWAAAAIBAJ&sjid=8uUDAAAAIBAJ&pg=7246%2C2727971",
+					"display_url": "news.google.com/newspapers?id=…",
+					"indices": [
+						113,
+						136
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1187754010423332900,
+					"id_str": "1187754010423332868",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/EHu_-r_UEAQXirR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EHu_-r_UEAQXirR.jpg",
+					"url": "https://t.co/ILqVQF8Tmg",
+					"display_url": "pic.twitter.com/ILqVQF8Tmg",
+					"expanded_url": "https://twitter.com/jmhorp/status/1187754016278638594/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1201,
+							"h": 633,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 358,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 632,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1187754010423332900,
+					"id_str": "1187754010423332868",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/EHu_-r_UEAQXirR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EHu_-r_UEAQXirR.jpg",
+					"url": "https://t.co/ILqVQF8Tmg",
+					"display_url": "pic.twitter.com/ILqVQF8Tmg",
+					"expanded_url": "https://twitter.com/jmhorp/status/1187754016278638594/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1201,
+							"h": 633,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 358,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 632,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22158854,
+			"id_str": "22158854",
+			"name": "Jeremy Horpedahl 🍍🍞",
+			"screen_name": "jmhorp",
+			"location": "Conway, AR",
+			"description": "Dad. Economist. Pineapples. Bread.",
+			"url": "https://t.co/6MAVb5cucW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6MAVb5cucW",
+							"expanded_url": "http://uca.edu/efirm/facultystaff/jhorpedahl/",
+							"display_url": "uca.edu/efirm/facultys…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4096,
+			"friends_count": 514,
+			"listed_count": 57,
+			"created_at": "Fri Feb 27 16:43:59 +0000 2009",
+			"favourites_count": 7574,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15557,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1142616125613793282/YhfdpI6F_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1142616125613793282/YhfdpI6F_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22158854/1567717756",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 163,
+		"favorite_count": 333,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "New study just released: \"Automation Might End Most Unskilled Jobs in 10 Years\"\n\nJust kidding it's from... 1961\n\nhttps://t.co/99x0nFNTlX https://t.co/ILqVQF8Tmg"
+	},
+	{
 		"created_at": "Sun Oct 27 08:05:05 +0000 2019",
 		"id": 1188365990838571000,
 		"id_str": "1188365990838571008",
