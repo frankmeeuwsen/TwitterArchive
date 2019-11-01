2909d991c0536d1845ea9dd04b497d20d485a56c
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-11-01 15:21:06 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70489,
-	"created_at": "Fri Nov 01 08:21:03 +0000 2019",
+	"tweets": 70491,
+	"created_at": "Fri Nov 01 14:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,422 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 01 14:00:04 +0000 2019",
+		"id": 1190267265155059700,
+		"id_str": "1190267265155059712",
+		"full_text": "It's time to get the band back together... @Marjolijn @johanvoets @erwblo @Markies @mathys @tonw @marlooz @JimStolze (Waar is de video?)(En waar is mijn jas?) #twttrallstars https://t.co/Kf93Z62xRr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "twttrallstars",
+					"indices": [
+						159,
+						173
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Marjolijn",
+					"name": "Marjolijn",
+					"id": 1610571,
+					"id_str": "1610571",
+					"indices": [
+						43,
+						53
+					]
+				},
+				{
+					"screen_name": "johanvoets",
+					"name": "Johan Voets",
+					"id": 7422272,
+					"id_str": "7422272",
+					"indices": [
+						54,
+						65
+					]
+				},
+				{
+					"screen_name": "erwblo",
+					"name": "erwin blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						66,
+						73
+					]
+				},
+				{
+					"screen_name": "Markies",
+					"name": "Mark de Kock",
+					"id": 1858541,
+					"id_str": "1858541",
+					"indices": [
+						74,
+						82
+					]
+				},
+				{
+					"screen_name": "mathys",
+					"name": "⭐ Mathys van Abbe ⭐️",
+					"id": 6493412,
+					"id_str": "6493412",
+					"indices": [
+						83,
+						90
+					]
+				},
+				{
+					"screen_name": "tonw",
+					"name": "Ton Wesseling - #measure #cro",
+					"id": 2558991,
+					"id_str": "2558991",
+					"indices": [
+						91,
+						96
+					]
+				},
+				{
+					"screen_name": "marlooz",
+					"name": "Marlooz",
+					"id": 6614602,
+					"id_str": "6614602",
+					"indices": [
+						97,
+						105
+					]
+				},
+				{
+					"screen_name": "JimStolze",
+					"name": "Jim Stolze",
+					"id": 3644821,
+					"id_str": "3644821",
+					"indices": [
+						106,
+						116
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Kf93Z62xRr",
+					"expanded_url": "https://twitter.com/Boris/status/1190259639960690690",
+					"display_url": "twitter.com/Boris/status/1…",
+					"indices": [
+						174,
+						197
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8866,
+			"friends_count": 283,
+			"listed_count": 584,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5492,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60595,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1190259639960690700,
+		"quoted_status_id_str": "1190259639960690690",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Kf93Z62xRr",
+			"expanded": "https://twitter.com/Boris/status/1190259639960690690",
+			"display": "twitter.com/Boris/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Nov 01 13:29:46 +0000 2019",
+			"id": 1190259639960690700,
+			"id_str": "1190259639960690690",
+			"full_text": "Proud to announce Bowlr Resurrected: https://t.co/Ev7QmHf7DX 😂 🎳 #tnwlife",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				73
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "tnwlife",
+						"indices": [
+							65,
+							73
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Ev7QmHf7DX",
+						"expanded_url": "https://thenextweb.com/bowlr",
+						"display_url": "thenextweb.com/bowlr",
+						"indices": [
+							37,
+							60
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 17463,
+				"id_str": "17463",
+				"name": "Boris Veldhuijzen van Zanten",
+				"screen_name": "Boris",
+				"location": "Amsterdam, The Netherlands",
+				"description": "TNW founder & CEO. Only 47% of what I say here is true or serious. ❤️ #TNWLife. Also @Boris on Instagram 📸",
+				"url": "https://t.co/cYs8oy8tk2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/cYs8oy8tk2",
+								"expanded_url": "https://thenextweb.com",
+								"display_url": "thenextweb.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 36957,
+				"friends_count": 1022,
+				"listed_count": 2098,
+				"created_at": "Thu Nov 23 08:41:34 +0000 2006",
+				"favourites_count": 2951,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 46928,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/935907447188983808/7lKigQQU_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/935907447188983808/7lKigQQU_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17463/1571654466",
+				"profile_link_color": "C70000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "614F6E",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "It's time to get the band back together... @Marjolijn @johanvoets @erwblo @Markies @mathys @tonw @marlooz @JimStolze (Waar is de video?)(En waar is mijn jas?) #twttrallstars https://t.co/Kf93Z62xRr"
+	},
+	{
+		"created_at": "Fri Nov 01 13:51:16 +0000 2019",
+		"id": 1190264356493299700,
+		"id_str": "1190265047949467649",
+		"full_text": "If I recall correctly, Alphabet acquiring FitBit means that Google now controls hardware that's tied to some people's health insurance coverage?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 36823,
+			"id_str": "36823",
+			"name": "Anil Dash is… Dark Mode",
+			"screen_name": "anildash",
+			"location": "NYC",
+			"description": "Helping everyone create the web together at 🎏@Glitch. Host of @podcastfunction. 💜 ethical tech, mangoes, funk, transit & justice. he/him",
+			"url": "https://t.co/IYfhqu0kNE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IYfhqu0kNE",
+							"expanded_url": "http://anildash.com/about/",
+							"display_url": "anildash.com/about/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 598421,
+			"friends_count": 1209,
+			"listed_count": 9128,
+			"created_at": "Sat Dec 02 09:15:15 +0000 2006",
+			"favourites_count": 670525,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 203310,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1040592282032984071/RC8We7BG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1040592282032984071/RC8We7BG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/36823/1530683410",
+			"profile_link_color": "800080",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 77,
+		"favorite_count": 190,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "If I recall correctly, Alphabet acquiring FitBit means that Google now controls hardware that's tied to some people's health insurance coverage?"
+	},
+	{
 		"created_at": "Fri Nov 01 07:30:35 +0000 2019",
 		"id": 1190169246652915700,
 		"id_str": "1190169246652915712",
