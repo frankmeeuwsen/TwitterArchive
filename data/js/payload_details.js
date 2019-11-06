f4e6313a959af2be71159d4c56857d9db4158ba2
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-11-06 10:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70509,
-	"created_at": "Wed Nov 06 07:21:04 +0000 2019",
+	"tweets": 70510,
+	"created_at": "Wed Nov 06 09:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,208 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Wed Nov 06 08:55:12 +0000 2019",
+		"id": 1192002479774425000,
+		"id_str": "1192002479774425088",
+		"full_text": "@bramkoster Hebben ze native uit hun hub gepusht via open API's gebaseerd op de A.I. driven businessrules van de persona's die added value bieden voor de core business in de supplychain van de businessunit. \n\nI'll see myself out... https://t.co/3ExDgN6dXf",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			231
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "bramkoster",
+					"name": "Bram Koster",
+					"id": 6893682,
+					"id_str": "6893682",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1192002471201259500,
+					"id_str": "1192002471201259521",
+					"indices": [
+						232,
+						255
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EIrX7ptWwAEvknC.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EIrX7ptWwAEvknC.jpg",
+					"url": "https://t.co/3ExDgN6dXf",
+					"display_url": "pic.twitter.com/3ExDgN6dXf",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1192002479774425088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1192002471201259500,
+					"id_str": "1192002471201259521",
+					"indices": [
+						232,
+						255
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EIrX7ptWwAEvknC.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EIrX7ptWwAEvknC.jpg",
+					"url": "https://t.co/3ExDgN6dXf",
+					"display_url": "pic.twitter.com/3ExDgN6dXf",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1192002479774425088/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 268,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							120,
+							67
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EIrX7ptWwAEvknC.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1191983772922785800,
+		"in_reply_to_status_id_str": "1191983772922785792",
+		"in_reply_to_user_id": 6893682,
+		"in_reply_to_user_id_str": "6893682",
+		"in_reply_to_screen_name": "bramkoster",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8867,
+			"friends_count": 284,
+			"listed_count": 584,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5498,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60620,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@bramkoster Hebben ze native uit hun hub gepusht via open API's gebaseerd op de A.I. driven businessrules van de persona's die added value bieden voor de core business in de supplychain van de businessunit. \n\nI'll see myself out... https://t.co/3ExDgN6dXf"
+	},
+	{
 		"created_at": "Wed Nov 06 06:37:22 +0000 2019",
 		"id": 1191967793253621800,
 		"id_str": "1191967793253621760",
