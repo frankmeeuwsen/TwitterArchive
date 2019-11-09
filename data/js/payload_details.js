b759b87daade87ef4d82abf75f20f79e1919ad1f
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-11-09 23:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70521,
-	"created_at": "Sat Nov 09 08:21:04 +0000 2019",
+	"tweets": 70522,
+	"created_at": "Sat Nov 09 22:21:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,261 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 09 22:20:48 +0000 2019",
+		"id": 1193243322225180700,
+		"id_str": "1193292379261943808",
+		"full_text": "\"Say '#okboomer' again! I dare you! I double dare you!\"\n\nBattlestar Galactica, the Tarantino reboot... https://t.co/ggO0euOuY6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "okboomer",
+					"indices": [
+						6,
+						15
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1193243301291405300,
+					"id_str": "1193243301291405313",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/EI9AdeAWsAEb0bD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EI9AdeAWsAEb0bD.jpg",
+					"url": "https://t.co/ggO0euOuY6",
+					"display_url": "pic.twitter.com/ggO0euOuY6",
+					"expanded_url": "https://twitter.com/PulpLibrarian/status/1193243322225180673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1245,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 778,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 441,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 508,
+									"y": 163,
+									"h": 196,
+									"w": 196
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 508,
+									"y": 163,
+									"h": 196,
+									"w": 196
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 317,
+									"y": 101,
+									"h": 122,
+									"w": 122
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 179,
+									"y": 57,
+									"h": 69,
+									"w": 69
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1193243301291405300,
+					"id_str": "1193243301291405313",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/EI9AdeAWsAEb0bD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EI9AdeAWsAEb0bD.jpg",
+					"url": "https://t.co/ggO0euOuY6",
+					"display_url": "pic.twitter.com/ggO0euOuY6",
+					"expanded_url": "https://twitter.com/PulpLibrarian/status/1193243322225180673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1245,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 778,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 441,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 508,
+									"y": 163,
+									"h": 196,
+									"w": 196
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 508,
+									"y": 163,
+									"h": 196,
+									"w": 196
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 317,
+									"y": 101,
+									"h": 122,
+									"w": 122
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 179,
+									"y": 57,
+									"h": 69,
+									"w": 69
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2928209793,
+			"id_str": "2928209793",
+			"name": "Pulp Librarian",
+			"screen_name": "PulpLibrarian",
+			"location": "A library of the mind",
+			"description": "Curator of the art, history and fiction of old dreams.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 100341,
+			"friends_count": 5264,
+			"listed_count": 2001,
+			"created_at": "Wed Dec 17 22:09:14 +0000 2014",
+			"favourites_count": 10937,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 38068,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152213205432840193/Z9ujL38W_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152213205432840193/Z9ujL38W_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2928209793/1563544010",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17,
+		"favorite_count": 99,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Say '#okboomer' again! I dare you! I double dare you!\"\n\nBattlestar Galactica, the Tarantino reboot... https://t.co/ggO0euOuY6"
+	},
+	{
 		"created_at": "Sat Nov 09 07:30:39 +0000 2019",
 		"id": 1192951517285736400,
 		"id_str": "1193068368582127616",
