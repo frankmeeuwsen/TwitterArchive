259ace4b48d176554fd142a74529368d84ebdcde
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-11-19 19:21:06 +0100

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,396 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Tue Nov 19 17:53:57 +0000 2019",
+		"id": 1196849103067603000,
+		"id_str": "1196849103067602945",
+		"full_text": "@PhotoJeanique kun jij helpen? https://t.co/OaXPfEtgYX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			30
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PhotoJeanique",
+					"name": "Jeanique✨ZP is racisme",
+					"id": 14224758,
+					"id_str": "14224758",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OaXPfEtgYX",
+					"expanded_url": "https://twitter.com/youdid/status/1196796305324068871",
+					"display_url": "twitter.com/youdid/status/…",
+					"indices": [
+						31,
+						54
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 14224758,
+		"in_reply_to_user_id_str": "14224758",
+		"in_reply_to_screen_name": "PhotoJeanique",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8870,
+			"friends_count": 293,
+			"listed_count": 583,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5532,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60696,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1196796305324068900,
+		"quoted_status_id_str": "1196796305324068871",
+		"quoted_status_permalink": {
+			"url": "https://t.co/OaXPfEtgYX",
+			"expanded": "https://twitter.com/youdid/status/1196796305324068871",
+			"display": "twitter.com/youdid/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Nov 19 14:24:09 +0000 2019",
+			"id": 1196796305324068900,
+			"id_str": "1196796305324068871",
+			"full_text": "Ok guys, @sabinavanwijk en ik zijn op zoek naar hanenveren. Weet iemand een plek waar we die kunnen krijgen voor een leuk prijsje? (vreemdste vraag van de dag dit 😂) https://t.co/hjVzlv0uDL",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				165
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "sabinavanwijk",
+						"name": "Sabina van Wijk",
+						"id": 18072669,
+						"id_str": "18072669",
+						"indices": [
+							9,
+							23
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1196796299082948600,
+						"id_str": "1196796299082948616",
+						"indices": [
+							166,
+							189
+						],
+						"media_url": "http://pbs.twimg.com/media/EJvf5KNXYAgvDBf.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EJvf5KNXYAgvDBf.jpg",
+						"url": "https://t.co/hjVzlv0uDL",
+						"display_url": "pic.twitter.com/hjVzlv0uDL",
+						"expanded_url": "https://twitter.com/youdid/status/1196796305324068871/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"medium": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"orig": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1196796299082948600,
+						"id_str": "1196796299082948616",
+						"indices": [
+							166,
+							189
+						],
+						"media_url": "http://pbs.twimg.com/media/EJvf5KNXYAgvDBf.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EJvf5KNXYAgvDBf.jpg",
+						"url": "https://t.co/hjVzlv0uDL",
+						"display_url": "pic.twitter.com/hjVzlv0uDL",
+						"expanded_url": "https://twitter.com/youdid/status/1196796305324068871/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"medium": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"orig": {
+								"faces": []
+							}
+						}
+					},
+					{
+						"id": 1196796299087102000,
+						"id_str": "1196796299087101952",
+						"indices": [
+							166,
+							189
+						],
+						"media_url": "http://pbs.twimg.com/media/EJvf5KOWwAA86KK.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EJvf5KOWwAA86KK.jpg",
+						"url": "https://t.co/hjVzlv0uDL",
+						"display_url": "pic.twitter.com/hjVzlv0uDL",
+						"expanded_url": "https://twitter.com/youdid/status/1196796305324068871/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 401,
+								"h": 600,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"medium": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"orig": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 12561762,
+				"id_str": "12561762",
+				"name": "Judith",
+				"screen_name": "youdid",
+				"location": "Rotterdam, The Netherlands",
+				"description": "✨ I don’t follow rules, I follow memes on social media",
+				"url": "https://t.co/DzRSvT8zNi",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/DzRSvT8zNi",
+								"expanded_url": "http://www.instagram.com/youdid",
+								"display_url": "instagram.com/youdid",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2533,
+				"friends_count": 332,
+				"listed_count": 75,
+				"created_at": "Tue Jan 22 20:47:14 +0000 2008",
+				"favourites_count": 14477,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4537,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "DEDEDE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1173188634943610886/6GPRkITR_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1173188634943610886/6GPRkITR_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/12561762/1556552657",
+				"profile_link_color": "EB0400",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E8E5DF",
+				"profile_text_color": "636363",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@PhotoJeanique kun jij helpen? https://t.co/OaXPfEtgYX"
+	},
+	{
 		"created_at": "Tue Nov 19 12:39:33 +0000 2019",
 		"id": 1196769981297549300,
 		"id_str": "1196769981297549314",
