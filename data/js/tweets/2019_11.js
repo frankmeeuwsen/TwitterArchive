d7f2f116ffb33fe572d60171350fbc9c2ece9bca
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-11-22 08:21:06 +0100

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 22 07:15:42 +0000 2019",
+		"id": 1197775646430244900,
+		"id_str": "1197775646430244864",
+		"full_text": "Och jongens toch. Met mijn ochtendkoffie eerst al Guns 'n' Roses Sweet Child O' Mine (corrected spelling galore!) En nu Epic van Faith No More!  #kink1500 https://t.co/qH8OyXBTAh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "kink1500",
+					"indices": [
+						145,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1197775634497441800,
+					"id_str": "1197775634497441792",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EJ9al-7WkAAdIre.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EJ9al-7WkAAdIre.jpg",
+					"url": "https://t.co/qH8OyXBTAh",
+					"display_url": "pic.twitter.com/qH8OyXBTAh",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1197775646430244864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 330,
+							"h": 204,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 330,
+							"h": 204,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 330,
+							"h": 204,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1197775634497441800,
+					"id_str": "1197775634497441792",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EJ9al-7WkAAdIre.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EJ9al-7WkAAdIre.jpg",
+					"url": "https://t.co/qH8OyXBTAh",
+					"display_url": "pic.twitter.com/qH8OyXBTAh",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1197775646430244864/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 330,
+							"h": 204,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 330,
+							"h": 204,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 330,
+							"h": 204,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							55,
+							34
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EJ9al-7WkAAdIre.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8866,
+			"friends_count": 293,
+			"listed_count": 583,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5545,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60716,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Och jongens toch. Met mijn ochtendkoffie eerst al Guns 'n' Roses Sweet Child O' Mine (corrected spelling galore!) En nu Epic van Faith No More!  #kink1500 https://t.co/qH8OyXBTAh"
+	},
+	{
 		"created_at": "Fri Nov 22 06:07:39 +0000 2019",
 		"id": 1197758520948600800,
 		"id_str": "1197758520948600832",
