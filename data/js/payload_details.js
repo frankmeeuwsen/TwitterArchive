25ed0d223ab864966646494e7e97c9d176128c21
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-11-30 08:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70617,
-	"created_at": "Fri Nov 29 19:21:05 +0000 2019",
+	"tweets": 70618,
+	"created_at": "Sat Nov 30 07:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 30 07:04:51 +0000 2019",
+		"id": 1200334432768577500,
+		"id_str": "1200672018015883266",
+		"full_text": "Ik heb niet zoveel volgers, maar als er eentje tussen zit die die retweet ben ik al blij, want dit is één van de mooiste columns die ik ooit gelezen heb met een prachtboodschap!. #wij-wij https://t.co/mFu8BhpJDw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			187
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "wij",
+					"indices": [
+						179,
+						183
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1200334427102077000,
+					"id_str": "1200334427102076928",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/EKhxzUMXYAATknP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKhxzUMXYAATknP.jpg",
+					"url": "https://t.co/mFu8BhpJDw",
+					"display_url": "pic.twitter.com/mFu8BhpJDw",
+					"expanded_url": "https://twitter.com/RotzakKleine/status/1200334432768577537/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 960,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200334427102077000,
+					"id_str": "1200334427102076928",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/EKhxzUMXYAATknP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKhxzUMXYAATknP.jpg",
+					"url": "https://t.co/mFu8BhpJDw",
+					"display_url": "pic.twitter.com/mFu8BhpJDw",
+					"expanded_url": "https://twitter.com/RotzakKleine/status/1200334432768577537/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 960,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1024714445942079500,
+			"id_str": "1024714445942079488",
+			"name": "Kleine-Rotzak",
+			"screen_name": "RotzakKleine",
+			"location": "",
+			"description": "Stuiterbal pur sang. Houdt van wijn, whisky en water. Heeft een hekel aan zuurkool, de bus, en aan veel mensen. Vindt vaak ergens niets van..",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 74,
+			"friends_count": 57,
+			"listed_count": 0,
+			"created_at": "Wed Aug 01 17:52:16 +0000 2018",
+			"favourites_count": 222,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 358,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1151578295726694401/s34j5X0R_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1151578295726694401/s34j5X0R_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1024714445942079488/1563392636",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 566,
+		"favorite_count": 855,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ik heb niet zoveel volgers, maar als er eentje tussen zit die die retweet ben ik al blij, want dit is één van de mooiste columns die ik ooit gelezen heb met een prachtboodschap!. #wij-wij https://t.co/mFu8BhpJDw"
+	},
+	{
 		"created_at": "Fri Nov 29 19:09:56 +0000 2019",
 		"id": 1200484343623438300,
 		"id_str": "1200492104000163840",
