d7aa2ba9a1e88b606001a16d9a2b02417ba067d3
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-12-23 19:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70704,
-	"created_at": "Fri Dec 20 19:21:04 +0000 2019",
+	"tweets": 70705,
+	"created_at": "Mon Dec 23 18:21:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 23 18:10:07 +0000 2019",
+		"id": 1209174359404880000,
+		"id_str": "1209174359404879872",
+		"full_text": "Als iemand van John Grisham achtige boeken houdt, wat raadt je dan aan als cadeau? Behalve boeken van John Grisham uiteraard. https://t.co/rMRqunHKzR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1209174334335602700,
+					"id_str": "1209174334335602690",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EMfZpo2X0AIsS6D.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EMfZpo2X0AIsS6D.jpg",
+					"url": "https://t.co/rMRqunHKzR",
+					"display_url": "pic.twitter.com/rMRqunHKzR",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1209174359404879872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 380,
+							"h": 286,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 380,
+							"h": 286,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 380,
+							"h": 286,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1209174334335602700,
+					"id_str": "1209174334335602690",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EMfZpo2X0AIsS6D.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EMfZpo2X0AIsS6D.jpg",
+					"url": "https://t.co/rMRqunHKzR",
+					"display_url": "pic.twitter.com/rMRqunHKzR",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1209174359404879872/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 380,
+							"h": 286,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 380,
+							"h": 286,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 380,
+							"h": 286,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							190,
+							143
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EMfZpo2X0AIsS6D.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8866,
+			"friends_count": 315,
+			"listed_count": 584,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5649,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60872,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Als iemand van John Grisham achtige boeken houdt, wat raadt je dan aan als cadeau? Behalve boeken van John Grisham uiteraard. https://t.co/rMRqunHKzR"
+	},
+	{
 		"created_at": "Fri Dec 20 19:05:09 +0000 2019",
 		"id": 1208101044401651700,
 		"id_str": "1208101044401651714",
