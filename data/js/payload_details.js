c1d23d211976743293800fb969dcb7590f5af7bc
Frank Meeuwsen <frank@incredibleadventure.nl>
2019-12-31 13:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70739,
-	"created_at": "Tue Dec 31 11:21:05 +0000 2019",
+	"tweets": 70740,
+	"created_at": "Tue Dec 31 12:21:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,347 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Tue Dec 31 11:57:03 +0000 2019",
+		"id": 1211979579658850300,
+		"id_str": "1211979579658850309",
+		"full_text": "Bewegen de blokken naar links of naar rechts? Is het grijs of wit? Hoor je Yanny of Laurel? Gaan ze met de klok mee of tegen? https://t.co/JJ4hyxKByn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JJ4hyxKByn",
+					"expanded_url": "https://twitter.com/ky0ju_art/status/1211753182667296769",
+					"display_url": "twitter.com/ky0ju_art/stat…",
+					"indices": [
+						126,
+						149
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8870,
+			"friends_count": 316,
+			"listed_count": 584,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5691,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 60916,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1211753182667296800,
+		"quoted_status_id_str": "1211753182667296769",
+		"quoted_status_permalink": {
+			"url": "https://t.co/JJ4hyxKByn",
+			"expanded": "https://twitter.com/ky0ju_art/status/1211753182667296769",
+			"display": "twitter.com/ky0ju_art/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Dec 30 20:57:26 +0000 2019",
+			"id": 1211753182667296800,
+			"id_str": "1211753182667296769",
+			"full_text": "多義図形ループ\n\n#p5js #Processing #javascript https://t.co/s4k6yIdWJd",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				38
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "p5js",
+						"indices": [
+							9,
+							14
+						]
+					},
+					{
+						"text": "Processing",
+						"indices": [
+							15,
+							26
+						]
+					},
+					{
+						"text": "javascript",
+						"indices": [
+							27,
+							38
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1211753138258006000,
+						"id_str": "1211753138258006016",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1211753138258006016/pu/img/YgIxI4Re1xVUxDyf.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1211753138258006016/pu/img/YgIxI4Re1xVUxDyf.jpg",
+						"url": "https://t.co/s4k6yIdWJd",
+						"display_url": "pic.twitter.com/s4k6yIdWJd",
+						"expanded_url": "https://twitter.com/ky0ju_art/status/1211753182667296769/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"features": {}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1211753138258006000,
+						"id_str": "1211753138258006016",
+						"indices": [
+							39,
+							62
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1211753138258006016/pu/img/YgIxI4Re1xVUxDyf.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1211753138258006016/pu/img/YgIxI4Re1xVUxDyf.jpg",
+						"url": "https://t.co/s4k6yIdWJd",
+						"display_url": "pic.twitter.com/s4k6yIdWJd",
+						"expanded_url": "https://twitter.com/ky0ju_art/status/1211753182667296769/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 14400,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1211753138258006016/pu/vid/480x480/F1ypunYEYKv0Z9j2.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1211753138258006016/pu/pl/Dp665v3_0verYWwc.m3u8?tag=10"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1211753138258006016/pu/vid/320x320/gt4jcWcTo8CAeXH7.mp4?tag=10"
+								},
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1211753138258006016/pu/vid/720x720/ZCXt26S5tPItBBl0.mp4?tag=10"
+								}
+							]
+						},
+						"features": {},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1135086851197300700,
+				"id_str": "1135086851197300737",
+				"name": "ky0ju",
+				"screen_name": "ky0ju_art",
+				"location": "愛知 名古屋市 中区",
+				"description": "【note】https://t.co/WZLdbNXTWU＿＿＿未経験からIT会社でプログラマ・ディレクターをしています。24歳です。I am a programmer, project manager. 24yo  Processing / unity / UE4",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/WZLdbNXTWU",
+								"expanded_url": "http://note.mu/ky0ju",
+								"display_url": "note.mu/ky0ju",
+								"indices": [
+									6,
+									29
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 437,
+				"friends_count": 161,
+				"listed_count": 13,
+				"created_at": "Sun Jun 02 07:32:48 +0000 2019",
+				"favourites_count": 419,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1098,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1146687820498522113/1L7OsVBp_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146687820498522113/1L7OsVBp_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 62,
+			"favorite_count": 255,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "ja"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Bewegen de blokken naar links of naar rechts? Is het grijs of wit? Hoor je Yanny of Laurel? Gaan ze met de klok mee of tegen? https://t.co/JJ4hyxKByn"
+	},
+	{
 		"created_at": "Tue Dec 31 10:28:40 +0000 2019",
 		"id": 1211957336337727500,
 		"id_str": "1211957336337727488",
