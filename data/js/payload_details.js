b148e940c9028b6b524bd8b48a041faedaef6369
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-01-21 21:21:09 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70839,
-	"created_at": "Tue Jan 21 08:21:06 +0000 2020",
+	"tweets": 70841,
+	"created_at": "Tue Jan 21 20:21:08 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,539 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 21 20:01:45 +0000 2020",
+		"id": 1219711700187975700,
+		"id_str": "1219711700187975681",
+		"full_text": "Hoe zit dat met de redactie bij @DeslimstemensNL? Heel vaak komt bij een vraag een obscure link met een kandidaat. Worden die vooraf doorgezaagd op dat soort weetjes en daar de vragen omheen verzameld? Of is het anders? https://t.co/N7AXSYpJdJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			219
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DeslimstemensNL",
+					"name": "DeslimstemensNL",
+					"id": 702771146,
+					"id_str": "702771146",
+					"indices": [
+						32,
+						48
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1219711688166998000,
+					"id_str": "1219711688166998016",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EO1JUW2WAAA7QjK.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EO1JUW2WAAA7QjK.jpg",
+					"url": "https://t.co/N7AXSYpJdJ",
+					"display_url": "pic.twitter.com/N7AXSYpJdJ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1219711700187975681/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1219711688166998000,
+					"id_str": "1219711688166998016",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EO1JUW2WAAA7QjK.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EO1JUW2WAAA7QjK.jpg",
+					"url": "https://t.co/N7AXSYpJdJ",
+					"display_url": "pic.twitter.com/N7AXSYpJdJ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1219711700187975681/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 278,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							249,
+							139
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EO1JUW2WAAA7QjK.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8867,
+			"friends_count": 335,
+			"listed_count": 582,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5743,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61040,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Hoe zit dat met de redactie bij @DeslimstemensNL? Heel vaak komt bij een vraag een obscure link met een kandidaat. Worden die vooraf doorgezaagd op dat soort weetjes en daar de vragen omheen verzameld? Of is het anders? https://t.co/N7AXSYpJdJ"
+	},
+	{
+		"created_at": "Tue Jan 21 19:53:07 +0000 2020",
+		"id": 1219709529044201500,
+		"id_str": "1219709529044201472",
+		"full_text": "Whole trains zijn terug in New York. Recent in Nederand en Duitsland ook al. Opvallend. https://t.co/NaNdNmqOzz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NaNdNmqOzz",
+					"expanded_url": "https://twitter.com/nycpba/status/1219455919441969152",
+					"display_url": "twitter.com/nycpba/status/…",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8867,
+			"friends_count": 335,
+			"listed_count": 582,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5743,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61040,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1219455919441969200,
+		"quoted_status_id_str": "1219455919441969152",
+		"quoted_status_permalink": {
+			"url": "https://t.co/NaNdNmqOzz",
+			"expanded": "https://twitter.com/nycpba/status/1219455919441969152",
+			"display": "twitter.com/nycpba/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 21 03:05:22 +0000 2020",
+			"id": 1219455919441969200,
+			"id_str": "1219455919441969152",
+			"full_text": "The 70s &amp; 80s, now in living color on a subway platform near you. A true sign of decay, one that we worked so hard to eradicate decades ago. The taggers had plenty of time to cover this entire train, because they know there are no more consequences. #backtothefuture https://t.co/7uWmg8YdzU",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				270
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "backtothefuture",
+						"indices": [
+							254,
+							270
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1219411349622677500,
+						"id_str": "1219411349622677504",
+						"indices": [
+							271,
+							294
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1219411349622677504/pu/img/tMYFbLc8KuEGxk0q.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1219411349622677504/pu/img/tMYFbLc8KuEGxk0q.jpg",
+						"url": "https://t.co/7uWmg8YdzU",
+						"display_url": "pic.twitter.com/7uWmg8YdzU",
+						"expanded_url": "https://twitter.com/NYCPBA/status/1219455919441969152/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"features": {}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1219411349622677500,
+						"id_str": "1219411349622677504",
+						"indices": [
+							271,
+							294
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1219411349622677504/pu/img/tMYFbLc8KuEGxk0q.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1219411349622677504/pu/img/tMYFbLc8KuEGxk0q.jpg",
+						"url": "https://t.co/7uWmg8YdzU",
+						"display_url": "pic.twitter.com/7uWmg8YdzU",
+						"expanded_url": "https://twitter.com/NYCPBA/status/1219455919441969152/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 56498,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1219411349622677504/pu/vid/480x480/T5A2yNU0YO9BeBP_.mp4?tag=10"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1219411349622677504/pu/vid/320x320/UyzbuEhMy1lYBzoG.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1219411349622677504/pu/pl/oW1A4K3eTECZbShA.m3u8?tag=10"
+								},
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1219411349622677504/pu/vid/720x720/A6JbWCYlaxtkOg5s.mp4?tag=10"
+								}
+							]
+						},
+						"features": {},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 117096653,
+				"id_str": "117096653",
+				"name": "NYC PBA",
+				"screen_name": "NYCPBA",
+				"location": "NYC",
+				"description": "The Police Benevolent Association of the City of New York, which represents over 50,000 active and retired New York City Police Officers",
+				"url": "https://t.co/TDp5jReq6E",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/TDp5jReq6E",
+								"expanded_url": "http://nycpba.org",
+								"display_url": "nycpba.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16346,
+				"friends_count": 1094,
+				"listed_count": 93,
+				"created_at": "Wed Feb 24 14:36:49 +0000 2010",
+				"favourites_count": 1292,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 3481,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1186271041829724161/ICKX4457_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1186271041829724161/ICKX4457_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/117096653/1547040116",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1067,
+			"favorite_count": 3881,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Whole trains zijn terug in New York. Recent in Nederand en Duitsland ook al. Opvallend. https://t.co/NaNdNmqOzz"
+	},
+	{
 		"created_at": "Tue Jan 21 07:42:22 +0000 2020",
 		"id": 1219525628283756500,
 		"id_str": "1219525628283756544",
