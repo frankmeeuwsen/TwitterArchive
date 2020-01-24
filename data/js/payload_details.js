71417849f9d6b3be2e87008f409849e5ea943439
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-01-24 18:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70855,
-	"created_at": "Fri Jan 24 12:21:06 +0000 2020",
+	"tweets": 70856,
+	"created_at": "Fri Jan 24 17:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,314 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Fri Jan 24 17:20:10 +0000 2020",
+		"id": 1220758201823645700,
+		"id_str": "1220758201823645701",
+		"full_text": "Reacties op Pearl Jam’s nieuwe dance-achtige single be like... https://t.co/AHcxaXe7Ir",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1220758197704843300,
+					"id_str": "1220758197704843271",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/EPEBHOrX4AcCQil.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPEBHOrX4AcCQil.jpg",
+					"url": "https://t.co/AHcxaXe7Ir",
+					"display_url": "pic.twitter.com/AHcxaXe7Ir",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1220758201823645701/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 639,
+							"h": 381,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 639,
+							"h": 381,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 639,
+							"h": 381,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1220758197704843300,
+					"id_str": "1220758197704843271",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/EPEBHOrX4AcCQil.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPEBHOrX4AcCQil.jpg",
+					"url": "https://t.co/AHcxaXe7Ir",
+					"display_url": "pic.twitter.com/AHcxaXe7Ir",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1220758201823645701/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 639,
+							"h": 381,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 639,
+							"h": 381,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 639,
+							"h": 381,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 190,
+									"y": 48,
+									"h": 120,
+									"w": 120
+								},
+								{
+									"x": 38,
+									"y": 81,
+									"h": 147,
+									"w": 147
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "schrijft en doet dingen over de impact van tech op mens en maatschappij. #TeamHuman | #technoUtopian aldus The New Yorker. #PearlJam. Doet verhalen bij Kaliber",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8868,
+			"friends_count": 343,
+			"listed_count": 0,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5751,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61060,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Reacties op Pearl Jam’s nieuwe dance-achtige single be like... https://t.co/AHcxaXe7Ir"
+	},
+	{
 		"created_at": "Fri Jan 24 12:04:15 +0000 2020",
 		"id": 1220678696928972800,
 		"id_str": "1220678696928972800",
