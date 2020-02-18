e2b42ab4343db96ce821384eb50626fd2f5aca03
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-02-18 08:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70955,
-	"created_at": "Tue Feb 18 06:21:05 +0000 2020",
+	"tweets": 70956,
+	"created_at": "Tue Feb 18 07:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 18 06:34:40 +0000 2020",
+		"id": 1229655454814556200,
+		"id_str": "1229655454814556160",
+		"full_text": "Add to number 4: we have this profession called Mobile Bike Repair(wo)man. While you stay at work, the repairman visits you and fixes your bike. Easy! https://t.co/4vsZ8WVxKp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4vsZ8WVxKp",
+					"expanded_url": "https://twitter.com/gary_fisher/status/1229499542707392513",
+					"display_url": "twitter.com/gary_fisher/st…",
+					"indices": [
+						151,
+						174
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8880,
+			"friends_count": 377,
+			"listed_count": 581,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5832,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61182,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1229499542707392500,
+		"quoted_status_id_str": "1229499542707392513",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4vsZ8WVxKp",
+			"expanded": "https://twitter.com/gary_fisher/status/1229499542707392513",
+			"display": "twitter.com/gary_fisher/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Feb 17 20:15:08 +0000 2020",
+			"id": 1229499542707392500,
+			"id_str": "1229499542707392513",
+			"full_text": "Because 35% of Americans have a very hard time getting in and out of a car, they could not imagine riding on or in any thing labeled \"bike\". Our big job is to take these folks by the hand and show them it's possible. Naysayers: You Can’t Do THAT On A Bike! https://t.co/6yB0aaTpHU",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/6yB0aaTpHU",
+						"expanded_url": "http://disq.us/t/3m67028",
+						"display_url": "disq.us/t/3m67028",
+						"indices": [
+							257,
+							280
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 21780251,
+				"id_str": "21780251",
+				"name": "Gary Fisher",
+				"screen_name": "Gary_Fisher",
+				"location": "San Francisco, USA",
+				"description": "Started racing  road and track as a small boy,  founded a company named MountainBikes in 1979,  still riding, making, and talking about bikes.",
+				"url": "https://t.co/LCcbaxQ3p7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/LCcbaxQ3p7",
+								"expanded_url": "https://www.facebook.com/gary.fisher.16100",
+								"display_url": "facebook.com/gary.fisher.16…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 42516,
+				"friends_count": 2114,
+				"listed_count": 1626,
+				"created_at": "Tue Feb 24 18:34:11 +0000 2009",
+				"favourites_count": 1646,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 16139,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "361627",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/101334899/gary_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/101334899/gary_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/21780251/1404820912",
+				"profile_link_color": "A48251",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "0633E0",
+				"profile_text_color": "F9130B",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 14,
+			"favorite_count": 70,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Add to number 4: we have this profession called Mobile Bike Repair(wo)man. While you stay at work, the repairman visits you and fixes your bike. Easy! https://t.co/4vsZ8WVxKp"
+	},
+	{
 		"created_at": "Tue Feb 18 06:10:34 +0000 2020",
 		"id": 1229649386826862600,
 		"id_str": "1229649386826862594",
