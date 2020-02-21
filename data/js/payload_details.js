8f6b7d536cb169c35687d3827447a09a75702077
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-02-21 21:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70967,
-	"created_at": "Fri Feb 21 16:21:06 +0000 2020",
+	"tweets": 70968,
+	"created_at": "Fri Feb 21 20:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Fri Feb 21 19:30:39 +0000 2020",
+		"id": 1230937899396407300,
+		"id_str": "1230937899396407296",
+		"full_text": "Ik denk dat ik alsnog naar 't Kielegat afzak voor carnaval. Verkleed als KOZP-controleur. Ga ik wat dierbare vrienden en vriendinnen onheus bejegenen. Hoe zou zo'n KOZP-veldwachter er uit zien eigenlijk? https://t.co/PuJKY5w5pr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			203
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1230937891649605600,
+					"id_str": "1230937891649605638",
+					"indices": [
+						204,
+						227
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/ERUrfZxX0AYeSql.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/ERUrfZxX0AYeSql.jpg",
+					"url": "https://t.co/PuJKY5w5pr",
+					"display_url": "pic.twitter.com/PuJKY5w5pr",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1230937899396407296/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 218,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 218,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 218,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1230937891649605600,
+					"id_str": "1230937891649605638",
+					"indices": [
+						204,
+						227
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/ERUrfZxX0AYeSql.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/ERUrfZxX0AYeSql.jpg",
+					"url": "https://t.co/PuJKY5w5pr",
+					"display_url": "pic.twitter.com/PuJKY5w5pr",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1230937899396407296/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 218,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 218,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 218,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							240,
+							109
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/ERUrfZxX0AYeSql.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8883,
+			"friends_count": 386,
+			"listed_count": 582,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5845,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61197,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1564177319",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ik denk dat ik alsnog naar 't Kielegat afzak voor carnaval. Verkleed als KOZP-controleur. Ga ik wat dierbare vrienden en vriendinnen onheus bejegenen. Hoe zou zo'n KOZP-veldwachter er uit zien eigenlijk? https://t.co/PuJKY5w5pr"
+	},
+	{
 		"created_at": "Fri Feb 21 15:30:15 +0000 2020",
 		"id": 1230877400893984800,
 		"id_str": "1230877400893984769",
