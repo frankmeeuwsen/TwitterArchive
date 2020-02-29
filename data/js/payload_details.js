90d56f7a106c9205858d043c4ab8ce0780cc02e9
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-02-29 21:21:09 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71004,
-	"created_at": "Sat Feb 29 11:21:05 +0000 2020",
+	"tweets": 71005,
+	"created_at": "Sat Feb 29 20:21:07 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sat Feb 29 20:09:34 +0000 2020",
+		"id": 1233846796746707000,
+		"id_str": "1233846796746706944",
+		"full_text": "@gill_works Built with @airtable @webflow @integromat and some Stout craftbeers. 😉🍻",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			83
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gill_works",
+					"name": "Michael Gill 💡🚀💰",
+					"id": 86678551,
+					"id_str": "86678551",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "airtable",
+					"name": "Airtable",
+					"id": 2786252385,
+					"id_str": "2786252385",
+					"indices": [
+						23,
+						32
+					]
+				},
+				{
+					"screen_name": "webflow",
+					"name": "Webflow",
+					"id": 403930194,
+					"id_str": "403930194",
+					"indices": [
+						33,
+						41
+					]
+				},
+				{
+					"screen_name": "integromat",
+					"name": "Integromat",
+					"id": 2214615696,
+					"id_str": "2214615696",
+					"indices": [
+						42,
+						53
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1233846390033440800,
+		"in_reply_to_status_id_str": "1233846390033440768",
+		"in_reply_to_user_id": 816203,
+		"in_reply_to_user_id_str": "816203",
+		"in_reply_to_screen_name": "frankmeeuwsen",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8890,
+			"friends_count": 389,
+			"listed_count": 582,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5873,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61241,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1582409873",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@gill_works Built with @airtable @webflow @integromat and some Stout craftbeers. 😉🍻"
+	},
+	{
 		"created_at": "Sat Feb 29 10:54:41 +0000 2020",
 		"id": 1233374643354062800,
 		"id_str": "1233707153254486016",
@@ -36,7 +182,7 @@ Grailbird.data.tweets_2020_02 =
 			"name": "Jarjour || Go buy a news subscription, friend.",
 			"screen_name": "derjarjour",
 			"location": "Amsterdam, The Netherlands",
-			"description": "Head of publisher relations @revue • product + journalism • optimist • none of my opinions are my own • past: @NZZ, @blendle, @srf 🏳️‍🌈• 📸Mirjam Kluka",
+			"description": "Head of publisher relations @revue • product + journalism • optimist • none of my opinions are my own • past: @NZZ, @blendle, @srf 🏳️🌈• 📸Mirjam Kluka",
 			"url": "https://t.co/jg1SpHZpUr",
 			"entities": {
 				"url": {
