029cf8e4700a87b292237b6fd8a28bf74201ea64
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-03-11 20:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71057,
-	"created_at": "Wed Mar 11 15:21:06 +0000 2020",
+	"tweets": 71058,
+	"created_at": "Wed Mar 11 19:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 11 18:39:12 +0000 2020",
+		"id": 1237810321882075100,
+		"id_str": "1237810321882075141",
+		"full_text": "Twitter is vandaag weer uitgespeeld hoor mensen. Even opruimen, terug in de kast en morgen een nieuw potje. https://t.co/kYchRWGuMy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			107
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kYchRWGuMy",
+					"expanded_url": "https://twitter.com/richardmarx/status/1237647398853152768",
+					"display_url": "twitter.com/richardmarx/st…",
+					"indices": [
+						108,
+						131
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8894,
+			"friends_count": 393,
+			"listed_count": 578,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 5906,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61307,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1009515918618189826/q3HWxJuD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1582409873",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1237647398853152800,
+		"quoted_status_id_str": "1237647398853152768",
+		"quoted_status_permalink": {
+			"url": "https://t.co/kYchRWGuMy",
+			"expanded": "https://twitter.com/richardmarx/status/1237647398853152768",
+			"display": "twitter.com/richardmarx/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 11 07:51:48 +0000 2020",
+			"id": 1237647398853152800,
+			"id_str": "1237647398853152768",
+			"full_text": "The ones your girl fucked to before she met you. https://t.co/n6whpEPXrK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				48
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/n6whpEPXrK",
+						"expanded_url": "https://twitter.com/bob_tona/status/1237646804201734147",
+						"display_url": "twitter.com/bob_tona/statu…",
+						"indices": [
+							49,
+							72
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 32876210,
+				"id_str": "32876210",
+				"name": "Richard Marx",
+				"screen_name": "richardmarx",
+				"location": "All Universes",
+				"description": "Songwriter/Performer/Recording Artist/Producer",
+				"url": "https://t.co/pOCRW3IyPt",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/pOCRW3IyPt",
+								"expanded_url": "http://www.richardmarx.com",
+								"display_url": "richardmarx.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 185206,
+				"friends_count": 111,
+				"listed_count": 1218,
+				"created_at": "Sat Apr 18 13:09:00 +0000 2009",
+				"favourites_count": 37986,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 39232,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1209598798890033152/_KdZN6nG_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1209598798890033152/_KdZN6nG_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/32876210/1581090924",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DCE0CF",
+				"profile_text_color": "646651",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1237646804201734100,
+			"quoted_status_id_str": "1237646804201734147",
+			"quoted_status_permalink": {
+				"url": "https://t.co/n6whpEPXrK",
+				"expanded": "https://twitter.com/bob_tona/status/1237646804201734147",
+				"display": "twitter.com/bob_tona/statu…"
+			},
+			"retweet_count": 2746,
+			"favorite_count": 18167,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Twitter is vandaag weer uitgespeeld hoor mensen. Even opruimen, terug in de kast en morgen een nieuw potje. https://t.co/kYchRWGuMy"
+	},
+	{
 		"created_at": "Wed Mar 11 14:53:24 +0000 2020",
 		"id": 1237753494838415400,
 		"id_str": "1237753494838415365",
