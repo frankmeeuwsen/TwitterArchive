fb405a0610d61bdb05437e1efc50e1be5b1ad8e4
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-03-22 19:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71186,
-	"created_at": "Sun Mar 22 17:21:06 +0000 2020",
+	"tweets": 71187,
+	"created_at": "Sun Mar 22 18:21:07 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 22 18:16:07 +0000 2020",
+		"id": 1241789001259942000,
+		"id_str": "1241790776956329986",
+		"full_text": "Wie is ziek of werkt in een vitaal beroep? Ik heb twee heerlijke vegetarische burrito’s voor je gemaakt. Vandaag opeten of na een dienst deze week. Ophalen #Sterrenberg of ik zet het voor je deur #Zeist #HuisterHeide. Wie maak ik blij? https://t.co/Lxq4BN9SY6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			235
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Sterrenberg",
+					"indices": [
+						156,
+						168
+					]
+				},
+				{
+					"text": "Zeist",
+					"indices": [
+						196,
+						202
+					]
+				},
+				{
+					"text": "HuisterHeide",
+					"indices": [
+						203,
+						216
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1241788989314674700,
+					"id_str": "1241788989314674688",
+					"indices": [
+						236,
+						259
+					],
+					"media_url": "http://pbs.twimg.com/media/ETu4gaoXsAArlFU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETu4gaoXsAArlFU.jpg",
+					"url": "https://t.co/Lxq4BN9SY6",
+					"display_url": "pic.twitter.com/Lxq4BN9SY6",
+					"expanded_url": "https://twitter.com/suzanneunck/status/1241789001259941899/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1861,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1090,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 618,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1241788989314674700,
+					"id_str": "1241788989314674688",
+					"indices": [
+						236,
+						259
+					],
+					"media_url": "http://pbs.twimg.com/media/ETu4gaoXsAArlFU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETu4gaoXsAArlFU.jpg",
+					"url": "https://t.co/Lxq4BN9SY6",
+					"display_url": "pic.twitter.com/Lxq4BN9SY6",
+					"expanded_url": "https://twitter.com/suzanneunck/status/1241789001259941899/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1861,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1090,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 618,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9883922,
+			"id_str": "9883922",
+			"name": "☀️ Suzanne Unck ☀️",
+			"screen_name": "suzanneunck",
+			"location": "Huis ter Heide - Utrecht",
+			"description": "https://t.co/vqE1or9FhD | never waste a good crisis | it’s the little things that count",
+			"url": "https://t.co/L5vSAo6afT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L5vSAo6afT",
+							"expanded_url": "http://www.suzanneunck.nl",
+							"display_url": "suzanneunck.nl",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/vqE1or9FhD",
+							"expanded_url": "http://AlliantieAfstandsonderwijs.nl",
+							"display_url": "AlliantieAfstandsonderwijs.nl",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8956,
+			"friends_count": 9327,
+			"listed_count": 345,
+			"created_at": "Fri Nov 02 13:16:16 +0000 2007",
+			"favourites_count": 37728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 149763,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030103",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1221409810589147136/6QRwbQ4U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1221409810589147136/6QRwbQ4U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9883922/1584809865",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "ADF1FC",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "FA8459",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Wie is ziek of werkt in een vitaal beroep? Ik heb twee heerlijke vegetarische burrito’s voor je gemaakt. Vandaag opeten of na een dienst deze week. Ophalen #Sterrenberg of ik zet het voor je deur #Zeist #HuisterHeide. Wie maak ik blij? https://t.co/Lxq4BN9SY6"
+	},
+	{
 		"created_at": "Sun Mar 22 16:34:01 +0000 2020",
 		"id": 1241765085225324500,
 		"id_str": "1241765085225324544",
