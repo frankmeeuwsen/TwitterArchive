0e522d262a0943a8ec935f8643ff4710da938fad
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-03-25 09:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71213,
-	"created_at": "Wed Mar 25 07:21:06 +0000 2020",
+	"tweets": 71214,
+	"created_at": "Wed Mar 25 08:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,186 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 25 07:29:41 +0000 2020",
+		"id": 1242710197468086300,
+		"id_str": "1242715262350213120",
+		"full_text": "Support voor personeel @diakhuis en voor de lokale ondernemer\n\n #utrechtOost https://t.co/pVA0DFRenX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "utrechtOost",
+					"indices": [
+						64,
+						76
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "diakhuis",
+					"name": "Diakonessenhuis",
+					"id": 226168697,
+					"id_str": "226168697",
+					"indices": [
+						23,
+						32
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1242710191201812500,
+					"id_str": "1242710191201812482",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/media/ET7-VbFXgAI55Zb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ET7-VbFXgAI55Zb.jpg",
+					"url": "https://t.co/pVA0DFRenX",
+					"display_url": "pic.twitter.com/pVA0DFRenX",
+					"expanded_url": "https://twitter.com/boerdeboer/status/1242710197468086277/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1242710191201812500,
+					"id_str": "1242710191201812482",
+					"indices": [
+						77,
+						100
+					],
+					"media_url": "http://pbs.twimg.com/media/ET7-VbFXgAI55Zb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ET7-VbFXgAI55Zb.jpg",
+					"url": "https://t.co/pVA0DFRenX",
+					"display_url": "pic.twitter.com/pVA0DFRenX",
+					"expanded_url": "https://twitter.com/boerdeboer/status/1242710197468086277/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 11798112,
+			"id_str": "11798112",
+			"name": "boerdeboer -",
+			"screen_name": "boerdeboer",
+			"location": "",
+			"description": "passenger of your timeline , ... AI generated since 2008 ...",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1046,
+			"friends_count": 532,
+			"listed_count": 111,
+			"created_at": "Thu Jan 03 14:10:31 +0000 2008",
+			"favourites_count": 8056,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1275,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1176049091878510592/lRSlKxet_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1176049091878510592/lRSlKxet_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/11798112/1569225837",
+			"profile_link_color": "4A6780",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFF1F2",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Support voor personeel @diakhuis en voor de lokale ondernemer\n\n #utrechtOost https://t.co/pVA0DFRenX"
+	},
+	{
 		"created_at": "Wed Mar 25 06:31:31 +0000 2020",
 		"id": 1242700622371205000,
 		"id_str": "1242700622371205120",
