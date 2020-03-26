9a0b7116b88d3fbabebc3f49ebf82ffc8dca1125
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-03-26 18:21:09 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71227,
-	"created_at": "Thu Mar 26 14:21:07 +0000 2020",
+	"tweets": 71228,
+	"created_at": "Thu Mar 26 17:21:07 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Thu Mar 26 17:19:33 +0000 2020",
+		"id": 1243226094934864000,
+		"id_str": "1243226094934863873",
+		"full_text": "Je hebt slangenleren handen van al het wassen? Check Sudocrem. Werkt als de brandweer. En elke liefhebber van de commandline kan zeggen “$sudo apt-get install crem” https://t.co/C7VkQqngJw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1243226087468998700,
+					"id_str": "1243226087468998658",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUDTiicWoAIYkg6.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUDTiicWoAIYkg6.jpg",
+					"url": "https://t.co/C7VkQqngJw",
+					"display_url": "pic.twitter.com/C7VkQqngJw",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1243226094934863873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 498,
+							"h": 288,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 288,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 288,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1243226087468998700,
+					"id_str": "1243226087468998658",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EUDTiicWoAIYkg6.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EUDTiicWoAIYkg6.jpg",
+					"url": "https://t.co/C7VkQqngJw",
+					"display_url": "pic.twitter.com/C7VkQqngJw",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1243226094934863873/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 498,
+							"h": 288,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 498,
+							"h": 288,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 288,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							83,
+							48
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EUDTiicWoAIYkg6.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8897,
+			"friends_count": 413,
+			"listed_count": 578,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6062,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61519,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1582409873",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Je hebt slangenleren handen van al het wassen? Check Sudocrem. Werkt als de brandweer. En elke liefhebber van de commandline kan zeggen “$sudo apt-get install crem” https://t.co/C7VkQqngJw"
+	},
+	{
 		"created_at": "Thu Mar 26 14:10:38 +0000 2020",
 		"id": 1243178552641667000,
 		"id_str": "1243178552641667073",
