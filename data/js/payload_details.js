fed628ae87e64d95be79af0a830eb18a488e5b6e
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-03-29 21:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71257,
-	"created_at": "Sun Mar 29 18:21:07 +0000 2020",
+	"tweets": 71258,
+	"created_at": "Sun Mar 29 19:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 29 18:41:34 +0000 2020",
+		"id": 1244333896554119200,
+		"id_str": "1244333896554119171",
+		"full_text": "Wij hebben een buschauffeur die Lovejoy heet maar deze wint toch wel... https://t.co/CLEzumO2tO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CLEzumO2tO",
+					"expanded_url": "https://twitter.com/eric_edholm/status/1243981101691031553",
+					"display_url": "twitter.com/eric_edholm/st…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8896,
+			"friends_count": 414,
+			"listed_count": 578,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6077,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61559,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1585350879",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1243981101691031600,
+		"quoted_status_id_str": "1243981101691031553",
+		"quoted_status_permalink": {
+			"url": "https://t.co/CLEzumO2tO",
+			"expanded": "https://twitter.com/eric_edholm/status/1243981101691031553",
+			"display": "twitter.com/eric_edholm/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Mar 28 19:19:41 +0000 2020",
+			"id": 1243981101691031600,
+			"id_str": "1243981101691031553",
+			"full_text": "Find me a better name \n\nDare you https://t.co/PV6MkhX9SQ",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				32
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1243981096750141400,
+						"id_str": "1243981096750141440",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/EUOCN3MX0AAglkJ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EUOCN3MX0AAglkJ.jpg",
+						"url": "https://t.co/PV6MkhX9SQ",
+						"display_url": "pic.twitter.com/PV6MkhX9SQ",
+						"expanded_url": "https://twitter.com/Eric_Edholm/status/1243981101691031553/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 960,
+								"h": 720,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1243981096750141400,
+						"id_str": "1243981096750141440",
+						"indices": [
+							33,
+							56
+						],
+						"media_url": "http://pbs.twimg.com/media/EUOCN3MX0AAglkJ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EUOCN3MX0AAglkJ.jpg",
+						"url": "https://t.co/PV6MkhX9SQ",
+						"display_url": "pic.twitter.com/PV6MkhX9SQ",
+						"expanded_url": "https://twitter.com/Eric_Edholm/status/1243981101691031553/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 960,
+								"h": 720,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 61520340,
+				"id_str": "61520340",
+				"name": "Eric Edholm",
+				"screen_name": "Eric_Edholm",
+				"location": "Chicago, IL",
+				"description": "Yahoo Sports, NFL draft. “Listen nerd, trash talk is a very big part of football.” @nprice14 to me",
+				"url": "https://t.co/PkU8dynjee",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/PkU8dynjee",
+								"expanded_url": "https://www.facebook.com/ericedholmnfl/",
+								"display_url": "facebook.com/ericedholmnfl/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 29508,
+				"friends_count": 4812,
+				"listed_count": 1156,
+				"created_at": "Thu Jul 30 15:49:25 +0000 2009",
+				"favourites_count": 66524,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 130892,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/901111210426474496/zjmzLpVr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/901111210426474496/zjmzLpVr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/61520340/1576432534",
+				"profile_link_color": "91D2FA",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "4b03b11ec52e251e",
+				"url": "https://api.twitter.com/1.1/geo/id/4b03b11ec52e251e.json",
+				"place_type": "city",
+				"name": "Cary",
+				"full_name": "Cary, IL",
+				"country_code": "US",
+				"country": "Verenigde Staten",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-88.290812,
+								42.190136
+							],
+							[
+								-88.174076,
+								42.190136
+							],
+							[
+								-88.174076,
+								42.242307
+							],
+							[
+								-88.290812,
+								42.242307
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6842,
+			"favorite_count": 42021,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Wij hebben een buschauffeur die Lovejoy heet maar deze wint toch wel... https://t.co/CLEzumO2tO"
+	},
+	{
 		"created_at": "Sun Mar 29 17:39:54 +0000 2020",
 		"id": 1244318377906143200,
 		"id_str": "1244318377906143232",
