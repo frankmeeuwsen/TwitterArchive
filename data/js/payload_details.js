b6f1059930ef00ebc846d805f3a694f4131aa209
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-04-05 16:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71304,
-	"created_at": "Sun Apr 05 09:21:04 +0000 2020",
+	"tweets": 71305,
+	"created_at": "Sun Apr 05 14:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sun Apr 05 14:18:08 +0000 2020",
+		"id": 1246804319489405000,
+		"id_str": "1246804319489404928",
+		"full_text": "OK twittervrienden. #Ukelele leren spelen. Welke cursus, remote teachings, howto, youtube, netflix, ebook, app raden jelui aan? En kom aub niet met “ik leerde op deze manier  djembe spelen”, “denk aan de buren” en “kindergitaar” want zero fucks. https://t.co/Rp4gG4fA8V",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Ukelele",
+					"indices": [
+						20,
+						28
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1246804312245895200,
+					"id_str": "1246804312245895168",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EU2J6ofXgAAjM2v.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EU2J6ofXgAAjM2v.jpg",
+					"url": "https://t.co/Rp4gG4fA8V",
+					"display_url": "pic.twitter.com/Rp4gG4fA8V",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1246804319489404928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1246804312245895200,
+					"id_str": "1246804312245895168",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EU2J6ofXgAAjM2v.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EU2J6ofXgAAjM2v.jpg",
+					"url": "https://t.co/Rp4gG4fA8V",
+					"display_url": "pic.twitter.com/Rp4gG4fA8V",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1246804319489404928/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 280,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							25,
+							14
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EU2J6ofXgAAjM2v.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8962,
+			"friends_count": 416,
+			"listed_count": 578,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6113,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61613,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1585350879",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "OK twittervrienden. #Ukelele leren spelen. Welke cursus, remote teachings, howto, youtube, netflix, ebook, app raden jelui aan? En kom aub niet met “ik leerde op deze manier  djembe spelen”, “denk aan de buren” en “kindergitaar” want zero fucks. https://t.co/Rp4gG4fA8V"
+	},
+	{
 		"created_at": "Sun Apr 05 08:22:16 +0000 2020",
 		"id": 1246714760957169700,
 		"id_str": "1246714760957169664",
