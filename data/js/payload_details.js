20f48327f8145cf820c61c427128c6ef4b5bf8a3
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-04-14 19:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71344,
-	"created_at": "Tue Apr 14 12:21:05 +0000 2020",
+	"tweets": 71345,
+	"created_at": "Tue Apr 14 17:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,313 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Tue Apr 14 17:12:30 +0000 2020",
+		"id": 1250109688194220000,
+		"id_str": "1250109688194220034",
+		"full_text": "Kijk je mee @Nelleke? Iets voor jou? https://t.co/4DJJmBfs2n",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Nelleke",
+					"name": "Nelleke Poorthuis",
+					"id": 6850052,
+					"id_str": "6850052",
+					"indices": [
+						12,
+						20
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/4DJJmBfs2n",
+					"expanded_url": "https://twitter.com/christaromp/status/1250107914515349504",
+					"display_url": "twitter.com/christaromp/st…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Schrijft over de impact van tech op mens en maatschappij \n@indiewebnl \n@kaliberInteract \n@ForSubscribing\n\nhttps://t.co/ODgIUTTMdh\n\n#TeamHuman \n#PearlJam",
+			"url": "https://t.co/GXSndpgrhE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GXSndpgrhE",
+							"expanded_url": "https://diggingthedigital.com/",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUTTMdh",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8976,
+			"friends_count": 421,
+			"listed_count": 578,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6154,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61659,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1585350879",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1250107914515349500,
+		"quoted_status_id_str": "1250107914515349504",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4DJJmBfs2n",
+			"expanded": "https://twitter.com/christaromp/status/1250107914515349504",
+			"display": "twitter.com/christaromp/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Apr 14 17:05:27 +0000 2020",
+			"id": 1250107914515349500,
+			"id_str": "1250107914515349504",
+			"full_text": "Ik fotograaf iedere avond een gezin uit de straat. Net maakte ik deze foto en nu wil ik m'n baan opzeggen en full-time fotograaf worden :) https://t.co/aglyyiCFqo",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				138
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1250107875583811600,
+						"id_str": "1250107875583811593",
+						"indices": [
+							139,
+							162
+						],
+						"media_url": "http://pbs.twimg.com/media/EVlGfU8XYAkc7mK.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EVlGfU8XYAkc7mK.jpg",
+						"url": "https://t.co/aglyyiCFqo",
+						"display_url": "pic.twitter.com/aglyyiCFqo",
+						"expanded_url": "https://twitter.com/ChristaRomp/status/1250107914515349504/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 799,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 999,
+								"h": 1500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 453,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1250107875583811600,
+						"id_str": "1250107875583811593",
+						"indices": [
+							139,
+							162
+						],
+						"media_url": "http://pbs.twimg.com/media/EVlGfU8XYAkc7mK.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EVlGfU8XYAkc7mK.jpg",
+						"url": "https://t.co/aglyyiCFqo",
+						"display_url": "pic.twitter.com/aglyyiCFqo",
+						"expanded_url": "https://twitter.com/ChristaRomp/status/1250107914515349504/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 799,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 999,
+								"h": 1500,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 453,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 26492815,
+				"id_str": "26492815",
+				"name": "Christa Romp",
+				"screen_name": "ChristaRomp",
+				"location": "Amsterdam-noord",
+				"description": "Plots docent op afstand geworden. \n\nJe mist meer dan je meemaakt.\nHelemaal niet erg.\n(Martin Bril)",
+				"url": "https://t.co/zGtJ3zaot8",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/zGtJ3zaot8",
+								"expanded_url": "http://themorewetravel.com/over/",
+								"display_url": "themorewetravel.com/over/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1362,
+				"friends_count": 589,
+				"listed_count": 56,
+				"created_at": "Wed Mar 25 13:10:14 +0000 2009",
+				"favourites_count": 9595,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4764,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EDECE9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1149966492999593984/b8ciSdvS_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149966492999593984/b8ciSdvS_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/26492815/1474354058",
+				"profile_link_color": "2D2E2E",
+				"profile_sidebar_border_color": "D3D2CF",
+				"profile_sidebar_fill_color": "A4A6A6",
+				"profile_text_color": "410869",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Kijk je mee @Nelleke? Iets voor jou? https://t.co/4DJJmBfs2n"
+	},
+	{
 		"created_at": "Tue Apr 14 12:17:42 +0000 2020",
 		"id": 1250035502004256800,
 		"id_str": "1250035502004256769",
