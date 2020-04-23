1d07d7222e795739d3953d968c749b891bd0637b
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-04-23 18:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71383,
-	"created_at": "Thu Apr 23 15:21:07 +0000 2020",
+	"tweets": 71384,
+	"created_at": "Thu Apr 23 16:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 23 16:01:44 +0000 2020",
+		"id": 1253353371483938800,
+		"id_str": "1253353371483938818",
+		"full_text": "@erwblo @PetradeBoevere Zoomaargasten! Pak de pollepel! https://t.co/7Dyh0B7t0T",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "erwblo",
+					"name": "erwin blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "PetradeBoevere",
+					"name": "Petra de Boevere",
+					"id": 1358811,
+					"id_str": "1358811",
+					"indices": [
+						8,
+						23
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7Dyh0B7t0T",
+					"expanded_url": "https://twitter.com/jor_id/status/1253294352325017600",
+					"display_url": "twitter.com/jor_id/status/…",
+					"indices": [
+						56,
+						79
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 713333,
+		"in_reply_to_user_id_str": "713333",
+		"in_reply_to_screen_name": "erwblo",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8977,
+			"friends_count": 430,
+			"listed_count": 578,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6196,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61714,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1585350879",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1253294352325017600,
+		"quoted_status_id_str": "1253294352325017600",
+		"quoted_status_permalink": {
+			"url": "https://t.co/7Dyh0B7t0T",
+			"expanded": "https://twitter.com/jor_id/status/1253294352325017600",
+			"display": "twitter.com/jor_id/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Apr 23 12:07:13 +0000 2020",
+			"id": 1253294352325017600,
+			"id_str": "1253294352325017600",
+			"full_text": "Is iemand al bezig met het ontwikkelen van Zoom'ergasten?\n3 uur ouwehoeren in een videocall?\n\nZo ja; let me help.\nZo niet: heeeere you go.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				138
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 82692289,
+				"id_str": "82692289",
+				"name": "Jordi van de Bovenkamp",
+				"screen_name": "JOR_ID",
+				"location": "Amsterdam",
+				"description": "Creative director @MediaMonks Amsterdam • Te zien @RTLBoulevard • Zit bij @Badum_Official • Heb een rode fiets • Maak graag grappen • Bezoek veel concerten •",
+				"url": "https://t.co/eT4uMS9sHr",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/eT4uMS9sHr",
+								"expanded_url": "http://www.instagram.com/jor_id",
+								"display_url": "instagram.com/jor_id",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 101681,
+				"friends_count": 154,
+				"listed_count": 392,
+				"created_at": "Thu Oct 15 19:27:41 +0000 2009",
+				"favourites_count": 2510,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 51642,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/914114307792699397/Wjn2jtHO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/914114307792699397/Wjn2jtHO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/82692289/1398231843",
+				"profile_link_color": "524D48",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "D1D1D1",
+				"profile_text_color": "303030",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 96,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "nl"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@erwblo @PetradeBoevere Zoomaargasten! Pak de pollepel! https://t.co/7Dyh0B7t0T"
+	},
+	{
 		"created_at": "Thu Apr 23 15:08:04 +0000 2020",
 		"id": 1253294738783993900,
 		"id_str": "1253339864898002944",
