e7b492da186e608d9d7211a7221bf3abacbf8b42
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-04-24 21:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71387,
-	"created_at": "Fri Apr 24 07:21:05 +0000 2020",
+	"tweets": 71388,
+	"created_at": "Fri Apr 24 19:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 24 18:29:24 +0000 2020",
+		"id": 1253752921877028900,
+		"id_str": "1253752921877028864",
+		"full_text": "Herkenbaar. Allemaal. https://t.co/dUSWPoDJXk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			21
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dUSWPoDJXk",
+					"expanded_url": "https://twitter.com/find_evil/status/1253395926653206529",
+					"display_url": "twitter.com/find_evil/stat…",
+					"indices": [
+						22,
+						45
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8976,
+			"friends_count": 430,
+			"listed_count": 578,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6202,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61717,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1253395926653206500,
+		"quoted_status_id_str": "1253395926653206529",
+		"quoted_status_permalink": {
+			"url": "https://t.co/dUSWPoDJXk",
+			"expanded": "https://twitter.com/find_evil/status/1253395926653206529",
+			"display": "twitter.com/find_evil/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Apr 23 18:50:50 +0000 2020",
+			"id": 1253395926653206500,
+			"id_str": "1253395926653206529",
+			"full_text": "most of the zoom personality types https://t.co/8wVuFiil7w",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				34
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1253395710063587300,
+						"id_str": "1253395710063587328",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1253395710063587328/pu/img/DZdjF70C6QGgIJv-.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1253395710063587328/pu/img/DZdjF70C6QGgIJv-.jpg",
+						"url": "https://t.co/8wVuFiil7w",
+						"display_url": "pic.twitter.com/8wVuFiil7w",
+						"expanded_url": "https://twitter.com/find_evil/status/1253395926653206529/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1253395710063587300,
+						"id_str": "1253395710063587328",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1253395710063587328/pu/img/DZdjF70C6QGgIJv-.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1253395710063587328/pu/img/DZdjF70C6QGgIJv-.jpg",
+						"url": "https://t.co/8wVuFiil7w",
+						"display_url": "pic.twitter.com/8wVuFiil7w",
+						"expanded_url": "https://twitter.com/find_evil/status/1253395926653206529/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 123400,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1253395710063587328/pu/vid/480x270/1vt6e-wzH3a3B2ia.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1253395710063587328/pu/vid/1280x720/yl7ChwjnObpvR999.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1253395710063587328/pu/pl/H9uxC2VoABETVX7D.m3u8?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1253395710063587328/pu/vid/640x360/oIZLECx5dK9rWywQ.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3004269184,
+				"id_str": "3004269184",
+				"name": "Jackie Singh 😷",
+				"screen_name": "find_evil",
+				"location": "New York, NY",
+				"description": "Cybersecurity Consultant/CEO at Spyglass Security. Iraq Veteran. Mother of 2. Partner to @0xBanana. Founder @HackingJobs. 🏳️‍🌈🏴‍☠️ Neutral Good. She/Her.",
+				"url": "https://t.co/0u42m3DmvG",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/0u42m3DmvG",
+								"expanded_url": "https://calendly.com/spyglass-jackie/",
+								"display_url": "calendly.com/spyglass-jacki…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 18198,
+				"friends_count": 1166,
+				"listed_count": 631,
+				"created_at": "Thu Jan 29 17:44:05 +0000 2015",
+				"favourites_count": 43257,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 9293,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1228914193664892929/8IKD7iQl_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1228914193664892929/8IKD7iQl_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3004269184/1587396803",
+				"profile_link_color": "B967FF",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3401,
+			"favorite_count": 11250,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Herkenbaar. Allemaal. https://t.co/dUSWPoDJXk"
+	},
+	{
 		"created_at": "Fri Apr 24 06:34:03 +0000 2020",
 		"id": 1253572898817802200,
 		"id_str": "1253572898817802240",
