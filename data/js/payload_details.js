9463eb6b3ef1fe28f5c55809fc4bf81a33ed5cee
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-05-01 18:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71421,
-	"created_at": "Fri May 01 04:21:05 +0000 2020",
+	"tweets": 71422,
+	"created_at": "Fri May 01 16:21:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 01 15:42:09 +0000 2020",
+		"id": 1256247545241755600,
+		"id_str": "1256247545241755648",
+		"full_text": "Bijna maffiapraktijken van Meyer. Maar goed... als ik me herinner hoe het in de jaren '90 in zijn tenten ging... https://t.co/F2ZGmZOdCn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			112
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/F2ZGmZOdCn",
+					"expanded_url": "https://twitter.com/bndestembreda/status/1256197183864311808",
+					"display_url": "twitter.com/bndestembreda/…",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8970,
+			"friends_count": 433,
+			"listed_count": 579,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6238,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61757,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1256197183864311800,
+		"quoted_status_id_str": "1256197183864311808",
+		"quoted_status_permalink": {
+			"url": "https://t.co/F2ZGmZOdCn",
+			"expanded": "https://twitter.com/bndestembreda/status/1256197183864311808",
+			"display": "twitter.com/bndestembreda/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 01 12:22:02 +0000 2020",
+			"id": 1256197183864311800,
+			"id_str": "1256197183864311808",
+			"full_text": "Laurens Meyer, grootste kroegbaas van Nederland: ‘We gaan uiterlijk 1 juni open’ https://t.co/ZNcpyt65Ga",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				104
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ZNcpyt65Ga",
+						"expanded_url": "http://dlvr.it/RVpyfZ",
+						"display_url": "dlvr.it/RVpyfZ",
+						"indices": [
+							81,
+							104
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 154501559,
+				"id_str": "154501559",
+				"name": "BN DeStem Breda",
+				"screen_name": "BNDeStemBreda",
+				"location": "Breda",
+				"description": "Redactie Breda schrijft o.a. over Achtmaal, Alphen-Chaam, Baarle-Nassau, Bavel, Breda, Prinsenbeek, Teteringen, Terheijden, Ulvenhout en Zundert.",
+				"url": "https://t.co/eEQeLUDANp",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/eEQeLUDANp",
+								"expanded_url": "http://www.bndestem.nl/breda",
+								"display_url": "bndestem.nl/breda",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 12398,
+				"friends_count": 245,
+				"listed_count": 160,
+				"created_at": "Fri Jun 11 11:51:23 +0000 2010",
+				"favourites_count": 37,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 55533,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "E6E6E6",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1226858148834807809/6zpyiZgk_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1226858148834807809/6zpyiZgk_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/154501559/1412673181",
+				"profile_link_color": "DD2E44",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Bijna maffiapraktijken van Meyer. Maar goed... als ik me herinner hoe het in de jaren '90 in zijn tenten ging... https://t.co/F2ZGmZOdCn"
+	},
+	{
 		"created_at": "Fri May 01 04:09:24 +0000 2020",
 		"id": 1256073208719118300,
 		"id_str": "1256073208719118336",
