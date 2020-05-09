2fccb20d96551f369340818f68dfacd5fc16c1c5
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-05-09 23:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71469,
-	"created_at": "Sat May 09 20:21:05 +0000 2020",
+	"tweets": 71470,
+	"created_at": "Sat May 09 21:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 09 20:26:03 +0000 2020",
+		"id": 1259218093986979800,
+		"id_str": "1259218093986979843",
+		"full_text": "@anthilemoon @RoamResearch Yesterday in the middle of conducting an interview for an article. All my questions were offscreen all of a sudden. Learning moment, just use Roam for small individual stuff. Not as the place you depend on. For now.",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			242
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "anthilemoon",
+					"name": "Anne-Laure Le Cunff",
+					"id": 15996668,
+					"id_str": "15996668",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "RoamResearch",
+					"name": "Roam Research",
+					"id": 1190410678273626000,
+					"id_str": "1190410678273626113",
+					"indices": [
+						13,
+						26
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1259213362229059600,
+		"in_reply_to_status_id_str": "1259213362229059585",
+		"in_reply_to_user_id": 15996668,
+		"in_reply_to_user_id_str": "15996668",
+		"in_reply_to_screen_name": "anthilemoon",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8960,
+			"friends_count": 447,
+			"listed_count": 577,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6280,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61815,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@anthilemoon @RoamResearch Yesterday in the middle of conducting an interview for an article. All my questions were offscreen all of a sudden. Learning moment, just use Roam for small individual stuff. Not as the place you depend on. For now."
+	},
+	{
 		"created_at": "Sat May 09 19:36:40 +0000 2020",
 		"id": 1259205665744138200,
 		"id_str": "1259205665744138240",
