87df08b5b5577f5bb0152791cbaadbd76d77c266
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-05-09 19:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71467,
-	"created_at": "Sat May 09 11:21:04 +0000 2020",
+	"tweets": 71468,
+	"created_at": "Sat May 09 17:21:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 09 16:26:43 +0000 2020",
+		"id": 1259143254852894700,
+		"id_str": "1259157864934973440",
+		"full_text": "James Brown got his big break as a Little Richard impersonator. Hendrix got his start playing guitar in Little Richard’s band. The Beatles formed around his work. Bob Dylan and Freddie Mercury began with his music. It’s impossible to overstate Little Richard’s influence.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 36823,
+			"id_str": "36823",
+			"name": "Anil Dash 😷",
+			"screen_name": "anildash",
+			"location": "NYC",
+			"description": "CEO of 🎏@Glitch, where we're helping everybody make the web, better. Been blogging since the last century. I like transit, ethical tech, funk & justice.",
+			"url": "https://t.co/nT5U9wrj2F",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nT5U9wrj2F",
+							"expanded_url": "https://anildash.com/about/",
+							"display_url": "anildash.com/about/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 599663,
+			"friends_count": 569,
+			"listed_count": 9014,
+			"created_at": "Sat Dec 02 09:15:15 +0000 2006",
+			"favourites_count": 704275,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 212761,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1040592282032984071/RC8We7BG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1040592282032984071/RC8We7BG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/36823/1530683410",
+			"profile_link_color": "800080",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2981,
+		"favorite_count": 8443,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "James Brown got his big break as a Little Richard impersonator. Hendrix got his start playing guitar in Little Richard’s band. The Beatles formed around his work. Bob Dylan and Freddie Mercury began with his music. It’s impossible to overstate Little Richard’s influence."
+	},
+	{
 		"created_at": "Sat May 09 10:54:11 +0000 2020",
 		"id": 1259074181230190600,
 		"id_str": "1259074181230190592",
