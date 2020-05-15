9f5cbafd95a97e5653ddf4064828ad5b0cc188b2
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-05-15 09:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71499,
-	"created_at": "Fri May 15 05:21:06 +0000 2020",
+	"tweets": 71500,
+	"created_at": "Fri May 15 07:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 15 07:00:15 +0000 2020",
+		"id": 1261189635931353000,
+		"id_str": "1261189635931353090",
+		"full_text": "Email, zines en blogs. Meer is niet nodig in deze editie van OPEN nieuwsbrief. Met links naar o.a. @reinier, @houshuang, @benhammersley en een vleugje webnostalgie 🔥 \n\nhttps://t.co/w9oC2DaeVQ (via @revue)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			204
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Reinier",
+					"name": "Reinier Ladan ⧖",
+					"id": 647393,
+					"id_str": "647393",
+					"indices": [
+						99,
+						107
+					]
+				},
+				{
+					"screen_name": "houshuang",
+					"name": "Stian Håklev (侯爽)",
+					"id": 9588752,
+					"id_str": "9588752",
+					"indices": [
+						109,
+						119
+					]
+				},
+				{
+					"screen_name": "benhammersley",
+					"name": "Ben Hammersley",
+					"id": 15765018,
+					"id_str": "15765018",
+					"indices": [
+						121,
+						135
+					]
+				},
+				{
+					"screen_name": "revue",
+					"name": "Revue",
+					"id": 3001165942,
+					"id_str": "3001165942",
+					"indices": [
+						197,
+						203
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/w9oC2DaeVQ",
+					"expanded_url": "http://nieuws.diggingthedigital.com/archive/246882",
+					"display_url": "nieuws.diggingthedigital.com/archive/246882",
+					"indices": [
+						168,
+						191
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.getrevue.co\" rel=\"nofollow\">Revue</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8966,
+			"friends_count": 453,
+			"listed_count": 572,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6306,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61857,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Email, zines en blogs. Meer is niet nodig in deze editie van OPEN nieuwsbrief. Met links naar o.a. @reinier, @houshuang, @benhammersley en een vleugje webnostalgie 🔥 \n\nhttps://t.co/w9oC2DaeVQ (via @revue)"
+	},
+	{
 		"created_at": "Fri May 15 05:07:07 +0000 2020",
 		"id": 1261161164937011200,
 		"id_str": "1261161164937011201",
