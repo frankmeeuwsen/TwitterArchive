7250c9e8f1d908e4a02b177ddafad8ba951a5562
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-05-18 08:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71509,
-	"created_at": "Sun May 17 19:21:05 +0000 2020",
+	"tweets": 71510,
+	"created_at": "Mon May 18 06:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Mon May 18 05:33:01 +0000 2020",
+		"id": 1262254844791980000,
+		"id_str": "1262254844791980032",
+		"full_text": "Vandaag is drie jaar geleden dat Chris Cornell stierf. Ik was onderweg naar The Next Web conference toen ik het las. Dat was voor het eerst dat ik een tijdje van slag was door het overlijden van een artiest. Zijn muziek is er gelukkig nog. #NoOneSingsLikeYouAnymore https://t.co/BDwTbAXzeI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NoOneSingsLikeYouAnymore",
+					"indices": [
+						240,
+						265
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1262254838462718000,
+					"id_str": "1262254838462717952",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/EYRuFn6WoAAGRcZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EYRuFn6WoAAGRcZ.jpg",
+					"url": "https://t.co/BDwTbAXzeI",
+					"display_url": "pic.twitter.com/BDwTbAXzeI",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1262254844791980032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 676,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1262254838462718000,
+					"id_str": "1262254838462717952",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/EYRuFn6WoAAGRcZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EYRuFn6WoAAGRcZ.jpg",
+					"url": "https://t.co/BDwTbAXzeI",
+					"display_url": "pic.twitter.com/BDwTbAXzeI",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1262254844791980032/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 676,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8963,
+			"friends_count": 465,
+			"listed_count": 572,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6317,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61871,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Vandaag is drie jaar geleden dat Chris Cornell stierf. Ik was onderweg naar The Next Web conference toen ik het las. Dat was voor het eerst dat ik een tijdje van slag was door het overlijden van een artiest. Zijn muziek is er gelukkig nog. #NoOneSingsLikeYouAnymore https://t.co/BDwTbAXzeI"
+	},
+	{
 		"created_at": "Sun May 17 19:21:03 +0000 2020",
 		"id": 1262100839151341600,
 		"id_str": "1262100839151341572",
