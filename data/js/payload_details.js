f02702133e38dff70d33c2c4333e591f9884f645
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-06-01 08:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71602,
-	"created_at": "Sun May 31 21:21:05 +0000 2020",
+	"tweets": 71603,
+	"created_at": "Mon Jun 01 06:21:04 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,330 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 05:31:57 +0000 2020",
+		"id": 1267328006529351700,
+		"id_str": "1267328006529351681",
+		"full_text": "Als je dacht dat het niet erger kon worden. Deze hele thread... het is niet te bevatten hoe de politie tekeer gaat in steden. Van zwangere vrouwen en studenten die boodschappen doen en  beschoten worden tot kinderen die pepperspray in hun ogen krijgen. https://t.co/4tr3zXeaNU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			252
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4tr3zXeaNU",
+					"expanded_url": "https://twitter.com/chadloder/status/1267176238960922624",
+					"display_url": "twitter.com/chadloder/stat…",
+					"indices": [
+						253,
+						276
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8960,
+			"friends_count": 474,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6379,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 61993,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1267176238960922600,
+		"quoted_status_id_str": "1267176238960922624",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4tr3zXeaNU",
+			"expanded": "https://twitter.com/chadloder/status/1267176238960922624",
+			"display": "twitter.com/chadloder/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Sun May 31 19:28:53 +0000 2020",
+			"id": 1267176238960922600,
+			"id_str": "1267176238960922624",
+			"full_text": "Denver. Police were firing pepper bullets and tear gas at cars stopped at an intersection, targeting a car with a pregnant woman.\n\nA man yells at them “You’re shooting my car with my girlfriend in it? My pregnant girlfriend in it?”\n\nThe cops open fire on him with pepper rounds. https://t.co/MrJjqo4ETB",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				278
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1267175226523058200,
+						"id_str": "1267175226523058176",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1267175226523058176/pu/img/YC3Jffm58bb2Mo7W.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1267175226523058176/pu/img/YC3Jffm58bb2Mo7W.jpg",
+						"url": "https://t.co/MrJjqo4ETB",
+						"display_url": "pic.twitter.com/MrJjqo4ETB",
+						"expanded_url": "https://twitter.com/chadloder/status/1267176238960922624/video/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 401,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1076,
+								"h": 634,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1076,
+								"h": 634,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1267175226523058200,
+						"id_str": "1267175226523058176",
+						"indices": [
+							279,
+							302
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1267175226523058176/pu/img/YC3Jffm58bb2Mo7W.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1267175226523058176/pu/img/YC3Jffm58bb2Mo7W.jpg",
+						"url": "https://t.co/MrJjqo4ETB",
+						"display_url": "pic.twitter.com/MrJjqo4ETB",
+						"expanded_url": "https://twitter.com/chadloder/status/1267176238960922624/video/1",
+						"type": "video",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 401,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1076,
+								"h": 634,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1076,
+								"h": 634,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								538,
+								317
+							],
+							"duration_millis": 54613,
+							"variants": [
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1267175226523058176/pu/vid/1076x634/M6MOuhVfzHff4BrY.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1267175226523058176/pu/vid/610x360/ALygsH1UOVbODY1O.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1267175226523058176/pu/pl/Q3b6rwjhR32pjSmM.m3u8?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1267175226523058176/pu/vid/458x270/C9m6DN7JQbY-szNN.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1267172098918637600,
+			"in_reply_to_status_id_str": "1267172098918637568",
+			"in_reply_to_user_id": 98575337,
+			"in_reply_to_user_id_str": "98575337",
+			"in_reply_to_screen_name": "chadloder",
+			"user": {
+				"id": 98575337,
+				"id_str": "98575337",
+				"name": "Chad Loder",
+				"screen_name": "chadloder",
+				"location": "Los Angeles, CA",
+				"description": "Founder/CEO @Habitu8 • Cofounder @MasksForDocs (volunteers giving away free PPE) • Previously: Founder, VP Engineering @Rapid7 • Anti-racist • #blacklivesmatter",
+				"url": "https://t.co/j2ABO3HoJN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/j2ABO3HoJN",
+								"expanded_url": "https://www.habitu8.io/",
+								"display_url": "habitu8.io",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 57955,
+				"friends_count": 4696,
+				"listed_count": 496,
+				"created_at": "Tue Dec 22 07:11:56 +0000 2009",
+				"favourites_count": 55642,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 26429,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1264269817265008640/2XqsXyoZ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1264269817265008640/2XqsXyoZ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/98575337/1590219908",
+				"profile_link_color": "F0CBCA",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9078,
+			"favorite_count": 14526,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Als je dacht dat het niet erger kon worden. Deze hele thread... het is niet te bevatten hoe de politie tekeer gaat in steden. Van zwangere vrouwen en studenten die boodschappen doen en  beschoten worden tot kinderen die pepperspray in hun ogen krijgen. https://t.co/4tr3zXeaNU"
+	}
+]
