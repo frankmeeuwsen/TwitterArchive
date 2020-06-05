21a328b0d195e17dcab4a6df9afc8d0aec5f9335
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-06-05 15:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71628,
-	"created_at": "Thu Jun 04 20:21:04 +0000 2020",
+	"tweets": 71629,
+	"created_at": "Fri Jun 05 13:21:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Fri Jun 05 12:58:54 +0000 2020",
+		"id": 1268851747503640600,
+		"id_str": "1268890039347564545",
+		"full_text": "Just to let you know: I finally decided to switch to @RoamResearch Will certainly write soon something about experiences, adaptation, and why (well, mostly because it's fun). Thanks, @Conaw for being insistent, @nateliason, @ShuOmi3, and @JoelChan86 for the intros.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RoamResearch",
+					"name": "Roam Research",
+					"id": 1190410678273626000,
+					"id_str": "1190410678273626113",
+					"indices": [
+						53,
+						66
+					]
+				},
+				{
+					"screen_name": "Conaw",
+					"name": "Conor White-Sullivan",
+					"id": 17562763,
+					"id_str": "17562763",
+					"indices": [
+						183,
+						189
+					]
+				},
+				{
+					"screen_name": "nateliason",
+					"name": "Nat Eliason",
+					"id": 19032289,
+					"id_str": "19032289",
+					"indices": [
+						211,
+						222
+					]
+				},
+				{
+					"screen_name": "ShuOmi3",
+					"name": "Shu Omi",
+					"id": 1199725133524545500,
+					"id_str": "1199725133524545536",
+					"indices": [
+						224,
+						232
+					]
+				},
+				{
+					"screen_name": "JoelChan86",
+					"name": "Joel Chan, #slowthespread, support your neighbors!",
+					"id": 384086602,
+					"id_str": "384086602",
+					"indices": [
+						238,
+						249
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 834872136477057000,
+			"id_str": "834872136477057025",
+			"name": "Sönke Ahrens",
+			"screen_name": "soenke_ahrens",
+			"location": "Hamburg, Germany",
+			"description": "Tweets about note-taking, value investing and 𝘣𝘪𝘭𝘥𝘶𝘯𝘨. It's all about compounding interest and interests",
+			"url": "https://t.co/uucF8shL7D",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uucF8shL7D",
+							"expanded_url": "https://www.goodreads.com/book/show/34507927-how-to-take-smart-notes",
+							"display_url": "goodreads.com/book/show/3450…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2318,
+			"friends_count": 139,
+			"listed_count": 88,
+			"created_at": "Thu Feb 23 21:06:43 +0000 2017",
+			"favourites_count": 678,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 193,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1155440830515167233/wVyBROEe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1155440830515167233/wVyBROEe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/834872136477057025/1488045313",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 172,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Just to let you know: I finally decided to switch to @RoamResearch Will certainly write soon something about experiences, adaptation, and why (well, mostly because it's fun). Thanks, @Conaw for being insistent, @nateliason, @ShuOmi3, and @JoelChan86 for the intros."
+	},
+	{
 		"created_at": "Thu Jun 04 20:11:28 +0000 2020",
 		"id": 1268636509873344500,
 		"id_str": "1268636509873344512",
