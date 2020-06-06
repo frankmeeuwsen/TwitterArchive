544c6a8f7849fc7eafc6a5ff8210bcd0477f3e52
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-06-06 20:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71632,
-	"created_at": "Sat Jun 06 16:21:04 +0000 2020",
+	"tweets": 71633,
+	"created_at": "Sat Jun 06 18:21:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 06 17:23:11 +0000 2020",
+		"id": 1269318934060302300,
+		"id_str": "1269318934060302336",
+		"full_text": "Ah. Ik zie aan de reclames dat we nu weer samen met Merk©️ aan het werk gaan. Een nieuwe dag. Samen. Vooruitkijken. Met Merk©️ aan je zijde. Want Merk©️ weet hoe het was. En hoe het kan worden. \n\nMerk©️. Voor ons. https://t.co/OmuH5KgPia",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			213
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1269318926497910800,
+					"id_str": "1269318926497910784",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EZ2G1qiWkAAF2kk.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EZ2G1qiWkAAF2kk.jpg",
+					"url": "https://t.co/OmuH5KgPia",
+					"display_url": "pic.twitter.com/OmuH5KgPia",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1269318934060302336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 372,
+							"h": 280,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 372,
+							"h": 280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 372,
+							"h": 280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1269318926497910800,
+					"id_str": "1269318926497910784",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EZ2G1qiWkAAF2kk.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EZ2G1qiWkAAF2kk.jpg",
+					"url": "https://t.co/OmuH5KgPia",
+					"display_url": "pic.twitter.com/OmuH5KgPia",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1269318934060302336/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 372,
+							"h": 280,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 372,
+							"h": 280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 372,
+							"h": 280,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							93,
+							70
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EZ2G1qiWkAAF2kk.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8957,
+			"friends_count": 475,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6396,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62031,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ah. Ik zie aan de reclames dat we nu weer samen met Merk©️ aan het werk gaan. Een nieuwe dag. Samen. Vooruitkijken. Met Merk©️ aan je zijde. Want Merk©️ weet hoe het was. En hoe het kan worden. \n\nMerk©️. Voor ons. https://t.co/OmuH5KgPia"
+	},
+	{
 		"created_at": "Sat Jun 06 15:40:34 +0000 2020",
 		"id": 1268999177536585700,
 		"id_str": "1269293109537574917",
