274b0c585acd6c1a2a2a9c92c88043003ae7d07e
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-06-09 22:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71644,
-	"created_at": "Tue Jun 09 19:21:04 +0000 2020",
+	"tweets": 71645,
+	"created_at": "Tue Jun 09 20:21:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 09 20:16:04 +0000 2020",
+		"id": 1270449604740493300,
+		"id_str": "1270449604740493315",
+		"full_text": "@mackaaij @arjanbroere @resourcerer @NielsGouman @jplattel Ik ga eens op zoek of ik nog een backup van de database heb toen we de boel hebben overgedragen. Wie weet kan het nog van pas komen. Ik kan geen belofte doen dát ik het kan vinden maar ik ga mijn best doen.",
+		"truncated": false,
+		"display_text_range": [
+			59,
+			265
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mackaaij",
+					"name": "Patrick Mackaaij",
+					"id": 16212528,
+					"id_str": "16212528",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "arjanbroere",
+					"name": "arjan broere",
+					"id": 2087771,
+					"id_str": "2087771",
+					"indices": [
+						10,
+						22
+					]
+				},
+				{
+					"screen_name": "resourcerer",
+					"name": "Resourcerer",
+					"id": 1248975810230435800,
+					"id_str": "1248975810230435840",
+					"indices": [
+						23,
+						35
+					]
+				},
+				{
+					"screen_name": "NielsGouman",
+					"name": "Niels Gouman",
+					"id": 25689874,
+					"id_str": "25689874",
+					"indices": [
+						36,
+						48
+					]
+				},
+				{
+					"screen_name": "jplattel",
+					"name": "Joost Plattel",
+					"id": 16376925,
+					"id_str": "16376925",
+					"indices": [
+						49,
+						58
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1270437231262646300,
+		"in_reply_to_status_id_str": "1270437231262646272",
+		"in_reply_to_user_id": 16212528,
+		"in_reply_to_user_id_str": "16212528",
+		"in_reply_to_screen_name": "mackaaij",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8951,
+			"friends_count": 477,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6404,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62046,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@mackaaij @arjanbroere @resourcerer @NielsGouman @jplattel Ik ga eens op zoek of ik nog een backup van de database heb toen we de boel hebben overgedragen. Wie weet kan het nog van pas komen. Ik kan geen belofte doen dát ik het kan vinden maar ik ga mijn best doen."
+	},
+	{
 		"created_at": "Tue Jun 09 19:02:42 +0000 2020",
 		"id": 1270431141129052200,
 		"id_str": "1270431141129052160",
