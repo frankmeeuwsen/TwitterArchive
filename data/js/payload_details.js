576f96ee362c483805b6bbb591c282874ce8e772
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-06-10 22:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71649,
-	"created_at": "Wed Jun 10 10:21:04 +0000 2020",
+	"tweets": 71650,
+	"created_at": "Wed Jun 10 20:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 10 19:21:11 +0000 2020",
+		"id": 1270776951226187800,
+		"id_str": "1270798181228560387",
+		"full_text": "Someone needs to make a Pocket that incorporates multiple forms of media:\n\n- webpages\n- newsletters\n-podcast episodes\n- tweets",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 158610400,
+			"id_str": "158610400",
+			"name": "Austin Rief",
+			"screen_name": "austin_rief",
+			"location": "NYC",
+			"description": "cofounder @MorningBrew",
+			"url": "https://t.co/9jyOEYgGUT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9jyOEYgGUT",
+							"expanded_url": "http://morningbrew.com",
+							"display_url": "morningbrew.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11150,
+			"friends_count": 777,
+			"listed_count": 103,
+			"created_at": "Wed Jun 23 04:09:47 +0000 2010",
+			"favourites_count": 19346,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3756,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1259285101705396225/M3ZKDvYO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1259285101705396225/M3ZKDvYO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/158610400/1572961351",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 137,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Someone needs to make a Pocket that incorporates multiple forms of media:\n\n- webpages\n- newsletters\n-podcast episodes\n- tweets"
+	},
+	{
 		"created_at": "Wed Jun 10 09:54:14 +0000 2020",
 		"id": 1270655502733672400,
 		"id_str": "1270655502733672449",
