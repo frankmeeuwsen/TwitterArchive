90af4b7a5fa0bc96416ac24b850e5f18b909a7dc
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-06-17 01:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71687,
-	"created_at": "Tue Jun 16 22:21:06 +0000 2020",
+	"tweets": 71688,
+	"created_at": "Tue Jun 16 23:21:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 16 22:28:59 +0000 2020",
+		"id": 1273019769134989300,
+		"id_str": "1273019769134989313",
+		"full_text": "@IndieHackers @webflow I use @webflow to build https://t.co/nnGx9gi7UQ, a curated directory with awesome newsletters to make your inbox (or Imbox...) happy again 💌",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			163
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "IndieHackers",
+					"name": "Indie Hackers",
+					"id": 756326958946922500,
+					"id_str": "756326958946922496",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "webflow",
+					"name": "Webflow",
+					"id": 403930194,
+					"id_str": "403930194",
+					"indices": [
+						14,
+						22
+					]
+				},
+				{
+					"screen_name": "webflow",
+					"name": "Webflow",
+					"id": 403930194,
+					"id_str": "403930194",
+					"indices": [
+						29,
+						37
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nnGx9gi7UQ",
+					"expanded_url": "https://thanksforsubscribing.app",
+					"display_url": "thanksforsubscribing.app",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1272926217038151700,
+		"in_reply_to_status_id_str": "1272926217038151681",
+		"in_reply_to_user_id": 756326958946922500,
+		"in_reply_to_user_id_str": "756326958946922496",
+		"in_reply_to_screen_name": "IndieHackers",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werkt bij @kaliberInteract\n\nBouwt zonder code aan https://t.co/ODgIUUbnBR en @ForSubscribing\n\nSchrijft de OPEN nieuwsbrief voor nieuwsgierige cybersurfers.",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								50,
+								73
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8944,
+			"friends_count": 500,
+			"listed_count": 572,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6429,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62101,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1587661070",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@IndieHackers @webflow I use @webflow to build https://t.co/nnGx9gi7UQ, a curated directory with awesome newsletters to make your inbox (or Imbox...) happy again 💌"
+	},
+	{
 		"created_at": "Tue Jun 16 22:05:05 +0000 2020",
 		"id": 1273013756486987800,
 		"id_str": "1273013756486987777",
