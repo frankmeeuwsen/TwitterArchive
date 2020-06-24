24f171e107c41870077076de67f145f3d3eb53c7
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-06-24 22:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71736,
-	"created_at": "Wed Jun 24 17:21:06 +0000 2020",
+	"tweets": 71737,
+	"created_at": "Wed Jun 24 20:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Wed Jun 24 20:11:13 +0000 2020",
+		"id": 1275884204786618400,
+		"id_str": "1275884204786618368",
+		"full_text": "Extra Premium Deluxe Tips: \n5) Cmd-Shift-5 gives even more screenshot options.\n6) Put screenshots in designated folder called \"Designated Folder for Screenshots\" and let Hazel app clean up this folder after x days (when you don't need the screenshots anymore) https://t.co/VhV8kuWUPl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VhV8kuWUPl",
+					"expanded_url": "https://twitter.com/hondanhon/status/1275877621155291137",
+					"display_url": "twitter.com/hondanhon/stat…",
+					"indices": [
+						260,
+						283
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8938,
+			"friends_count": 516,
+			"listed_count": 572,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6480,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62161,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238895651896860672/UOuWcVZz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1275877621155291100,
+		"quoted_status_id_str": "1275877621155291137",
+		"quoted_status_permalink": {
+			"url": "https://t.co/VhV8kuWUPl",
+			"expanded": "https://twitter.com/hondanhon/status/1275877621155291137",
+			"display": "twitter.com/hondanhon/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jun 24 19:45:04 +0000 2020",
+			"id": 1275877621155291100,
+			"id_str": "1275877621155291137",
+			"full_text": "Pro tip that has made my work life much better:\n\n1) Set your macOS screenshots to save to a specific folder\n2) Put that folder in your dock\n3) Configure that dock folder to display thumbnails, sorted by most recent\n4) Easily access (drag, drop etc) your most recent screenshots",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				277
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 32124032,
+				"id_str": "32124032",
+				"name": "Dan Hon",
+				"screen_name": "hondanhon",
+				"location": "Portland, OR",
+				"description": "Co-chaired #CfASummit 2020. Make friends, break silos, build bridges. Chaotic good. DMs open & writing at: https://t.co/UTJUqhMNXn. he/him",
+				"url": "https://t.co/Ugx0rKQKLU",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Ugx0rKQKLU",
+								"expanded_url": "http://danhon.com",
+								"display_url": "danhon.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/UTJUqhMNXn",
+								"expanded_url": "http://danhon.substack.com",
+								"display_url": "danhon.substack.com",
+								"indices": [
+									107,
+									130
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 11928,
+				"friends_count": 7593,
+				"listed_count": 609,
+				"created_at": "Thu Apr 16 19:36:01 +0000 2009",
+				"favourites_count": 120624,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 96130,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1253505700744974337/mJHP_Mbj_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1253505700744974337/mJHP_Mbj_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/32124032/1510078618",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 8,
+			"favorite_count": 63,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Extra Premium Deluxe Tips: \n5) Cmd-Shift-5 gives even more screenshot options.\n6) Put screenshots in designated folder called \"Designated Folder for Screenshots\" and let Hazel app clean up this folder after x days (when you don't need the screenshots anymore) https://t.co/VhV8kuWUPl"
+	},
+	{
 		"created_at": "Wed Jun 24 16:46:39 +0000 2020",
 		"id": 1275832722905235500,
 		"id_str": "1275832722905235456",
