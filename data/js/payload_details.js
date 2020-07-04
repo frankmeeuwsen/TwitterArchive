f3d7881c3878a656b1530924c569dc9f739bf4bf
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-07-04 20:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71808,
-	"created_at": "Sat Jul 04 12:21:05 +0000 2020",
+	"tweets": 71809,
+	"created_at": "Sat Jul 04 18:21:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 04 17:21:57 +0000 2020",
+		"id": 1279462461524119600,
+		"id_str": "1279465482802921474",
+		"full_text": "At #3: vodka! The most important meal of the day... https://t.co/B3EzQ125SE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1279462456780300300,
+					"id_str": "1279462456780300291",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/EcGQU1fWoAMRN5a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcGQU1fWoAMRN5a.jpg",
+					"url": "https://t.co/B3EzQ125SE",
+					"display_url": "pic.twitter.com/B3EzQ125SE",
+					"expanded_url": "https://twitter.com/PulpLibrarian/status/1279462461524119554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 915,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 535,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 915,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1279462456780300300,
+					"id_str": "1279462456780300291",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/EcGQU1fWoAMRN5a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcGQU1fWoAMRN5a.jpg",
+					"url": "https://t.co/B3EzQ125SE",
+					"display_url": "pic.twitter.com/B3EzQ125SE",
+					"expanded_url": "https://twitter.com/PulpLibrarian/status/1279462461524119554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 915,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 535,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 915,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1279462113061351400,
+		"in_reply_to_status_id_str": "1279462113061351430",
+		"in_reply_to_user_id": 2928209793,
+		"in_reply_to_user_id_str": "2928209793",
+		"in_reply_to_screen_name": "PulpLibrarian",
+		"user": {
+			"id": 2928209793,
+			"id_str": "2928209793",
+			"name": "Pulp Librarian",
+			"screen_name": "PulpLibrarian",
+			"location": "A library of the mind",
+			"description": "Curator of the art, history and fiction of old dreams.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 117344,
+			"friends_count": 3862,
+			"listed_count": 0,
+			"created_at": "Wed Dec 17 22:09:14 +0000 2014",
+			"favourites_count": 11370,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 45466,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1152213205432840193/Z9ujL38W_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1152213205432840193/Z9ujL38W_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2928209793/1584793877",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 45,
+		"favorite_count": 168,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "At #3: vodka! The most important meal of the day... https://t.co/B3EzQ125SE"
+	},
+	{
 		"created_at": "Sat Jul 04 11:25:41 +0000 2020",
 		"id": 1279375827348738000,
 		"id_str": "1279375827348738054",
