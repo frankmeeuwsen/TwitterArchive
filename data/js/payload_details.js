872ac758fb32c7abbcb6d9288175a6671d62a576
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-07-13 14:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71840,
-	"created_at": "Sun Jul 12 21:21:04 +0000 2020",
+	"tweets": 71841,
+	"created_at": "Mon Jul 13 12:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Mon Jul 13 11:51:54 +0000 2020",
+		"id": 1282643914525212700,
+		"id_str": "1282643914525212675",
+		"full_text": "Which is why we need more feel-good stories about writing 25 newsletters instead of one-off lucky shot Medium blueprint listicles on how to get x000 subscribers in x weeks... 😉 https://t.co/4NWQP8zjcg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4NWQP8zjcg",
+					"expanded_url": "https://twitter.com/benbradbury_/status/1282637885674590209",
+					"display_url": "twitter.com/benbradbury_/s…",
+					"indices": [
+						177,
+						200
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8942,
+			"friends_count": 539,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6528,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62285,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1282637885674590200,
+		"quoted_status_id_str": "1282637885674590209",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4NWQP8zjcg",
+			"expanded": "https://twitter.com/benbradbury_/status/1282637885674590209",
+			"display": "twitter.com/benbradbury_/s…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jul 13 11:27:56 +0000 2020",
+			"id": 1282637885674590200,
+			"id_str": "1282637885674590209",
+			"full_text": "Good goal: “Publish 25 newsletters over the next 25 weeks.”\n\nBad content creator goal: “Get 2,000 subscribers by the end of 25 weeks.\"\n\nFocusing on intrinsic effort = Avoiding needless disappointment.\n\n(h/t @anthilemoon)",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				220
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "anthilemoon",
+						"name": "Anne-Laure Le Cunff",
+						"id": 15996668,
+						"id_str": "15996668",
+						"indices": [
+							207,
+							219
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 409199791,
+				"id_str": "409199791",
+				"name": "Ben Bradbury",
+				"screen_name": "benbradbury_",
+				"location": "London",
+				"description": "Helping founders communicate astutely | Subject Matter podcast host | @GA Instructor (Storytelling) | Writing at https://t.co/m99RtFd9q4",
+				"url": "https://t.co/m99RtFuKhC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/m99RtFuKhC",
+								"expanded_url": "http://benbradbury.com",
+								"display_url": "benbradbury.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/m99RtFd9q4",
+								"expanded_url": "http://benbradbury.com",
+								"display_url": "benbradbury.com",
+								"indices": [
+									113,
+									136
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 836,
+				"friends_count": 507,
+				"listed_count": 28,
+				"created_at": "Thu Nov 10 12:11:16 +0000 2011",
+				"favourites_count": 3157,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 2405,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0CF0C2",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1157229204536098816/H5BrRIGd_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1157229204536098816/H5BrRIGd_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/409199791/1564739919",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 41,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Which is why we need more feel-good stories about writing 25 newsletters instead of one-off lucky shot Medium blueprint listicles on how to get x000 subscribers in x weeks... 😉 https://t.co/4NWQP8zjcg"
+	},
+	{
 		"created_at": "Sun Jul 12 21:05:58 +0000 2020",
 		"id": 1282420965176234000,
 		"id_str": "1282420965176233991",
