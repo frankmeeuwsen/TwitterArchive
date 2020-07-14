ef374e23cb8ad16870b3e204c1a2b54e2c92ce58
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-07-14 11:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71849,
-	"created_at": "Tue Jul 14 08:21:12 +0000 2020",
+	"tweets": 71850,
+	"created_at": "Tue Jul 14 09:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Tue Jul 14 08:55:32 +0000 2020",
+		"id": 1282961918483996700,
+		"id_str": "1282961918483996672",
+		"full_text": "Totdat het geen Fitbit meer heet omdat het de nek wordt omgedraaid en het ineens Google Health heet. Dan gelden mogelijk weer andere afspraken... https://t.co/fwsmI9HSWs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fwsmI9HSWs",
+					"expanded_url": "https://twitter.com/tweakers/status/1282915489128185856",
+					"display_url": "twitter.com/tweakers/statu…",
+					"indices": [
+						146,
+						169
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8945,
+			"friends_count": 543,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6536,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62298,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1282915489128185900,
+		"quoted_status_id_str": "1282915489128185856",
+		"quoted_status_permalink": {
+			"url": "https://t.co/fwsmI9HSWs",
+			"expanded": "https://twitter.com/tweakers/status/1282915489128185856",
+			"display": "twitter.com/tweakers/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jul 14 05:51:02 +0000 2020",
+			"id": 1282915489128185900,
+			"id_str": "1282915489128185856",
+			"full_text": "Google belooft EU Fitbit-data niet voor advertenties te gebruiken https://t.co/MwLjGGmthK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				89
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/MwLjGGmthK",
+						"expanded_url": "http://dlvr.it/RbZ6zh",
+						"display_url": "dlvr.it/RbZ6zh",
+						"indices": [
+							66,
+							89
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 31080694,
+				"id_str": "31080694",
+				"name": "Tweakers",
+				"screen_name": "tweakers",
+				"location": "Amsterdam, Nederland",
+				"description": "Officieel account van Tweakers, de grootste technologie- en elektronicasite van Nederland en België #tweakers",
+				"url": "http://t.co/nd3dxjNdgl",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/nd3dxjNdgl",
+								"expanded_url": "http://tweakers.net",
+								"display_url": "tweakers.net",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 77360,
+				"friends_count": 23,
+				"listed_count": 1122,
+				"created_at": "Tue Apr 14 07:43:05 +0000 2009",
+				"favourites_count": 71,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 25784,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B10D36",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/592979020041564160/RYTzdn9l_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/592979020041564160/RYTzdn9l_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/31080694/1430212230",
+				"profile_link_color": "B10D36",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Totdat het geen Fitbit meer heet omdat het de nek wordt omgedraaid en het ineens Google Health heet. Dan gelden mogelijk weer andere afspraken... https://t.co/fwsmI9HSWs"
+	},
+	{
 		"created_at": "Tue Jul 14 08:15:32 +0000 2020",
 		"id": 1282951854222184400,
 		"id_str": "1282951854222184448",
