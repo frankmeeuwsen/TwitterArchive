c904f46a31a1ed1085fbb79bb80d385f6ccdd731
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-07-21 20:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71882,
-	"created_at": "Tue Jul 21 17:21:06 +0000 2020",
+	"tweets": 71883,
+	"created_at": "Tue Jul 21 18:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Tue Jul 21 17:56:03 +0000 2020",
+		"id": 1285346849298292700,
+		"id_str": "1285634659901157376",
+		"full_text": "https://t.co/36NiN1xi88",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1285346834614034400,
+					"id_str": "1285346834614034432",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/EdZ4ItJXoAAhUs0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdZ4ItJXoAAhUs0.jpg",
+					"url": "https://t.co/36NiN1xi88",
+					"display_url": "pic.twitter.com/36NiN1xi88",
+					"expanded_url": "https://twitter.com/Shitty_Future/status/1285346849298292736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 729,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 729,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 516,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1285346834614034400,
+					"id_str": "1285346834614034432",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/EdZ4ItJXoAAhUs0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EdZ4ItJXoAAhUs0.jpg",
+					"url": "https://t.co/36NiN1xi88",
+					"display_url": "pic.twitter.com/36NiN1xi88",
+					"expanded_url": "https://twitter.com/Shitty_Future/status/1285346849298292736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 729,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 729,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 516,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4727899720,
+			"id_str": "4727899720",
+			"name": "Shitty Future",
+			"screen_name": "Shitty_Future",
+			"location": "",
+			"description": "A future where unexpectedly resurrected Arthur C. Clarke meets the guy who sold his kidney to buy an iPhone",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 62806,
+			"friends_count": 6,
+			"listed_count": 459,
+			"created_at": "Wed Jan 06 20:18:07 +0000 2016",
+			"favourites_count": 76,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 743,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/684836317553901572/KTsgdTN1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/684836317553901572/KTsgdTN1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4727899720/1487705219",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 877,
+		"favorite_count": 3601,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/36NiN1xi88"
+	},
+	{
 		"created_at": "Tue Jul 21 17:03:44 +0000 2020",
 		"id": 1285621493997752300,
 		"id_str": "1285621493997752320",
