7cc24058277d99063ebd450ca7cf31955236020c
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-07-23 07:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71896,
-	"created_at": "Wed Jul 22 11:21:07 +0000 2020",
+	"tweets": 71897,
+	"created_at": "Thu Jul 23 05:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,182 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 23 05:05:34 +0000 2020",
+		"id": 1286165536704012300,
+		"id_str": "1286165536704012288",
+		"full_text": "@newslettercrew @Kamphey @KintuLabs @amlewis4 @TalktoHenryJ @jspector @stephsmithio Those are some big names to be surrounded by. Thanks!",
+		"truncated": false,
+		"display_text_range": [
+			84,
+			137
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "newslettercrew",
+					"name": "Newsletter Crew",
+					"id": 1108427615877115900,
+					"id_str": "1108427615877115907",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "Kamphey",
+					"name": "Kamphey",
+					"id": 880930110,
+					"id_str": "880930110",
+					"indices": [
+						16,
+						24
+					]
+				},
+				{
+					"screen_name": "KintuLabs",
+					"name": "Chris ✈️",
+					"id": 14853133,
+					"id_str": "14853133",
+					"indices": [
+						25,
+						35
+					]
+				},
+				{
+					"screen_name": "amlewis4",
+					"name": "Ari Lewis",
+					"id": 221966379,
+					"id_str": "221966379",
+					"indices": [
+						36,
+						45
+					]
+				},
+				{
+					"screen_name": "TalktoHenryJ",
+					"name": "Henry Johnson",
+					"id": 1105624200,
+					"id_str": "1105624200",
+					"indices": [
+						46,
+						59
+					]
+				},
+				{
+					"screen_name": "jspector",
+					"name": "Josh Spector",
+					"id": 15708823,
+					"id_str": "15708823",
+					"indices": [
+						60,
+						69
+					]
+				},
+				{
+					"screen_name": "stephsmithio",
+					"name": "Steph Smith",
+					"id": 626691391,
+					"id_str": "626691391",
+					"indices": [
+						70,
+						83
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1286133531865907200,
+		"in_reply_to_status_id_str": "1286133531865907201",
+		"in_reply_to_user_id": 1108427615877115900,
+		"in_reply_to_user_id_str": "1108427615877115907",
+		"in_reply_to_screen_name": "newslettercrew",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8951,
+			"friends_count": 544,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6568,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62355,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@newslettercrew @Kamphey @KintuLabs @amlewis4 @TalktoHenryJ @jspector @stephsmithio Those are some big names to be surrounded by. Thanks!"
+	},
+	{
 		"created_at": "Wed Jul 22 10:56:25 +0000 2020",
 		"id": 1285891443459010600,
 		"id_str": "1285891443459010561",
