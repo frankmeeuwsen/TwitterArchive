ec958fa865b0cd13d725fd96bc5bc6f21458d9ff
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-07-30 10:21:10 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71932,
-	"created_at": "Wed Jul 29 22:21:06 +0000 2020",
+	"tweets": 71933,
+	"created_at": "Thu Jul 30 08:21:07 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_07.js",
 		"year": 2020,
 		"var_name": "tweets_2020_07",
-		"tweet_count": 157,
+		"tweet_count": 158,
 		"month": 7
 	},
 	{
