6493fe006068d713c968c3f623b6f0cabadef262
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-08-07 22:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71962,
-	"created_at": "Fri Aug 07 19:21:05 +0000 2020",
+	"tweets": 71963,
+	"created_at": "Fri Aug 07 20:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 07 20:10:39 +0000 2020",
+		"id": 1291829125242671000,
+		"id_str": "1291829125242671106",
+		"full_text": "Gratis advies voor de volgende persconferentie: Alles is uit te leggen met de Wu-Tang Clan! https://t.co/3GYSqJRKcc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3GYSqJRKcc",
+					"expanded_url": "https://twitter.com/Poodle_Soup/status/1287811182435467266",
+					"display_url": "twitter.com/Poodle_Soup/st…",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8960,
+			"friends_count": 552,
+			"listed_count": 570,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6605,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62441,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1287811182435467300,
+		"quoted_status_id_str": "1287811182435467266",
+		"quoted_status_permalink": {
+			"url": "https://t.co/3GYSqJRKcc",
+			"expanded": "https://twitter.com/Poodle_Soup/status/1287811182435467266",
+			"display": "twitter.com/Poodle_Soup/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jul 27 18:04:46 +0000 2020",
+			"id": 1287811182435467300,
+			"id_str": "1287811182435467266",
+			"full_text": "Het is weer verwarring troef, dus allow me to probeer een beke duidelijkheid te scheppen met behulp van de Wu-Tang Clan. Stel, ge zijt GZA aka the Genius. https://t.co/4UFaaSoxBc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				154
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1287811175703609300,
+						"id_str": "1287811175703609345",
+						"indices": [
+							155,
+							178
+						],
+						"media_url": "http://pbs.twimg.com/media/Ed85cPGXgAEGluT.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ed85cPGXgAEGluT.jpg",
+						"url": "https://t.co/4UFaaSoxBc",
+						"display_url": "pic.twitter.com/4UFaaSoxBc",
+						"expanded_url": "https://twitter.com/Poodle_Soup/status/1287811182435467266/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1118,
+								"h": 1127,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1118,
+								"h": 1127,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 675,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1287811175703609300,
+						"id_str": "1287811175703609345",
+						"indices": [
+							155,
+							178
+						],
+						"media_url": "http://pbs.twimg.com/media/Ed85cPGXgAEGluT.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ed85cPGXgAEGluT.jpg",
+						"url": "https://t.co/4UFaaSoxBc",
+						"display_url": "pic.twitter.com/4UFaaSoxBc",
+						"expanded_url": "https://twitter.com/Poodle_Soup/status/1287811182435467266/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1118,
+								"h": 1127,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1118,
+								"h": 1127,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 675,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 387825721,
+				"id_str": "387825721",
+				"name": "Stephanie Dehennin",
+				"screen_name": "Poodle_Soup",
+				"location": "Antwerpen, België",
+				"description": "Kids & Science illustrator - Represented by MB Artists for US - ‘Ik heb een Vraag’ - Nerdland podcaster - Geek Rubriek @Charliemagazine- tweets NL/ENG-🖖#IDIC",
+				"url": "https://t.co/BscyddeCc1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/BscyddeCc1",
+								"expanded_url": "http://www.poodlesoup.be",
+								"display_url": "poodlesoup.be",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4611,
+				"friends_count": 1660,
+				"listed_count": 56,
+				"created_at": "Sun Oct 09 18:47:19 +0000 2011",
+				"favourites_count": 38636,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 14355,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FBCA9F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1039469639963693056/v0-gagYS_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1039469639963693056/v0-gagYS_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/387825721/1565190697",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 42,
+			"favorite_count": 168,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Gratis advies voor de volgende persconferentie: Alles is uit te leggen met de Wu-Tang Clan! https://t.co/3GYSqJRKcc"
+	},
+	{
 		"created_at": "Fri Aug 07 18:58:29 +0000 2020",
 		"id": 1291810965026222000,
 		"id_str": "1291810965026222085",
