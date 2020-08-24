5fbbb3127b609cca6e16adc8ca30f04467abe8e5
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-08-24 22:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71982,
-	"created_at": "Mon Aug 24 14:21:05 +0000 2020",
+	"tweets": 71983,
+	"created_at": "Mon Aug 24 20:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 24 19:42:15 +0000 2020",
+		"id": 1297980165058777000,
+		"id_str": "1297982574132961280",
+		"full_text": "Keanu Reeves and Alex @Winter are back as Bill &amp; Ted, and totally ready to Face the Music! Pre-order your copy of Bill &amp; Ted Face the Music to receive bonus content! The film will also be available On Demand and in theaters FRIDAY, dudes! https://t.co/8YSQdnhi4C https://t.co/cYmJ4UZdOs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Winter",
+					"name": "Alex Winter 😷",
+					"id": 112243614,
+					"id_str": "112243614",
+					"indices": [
+						22,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8YSQdnhi4C",
+					"expanded_url": "http://billandted3.com/watch",
+					"display_url": "billandted3.com/watch",
+					"indices": [
+						247,
+						270
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1297980038428516400,
+					"id_str": "1297980038428516353",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/EgNZ98wUYAEmNYI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgNZ98wUYAEmNYI.jpg",
+					"url": "https://t.co/cYmJ4UZdOs",
+					"display_url": "pic.twitter.com/cYmJ4UZdOs",
+					"expanded_url": "https://twitter.com/BillandTed3/status/1297980165058777088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1297980038428516400,
+					"id_str": "1297980038428516353",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/EgNZ98wUYAEmNYI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgNZ98wUYAEmNYI.jpg",
+					"url": "https://t.co/cYmJ4UZdOs",
+					"display_url": "pic.twitter.com/cYmJ4UZdOs",
+					"expanded_url": "https://twitter.com/BillandTed3/status/1297980165058777088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 284929130,
+			"id_str": "284929130",
+			"name": "Bill & Ted 3",
+			"screen_name": "BillandTed3",
+			"location": "San Dimas, California",
+			"description": "Yes way, dude! Excellence returns August 28. 🎸⚡️ PRE-ORDER NOW: https://t.co/15cOzuAHwZ",
+			"url": "https://t.co/PQ2dTPb3QE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PQ2dTPb3QE",
+							"expanded_url": "http://BillAndTed3.com",
+							"display_url": "BillAndTed3.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/15cOzuAHwZ",
+							"expanded_url": "http://billandted3.com/watch",
+							"display_url": "billandted3.com/watch",
+							"indices": [
+								64,
+								87
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 131654,
+			"friends_count": 40,
+			"listed_count": 249,
+			"created_at": "Wed Apr 20 06:42:55 +0000 2011",
+			"favourites_count": 2288,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 966,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1270249400778936325/UPkxqBFA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270249400778936325/UPkxqBFA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/284929130/1596735610",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 46,
+		"favorite_count": 221,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Keanu Reeves and Alex @Winter are back as Bill &amp; Ted, and totally ready to Face the Music! Pre-order your copy of Bill &amp; Ted Face the Music to receive bonus content! The film will also be available On Demand and in theaters FRIDAY, dudes! https://t.co/8YSQdnhi4C https://t.co/cYmJ4UZdOs"
+	},
+	{
 		"created_at": "Mon Aug 24 14:18:17 +0000 2020",
 		"id": 1297901043276284000,
 		"id_str": "1297901043276283904",
