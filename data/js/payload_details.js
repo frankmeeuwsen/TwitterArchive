1bc5c2d2721ca3fbb71a831a135edbfd3bf3b18d
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-08-24 16:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71981,
-	"created_at": "Mon Aug 24 07:21:04 +0000 2020",
+	"tweets": 71982,
+	"created_at": "Mon Aug 24 14:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,293 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 24 14:18:17 +0000 2020",
+		"id": 1297901043276284000,
+		"id_str": "1297901043276283904",
+		"full_text": "Het wordt tijd voor een update van mijn artikel over nieuwsbrief-directories. De laatste maanden zijn er weer een paar bijgekomen. Leuk! https://t.co/vvjYLGhI5U",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vvjYLGhI5U",
+					"expanded_url": "https://twitter.com/RobertJBye/status/1297878573500583937",
+					"display_url": "twitter.com/RobertJBye/sta…",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8950,
+			"friends_count": 553,
+			"listed_count": 570,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6624,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62467,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1297878573500584000,
+		"quoted_status_id_str": "1297878573500583937",
+		"quoted_status_permalink": {
+			"url": "https://t.co/vvjYLGhI5U",
+			"expanded": "https://twitter.com/RobertJBye/status/1297878573500583937",
+			"display": "twitter.com/RobertJBye/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Aug 24 12:48:59 +0000 2020",
+			"id": 1297878573500584000,
+			"id_str": "1297878573500583937",
+			"full_text": "My latest #NoCode side project is live! It's a place to discover and upvote the best newsletters on the web. \n\nCurrently featured are newsletters from @anthilemoon, @antdke, @robhope, @IndieHackers, and @theskimm \n\nhttps://t.co/iUGPtN6Thc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				238
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "NoCode",
+						"indices": [
+							10,
+							17
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "anthilemoon",
+						"name": "Anne-Laure Le Cunff",
+						"id": 15996668,
+						"id_str": "15996668",
+						"indices": [
+							151,
+							163
+						]
+					},
+					{
+						"screen_name": "antdke",
+						"name": "Anthony Diké",
+						"id": 884075589577134100,
+						"id_str": "884075589577134081",
+						"indices": [
+							165,
+							172
+						]
+					},
+					{
+						"screen_name": "robhope",
+						"name": "Rob Hope 🇿🇦",
+						"id": 14931736,
+						"id_str": "14931736",
+						"indices": [
+							174,
+							182
+						]
+					},
+					{
+						"screen_name": "IndieHackers",
+						"name": "Indie Hackers",
+						"id": 756326958946922500,
+						"id_str": "756326958946922496",
+						"indices": [
+							184,
+							197
+						]
+					},
+					{
+						"screen_name": "theskimm",
+						"name": "theSkimm",
+						"id": 515034648,
+						"id_str": "515034648",
+						"indices": [
+							203,
+							212
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/iUGPtN6Thc",
+						"expanded_url": "https://www.pigeonnewsletters.com",
+						"display_url": "pigeonnewsletters.com",
+						"indices": [
+							215,
+							238
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1283516355757723600,
+				"id_str": "1283516355757723656",
+				"name": "Robert Bye",
+				"screen_name": "RobertJBye",
+				"location": "NYC via London",
+				"description": "Product Manager at @Fueled | Previously @Google & other places | Co-founded @MorramaDesign | Teaching #nocode via @howtoproductco | Jesus Follower | Husband",
+				"url": "https://t.co/rdMTFxO7BQ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/rdMTFxO7BQ",
+								"expanded_url": "http://robertbye.com",
+								"display_url": "robertbye.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 189,
+				"friends_count": 298,
+				"listed_count": 6,
+				"created_at": "Wed Jul 15 21:38:51 +0000 2020",
+				"favourites_count": 260,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 214,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1285728589447598080/GJcsGuVx_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1285728589447598080/GJcsGuVx_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1283516355757723656/1597092959",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 12,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Het wordt tijd voor een update van mijn artikel over nieuwsbrief-directories. De laatste maanden zijn er weer een paar bijgekomen. Leuk! https://t.co/vvjYLGhI5U"
+	},
+	{
 		"created_at": "Mon Aug 24 05:38:04 +0000 2020",
 		"id": 1297770126092181500,
 		"id_str": "1297770126092181505",
