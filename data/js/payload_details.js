1a6754f7b8465fe0cfc3e3cbd4a8b316192c3a74
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-08-28 21:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71988,
-	"created_at": "Thu Aug 27 07:21:05 +0000 2020",
+	"tweets": 71989,
+	"created_at": "Fri Aug 28 19:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 28 18:28:02 +0000 2020",
+		"id": 1299413449487900700,
+		"id_str": "1299413449487900674",
+		"full_text": "I’d buy it in a heartbeat. Unfortunately the movie moguls are less excellent for Europe. It’s not available here. Why not? https://t.co/MKB9qUhZ5V",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MKB9qUhZ5V",
+					"expanded_url": "https://twitter.com/BillandTed3/status/1299411764010987523",
+					"display_url": "twitter.com/BillandTed3/st…",
+					"indices": [
+						123,
+						146
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8944,
+			"friends_count": 555,
+			"listed_count": 570,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6632,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62474,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1299411764010987500,
+		"quoted_status_id_str": "1299411764010987523",
+		"quoted_status_permalink": {
+			"url": "https://t.co/MKB9qUhZ5V",
+			"expanded": "https://twitter.com/BillandTed3/status/1299411764010987523",
+			"display": "twitter.com/BillandTed3/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Aug 28 18:21:21 +0000 2020",
+			"id": 1299411764010987500,
+			"id_str": "1299411764010987523",
+			"full_text": "DUDE. Our film was totally just Certified Fresh by @RottenTomatoes! *Air Guitar Shred*",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				86
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "RottenTomatoes",
+						"name": "Rotten Tomatoes",
+						"id": 20108560,
+						"id_str": "20108560",
+						"indices": [
+							51,
+							66
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 284929130,
+				"id_str": "284929130",
+				"name": "Bill & Ted 3",
+				"screen_name": "BillandTed3",
+				"location": "San Dimas, California",
+				"description": "Yes way, dude! Excellence has returned. 🎸⚡️ Available On Demand and in theaters NOW: https://t.co/15cOzuSiVz",
+				"url": "https://t.co/PQ2dTOTsZ6",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/PQ2dTOTsZ6",
+								"expanded_url": "http://BillAndTed3.com",
+								"display_url": "BillAndTed3.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/15cOzuSiVz",
+								"expanded_url": "http://billandted3.com/watch",
+								"display_url": "billandted3.com/watch",
+								"indices": [
+									85,
+									108
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 132889,
+				"friends_count": 41,
+				"listed_count": 253,
+				"created_at": "Wed Apr 20 06:42:55 +0000 2011",
+				"favourites_count": 2479,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 1045,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1270249400778936325/UPkxqBFA_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270249400778936325/UPkxqBFA_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/284929130/1598587150",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 127,
+			"favorite_count": 1134,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’d buy it in a heartbeat. Unfortunately the movie moguls are less excellent for Europe. It’s not available here. Why not? https://t.co/MKB9qUhZ5V"
+	},
+	{
 		"created_at": "Thu Aug 27 07:11:39 +0000 2020",
 		"id": 1298880843579039700,
 		"id_str": "1298880843579039745",
