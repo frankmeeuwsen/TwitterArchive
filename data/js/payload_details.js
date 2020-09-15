6bd0a2c3db872b6dcf97021c458063109668088b
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-09-15 09:21:08 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72069,
-	"created_at": "Mon Sep 14 20:21:06 +0000 2020",
+	"tweets": 72070,
+	"created_at": "Tue Sep 15 07:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,347 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Tue Sep 15 07:13:43 +0000 2020",
+		"id": 1305766733337243600,
+		"id_str": "1305766733337243648",
+		"full_text": "Be like Dave. https://t.co/KGtT0J6uGw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/KGtT0J6uGw",
+					"expanded_url": "https://twitter.com/foofighters/status/1305517568946438144",
+					"display_url": "twitter.com/foofighters/st…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8944,
+			"friends_count": 567,
+			"listed_count": 570,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6679,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62568,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1305517568946438100,
+		"quoted_status_id_str": "1305517568946438144",
+		"quoted_status_permalink": {
+			"url": "https://t.co/KGtT0J6uGw",
+			"expanded": "https://twitter.com/foofighters/status/1305517568946438144",
+			"display": "twitter.com/foofighters/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Sep 14 14:43:38 +0000 2020",
+			"id": 1305517568946438100,
+			"id_str": "1305517568946438144",
+			"full_text": "Ok @Nandi_Bushell....Round 2! Every superhero needs a theme song. Here’s one for you! Mad props to The Grohlettes for the background vocals. https://t.co/js9xBasbpw",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				140
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Nandi_Bushell",
+						"name": "Nandi Bushell",
+						"id": 899613012851654700,
+						"id_str": "899613012851654656",
+						"indices": [
+							3,
+							17
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1305514849300934700,
+						"id_str": "1305514849300934656",
+						"indices": [
+							141,
+							164
+						],
+						"media_url": "http://pbs.twimg.com/media/Eh4gLSCUMAAO21x.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eh4gLSCUMAAO21x.jpg",
+						"url": "https://t.co/js9xBasbpw",
+						"display_url": "pic.twitter.com/js9xBasbpw",
+						"expanded_url": "https://twitter.com/foofighters/status/1305517568946438144/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1305514849300934700,
+						"id_str": "1305514849300934656",
+						"indices": [
+							141,
+							164
+						],
+						"media_url": "http://pbs.twimg.com/media/Eh4gLSCUMAAO21x.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Eh4gLSCUMAAO21x.jpg",
+						"url": "https://t.co/js9xBasbpw",
+						"display_url": "pic.twitter.com/js9xBasbpw",
+						"expanded_url": "https://twitter.com/foofighters/status/1305517568946438144/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1920,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 114573,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1305514849300934656/pl/PRA8CkWHmDFfz5xY.m3u8?tag=13"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1305514849300934656/vid/640x360/xh502VCeLLKXD5Q_.mp4?tag=13"
+								},
+								{
+									"bitrate": 288000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1305514849300934656/vid/480x270/QAcwwebtCjUumpw6.mp4?tag=13"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1305514849300934656/vid/1280x720/QXa_uP_ruuZqjqG6.mp4?tag=13"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "Round 2! ",
+							"description": "",
+							"embeddable": true,
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19081001,
+				"id_str": "19081001",
+				"name": "Foo Fighters",
+				"screen_name": "foofighters",
+				"location": "",
+				"description": "Rock band",
+				"url": "https://t.co/bTvwIW5FR2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/bTvwIW5FR2",
+								"expanded_url": "http://www.foofighters.com",
+								"display_url": "foofighters.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3099997,
+				"friends_count": 14375,
+				"listed_count": 14648,
+				"created_at": "Fri Jan 16 19:26:30 +0000 2009",
+				"favourites_count": 1632,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 3699,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1254777897664954371/yoUbAPOg_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1254777897664954371/yoUbAPOg_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19081001/1587997339",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "141414",
+				"profile_text_color": "4E4D4F",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 15811,
+			"favorite_count": 76266,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Be like Dave. https://t.co/KGtT0J6uGw"
+	},
+	{
 		"created_at": "Mon Sep 14 20:20:25 +0000 2020",
 		"id": 1305602324832956400,
 		"id_str": "1305602324832956420",
