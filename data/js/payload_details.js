9b7a2a31d62cbbdc91a1fc870d25a9a279aa8a3e
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-09-20 22:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72098,
-	"created_at": "Sun Sep 20 17:21:06 +0000 2020",
+	"tweets": 72099,
+	"created_at": "Sun Sep 20 20:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,415 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sun Sep 20 19:58:13 +0000 2020",
+		"id": 1307771063187759000,
+		"id_str": "1307771063187759106",
+		"full_text": "Dit is cool. Een podcast (en wát voor een) wordt een Netflix serie (/cc @ehpo @elger) https://t.co/uSFfZ8LFjZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ehpo",
+					"name": "Niels Aalberts",
+					"id": 20199054,
+					"id_str": "20199054",
+					"indices": [
+						72,
+						77
+					]
+				},
+				{
+					"screen_name": "elger",
+					"name": "Elger van der Wel",
+					"id": 14142375,
+					"id_str": "14142375",
+					"indices": [
+						78,
+						84
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/uSFfZ8LFjZ",
+					"expanded_url": "https://twitter.com/HrishiHirway/status/1306598172567904267",
+					"display_url": "twitter.com/HrishiHirway/s…",
+					"indices": [
+						86,
+						109
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8935,
+			"friends_count": 572,
+			"listed_count": 570,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6685,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62606,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1306598172567904300,
+		"quoted_status_id_str": "1306598172567904267",
+		"quoted_status_permalink": {
+			"url": "https://t.co/uSFfZ8LFjZ",
+			"expanded": "https://twitter.com/HrishiHirway/status/1306598172567904267",
+			"display": "twitter.com/HrishiHirway/s…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Sep 17 14:17:34 +0000 2020",
+			"id": 1306598172567904300,
+			"id_str": "1306598172567904267",
+			"full_text": "I still don’t fully believe it but @SongExploder, a podcast I started in my bedroom!, is going to be a @netflix series.  Featuring @aliciakeys, @Lin_Manuel, @remhq, and @tydollasign. Coming October 2nd! https://t.co/8FdYSS60dV",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				202
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "SongExploder",
+						"name": "Song Exploder",
+						"id": 2249590315,
+						"id_str": "2249590315",
+						"indices": [
+							35,
+							48
+						]
+					},
+					{
+						"screen_name": "netflix",
+						"name": "Netflix",
+						"id": 16573941,
+						"id_str": "16573941",
+						"indices": [
+							103,
+							111
+						]
+					},
+					{
+						"screen_name": "aliciakeys",
+						"name": "Alicia Keys",
+						"id": 35094637,
+						"id_str": "35094637",
+						"indices": [
+							131,
+							142
+						]
+					},
+					{
+						"screen_name": "Lin_Manuel",
+						"name": "Lin-Manuel Miranda",
+						"id": 79923701,
+						"id_str": "79923701",
+						"indices": [
+							144,
+							155
+						]
+					},
+					{
+						"screen_name": "remhq",
+						"name": "R.E.M. HQ",
+						"id": 14100619,
+						"id_str": "14100619",
+						"indices": [
+							157,
+							163
+						]
+					},
+					{
+						"screen_name": "tydollasign",
+						"name": "Ty Dolla $ign",
+						"id": 25932445,
+						"id_str": "25932445",
+						"indices": [
+							169,
+							181
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1306528851078471700,
+						"id_str": "1306528851078471680",
+						"indices": [
+							203,
+							226
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"url": "https://t.co/8FdYSS60dV",
+						"display_url": "pic.twitter.com/8FdYSS60dV",
+						"expanded_url": "https://twitter.com/HrishiHirway/status/1306598172567904267/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1306528851078471700,
+						"id_str": "1306528851078471680",
+						"indices": [
+							203,
+							226
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1306528851078471680/pu/img/mCjAz5rdHc6-ZrX2.jpg",
+						"url": "https://t.co/8FdYSS60dV",
+						"display_url": "pic.twitter.com/8FdYSS60dV",
+						"expanded_url": "https://twitter.com/HrishiHirway/status/1306598172567904267/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 102978,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/pl/ukst7ZYoSCkMAvpK.m3u8?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/vid/640x360/NKj4OJKqfZbf5i5j.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/vid/1280x720/O651TleIpPVCan2L.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1306528851078471680/pu/vid/480x270/vZYePQZLHh_KAxpD.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18695490,
+				"id_str": "18695490",
+				"name": "Hrishikesh Hirway",
+				"screen_name": "HrishiHirway",
+				"location": "Los Angeles, CA",
+				"description": "I make music & podcasts. host/creator of @songexploder · co-host/producer of Home Cooking",
+				"url": "https://t.co/Cemmd7hlN4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Cemmd7hlN4",
+								"expanded_url": "http://hrishikesh.co",
+								"display_url": "hrishikesh.co",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 66019,
+				"friends_count": 1166,
+				"listed_count": 547,
+				"created_at": "Tue Jan 06 21:08:58 +0000 2009",
+				"favourites_count": 36104,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 9850,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1267838089931546631/P-R7ZEbc_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267838089931546631/P-R7ZEbc_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18695490/1591140860",
+				"profile_link_color": "333344",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1591,
+			"favorite_count": 16343,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Dit is cool. Een podcast (en wát voor een) wordt een Netflix serie (/cc @ehpo @elger) https://t.co/uSFfZ8LFjZ"
+	},
+	{
 		"created_at": "Sun Sep 20 17:09:49 +0000 2020",
 		"id": 1307728686012149800,
 		"id_str": "1307728686012149760",
