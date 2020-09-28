dbed0172c59cc2b99582630906aea603c025fde8
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-09-28 12:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72129,
-	"created_at": "Mon Sep 28 06:21:06 +0000 2020",
+	"tweets": 72130,
+	"created_at": "Mon Sep 28 10:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 28 09:38:52 +0000 2020",
+		"id": 1310514303880040400,
+		"id_str": "1310514303880040449",
+		"full_text": "@Gijs_Molsbergen Met mooie features in het register: \n- \"Als je dit algoritme leuk vindt, kunnen we je de volgende aanbevelen\"\n- \"Deze algoritmes vinden je vrienden leuk\"\n- \"Klik hier om de algoritmes in je persoonlijke feed te zetten\"",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			235
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Gijs_Molsbergen",
+					"name": "𝔾𝕚𝕛𝕤 𝕄𝕠𝕝𝕤𝕓𝕖𝕣𝕘𝕖𝕟",
+					"id": 25256516,
+					"id_str": "25256516",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1310503427814948900,
+		"in_reply_to_status_id_str": "1310503427814948864",
+		"in_reply_to_user_id": 25256516,
+		"in_reply_to_user_id_str": "25256516",
+		"in_reply_to_screen_name": "Gijs_Molsbergen",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8932,
+			"friends_count": 579,
+			"listed_count": 569,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6704,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62644,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@Gijs_Molsbergen Met mooie features in het register: \n- \"Als je dit algoritme leuk vindt, kunnen we je de volgende aanbevelen\"\n- \"Deze algoritmes vinden je vrienden leuk\"\n- \"Klik hier om de algoritmes in je persoonlijke feed te zetten\""
+	},
+	{
 		"created_at": "Mon Sep 28 05:56:49 +0000 2020",
 		"id": 1310458420609327000,
 		"id_str": "1310458420609327105",
