76d4c48e053fc81da051eee48ffe622abdca700a
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-10-12 08:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72227,
-	"created_at": "Sun Oct 11 20:21:05 +0000 2020",
+	"tweets": 72228,
+	"created_at": "Mon Oct 12 06:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 12 05:52:54 +0000 2020",
+		"id": 1315524008561971200,
+		"id_str": "1315530864248467456",
+		"full_text": "This bot went online 1 year ago. It's run over 8849 BASIC programs since then. Here's the top 100 ranked by ❤️ https://t.co/dYiXbK5Aar",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1315516520865263600,
+					"id_str": "1315516520865263618",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1315516520865263618/pu/img/CXMhTUxAe30MaxX9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1315516520865263618/pu/img/CXMhTUxAe30MaxX9.jpg",
+					"url": "https://t.co/dYiXbK5Aar",
+					"display_url": "pic.twitter.com/dYiXbK5Aar",
+					"expanded_url": "https://twitter.com/bbcmicrobot/status/1315524008561971200/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 902,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 543,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 902,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1315516520865263600,
+					"id_str": "1315516520865263618",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1315516520865263618/pu/img/CXMhTUxAe30MaxX9.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1315516520865263618/pu/img/CXMhTUxAe30MaxX9.jpg",
+					"url": "https://t.co/dYiXbK5Aar",
+					"display_url": "pic.twitter.com/dYiXbK5Aar",
+					"expanded_url": "https://twitter.com/bbcmicrobot/status/1315524008561971200/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 902,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 543,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 902,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							451,
+							360
+						],
+						"duration_millis": 125383,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1315516520865263618/pu/pl/79m0z3FH9oFPa-W3.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1315516520865263618/pu/vid/902x720/ySTaxm5rlYdPOzPH.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1315516520865263618/pu/vid/338x270/FiSZL_rE-ESA7zCY.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1315516520865263618/pu/vid/450x360/1QPENHkWij6EldVv.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1225968288842182700,
+			"id_str": "1225968288842182657",
+			"name": "BBC Micro 👻 !BOOt 🦇",
+			"screen_name": "bbcmicrobot",
+			"location": "",
+			"description": "10 PRINT \"Runs your tweet on an 1980s computer emulation\"\n20 GOTO 10 \nUnofficial. Bad words = auto 🚫 | by @DominicPajak | src https://t.co/hpukHoHB9i",
+			"url": "https://t.co/z90MkPPFJ8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/z90MkPPFJ8",
+							"expanded_url": "http://bot.dompajak.com",
+							"display_url": "bot.dompajak.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/hpukHoHB9i",
+							"expanded_url": "http://github.com/8bitkick/BBCMicrobot",
+							"display_url": "github.com/8bitkick/BBCMi…",
+							"indices": [
+								126,
+								149
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 5972,
+			"friends_count": 19,
+			"listed_count": 76,
+			"created_at": "Sat Feb 08 02:23:40 +0000 2020",
+			"favourites_count": 10058,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10241,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1225996399113728002/N6ogjfYG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225996399113728002/N6ogjfYG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1225968288842182657/1581568715",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24,
+		"favorite_count": 72,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This bot went online 1 year ago. It's run over 8849 BASIC programs since then. Here's the top 100 ranked by ❤️ https://t.co/dYiXbK5Aar"
+	},
+	{
 		"created_at": "Sun Oct 11 19:23:32 +0000 2020",
 		"id": 1315372482375884800,
 		"id_str": "1315372482375884801",
