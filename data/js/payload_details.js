a1a944c153cd6c32857cd04452a5d115096e54fa
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-10-15 21:21:07 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72247,
-	"created_at": "Thu Oct 15 14:21:05 +0000 2020",
+	"tweets": 72248,
+	"created_at": "Thu Oct 15 19:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,183 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 15 18:42:32 +0000 2020",
+		"id": 1316811715829526500,
+		"id_str": "1316811715829526530",
+		"full_text": "@gvenk @martijnreintjes @bvdhaterd @erwblo @marcoderksen @puur Die is er natuurlijk gewoon al ;-) https://t.co/vRkKxgqqwN",
+		"truncated": false,
+		"display_text_range": [
+			63,
+			121
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gvenk",
+					"name": "Gerard van Enk",
+					"id": 1789221,
+					"id_str": "1789221",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"screen_name": "martijnreintjes",
+					"name": "Martijn Reintjes",
+					"id": 10302172,
+					"id_str": "10302172",
+					"indices": [
+						7,
+						23
+					]
+				},
+				{
+					"screen_name": "bvdhaterd",
+					"name": "1.5m 🚧 Bas van de Haterd 🚴‍♂️",
+					"id": 13165232,
+					"id_str": "13165232",
+					"indices": [
+						24,
+						34
+					]
+				},
+				{
+					"screen_name": "erwblo",
+					"name": "erwin blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						35,
+						42
+					]
+				},
+				{
+					"screen_name": "marcoderksen",
+					"name": "Marco Derksen",
+					"id": 1252681,
+					"id_str": "1252681",
+					"indices": [
+						43,
+						56
+					]
+				},
+				{
+					"screen_name": "puur",
+					"name": "puur",
+					"id": 5776422,
+					"id_str": "5776422",
+					"indices": [
+						57,
+						62
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/vRkKxgqqwN",
+					"expanded_url": "https://addons.mozilla.org/en-US/firefox/addon/min-twitter/",
+					"display_url": "addons.mozilla.org/en-US/firefox/…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1316744187333824500,
+		"in_reply_to_status_id_str": "1316744187333824513",
+		"in_reply_to_user_id": 1789221,
+		"in_reply_to_user_id_str": "1789221",
+		"in_reply_to_screen_name": "gvenk",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8932,
+			"friends_count": 587,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6823,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62789,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@gvenk @martijnreintjes @bvdhaterd @erwblo @marcoderksen @puur Die is er natuurlijk gewoon al ;-) https://t.co/vRkKxgqqwN"
+	},
+	{
 		"created_at": "Thu Oct 15 14:11:17 +0000 2020",
 		"id": 1316743453691252700,
 		"id_str": "1316743453691252738",
@@ -36,7 +213,7 @@ Grailbird.data.tweets_2020_10 =
 				},
 				{
 					"screen_name": "bvdhaterd",
-					"name": "1.5m 🚧 Bas van de Haterd 🚴‍♂️",
+					"name": "1.5m 🚧 Bas van de Haterd 🚴♂️",
 					"id": 13165232,
 					"id_str": "13165232",
 					"indices": [
