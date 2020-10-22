b1479c4fda1e666d1e7f9257b95610c7929efbd5
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-10-22 18:21:07 +0200

    Added 1 tweets

--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,389 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 22 16:05:46 +0000 2020",
+		"id": 1319308977113620500,
+		"id_str": "1319308977113620480",
+		"full_text": "Dit is toch fantastisch https://t.co/CjysSCFxfu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CjysSCFxfu",
+					"expanded_url": "https://twitter.com/ziyatong/status/1319295233008586752",
+					"display_url": "twitter.com/ziyatong/statu…",
+					"indices": [
+						24,
+						47
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8933,
+			"friends_count": 595,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6862,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62850,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1319295233008586800,
+		"quoted_status_id_str": "1319295233008586752",
+		"quoted_status_permalink": {
+			"url": "https://t.co/CjysSCFxfu",
+			"expanded": "https://twitter.com/ziyatong/status/1319295233008586752",
+			"display": "twitter.com/ziyatong/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Oct 22 15:11:09 +0000 2020",
+			"id": 1319295233008586800,
+			"id_str": "1319295233008586752",
+			"full_text": "Dostoevsky wrote: “Beauty Will Save the World.” It’s why during the darkness of the last year I’ve tweeted primarily about the beauty of the natural world. I genuinely believe when you fall in love with the world, you will fight to protect it. https://t.co/HPTyPlF9jd",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				267
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1317494299408265200,
+						"id_str": "1317494299408265216",
+						"indices": [
+							244,
+							267
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1317494299408265216/pu/img/E29NkVeqJ37BvqoQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1317494299408265216/pu/img/E29NkVeqJ37BvqoQ.jpg",
+						"url": "https://t.co/HPTyPlF9jd",
+						"display_url": "pic.twitter.com/HPTyPlF9jd",
+						"expanded_url": "https://twitter.com/OnlyNatureplace/status/1317494460268212224/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1317494460268212200,
+						"source_status_id_str": "1317494460268212224",
+						"source_user_id": 1200400239284244500,
+						"source_user_id_str": "1200400239284244480"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1317494299408265200,
+						"id_str": "1317494299408265216",
+						"indices": [
+							244,
+							267
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1317494299408265216/pu/img/E29NkVeqJ37BvqoQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1317494299408265216/pu/img/E29NkVeqJ37BvqoQ.jpg",
+						"url": "https://t.co/HPTyPlF9jd",
+						"display_url": "pic.twitter.com/HPTyPlF9jd",
+						"expanded_url": "https://twitter.com/OnlyNatureplace/status/1317494460268212224/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1317494460268212200,
+						"source_status_id_str": "1317494460268212224",
+						"source_user_id": 1200400239284244500,
+						"source_user_id_str": "1200400239284244480",
+						"video_info": {
+							"aspect_ratio": [
+								4,
+								5
+							],
+							"duration_millis": 13233,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1317494299408265216/pu/vid/360x450/edHmRmiPRsppQFsU.mp4?tag=10"
+								},
+								{
+									"bitrate": 632000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1317494299408265216/pu/vid/320x400/JfpAvVbcRTIoqd4X.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1317494299408265216/pu/vid/640x800/skAkVivmVB8vx0w0.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1317494299408265216/pu/pl/l9XaWtOsNS0FDick.m3u8?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false,
+							"source_user": {
+								"id": 1200400239284244500,
+								"id_str": "1200400239284244480",
+								"name": "🍁🍃🌴🌼 भूपेन्द्र ️🏝️🌈🌷🍀",
+								"screen_name": "OnlyNatureplace",
+								"location": "",
+								"description": "#Nature #Beautiful #Morning #Sunrise #Sunset #Photos #Evening",
+								"url": null,
+								"entities": {
+									"description": {
+										"urls": []
+									}
+								},
+								"protected": false,
+								"followers_count": 10416,
+								"friends_count": 4820,
+								"listed_count": 58,
+								"created_at": "Fri Nov 29 13:05:12 +0000 2019",
+								"favourites_count": 23746,
+								"utc_offset": null,
+								"time_zone": null,
+								"geo_enabled": true,
+								"verified": false,
+								"statuses_count": 4339,
+								"lang": null,
+								"contributors_enabled": false,
+								"is_translator": false,
+								"is_translation_enabled": false,
+								"profile_background_color": "F5F8FA",
+								"profile_background_image_url": null,
+								"profile_background_image_url_https": null,
+								"profile_background_tile": false,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/1294516604823248896/cdYC1pHH_normal.jpg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/1294516604823248896/cdYC1pHH_normal.jpg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/1200400239284244480/1600406690",
+								"profile_link_color": "1DA1F2",
+								"profile_sidebar_border_color": "C0DEED",
+								"profile_sidebar_fill_color": "DDEEF6",
+								"profile_text_color": "333333",
+								"profile_use_background_image": true,
+								"has_extended_profile": false,
+								"default_profile": true,
+								"default_profile_image": false,
+								"following": false,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "none"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 948104532,
+				"id_str": "948104532",
+				"name": "Earthling",
+				"screen_name": "ziyatong",
+				"location": "Laniakea Supercluster",
+				"description": "Author of The Reality Bubble. TV Presenter. Boards: @WWF Intl, @thenarwhalca, @PENCanada, @WeAnimals Fmr Vice Chair @WWFCanada",
+				"url": "https://t.co/NXdhsSCI5x",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/NXdhsSCI5x",
+								"expanded_url": "https://www.indiebound.org/book/9780735235564?aff=penguinrandom",
+								"display_url": "indiebound.org/book/978073523…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 40424,
+				"friends_count": 2263,
+				"listed_count": 666,
+				"created_at": "Wed Nov 14 17:03:42 +0000 2012",
+				"favourites_count": 45246,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 58344,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "09BEDE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1164720962677297152/lLWyZByw_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164720962677297152/lLWyZByw_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/948104532/1555102814",
+				"profile_link_color": "001F3F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 41,
+			"favorite_count": 181,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Dit is toch fantastisch https://t.co/CjysSCFxfu"
+	},
+	{
 		"created_at": "Thu Oct 22 13:00:28 +0000 2020",
 		"id": 1319262344279621600,
 		"id_str": "1319262344279621636",
