04c7f55efa85d7e5c3932550535718105adec58d
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-10-28 10:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72321,
-	"created_at": "Wed Oct 28 07:21:06 +0000 2020",
+	"tweets": 72322,
+	"created_at": "Wed Oct 28 09:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,334 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 28 08:27:20 +0000 2020",
+		"id": 1321367935475134500,
+		"id_str": "1321367935475134465",
+		"full_text": "Pretty awesome posse if you ask me! https://t.co/oKSfCmQ7Vg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oKSfCmQ7Vg",
+					"expanded_url": "https://twitter.com/listoryapp/status/1321224734659514376",
+					"display_url": "twitter.com/listoryapp/sta…",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8924,
+			"friends_count": 598,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6885,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1321224734659514400,
+		"quoted_status_id_str": "1321224734659514376",
+		"quoted_status_permalink": {
+			"url": "https://t.co/oKSfCmQ7Vg",
+			"expanded": "https://twitter.com/listoryapp/status/1321224734659514376",
+			"display": "twitter.com/listoryapp/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Oct 27 22:58:18 +0000 2020",
+			"id": 1321224734659514400,
+			"id_str": "1321224734659514376",
+			"full_text": "@jonastwt A few favorites:\n\n@scottlamb's i18n\n@JanelSGM's BrainPint\n@danoshinsky's Not A Newsletter\n@ryansager's Newspackr\n@PhilipSkogsberg \n@simonowens \n@revue's newsletter by @mschiefelbein \n@frankmeeuwsen's Thanks for Subscribing\n\n+ @austinkleon just sent over his favorite newsletters",
+			"truncated": false,
+			"display_text_range": [
+				10,
+				288
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "jonastwt",
+						"name": "Jonas",
+						"id": 943723808,
+						"id_str": "943723808",
+						"indices": [
+							0,
+							9
+						]
+					},
+					{
+						"screen_name": "scottlamb",
+						"name": "Scott Lamb",
+						"id": 8113622,
+						"id_str": "8113622",
+						"indices": [
+							28,
+							38
+						]
+					},
+					{
+						"screen_name": "JanelSGM",
+						"name": "Janel",
+						"id": 1688823739,
+						"id_str": "1688823739",
+						"indices": [
+							46,
+							55
+						]
+					},
+					{
+						"screen_name": "danoshinsky",
+						"name": "Dan Oshinsky",
+						"id": 16892321,
+						"id_str": "16892321",
+						"indices": [
+							68,
+							80
+						]
+					},
+					{
+						"screen_name": "ryansager",
+						"name": "Ryan Sager",
+						"id": 15473429,
+						"id_str": "15473429",
+						"indices": [
+							100,
+							110
+						]
+					},
+					{
+						"screen_name": "PhilipSkogsberg",
+						"name": "Philip Skogsberg",
+						"id": 1115661589,
+						"id_str": "1115661589",
+						"indices": [
+							123,
+							139
+						]
+					},
+					{
+						"screen_name": "simonowens",
+						"name": "Simon Owens",
+						"id": 16248619,
+						"id_str": "16248619",
+						"indices": [
+							141,
+							152
+						]
+					},
+					{
+						"screen_name": "revue",
+						"name": "Revue",
+						"id": 3001165942,
+						"id_str": "3001165942",
+						"indices": [
+							154,
+							160
+						]
+					},
+					{
+						"screen_name": "mschiefelbein",
+						"name": "Mark Schiefelbein",
+						"id": 16915121,
+						"id_str": "16915121",
+						"indices": [
+							177,
+							191
+						]
+					},
+					{
+						"screen_name": "frankmeeuwsen",
+						"name": "Frank Meeuwsen",
+						"id": 816203,
+						"id_str": "816203",
+						"indices": [
+							193,
+							207
+						]
+					},
+					{
+						"screen_name": "austinkleon",
+						"name": "Austin Kleon",
+						"id": 9698942,
+						"id_str": "9698942",
+						"indices": [
+							236,
+							248
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": 1321210807624106000,
+			"in_reply_to_status_id_str": "1321210807624105985",
+			"in_reply_to_user_id": 943723808,
+			"in_reply_to_user_id_str": "943723808",
+			"in_reply_to_screen_name": "jonastwt",
+			"user": {
+				"id": 1134860405862539300,
+				"id_str": "1134860405862539266",
+				"name": "Listory",
+				"screen_name": "ListoryApp",
+				"location": "Manhattan, NY",
+				"description": "Listory is the world’s first content refinery. \nPowered by human-curation to mine the best of the web.",
+				"url": "https://t.co/m0CIKIrysc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/m0CIKIrysc",
+								"expanded_url": "https://www.listory.com/en/",
+								"display_url": "listory.com/en/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 360,
+				"friends_count": 2225,
+				"listed_count": 6,
+				"created_at": "Sat Jun 01 16:33:00 +0000 2019",
+				"favourites_count": 1091,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 678,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1270019809447731200/Wgd-o5Gw_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270019809447731200/Wgd-o5Gw_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1134860405862539266/1591631294",
+				"profile_link_color": "FF7572",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Pretty awesome posse if you ask me! https://t.co/oKSfCmQ7Vg"
+	},
+	{
 		"created_at": "Wed Oct 28 06:41:27 +0000 2020",
 		"id": 1321341288541728800,
 		"id_str": "1321341288541728768",
