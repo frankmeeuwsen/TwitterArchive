765c210f5262b98f750a655d921d327fe63dd69e
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-10-28 16:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72324,
-	"created_at": "Wed Oct 28 13:21:06 +0000 2020",
+	"tweets": 72325,
+	"created_at": "Wed Oct 28 15:21:07 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 28 15:20:13 +0000 2020",
+		"id": 1321466107446939600,
+		"id_str": "1321471842943705088",
+		"full_text": "Op zoek naar een leuke en interessante online communicatie stageplek op HBO/MBO-niveau in de media? Bij de VPRO is men op zoek naar een nieuwsgierige en ambiteuze redactie stagiair voor PublicSpaces. #vacature #stage #publicspaces https://t.co/6aFU6mrB5c",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			254
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "vacature",
+					"indices": [
+						200,
+						209
+					]
+				},
+				{
+					"text": "stage",
+					"indices": [
+						210,
+						216
+					]
+				},
+				{
+					"text": "publicspaces",
+					"indices": [
+						217,
+						230
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6aFU6mrB5c",
+					"expanded_url": "https://www.mediastages.nl/stages/online-redactie-en-research/nieuwsgierige-en-ambitieuze-redactie-stagiair-voor-publicspaces",
+					"display_url": "mediastages.nl/stages/online-…",
+					"indices": [
+						231,
+						254
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1252681,
+			"id_str": "1252681",
+			"name": "Marco Derksen",
+			"screen_name": "marcoderksen",
+			"location": "Arnhem, Netherlands ",
+			"description": "Oprichter/partner Upstream, Marketingfacts, Dagstage, AOG, HAN, VPRO, DDMA, Luxor Live, leiderschap bij digitale transformatie, onderwijs, fotografie en vader!",
+			"url": "https://t.co/XN0TBMzQnD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XN0TBMzQnD",
+							"expanded_url": "https://www.linkedin.com/in/mderksen/",
+							"display_url": "linkedin.com/in/mderksen/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 27556,
+			"friends_count": 5435,
+			"listed_count": 1423,
+			"created_at": "Thu Mar 15 22:44:18 +0000 2007",
+			"favourites_count": 11613,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 154718,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DBE9ED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1260459260858830848/H1Zyfo8O_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1260459260858830848/H1Zyfo8O_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1252681/1348906748",
+			"profile_link_color": "EB8415",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E6F6F9",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Op zoek naar een leuke en interessante online communicatie stageplek op HBO/MBO-niveau in de media? Bij de VPRO is men op zoek naar een nieuwsgierige en ambiteuze redactie stagiair voor PublicSpaces. #vacature #stage #publicspaces https://t.co/6aFU6mrB5c"
+	},
+	{
 		"created_at": "Wed Oct 28 12:58:48 +0000 2020",
 		"id": 1321436254412644400,
 		"id_str": "1321436254412644353",
