02e5f03d7107ffe06c5223553344c0b96f496a07
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-10-31 00:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72356,
-	"created_at": "Fri Oct 30 22:21:07 +0000 2020",
+	"tweets": 72357,
+	"created_at": "Fri Oct 30 23:21:07 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Fri Oct 30 23:13:29 +0000 2020",
+		"id": 1322256861953929200,
+		"id_str": "1322315718042570752",
+		"full_text": "Launched Newsletter Operating System!\n\n150+ hours of work went into this one.\n\nProblem: \nManaging a newsletter is time-consuming\n\nSolution: \nI've created a dashboard that helps save you hours in the curation, writing &amp; growth process\n\nMore below 👇 https://t.co/pUfkEhvGLj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			251
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1322250492026781700,
+					"id_str": "1322250492026781696",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/ElmT0SFXgAA7ndx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ElmT0SFXgAA7ndx.jpg",
+					"url": "https://t.co/pUfkEhvGLj",
+					"display_url": "pic.twitter.com/pUfkEhvGLj",
+					"expanded_url": "https://twitter.com/JanelSGM/status/1322256861953929217/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2024,
+							"h": 1012,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1322250492026781700,
+					"id_str": "1322250492026781696",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/ElmT0SFXgAA7ndx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ElmT0SFXgAA7ndx.jpg",
+					"url": "https://t.co/pUfkEhvGLj",
+					"display_url": "pic.twitter.com/pUfkEhvGLj",
+					"expanded_url": "https://twitter.com/JanelSGM/status/1322256861953929217/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2024,
+							"h": 1012,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1688823739,
+			"id_str": "1688823739",
+			"name": "Janel",
+			"screen_name": "JanelSGM",
+			"location": "Barcelona | DMs open",
+			"description": "Marketer + Maker. Exploring my curiosity at the intersection of business & tech. I read a lot & love no-code. Building @pheedbackco & https://t.co/YNGpctkuYO",
+			"url": "https://t.co/welCrY72dr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/welCrY72dr",
+							"expanded_url": "http://brainpint.com",
+							"display_url": "brainpint.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/YNGpctkuYO",
+							"expanded_url": "http://NewsletterOS.com",
+							"display_url": "NewsletterOS.com",
+							"indices": [
+								134,
+								157
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2199,
+			"friends_count": 337,
+			"listed_count": 78,
+			"created_at": "Wed Aug 21 17:13:36 +0000 2013",
+			"favourites_count": 3321,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2288,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1281644754040696832/kOqgYgKQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1281644754040696832/kOqgYgKQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1688823739/1447203274",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 59,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Launched Newsletter Operating System!\n\n150+ hours of work went into this one.\n\nProblem: \nManaging a newsletter is time-consuming\n\nSolution: \nI've created a dashboard that helps save you hours in the curation, writing &amp; growth process\n\nMore below 👇 https://t.co/pUfkEhvGLj"
+	},
+	{
 		"created_at": "Fri Oct 30 21:33:13 +0000 2020",
 		"id": 1322290487613706200,
 		"id_str": "1322290487613706240",
