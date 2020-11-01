9dcda015070414cce4431036e932f91ec76aebe8
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-11-01 19:21:05 +0100

    Added 1 tweets

--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,338 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sun Nov 01 17:36:57 +0000 2020",
+		"id": 1322955803356209200,
+		"id_str": "1322955803356209152",
+		"full_text": "Y’allqaida https://t.co/1r0rWs0lv6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1r0rWs0lv6",
+					"expanded_url": "https://twitter.com/chrisinsilico/status/1322926327477972994",
+					"display_url": "twitter.com/chrisinsilico/…",
+					"indices": [
+						11,
+						34
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8926,
+			"friends_count": 599,
+			"listed_count": 571,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6904,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 62933,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1322926327477973000,
+		"quoted_status_id_str": "1322926327477972994",
+		"quoted_status_permalink": {
+			"url": "https://t.co/1r0rWs0lv6",
+			"expanded": "https://twitter.com/chrisinsilico/status/1322926327477972994",
+			"display": "twitter.com/chrisinsilico/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Nov 01 15:39:49 +0000 2020",
+			"id": 1322926327477973000,
+			"id_str": "1322926327477972994",
+			"full_text": "Different country. Same energy. https://t.co/yZ3VGvEeIi",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				31
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1322926324181336000,
+						"id_str": "1322926324181336064",
+						"indices": [
+							32,
+							55
+						],
+						"media_url": "http://pbs.twimg.com/media/Elv6e5DXgAA4NPv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Elv6e5DXgAA4NPv.jpg",
+						"url": "https://t.co/yZ3VGvEeIi",
+						"display_url": "pic.twitter.com/yZ3VGvEeIi",
+						"expanded_url": "https://twitter.com/chrisinsilico/status/1322926327477972994/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 640,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 640,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1322926324181336000,
+						"id_str": "1322926324181336064",
+						"indices": [
+							32,
+							55
+						],
+						"media_url": "http://pbs.twimg.com/media/Elv6e5DXgAA4NPv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Elv6e5DXgAA4NPv.jpg",
+						"url": "https://t.co/yZ3VGvEeIi",
+						"display_url": "pic.twitter.com/yZ3VGvEeIi",
+						"expanded_url": "https://twitter.com/chrisinsilico/status/1322926327477972994/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 640,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 960,
+								"h": 640,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1322926324160352300,
+						"id_str": "1322926324160352256",
+						"indices": [
+							32,
+							55
+						],
+						"media_url": "http://pbs.twimg.com/media/Elv6e4-XUAAqo9K.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Elv6e4-XUAAqo9K.jpg",
+						"url": "https://t.co/yZ3VGvEeIi",
+						"display_url": "pic.twitter.com/yZ3VGvEeIi",
+						"expanded_url": "https://twitter.com/chrisinsilico/status/1322926327477972994/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 600,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 900,
+								"h": 600,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 787149253302812700,
+				"id_str": "787149253302812672",
+				"name": "Christopher Wylie 🏳️‍🌈",
+				"screen_name": "chrisinsilico",
+				"location": "London, England",
+				"description": "My book MINDF*CK is now out / Whistleblower. Immigrant. Queer. / Speaking: ShivanieG(at)harrywalker(dot)com / Book: RRokicki(at)penguinrandomhouse(dot)com",
+				"url": "https://t.co/qwCjT2AdRR",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/qwCjT2AdRR",
+								"expanded_url": "http://www.randomhousebooks.com/books/604375/",
+								"display_url": "randomhousebooks.com/books/604375/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 168108,
+				"friends_count": 2297,
+				"listed_count": 1127,
+				"created_at": "Sat Oct 15 04:33:01 +0000 2016",
+				"favourites_count": 3177,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 2842,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1015405378648068096/5aS8kNKm_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1015405378648068096/5aS8kNKm_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/787149253302812672/1570374670",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 450,
+			"favorite_count": 1907,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fi",
+		"text": "Y’allqaida https://t.co/1r0rWs0lv6"
+	},
+	{
 		"created_at": "Sun Nov 01 13:44:28 +0000 2020",
 		"id": 1322897298955927600,
 		"id_str": "1322897298955927552",
