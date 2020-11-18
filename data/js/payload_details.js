97b8c8c9640d7a2b2bda0074853fcd12999f9c3a
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-11-18 13:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72439,
-	"created_at": "Wed Nov 18 07:21:05 +0000 2020",
+	"tweets": 72440,
+	"created_at": "Wed Nov 18 12:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 18 12:09:33 +0000 2020",
+		"id": 1329034005795188700,
+		"id_str": "1329034005795188737",
+		"full_text": "@Scarbir Ik lees iets minder hoera-berichten als je doorrekent wat het betekent. https://t.co/AYMGwRx55P",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Scarbir",
+					"name": "Bart Breij",
+					"id": 12458682,
+					"id_str": "12458682",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AYMGwRx55P",
+					"expanded_url": "https://twitter.com/jacknicas/status/1329025297082150912",
+					"display_url": "twitter.com/jacknicas/stat…",
+					"indices": [
+						81,
+						104
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1329026551199961000,
+		"in_reply_to_status_id_str": "1329026551199961088",
+		"in_reply_to_user_id": 12458682,
+		"in_reply_to_user_id_str": "12458682",
+		"in_reply_to_screen_name": "Scarbir",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8918,
+			"friends_count": 613,
+			"listed_count": 563,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6952,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63036,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1329025297082151000,
+		"quoted_status_id_str": "1329025297082150912",
+		"quoted_status_permalink": {
+			"url": "https://t.co/AYMGwRx55P",
+			"expanded": "https://twitter.com/jacknicas/status/1329025297082150912",
+			"display": "twitter.com/jacknicas/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Nov 18 11:34:57 +0000 2020",
+			"id": 1329025297082151000,
+			"id_str": "1329025297082150912",
+			"full_text": "The move could help Apple deflect scrutiny of its commission without costing it much.\n\nThe change will affect roughly 98% of developers who pay the commission -- but they account for less than 5% of Apple’s overall App Store revenues, according to @SensorTower.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				261
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "SensorTower",
+						"name": "Sensor Tower",
+						"id": 851882208,
+						"id_str": "851882208",
+						"indices": [
+							248,
+							260
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1329024035003527200,
+			"in_reply_to_status_id_str": "1329024035003527168",
+			"in_reply_to_user_id": 17892496,
+			"in_reply_to_user_id_str": "17892496",
+			"in_reply_to_screen_name": "jacknicas",
+			"user": {
+				"id": 17892496,
+				"id_str": "17892496",
+				"name": "Jack Nicas",
+				"screen_name": "jacknicas",
+				"location": "",
+				"description": "New York Times reporter. I cover Silicon Valley and more. WSJ, Boston Globe alum. Jumbotron regular. DMs open.",
+				"url": "https://t.co/GOzxIic4ma",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/GOzxIic4ma",
+								"expanded_url": "https://www.nytimes.com/by/jack-nicas",
+								"display_url": "nytimes.com/by/jack-nicas",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 28295,
+				"friends_count": 1992,
+				"listed_count": 777,
+				"created_at": "Fri Dec 05 07:04:33 +0000 2008",
+				"favourites_count": 8651,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 699,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1181375989068091392/AnLjBAhW_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1181375989068091392/AnLjBAhW_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17892496/1543306310",
+				"profile_link_color": "DD2E44",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "FFFFFF",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 8,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@Scarbir Ik lees iets minder hoera-berichten als je doorrekent wat het betekent. https://t.co/AYMGwRx55P"
+	},
+	{
 		"created_at": "Wed Nov 18 06:36:47 +0000 2020",
 		"id": 1328950260018851800,
 		"id_str": "1328950260018851840",
