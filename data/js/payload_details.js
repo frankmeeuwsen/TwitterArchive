040d90bff40052ae4b90d1929df7454f845097f1
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-11-18 20:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72442,
-	"created_at": "Wed Nov 18 18:21:05 +0000 2020",
+	"tweets": 72443,
+	"created_at": "Wed Nov 18 19:21:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 18 18:23:41 +0000 2020",
+		"id": 1329128159925727200,
+		"id_str": "1329128159925727235",
+		"full_text": "@dr WordPress + @NewsletterGlue (which hooks on MailerLite) for my main newsletter and @mailbrew for some small automated newsletters.",
+		"truncated": false,
+		"display_text_range": [
+			4,
+			134
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dr",
+					"name": "Dan Rowden 👨🏻‍💻",
+					"id": 5264,
+					"id_str": "5264",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"screen_name": "NewsletterGlue",
+					"name": "Newsletter Glue",
+					"id": 1273282653773377500,
+					"id_str": "1273282653773377536",
+					"indices": [
+						16,
+						31
+					]
+				},
+				{
+					"screen_name": "mailbrew",
+					"name": "Mailbrew",
+					"id": 1185826789454729200,
+					"id_str": "1185826789454729216",
+					"indices": [
+						87,
+						96
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1329017507282182100,
+		"in_reply_to_status_id_str": "1329017507282182146",
+		"in_reply_to_user_id": 5264,
+		"in_reply_to_user_id_str": "5264",
+		"in_reply_to_screen_name": "dr",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8918,
+			"friends_count": 613,
+			"listed_count": 563,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6952,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63037,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@dr WordPress + @NewsletterGlue (which hooks on MailerLite) for my main newsletter and @mailbrew for some small automated newsletters."
+	},
+	{
 		"created_at": "Wed Nov 18 17:50:35 +0000 2020",
 		"id": 1329119828209242000,
 		"id_str": "1329119828209242112",
