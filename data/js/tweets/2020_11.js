eaa5e407570f189137641f822607433658724efd
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-11-19 18:21:06 +0100

    Added 1 tweets

--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 19 16:34:24 +0000 2020",
+		"id": 1329463043093373000,
+		"id_str": "1329463043093372939",
+		"full_text": "Een sterke aanrader! https://t.co/qsGX7sRIdR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qsGX7sRIdR",
+					"expanded_url": "https://twitter.com/janelsgm/status/1329339027477159938",
+					"display_url": "twitter.com/janelsgm/statu…",
+					"indices": [
+						21,
+						44
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8918,
+			"friends_count": 613,
+			"listed_count": 562,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6957,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63044,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1277260417644068864/h6QTSCGq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1329339027477160000,
+		"quoted_status_id_str": "1329339027477159938",
+		"quoted_status_permalink": {
+			"url": "https://t.co/qsGX7sRIdR",
+			"expanded": "https://twitter.com/janelsgm/status/1329339027477159938",
+			"display": "twitter.com/janelsgm/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 19 08:21:36 +0000 2020",
+			"id": 1329339027477160000,
+			"id_str": "1329339027477159938",
+			"full_text": "It's launch day!\n\nNewsletter OS is now live on Product Hunt\n\nWould love for you to support &amp; join in the conversation here.\n\nhttps://t.co/pt0HJyn4WT",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				152
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/pt0HJyn4WT",
+						"expanded_url": "https://www.producthunt.com/posts/newsletter-operating-system",
+						"display_url": "producthunt.com/posts/newslett…",
+						"indices": [
+							129,
+							152
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1688823739,
+				"id_str": "1688823739",
+				"name": "Janel",
+				"screen_name": "JanelSGM",
+				"location": "Barcelona | DMs open",
+				"description": "Marketer + Maker. Exploring my curiosity at the intersection of business & tech. I read a lot & love no-code. Obsessed with newsletters. https://t.co/YNGpctkuYO",
+				"url": "https://t.co/welCrY72dr",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/welCrY72dr",
+								"expanded_url": "http://brainpint.com",
+								"display_url": "brainpint.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/YNGpctkuYO",
+								"expanded_url": "http://NewsletterOS.com",
+								"display_url": "NewsletterOS.com",
+								"indices": [
+									137,
+									160
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 2868,
+				"friends_count": 350,
+				"listed_count": 104,
+				"created_at": "Wed Aug 21 17:13:36 +0000 2013",
+				"favourites_count": 4345,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 2961,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1281644754040696832/kOqgYgKQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1281644754040696832/kOqgYgKQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1688823739/1447203274",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 27,
+			"favorite_count": 186,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Een sterke aanrader! https://t.co/qsGX7sRIdR"
+	},
+	{
 		"created_at": "Thu Nov 19 16:10:55 +0000 2020",
 		"id": 1329457133272838100,
 		"id_str": "1329457133272838145",
