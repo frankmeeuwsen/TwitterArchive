91ca8a44c06114e14e4c01b8b270a585f3084239
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-11-27 08:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72471,
-	"created_at": "Thu Nov 26 13:21:07 +0000 2020",
+	"tweets": 72472,
+	"created_at": "Fri Nov 27 07:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Fri Nov 27 06:33:09 +0000 2020",
+		"id": 1332150451714863000,
+		"id_str": "1332210835763191809",
+		"full_text": "@qntm The year is 10^280\n\nHumanity, or what's left of it, has uploaded itself to machines\n\nNow, what we call \"people\" are strings of code in self-propagating tweets\n\nJack Dorsey Eternal, Benefactor of Humanity Praise Be His Name, has expanded the character limit to 10000 in response",
+		"truncated": false,
+		"display_text_range": [
+			6,
+			283
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "qntm",
+					"name": "qntm",
+					"id": 71057512,
+					"id_str": "71057512",
+					"indices": [
+						0,
+						5
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1332149582357274600,
+		"in_reply_to_status_id_str": "1332149582357274625",
+		"in_reply_to_user_id": 1328172887073828900,
+		"in_reply_to_user_id_str": "1328172887073828865",
+		"in_reply_to_screen_name": "SophieErisLuna",
+		"user": {
+			"id": 1328172887073828900,
+			"id_str": "1328172887073828865",
+			"name": "Sophie Luna",
+			"screen_name": "SophieErisLuna",
+			"location": "A Superposition Of Virginia/SC",
+			"description": "Astronomy and spaceflight lover, custom flag designer, and proud sword lesbian! ⚔️🏳️‍⚧️🛰️ President of @FemCoUSC she/her, they/them",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9,
+			"friends_count": 4,
+			"listed_count": 0,
+			"created_at": "Mon Nov 16 03:10:25 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 64,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1328174507119550464/jqa91HME_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1328174507119550464/jqa91HME_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1328172887073828865/1605496503",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 11,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@qntm The year is 10^280\n\nHumanity, or what's left of it, has uploaded itself to machines\n\nNow, what we call \"people\" are strings of code in self-propagating tweets\n\nJack Dorsey Eternal, Benefactor of Humanity Praise Be His Name, has expanded the character limit to 10000 in response"
+	},
+	{
 		"created_at": "Thu Nov 26 12:58:22 +0000 2020",
 		"id": 1331945392100044800,
 		"id_str": "1331945392100044802",
