737c5d87788da357791a580a82afa2ee068919cc
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-11-27 20:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72476,
-	"created_at": "Fri Nov 27 18:21:05 +0000 2020",
+	"tweets": 72477,
+	"created_at": "Fri Nov 27 19:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,346 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Fri Nov 27 18:50:34 +0000 2020",
+		"id": 1332396412311646200,
+		"id_str": "1332396412311646208",
+		"full_text": "En thuiswerken maakt het mogelijk om 1-komma-veel miljard te besteden aan een extra rijbaan ten koste van natuur bij Amelisweerd. Want ja, auto’s he. Schandalig. #stembustweet https://t.co/hm7aeqMSbC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "stembustweet",
+					"indices": [
+						162,
+						175
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hm7aeqMSbC",
+					"expanded_url": "https://twitter.com/rutgerschonis/status/1332292123727716352",
+					"display_url": "twitter.com/rutgerschonis/…",
+					"indices": [
+						176,
+						199
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8919,
+			"friends_count": 625,
+			"listed_count": 560,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6978,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63085,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1332292123727716400,
+		"quoted_status_id_str": "1332292123727716352",
+		"quoted_status_permalink": {
+			"url": "https://t.co/hm7aeqMSbC",
+			"expanded": "https://twitter.com/rutgerschonis/status/1332292123727716352",
+			"display": "twitter.com/rutgerschonis/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Nov 27 11:56:09 +0000 2020",
+			"id": 1332292123727716400,
+			"id_str": "1332292123727716352",
+			"full_text": "Hoe een minister wel of niet denkt over thuiswerken: een tweeluik.\n\nThuiswerken voorkomt rekeningrijden.\n\nThuiswerken maakt nieuw asfalt nog steeds nodig. https://t.co/hh2dJkyGx5",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				154
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1332292117994082300,
+						"id_str": "1332292117994082306",
+						"indices": [
+							155,
+							178
+						],
+						"media_url": "http://pbs.twimg.com/media/En1An14XEAIJ0_C.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/En1An14XEAIJ0_C.jpg",
+						"url": "https://t.co/hh2dJkyGx5",
+						"display_url": "pic.twitter.com/hh2dJkyGx5",
+						"expanded_url": "https://twitter.com/RutgerSchonis/status/1332292123727716352/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 795,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 528,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 795,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1332292117994082300,
+						"id_str": "1332292117994082306",
+						"indices": [
+							155,
+							178
+						],
+						"media_url": "http://pbs.twimg.com/media/En1An14XEAIJ0_C.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/En1An14XEAIJ0_C.jpg",
+						"url": "https://t.co/hh2dJkyGx5",
+						"display_url": "pic.twitter.com/hh2dJkyGx5",
+						"expanded_url": "https://twitter.com/RutgerSchonis/status/1332292123727716352/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 795,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 528,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 795,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1332292118010876000,
+						"id_str": "1332292118010875904",
+						"indices": [
+							155,
+							178
+						],
+						"media_url": "http://pbs.twimg.com/media/En1An18XUAA-4qs.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/En1An18XUAA-4qs.jpg",
+						"url": "https://t.co/hh2dJkyGx5",
+						"display_url": "pic.twitter.com/hh2dJkyGx5",
+						"expanded_url": "https://twitter.com/RutgerSchonis/status/1332292123727716352/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 739,
+								"h": 1600,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 314,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 554,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 307232214,
+				"id_str": "307232214",
+				"name": "Rutger Sch⭕️nis",
+				"screen_name": "RutgerSchonis",
+				"location": "Middelburg",
+				"description": "Rutger Schonis #D66 #Kamerlid #Middelburg #Mobiliteit #Fietsen #Verduurzamen #Trein #Toerisme #Binnenvaart #Maritiem #Regios",
+				"url": "https://t.co/JR38IrXaAw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JR38IrXaAw",
+								"expanded_url": "http://nl.linkedin.com/in/rutgerschonis",
+								"display_url": "nl.linkedin.com/in/rutgerschon…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2430,
+				"friends_count": 1499,
+				"listed_count": 84,
+				"created_at": "Sun May 29 07:59:39 +0000 2011",
+				"favourites_count": 15243,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 13862,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1155033484811022336/dxgKgb3s_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1155033484811022336/dxgKgb3s_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/307232214/1541673131",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 66,
+			"favorite_count": 186,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "En thuiswerken maakt het mogelijk om 1-komma-veel miljard te besteden aan een extra rijbaan ten koste van natuur bij Amelisweerd. Want ja, auto’s he. Schandalig. #stembustweet https://t.co/hm7aeqMSbC"
+	},
+	{
 		"created_at": "Fri Nov 27 18:14:55 +0000 2020",
 		"id": 1332387442155020300,
 		"id_str": "1332387442155020288",
