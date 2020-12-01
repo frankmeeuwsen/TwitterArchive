1bb0d23fafc6dd63bd340219230f1998062909e5
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-12-01 11:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72485,
-	"created_at": "Mon Nov 30 19:22:37 +0000 2020",
+	"tweets": 72486,
+	"created_at": "Tue Dec 01 10:21:05 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,271 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Tue Dec 01 09:40:24 +0000 2020",
+		"id": 1333707509899653000,
+		"id_str": "1333707509899653123",
+		"full_text": "Vacatures voor nieuwsbrief-editors bij grote kranten. Dat zijn interessante ontwikkelingen. https://t.co/da1d2hxO2z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/da1d2hxO2z",
+					"expanded_url": "https://twitter.com/danoshinsky/status/1333449631578066958",
+					"display_url": "twitter.com/danoshinsky/st…",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8920,
+			"friends_count": 623,
+			"listed_count": 560,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 6980,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63096,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1333449631578067000,
+		"quoted_status_id_str": "1333449631578066958",
+		"quoted_status_permalink": {
+			"url": "https://t.co/da1d2hxO2z",
+			"expanded": "https://twitter.com/danoshinsky/status/1333449631578066958",
+			"display": "twitter.com/danoshinsky/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 30 16:35:41 +0000 2020",
+			"id": 1333449631578067000,
+			"id_str": "1333449631578066958",
+			"full_text": "Three pretty big newsletter jobs open at the moment:\n\n@washingtonpost is hiring two newsletter editors https://t.co/3Zuf9t3yjg\n\n@Telegraph is hiring a Deputy Head of Email https://t.co/E51oNsGk7G\n\nThey're two of the most advanced newsroom newsletter operations anywhere, IMO.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				275
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "washingtonpost",
+						"name": "The Washington Post",
+						"id": 2467791,
+						"id_str": "2467791",
+						"indices": [
+							54,
+							69
+						]
+					},
+					{
+						"screen_name": "Telegraph",
+						"name": "The Telegraph",
+						"id": 16343974,
+						"id_str": "16343974",
+						"indices": [
+							128,
+							138
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/3Zuf9t3yjg",
+						"expanded_url": "https://washpost.wd5.myworkdayjobs.com/washingtonpostcareers/job/DC-Washington-TWP-Headquarters/Newsletter-Editor_JR-90271766?fbclid=IwAR2v-6OizNTGjk1NeI09QPTHcVBWmwAy0KsguLhkkihcizqi4mPIpFDh0fg#path=",
+						"display_url": "washpost.wd5.myworkdayjobs.com/washingtonpost…",
+						"indices": [
+							103,
+							126
+						]
+					},
+					{
+						"url": "https://t.co/E51oNsGk7G",
+						"expanded_url": "https://careers.telegraph.co.uk/jobs/job-details/Deputy-Head-of-Email-and-Newsletters-London",
+						"display_url": "careers.telegraph.co.uk/jobs/job-detai…",
+						"indices": [
+							172,
+							195
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16892321,
+				"id_str": "16892321",
+				"name": "Dan Oshinsky",
+				"screen_name": "danoshinsky",
+				"location": "New York, NY",
+				"description": "@inboxcollective and https://t.co/RHE0P5TIJz. Formerly: @newyorker @BuzzFeed, @StryUs, @tools4reporters. M-I-Z.",
+				"url": "http://t.co/JPKxb54Bo1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/JPKxb54Bo1",
+								"expanded_url": "http://danoshinsky.com",
+								"display_url": "danoshinsky.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/RHE0P5TIJz",
+								"expanded_url": "http://notanewsletter.com",
+								"display_url": "notanewsletter.com",
+								"indices": [
+									21,
+									44
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 4180,
+				"friends_count": 115,
+				"listed_count": 218,
+				"created_at": "Tue Oct 21 19:42:03 +0000 2008",
+				"favourites_count": 1874,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 11619,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1238224250500190208/EOQibeHz_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1238224250500190208/EOQibeHz_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16892321/1502229251",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 11,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Vacatures voor nieuwsbrief-editors bij grote kranten. Dat zijn interessante ontwikkelingen. https://t.co/da1d2hxO2z"
+	}
+]
