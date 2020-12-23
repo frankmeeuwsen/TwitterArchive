b97c4061638c98a76cfe357b1678d4616cb38365
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-12-24 00:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72606,
-	"created_at": "Wed Dec 23 17:21:05 +0000 2020",
+	"tweets": 72607,
+	"created_at": "Wed Dec 23 23:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Wed Dec 23 22:32:28 +0000 2020",
+		"id": 1341874341059936300,
+		"id_str": "1341874341059936263",
+		"full_text": "Here’s to you @NewsletterGlue https://t.co/oGi302EXCP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NewsletterGlue",
+					"name": "Newsletter Glue",
+					"id": 1273282653773377500,
+					"id_str": "1273282653773377536",
+					"indices": [
+						14,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oGi302EXCP",
+					"expanded_url": "https://twitter.com/austin_rief/status/1341873313665171457",
+					"display_url": "twitter.com/austin_rief/st…",
+					"indices": [
+						30,
+						53
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8911,
+			"friends_count": 635,
+			"listed_count": 560,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7054,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63246,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1341873313665171500,
+		"quoted_status_id_str": "1341873313665171457",
+		"quoted_status_permalink": {
+			"url": "https://t.co/oGi302EXCP",
+			"expanded": "https://twitter.com/austin_rief/status/1341873313665171457",
+			"display": "twitter.com/austin_rief/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Dec 23 22:28:23 +0000 2020",
+			"id": 1341873313665171500,
+			"id_str": "1341873313665171457",
+			"full_text": "Product request: \n\nA better way for newsletter writers to house their content in a sortable and searchable database. \n\nToo much amazing evergreen content gets lost in newsletter archives.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				187
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 158610400,
+				"id_str": "158610400",
+				"name": "Austin Rief ☕️",
+				"screen_name": "austin_rief",
+				"location": "NYC",
+				"description": "cofounder @MorningBrew\nI run a rolling fund. Become an LP: https://t.co/okX5fAmpCo\nSign up for my newsletter about raising a rolling fund: https://t.co/MGu7jXQUHF",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/okX5fAmpCo",
+								"expanded_url": "https://bit.ly/30sT04K",
+								"display_url": "bit.ly/30sT04K",
+								"indices": [
+									59,
+									82
+								]
+							},
+							{
+								"url": "https://t.co/MGu7jXQUHF",
+								"expanded_url": "http://bit.ly/33pNIrF",
+								"display_url": "bit.ly/33pNIrF",
+								"indices": [
+									139,
+									162
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 29715,
+				"friends_count": 824,
+				"listed_count": 409,
+				"created_at": "Wed Jun 23 04:09:47 +0000 2010",
+				"favourites_count": 35271,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 7446,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1259285101705396225/M3ZKDvYO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1259285101705396225/M3ZKDvYO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/158610400/1572961351",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 66,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Here’s to you @NewsletterGlue https://t.co/oGi302EXCP"
+	},
+	{
 		"created_at": "Wed Dec 23 17:01:58 +0000 2020",
 		"id": 1341791168988897300,
 		"id_str": "1341791168988897281",
