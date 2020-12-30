cec13db742155c87fb2e2302fc70866675be3b9d
Frank Meeuwsen <frank@incredibleadventure.nl>
2020-12-30 15:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72632,
-	"created_at": "Wed Dec 30 12:21:06 +0000 2020",
+	"tweets": 72633,
+	"created_at": "Wed Dec 30 14:21:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,338 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Wed Dec 30 13:42:52 +0000 2020",
+		"id": 1344277780452814800,
+		"id_str": "1344277780452814854",
+		"full_text": "Hoera! Dank allen! (En nu niet unsubscriben voor morgen he... ;-)) https://t.co/T5TQzJqiuU https://t.co/b5maxa3Z2B",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/T5TQzJqiuU",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1344246487149125633",
+					"display_url": "twitter.com/frankmeeuwsen/…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1344277776665342000,
+					"id_str": "1344277776665341953",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/EqfVg_2W8AEfbb6.png",
+					"media_url_https": "https://pbs.twimg.com/media/EqfVg_2W8AEfbb6.png",
+					"url": "https://t.co/b5maxa3Z2B",
+					"display_url": "pic.twitter.com/b5maxa3Z2B",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1344277780452814854/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 663,
+							"h": 326,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 663,
+							"h": 326,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 663,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1344277776665342000,
+					"id_str": "1344277776665341953",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/EqfVg_2W8AEfbb6.png",
+					"media_url_https": "https://pbs.twimg.com/media/EqfVg_2W8AEfbb6.png",
+					"url": "https://t.co/b5maxa3Z2B",
+					"display_url": "pic.twitter.com/b5maxa3Z2B",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1344277780452814854/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 663,
+							"h": 326,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 663,
+							"h": 326,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 663,
+							"h": 326,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8914,
+			"friends_count": 642,
+			"listed_count": 558,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7084,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63279,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1344246487149125600,
+		"quoted_status_id_str": "1344246487149125633",
+		"quoted_status_permalink": {
+			"url": "https://t.co/T5TQzJqiuU",
+			"expanded": "https://twitter.com/frankmeeuwsen/status/1344246487149125633",
+			"display": "twitter.com/frankmeeuwsen/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Dec 30 11:38:32 +0000 2020",
+			"id": 1344246487149125600,
+			"id_str": "1344246487149125633",
+			"full_text": "Ik heb nu 241 abonnees op mijn OPEN nieuwsbrief. Het zou supertof zijn om met 250 abonnees het nieuwe jaar in te sliden.  \nDe OPEN nieuwsbrief is er voor nieuwsbriefmakers en bloggers. Tips, nieuwtjes en handige diensten. Abonneren is meemaken! https://t.co/sUWEQRJEXM https://t.co/vR1S2duIFB",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				268
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/sUWEQRJEXM",
+						"expanded_url": "http://nieuws.diggingthedigital.com/",
+						"display_url": "nieuws.diggingthedigital.com",
+						"indices": [
+							245,
+							268
+						]
+					},
+					{
+						"url": "https://t.co/vR1S2duIFB",
+						"expanded_url": "https://twitter.com/frankmeeuwsen/status/1211613871682859008",
+						"display_url": "twitter.com/frankmeeuwsen/…",
+						"indices": [
+							269,
+							292
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 816203,
+				"id_str": "816203",
+				"name": "Frank Meeuwsen",
+				"screen_name": "frankmeeuwsen",
+				"location": "Utrecht, the Netherlands",
+				"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+				"url": "https://t.co/6YB8i11ryl",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/6YB8i11ryl",
+								"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+								"display_url": "diggingthedigital.com/nieuwsbrief",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/ODgIUUbnBR",
+								"expanded_url": "http://Thanksforsubscribing.app",
+								"display_url": "Thanksforsubscribing.app",
+								"indices": [
+									40,
+									63
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 8914,
+				"friends_count": 642,
+				"listed_count": 558,
+				"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+				"favourites_count": 7084,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 63279,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "709397",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+				"profile_link_color": "FF3300",
+				"profile_sidebar_border_color": "86A4A6",
+				"profile_sidebar_fill_color": "A0C5C7",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1211613871682859000,
+			"quoted_status_id_str": "1211613871682859008",
+			"quoted_status_permalink": {
+				"url": "https://t.co/vR1S2duIFB",
+				"expanded": "https://twitter.com/frankmeeuwsen/status/1211613871682859008",
+				"display": "twitter.com/frankmeeuwsen/…"
+			},
+			"retweet_count": 8,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Hoera! Dank allen! (En nu niet unsubscriben voor morgen he... ;-)) https://t.co/T5TQzJqiuU https://t.co/b5maxa3Z2B"
+	},
+	{
 		"created_at": "Wed Dec 30 11:38:32 +0000 2020",
 		"id": 1344246487149125600,
 		"id_str": "1344246487149125633",
