907a02f210f25c6c6415fc716593700377c82099
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-01-02 09:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72644,
-	"created_at": "Fri Jan 01 12:21:03 +0000 2021",
+	"tweets": 72645,
+	"created_at": "Sat Jan 02 08:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sat Jan 02 08:14:32 +0000 2021",
+		"id": 1345282314490826800,
+		"id_str": "1345282314490826757",
+		"full_text": "Hardoplachen. Inclusief de reacties. https://t.co/0cwhpSdPRh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0cwhpSdPRh",
+					"expanded_url": "https://twitter.com/dotorii_muk/status/1344363422805356544",
+					"display_url": "twitter.com/dotorii_muk/st…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8906,
+			"friends_count": 643,
+			"listed_count": 557,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7105,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63293,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1344363422805356500,
+		"quoted_status_id_str": "1344363422805356544",
+		"quoted_status_permalink": {
+			"url": "https://t.co/0cwhpSdPRh",
+			"expanded": "https://twitter.com/dotorii_muk/status/1344363422805356544",
+			"display": "twitter.com/dotorii_muk/st…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Dec 30 19:23:11 +0000 2020",
+			"id": 1344363422805356500,
+			"id_str": "1344363422805356544",
+			"full_text": "Take your shirt off\n\n\"but...I can roll up my sleeve--\"\n\nI am a medical professional and I said TAKE IT OFF BOI https://t.co/olFDPn9BXI",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				110
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1344363420733288400,
+						"id_str": "1344363420733288448",
+						"indices": [
+							111,
+							134
+						],
+						"media_url": "http://pbs.twimg.com/media/EqgjaI3WMAArAg7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EqgjaI3WMAArAg7.jpg",
+						"url": "https://t.co/olFDPn9BXI",
+						"display_url": "pic.twitter.com/olFDPn9BXI",
+						"expanded_url": "https://twitter.com/dotorii_muk/status/1344363422805356544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1080,
+								"h": 904,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 569,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 904,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1344363420733288400,
+						"id_str": "1344363420733288448",
+						"indices": [
+							111,
+							134
+						],
+						"media_url": "http://pbs.twimg.com/media/EqgjaI3WMAArAg7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EqgjaI3WMAArAg7.jpg",
+						"url": "https://t.co/olFDPn9BXI",
+						"display_url": "pic.twitter.com/olFDPn9BXI",
+						"expanded_url": "https://twitter.com/dotorii_muk/status/1344363422805356544/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1080,
+								"h": 904,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 569,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 904,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1925435125,
+				"id_str": "1925435125",
+				"name": "dotori-muk, gelatina coreana de bellota",
+				"screen_name": "dotorii_muk",
+				"location": "",
+				"description": "🇰🇷 with 🇲🇽 characteristics\n🏳️‍🌈 queer, demisexual, polyam\n♀️ she/her\n🚫🇺🇸🚮USA GTFO of Korea\n🔞 minors dni",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2025,
+				"friends_count": 549,
+				"listed_count": 6,
+				"created_at": "Wed Oct 02 02:43:01 +0000 2013",
+				"favourites_count": 73183,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17014,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1337324292246269952/BLqa_oxh_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1337324292246269952/BLqa_oxh_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1925435125/1606154823",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 223,
+			"favorite_count": 1417,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hardoplachen. Inclusief de reacties. https://t.co/0cwhpSdPRh"
+	},
+	{
 		"created_at": "Fri Jan 01 11:25:48 +0000 2021",
 		"id": 1344968059803623400,
 		"id_str": "1344968059803623425",
