ba60a451b04b003f398203f581ee8ec5e57edb38
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-01-07 00:21:06 +0100

    Added 1 tweets

--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,367 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Wed Jan 06 22:38:48 +0000 2021",
+		"id": 1346949367245234200,
+		"id_str": "1346949367245234176",
+		"full_text": "Ziek fuckers. https://t.co/ccI7hdQIoP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ccI7hdQIoP",
+					"expanded_url": "https://twitter.com/anthonymague/status/1346856138546016262",
+					"display_url": "twitter.com/anthonymague/s…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8906,
+			"friends_count": 645,
+			"listed_count": 557,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7129,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63371,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1346856138546016300,
+		"quoted_status_id_str": "1346856138546016262",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ccI7hdQIoP",
+			"expanded": "https://twitter.com/anthonymague/status/1346856138546016262",
+			"display": "twitter.com/anthonymague/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jan 06 16:28:21 +0000 2021",
+			"id": 1346856138546016300,
+			"id_str": "1346856138546016262",
+			"full_text": "Moments after @nationalcitycc unfolds a #BlackLivesMatter banner, two Trump supporters are seen recreating the death of George Floyd on the church's steps. Stay with @corynbc and @nbcwashington all day long for LIVE coverage https://t.co/QIF0eC08hi",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				224
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "BlackLivesMatter",
+						"indices": [
+							40,
+							57
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "nationalcitycc",
+						"name": "National City Christian Church",
+						"id": 183318320,
+						"id_str": "183318320",
+						"indices": [
+							14,
+							29
+						]
+					},
+					{
+						"screen_name": "CoryNBC",
+						"name": "Cory Smith",
+						"id": 143580061,
+						"id_str": "143580061",
+						"indices": [
+							166,
+							174
+						]
+					},
+					{
+						"screen_name": "nbcwashington",
+						"name": "NBCWashington",
+						"id": 14980820,
+						"id_str": "14980820",
+						"indices": [
+							179,
+							193
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1346855865069031400,
+						"id_str": "1346855865069031424",
+						"indices": [
+							225,
+							248
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1346855865069031424/pu/img/rGJW9oAifkkw3ynG.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1346855865069031424/pu/img/rGJW9oAifkkw3ynG.jpg",
+						"url": "https://t.co/QIF0eC08hi",
+						"display_url": "pic.twitter.com/QIF0eC08hi",
+						"expanded_url": "https://twitter.com/AnthonyMague/status/1346856138546016262/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 720,
+								"h": 404,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 404,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1346855865069031400,
+						"id_str": "1346855865069031424",
+						"indices": [
+							225,
+							248
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1346855865069031424/pu/img/rGJW9oAifkkw3ynG.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1346855865069031424/pu/img/rGJW9oAifkkw3ynG.jpg",
+						"url": "https://t.co/QIF0eC08hi",
+						"display_url": "pic.twitter.com/QIF0eC08hi",
+						"expanded_url": "https://twitter.com/AnthonyMague/status/1346856138546016262/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 720,
+								"h": 404,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 720,
+								"h": 404,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								180,
+								101
+							],
+							"duration_millis": 8675,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1346855865069031424/pu/vid/720x404/EgTMfz_154lOjVDF.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1346855865069031424/pu/vid/480x270/JY8XhJRj5RCkF_te.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1346855865069031424/pu/pl/3RhqQPyDOQJxSGpK.m3u8?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1374662364,
+				"id_str": "1374662364",
+				"name": "Anthony Mague",
+				"screen_name": "AnthonyMague",
+				"location": "Washington, DC",
+				"description": "Executive Producer, NBC4 Washington @SyracuseU alum",
+				"url": "https://t.co/ULCQRFXgV0",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ULCQRFXgV0",
+								"expanded_url": "http://nbcwashington.com",
+								"display_url": "nbcwashington.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 385,
+				"friends_count": 415,
+				"listed_count": 7,
+				"created_at": "Tue Apr 23 14:12:53 +0000 2013",
+				"favourites_count": 369,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 723,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/481281573128400896/ZOm8fSr4_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/481281573128400896/ZOm8fSr4_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1374662364/1534718883",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1654,
+			"favorite_count": 1781,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ziek fuckers. https://t.co/ccI7hdQIoP"
+	},
+	{
 		"created_at": "Wed Jan 06 22:13:44 +0000 2021",
 		"id": 1346942667985363000,
 		"id_str": "1346943057317539848",
