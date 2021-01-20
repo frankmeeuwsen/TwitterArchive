67f74782b7d39d498c5019922b0bcd105117346f
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-01-20 19:23:03 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72837,
-	"created_at": "Wed Jan 20 15:21:06 +0000 2021",
+	"tweets": 72838,
+	"created_at": "Wed Jan 20 18:23:01 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Wed Jan 20 18:01:08 +0000 2021",
+		"id": 1351950673487790000,
+		"id_str": "1351952917721395201",
+		"full_text": "BREAKING: Pentagon officials say that four years ago they handed Trump one of those football phones and told him it was the \"nuclear football.\"  The actual nuclear football was given to Mike Pence. https://t.co/lVJC5IMKdz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			197
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1351950636905017300,
+					"id_str": "1351950636905017344",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/EsMX8PqWMAAq1Jq.png",
+					"media_url_https": "https://pbs.twimg.com/media/EsMX8PqWMAAq1Jq.png",
+					"url": "https://t.co/lVJC5IMKdz",
+					"display_url": "pic.twitter.com/lVJC5IMKdz",
+					"expanded_url": "https://twitter.com/simonowens/status/1351950673487790081/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 641,
+							"h": 405,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 641,
+							"h": 405,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 641,
+							"h": 405,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1351950636905017300,
+					"id_str": "1351950636905017344",
+					"indices": [
+						198,
+						221
+					],
+					"media_url": "http://pbs.twimg.com/media/EsMX8PqWMAAq1Jq.png",
+					"media_url_https": "https://pbs.twimg.com/media/EsMX8PqWMAAq1Jq.png",
+					"url": "https://t.co/lVJC5IMKdz",
+					"display_url": "pic.twitter.com/lVJC5IMKdz",
+					"expanded_url": "https://twitter.com/simonowens/status/1351950673487790081/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 641,
+							"h": 405,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 641,
+							"h": 405,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 641,
+							"h": 405,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16248619,
+			"id_str": "16248619",
+			"name": "Simon Owens",
+			"screen_name": "simonowens",
+			"location": "Washington, DC",
+			"description": "Tech and media journalist. Email me: simonowens@gmail.com. Full bio in link below.",
+			"url": "https://t.co/GRFE1znbsp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/GRFE1znbsp",
+							"expanded_url": "https://medium.com/@simonowens/about-simon-owens-75f5cd470f3f",
+							"display_url": "medium.com/@simonowens/ab…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7131,
+			"friends_count": 809,
+			"listed_count": 657,
+			"created_at": "Thu Sep 11 22:10:10 +0000 2008",
+			"favourites_count": 1326,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16784,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1206741338106056709/FHNIYphO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1206741338106056709/FHNIYphO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16248619/1412614549",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BREAKING: Pentagon officials say that four years ago they handed Trump one of those football phones and told him it was the \"nuclear football.\"  The actual nuclear football was given to Mike Pence. https://t.co/lVJC5IMKdz"
+	},
+	{
 		"created_at": "Wed Jan 20 15:11:47 +0000 2021",
 		"id": 1351910300195418000,
 		"id_str": "1351910300195418112",
