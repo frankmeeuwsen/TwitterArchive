3ff5f1e14f04c8af2b5a7bd297f448fe68691a9b
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-01-29 22:21:09 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72940,
-	"created_at": "Fri Jan 29 15:21:07 +0000 2021",
+	"tweets": 72941,
+	"created_at": "Fri Jan 29 21:21:07 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,355 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Fri Jan 29 21:06:52 +0000 2021",
+		"id": 1355261152658391000,
+		"id_str": "1355261152658391040",
+		"full_text": "Vaporwave vibe is big on this one. @land_ofrandom would approve! Congrats on the launch Andrew! 💪 https://t.co/nlJRCPJ0L8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "land_ofrandom",
+					"name": "thelandofrandom 🐙",
+					"id": 1229223273508065300,
+					"id_str": "1229223273508065281",
+					"indices": [
+						35,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nlJRCPJ0L8",
+					"expanded_url": "https://twitter.com/Kamphey/status/1354917755196035073",
+					"display_url": "twitter.com/Kamphey/status…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8894,
+			"friends_count": 673,
+			"listed_count": 555,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7273,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63661,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1354917755196035000,
+		"quoted_status_id_str": "1354917755196035073",
+		"quoted_status_permalink": {
+			"url": "https://t.co/nlJRCPJ0L8",
+			"expanded": "https://twitter.com/Kamphey/status/1354917755196035073",
+			"display": "twitter.com/Kamphey/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jan 28 22:22:20 +0000 2021",
+			"id": 1354917755196035000,
+			"id_str": "1354917755196035073",
+			"full_text": "Announcing OnlySheets\n\nA @bettersheets drop\n\nTurn Your Sound Up https://t.co/Ef6IRIttDK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				63
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "BetterSheets",
+						"name": "BetterSheets",
+						"id": 1251661241803657200,
+						"id_str": "1251661241803657216",
+						"indices": [
+							25,
+							38
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1354912044965740500,
+						"id_str": "1354912044965740545",
+						"indices": [
+							64,
+							87
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1354912044965740545/pu/img/rdqXNhTkt500M07A.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1354912044965740545/pu/img/rdqXNhTkt500M07A.jpg",
+						"url": "https://t.co/Ef6IRIttDK",
+						"display_url": "pic.twitter.com/Ef6IRIttDK",
+						"expanded_url": "https://twitter.com/Kamphey/status/1354917755196035073/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1354912044965740500,
+						"id_str": "1354912044965740545",
+						"indices": [
+							64,
+							87
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1354912044965740545/pu/img/rdqXNhTkt500M07A.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1354912044965740545/pu/img/rdqXNhTkt500M07A.jpg",
+						"url": "https://t.co/Ef6IRIttDK",
+						"display_url": "pic.twitter.com/Ef6IRIttDK",
+						"expanded_url": "https://twitter.com/Kamphey/status/1354917755196035073/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 30072,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1354912044965740545/pu/pl/5YtR25VhZYjE6fLN.m3u8?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1354912044965740545/pu/vid/480x270/ghuzp8EZwDWBJGx3.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1354912044965740545/pu/vid/1280x720/AYhrLM5Bg0OF3Qco.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1354912044965740545/pu/vid/640x360/jMdd3tEikLf4EPvP.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 880930110,
+				"id_str": "880930110",
+				"name": "Kamphey👍",
+				"screen_name": "Kamphey",
+				"location": "SheetsVille, USA",
+				"description": "Former Santa\nNow: Spreadsheets @BetterSheets\nI write about How to Start, Grow, and Monetize Newsletters",
+				"url": "https://t.co/TCXKevYJoR",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/TCXKevYJoR",
+								"expanded_url": "http://xn--ug8h.kamphey.com/?unique=kampheyworldsheet",
+								"display_url": "🌎.kamphey.com/?unique=kamphe…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11733,
+				"friends_count": 323,
+				"listed_count": 224,
+				"created_at": "Sun Oct 14 20:32:38 +0000 2012",
+				"favourites_count": 22469,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 8144,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1256533331404967936/wg1EDbf2_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1256533331404967936/wg1EDbf2_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/880930110/1611864679",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 45,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Vaporwave vibe is big on this one. @land_ofrandom would approve! Congrats on the launch Andrew! 💪 https://t.co/nlJRCPJ0L8"
+	},
+	{
 		"created_at": "Fri Jan 29 15:12:44 +0000 2021",
 		"id": 1355172028987400200,
 		"id_str": "1355172028987400192",
