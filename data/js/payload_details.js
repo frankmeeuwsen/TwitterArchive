f1e1ec0893965692290190f3bb812eb03974179e
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-01-30 08:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72943,
-	"created_at": "Fri Jan 29 22:21:09 +0000 2021",
+	"tweets": 72944,
+	"created_at": "Sat Jan 30 07:21:07 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sat Jan 30 07:15:50 +0000 2021",
+		"id": 1355414402015617000,
+		"id_str": "1355414402015617026",
+		"full_text": "Ze zetten vaart achter de integratie met @revue https://t.co/marXrsRdUR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "revue",
+					"name": "Revue",
+					"id": 3001165942,
+					"id_str": "3001165942",
+					"indices": [
+						41,
+						47
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/marXrsRdUR",
+					"expanded_url": "https://twitter.com/wongmjane/status/1354564523038924805",
+					"display_url": "twitter.com/wongmjane/stat…",
+					"indices": [
+						48,
+						71
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8892,
+			"friends_count": 674,
+			"listed_count": 555,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7273,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63665,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1593008431",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1354564523038924800,
+		"quoted_status_id_str": "1354564523038924805",
+		"quoted_status_permalink": {
+			"url": "https://t.co/marXrsRdUR",
+			"expanded": "https://twitter.com/wongmjane/status/1354564523038924805",
+			"display": "twitter.com/wongmjane/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jan 27 22:58:43 +0000 2021",
+			"id": 1354564523038924800,
+			"id_str": "1354564523038924805",
+			"full_text": "Twitter is working on promoting their newsletter tool @revue in the web app following the acquisition announcement https://t.co/WoyuSjzprN",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				114
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "revue",
+						"name": "Revue",
+						"id": 3001165942,
+						"id_str": "3001165942",
+						"indices": [
+							54,
+							60
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1354564011413524500,
+						"id_str": "1354564011413524485",
+						"indices": [
+							115,
+							138
+						],
+						"media_url": "http://pbs.twimg.com/media/EsxgyqXU0AUfliN.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EsxgyqXU0AUfliN.jpg",
+						"url": "https://t.co/WoyuSjzprN",
+						"display_url": "pic.twitter.com/WoyuSjzprN",
+						"expanded_url": "https://twitter.com/wongmjane/status/1354564523038924805/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 571,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1007,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1876,
+								"h": 1574,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1354564011413524500,
+						"id_str": "1354564011413524485",
+						"indices": [
+							115,
+							138
+						],
+						"media_url": "http://pbs.twimg.com/media/EsxgyqXU0AUfliN.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EsxgyqXU0AUfliN.jpg",
+						"url": "https://t.co/WoyuSjzprN",
+						"display_url": "pic.twitter.com/WoyuSjzprN",
+						"expanded_url": "https://twitter.com/wongmjane/status/1354564523038924805/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 571,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1007,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1876,
+								"h": 1574,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 337119125,
+				"id_str": "337119125",
+				"name": "Jane Manchun Wong",
+				"screen_name": "wongmjane",
+				"location": "Null Island",
+				"description": "the real hidden features are the friends we made along the way 👩‍💻",
+				"url": "https://t.co/EYNlFej5rE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/EYNlFej5rE",
+								"expanded_url": "https://wongmjane.com",
+								"display_url": "wongmjane.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 93019,
+				"friends_count": 1066,
+				"listed_count": 1241,
+				"created_at": "Sun Jul 17 13:48:53 +0000 2011",
+				"favourites_count": 106348,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 21483,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1337084860792356866/h10V5_yC_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1337084860792356866/h10V5_yC_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/337119125/1607621025",
+				"profile_link_color": "1D709B",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 68,
+			"favorite_count": 324,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ze zetten vaart achter de integratie met @revue https://t.co/marXrsRdUR"
+	},
+	{
 		"created_at": "Fri Jan 29 21:37:32 +0000 2021",
 		"id": 1355268869947068400,
 		"id_str": "1355268869947068416",
