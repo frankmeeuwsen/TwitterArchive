e6845400a7d923033e8c8b1c4fcbcbb754522e8d
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-02-17 15:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73111,
-	"created_at": "Wed Feb 17 12:21:05 +0000 2021",
+	"tweets": 73112,
+	"created_at": "Wed Feb 17 14:21:06 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Wed Feb 17 13:48:15 +0000 2021",
+		"id": 1362036140413497300,
+		"id_str": "1362036140413497347",
+		"full_text": "@martijnarets @mmhmmapp Ik gebruik mmhmm soms als camera, maar dan moet ik dus switchen tussen Meet en Mmhmm om iets met mijn zichtbaarheid te doen. Dus Mmhmm moet altijd de front app zijn om keyboard shortcuts te gebruiken. Meet soms ook om bv op mute te gaan. Vandaar het cmd-tabben…",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			285
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "martijnarets",
+					"name": "Martijn Arets",
+					"id": 44898326,
+					"id_str": "44898326",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "mmhmmapp",
+					"name": "mmhmm",
+					"id": 1266014530313699300,
+					"id_str": "1266014530313699329",
+					"indices": [
+						14,
+						23
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1362011899219615700,
+		"in_reply_to_status_id_str": "1362011899219615745",
+		"in_reply_to_user_id": 44898326,
+		"in_reply_to_user_id_str": "44898326",
+		"in_reply_to_screen_name": "martijnarets",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8894,
+			"friends_count": 704,
+			"listed_count": 555,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7391,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63879,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1613162785",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": [
+			1221904161492566000
+		],
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@martijnarets @mmhmmapp Ik gebruik mmhmm soms als camera, maar dan moet ik dus switchen tussen Meet en Mmhmm om iets met mijn zichtbaarheid te doen. Dus Mmhmm moet altijd de front app zijn om keyboard shortcuts te gebruiken. Meet soms ook om bv op mute te gaan. Vandaar het cmd-tabben…"
+	},
+	{
 		"created_at": "Wed Feb 17 11:57:58 +0000 2021",
 		"id": 1362008386401939500,
 		"id_str": "1362008386401939456",
