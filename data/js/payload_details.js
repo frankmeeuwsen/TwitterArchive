9b1b9276c59a79d3cba5c5992ba2ec7ab336016a
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-02-22 17:21:07 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73161,
-	"created_at": "Mon Feb 22 12:21:06 +0000 2021",
+	"tweets": 73162,
+	"created_at": "Mon Feb 22 16:21:06 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Mon Feb 22 16:18:34 +0000 2021",
+		"id": 1363885905300361200,
+		"id_str": "1363885905300361219",
+		"full_text": "Dit weekend Finn wat video's laten zien van Daft Punk laten zien omdat hij constant Harder, Faster, Better, Strong(k)er speelt uit een Youtube animatie video. En dan nu dit. Tsk.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract\nNieuwsbriefgeek: https://t.co/ODgIUUbnBR / @ForSubscribing\nCheck ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfers. ⬇️\n\n#PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/ODgIUUbnBR",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8890,
+			"friends_count": 708,
+			"listed_count": 556,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7429,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 63943,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1613162785",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "Dit weekend Finn wat video's laten zien van Daft Punk laten zien omdat hij constant Harder, Faster, Better, Strong(k)er speelt uit een Youtube animatie video. En dan nu dit. Tsk."
+	},
+	{
 		"created_at": "Mon Feb 22 11:56:14 +0000 2021",
 		"id": 1363819889522323500,
 		"id_str": "1363819889522323457",
@@ -232,7 +337,7 @@ Grailbird.data.tweets_2021_02 =
 				"name": "Frans Super",
 				"screen_name": "franssuper",
 				"location": "The Netherlands",
-				"description": "#FreeJulianAssange, 🏳️‍🌈🏳️‍⚧️-ally, #TeamEmine, #TeamNoortje, Worrier, Activist, Leftist, Antifascist, #BLM, #FreePalestine🇵🇸.",
+				"description": "#FreeJulianAssange, 🏳️🌈🏳️⚧️-ally, #TeamEmine, #TeamNoortje, Worrier, Activist, Leftist, Antifascist, #BLM, #FreePalestine🇵🇸.",
 				"url": "https://t.co/blEZ18qGlY",
 				"entities": {
 					"url": {
