46d7863c2d5fb49caa4249f4af5dfc8d11e5ee56
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-03-10 14:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73297,
-	"created_at": "Wed Mar 10 12:21:04 +0000 2021",
+	"tweets": 73298,
+	"created_at": "Wed Mar 10 13:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Wed Mar 10 12:45:06 +0000 2021",
+		"id": 1369630391477403600,
+		"id_str": "1369630391477403649",
+		"full_text": "@Vetustioribus @erwblo @VoltNederland En ik begrijp je punt hoor. Naar mijn gevoel is EU geen bijzaak. De intrinieke problemen los je niet op door alleen naar binnen te kijken. Over de kosten kan ik onvoldoende oordelen.",
+		"truncated": false,
+		"display_text_range": [
+			38,
+			220
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Vetustioribus",
+					"name": "Albert Treur Jr.",
+					"id": 87180678,
+					"id_str": "87180678",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "erwblo",
+					"name": "Erwin Blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						15,
+						22
+					]
+				},
+				{
+					"screen_name": "VoltNederland",
+					"name": "Volt Nederland",
+					"id": 966790780869316600,
+					"id_str": "966790780869316608",
+					"indices": [
+						23,
+						37
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1369628751785582600,
+		"in_reply_to_status_id_str": "1369628751785582596",
+		"in_reply_to_user_id": 87180678,
+		"in_reply_to_user_id_str": "87180678",
+		"in_reply_to_screen_name": "Vetustioribus",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8891,
+			"friends_count": 719,
+			"listed_count": 555,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7521,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64119,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1615141708",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@Vetustioribus @erwblo @VoltNederland En ik begrijp je punt hoor. Naar mijn gevoel is EU geen bijzaak. De intrinieke problemen los je niet op door alleen naar binnen te kijken. Over de kosten kan ik onvoldoende oordelen."
+	},
+	{
 		"created_at": "Wed Mar 10 11:58:19 +0000 2021",
 		"id": 1369618618888183800,
 		"id_str": "1369618618888183808",
