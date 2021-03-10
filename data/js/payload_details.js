9339ff973733c76953d5b77a3084700a2f2dc674
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-03-10 19:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73300,
-	"created_at": "Wed Mar 10 17:21:04 +0000 2021",
+	"tweets": 73301,
+	"created_at": "Wed Mar 10 18:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,182 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Wed Mar 10 18:15:10 +0000 2021",
+		"id": 1369713455066980400,
+		"id_str": "1369713455066980362",
+		"full_text": "@tyno @rvandenhoff @Luukros @marcoderksen @PvdA @D66 @VoltNederland Hmm. Nu zet je me weer aan het denken. Goede argumenten.",
+		"truncated": false,
+		"display_text_range": [
+			68,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tyno",
+					"name": "Martijn Verver",
+					"id": 963111,
+					"id_str": "963111",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"screen_name": "rvandenhoff",
+					"name": "Ronald van den Hoff",
+					"id": 6044112,
+					"id_str": "6044112",
+					"indices": [
+						6,
+						18
+					]
+				},
+				{
+					"screen_name": "Luukros",
+					"name": "Luuk Ros ♻️",
+					"id": 22533801,
+					"id_str": "22533801",
+					"indices": [
+						19,
+						27
+					]
+				},
+				{
+					"screen_name": "marcoderksen",
+					"name": "Marco Derksen",
+					"id": 1252681,
+					"id_str": "1252681",
+					"indices": [
+						28,
+						41
+					]
+				},
+				{
+					"screen_name": "PvdA",
+					"name": "Partij van de Arbeid",
+					"id": 7343682,
+					"id_str": "7343682",
+					"indices": [
+						42,
+						47
+					]
+				},
+				{
+					"screen_name": "D66",
+					"name": "D66",
+					"id": 28076891,
+					"id_str": "28076891",
+					"indices": [
+						48,
+						52
+					]
+				},
+				{
+					"screen_name": "VoltNederland",
+					"name": "Volt Nederland",
+					"id": 966790780869316600,
+					"id_str": "966790780869316608",
+					"indices": [
+						53,
+						67
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1369389680291111000,
+		"in_reply_to_status_id_str": "1369389680291110916",
+		"in_reply_to_user_id": 963111,
+		"in_reply_to_user_id_str": "963111",
+		"in_reply_to_screen_name": "tyno",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8892,
+			"friends_count": 719,
+			"listed_count": 555,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7522,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64123,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1615141708",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@tyno @rvandenhoff @Luukros @marcoderksen @PvdA @D66 @VoltNederland Hmm. Nu zet je me weer aan het denken. Goede argumenten."
+	},
+	{
 		"created_at": "Wed Mar 10 16:56:59 +0000 2021",
 		"id": 1369693780023115800,
 		"id_str": "1369693780023115778",
