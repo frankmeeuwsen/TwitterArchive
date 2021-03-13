d8ac02e912e124ff98d1df6af180a36ae3cefad7
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-03-13 16:21:06 +0100

    Added 1 tweets

--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Sat Mar 13 15:18:25 +0000 2021",
+		"id": 1370756140397686800,
+		"id_str": "1370756140397686786",
+		"full_text": "NFT TulipMania. Wie pakt hem op? https://t.co/RsT1n74sul",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RsT1n74sul",
+					"expanded_url": "https://twitter.com/BiellaColeman/status/1370715921573105667",
+					"display_url": "twitter.com/BiellaColeman/…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8891,
+			"friends_count": 722,
+			"listed_count": 555,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7544,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64156,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1615141708",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1370715921573105700,
+		"quoted_status_id_str": "1370715921573105667",
+		"quoted_status_permalink": {
+			"url": "https://t.co/RsT1n74sul",
+			"expanded": "https://twitter.com/BiellaColeman/status/1370715921573105667",
+			"display": "twitter.com/BiellaColeman/…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Mar 13 12:38:37 +0000 2021",
+			"id": 1370715921573105700,
+			"id_str": "1370715921573105667",
+			"full_text": "Someone should take a picture of the first tulips in .nl to bloom in each region/city of the Netherlands, make an NFT of it, and start the great NFT TulipMania of 2021 to remind humans that their dumb drive for distinction, status, and money never ends, just takes new forms",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				274
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 31030273,
+				"id_str": "31030273",
+				"name": "Gabriella “Biella” Coleman",
+				"screen_name": "BiellaColeman",
+				"location": "Montréal, Québec",
+				"description": "Anthropologist and Wolfe Chair at @McGillU, author of two books on hackers. Curator @HackCurio (https://t.co/r8vKq7naye) Board member of @torproject",
+				"url": "https://t.co/quRjgMzIlS",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/quRjgMzIlS",
+								"expanded_url": "http://gabriellacoleman.org/",
+								"display_url": "gabriellacoleman.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/r8vKq7naye",
+								"expanded_url": "http://hackcur.io",
+								"display_url": "hackcur.io",
+								"indices": [
+									96,
+									119
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 49753,
+				"friends_count": 2869,
+				"listed_count": 2189,
+				"created_at": "Tue Apr 14 02:14:23 +0000 2009",
+				"favourites_count": 35354,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 25876,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "DBE9ED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1346866734574284800/MHvS9ceE_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1346866734574284800/MHvS9ceE_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/31030273/1564701943",
+				"profile_link_color": "CC3366",
+				"profile_sidebar_border_color": "DBE9ED",
+				"profile_sidebar_fill_color": "E6F6F9",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 38,
+			"favorite_count": 146,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "NFT TulipMania. Wie pakt hem op? https://t.co/RsT1n74sul"
+	},
+	{
 		"created_at": "Sat Mar 13 14:02:22 +0000 2021",
 		"id": 1370737000140189700,
 		"id_str": "1370737000140189701",
