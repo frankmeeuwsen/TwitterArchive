de9b942c68a73d0f16f568f90792577e6f23214b
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-04-01 13:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73442,
-	"created_at": "Thu Apr 01 08:21:03 +0000 2021",
+	"tweets": 73443,
+	"created_at": "Thu Apr 01 11:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Thu Apr 01 10:53:43 +0000 2021",
+		"id": 1377574892984541200,
+		"id_str": "1377574892984541190",
+		"full_text": "Hoe noemen mensen die in conspiracy theories geloven ook weer  iemand anders die ogenschijnlijk bij de \"tegenstander\" zit, maar toch aan de goede kant hoort. Soort reverse insider. Daar hebben ze een term voor. Maar wat is die term... https://t.co/o0ag89spp4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			234
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1377574885497667600,
+					"id_str": "1377574885497667588",
+					"indices": [
+						235,
+						258
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Ex4hD0WWgAQkhb5.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Ex4hD0WWgAQkhb5.jpg",
+					"url": "https://t.co/o0ag89spp4",
+					"display_url": "pic.twitter.com/o0ag89spp4",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1377574892984541190/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 244,
+							"h": 120,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 244,
+							"h": 120,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 244,
+							"h": 120,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 120,
+							"h": 120,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1377574885497667600,
+					"id_str": "1377574885497667588",
+					"indices": [
+						235,
+						258
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Ex4hD0WWgAQkhb5.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Ex4hD0WWgAQkhb5.jpg",
+					"url": "https://t.co/o0ag89spp4",
+					"display_url": "pic.twitter.com/o0ag89spp4",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1377574892984541190/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 244,
+							"h": 120,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 244,
+							"h": 120,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 244,
+							"h": 120,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 120,
+							"h": 120,
+							"resize": "crop"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							61,
+							30
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Ex4hD0WWgAQkhb5.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8884,
+			"friends_count": 732,
+			"listed_count": 554,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7645,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64308,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Hoe noemen mensen die in conspiracy theories geloven ook weer  iemand anders die ogenschijnlijk bij de \"tegenstander\" zit, maar toch aan de goede kant hoort. Soort reverse insider. Daar hebben ze een term voor. Maar wat is die term... https://t.co/o0ag89spp4"
+	},
+	{
 		"created_at": "Thu Apr 01 08:05:31 +0000 2021",
 		"id": 1377532564181815300,
 		"id_str": "1377532564181815297",
