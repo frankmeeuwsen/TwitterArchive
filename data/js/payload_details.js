564512be0d191a1d3561c9633790b1b491ed948c
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-04-25 20:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73570,
-	"created_at": "Sun Apr 25 14:21:04 +0000 2021",
+	"tweets": 73571,
+	"created_at": "Sun Apr 25 18:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,304 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sun Apr 25 17:28:46 +0000 2021",
+		"id": 1386371619711557600,
+		"id_str": "1386371619711557636",
+		"full_text": "groter als... https://t.co/HvQ7uWbZ6h",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HvQ7uWbZ6h",
+					"expanded_url": "https://twitter.com/fakehistoryhunt/status/1385991829095649286",
+					"display_url": "twitter.com/fakehistoryhun…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8886,
+			"friends_count": 745,
+			"listed_count": 553,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7729,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1385991829095649300,
+		"quoted_status_id_str": "1385991829095649286",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HvQ7uWbZ6h",
+			"expanded": "https://twitter.com/fakehistoryhunt/status/1385991829095649286",
+			"display": "twitter.com/fakehistoryhun…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Apr 24 16:19:37 +0000 2021",
+			"id": 1385991829095649300,
+			"id_str": "1385991829095649286",
+			"full_text": "About time somebody said this, we were all thinking it. https://t.co/quroqSJZ3K",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				55
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1385991813518045200,
+						"id_str": "1385991813518045189",
+						"indices": [
+							56,
+							79
+						],
+						"media_url": "http://pbs.twimg.com/media/EzwINgpXsAUT4pY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EzwINgpXsAUT4pY.jpg",
+						"url": "https://t.co/quroqSJZ3K",
+						"display_url": "pic.twitter.com/quroqSJZ3K",
+						"expanded_url": "https://twitter.com/fakehistoryhunt/status/1385991829095649286/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 512,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 484,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 512,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1385991813518045200,
+						"id_str": "1385991813518045189",
+						"indices": [
+							56,
+							79
+						],
+						"media_url": "http://pbs.twimg.com/media/EzwINgpXsAUT4pY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EzwINgpXsAUT4pY.jpg",
+						"url": "https://t.co/quroqSJZ3K",
+						"display_url": "pic.twitter.com/quroqSJZ3K",
+						"expanded_url": "https://twitter.com/fakehistoryhunt/status/1385991829095649286/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 512,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 484,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 512,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 878446695016800300,
+				"id_str": "878446695016800257",
+				"name": "Fake History Hunter",
+				"screen_name": "fakehistoryhunt",
+				"location": "The Past",
+				"description": "I hunt fake history and correct it, but also post amazing real history stuff.   Rude, obscene, vulgar, racist people who can't act like grown-ups get blocked.",
+				"url": "https://t.co/a5XhrxViSk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/a5XhrxViSk",
+								"expanded_url": "http://www.fakehistoryhunter.net",
+								"display_url": "fakehistoryhunter.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 87882,
+				"friends_count": 2996,
+				"listed_count": 430,
+				"created_at": "Sat Jun 24 02:56:27 +0000 2017",
+				"favourites_count": 37749,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 67812,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/878755260487041024/QTSLY8lV_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/878755260487041024/QTSLY8lV_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/878446695016800257/1568129918",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3779,
+			"favorite_count": 14711,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "groter als... https://t.co/HvQ7uWbZ6h"
+	},
+	{
 		"created_at": "Sun Apr 25 13:27:06 +0000 2021",
 		"id": 1386310803612217300,
 		"id_str": "1386310803612217349",
