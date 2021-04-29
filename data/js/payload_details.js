23d114c6b818b51e85777d8bed97fd3413ce152a
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-04-29 14:21:06 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73594,
-	"created_at": "Thu Apr 29 06:21:05 +0000 2021",
+	"tweets": 73595,
+	"created_at": "Thu Apr 29 12:21:05 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Thu Apr 29 11:54:03 +0000 2021",
+		"id": 1387716529144406000,
+		"id_str": "1387736936647962626",
+		"full_text": "Gisteren dronken we het 1e terrasbiertje! En dat de bitterbal mogelijk geschrapt werd, konden we niet verkroppen. Daarom maakten we de Bitterbel: met 1 druk op de knop bestel je 'n portie. Een portie wat? Een portie Vocking Bitterballen natuurlijk.#bellenvoorballen @Vockingworst https://t.co/xuA7nSFmzc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "bellenvoorballen",
+					"indices": [
+						248,
+						265
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Vockingworst",
+					"name": "Vocking",
+					"id": 66475455,
+					"id_str": "66475455",
+					"indices": [
+						266,
+						279
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1387716457174286300,
+					"id_str": "1387716457174286340",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1387716457174286340/pu/img/5NGizJXlWjX36H0j.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1387716457174286340/pu/img/5NGizJXlWjX36H0j.jpg",
+					"url": "https://t.co/xuA7nSFmzc",
+					"display_url": "pic.twitter.com/xuA7nSFmzc",
+					"expanded_url": "https://twitter.com/Kaliberinteract/status/1387716529144406017/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1387716457174286300,
+					"id_str": "1387716457174286340",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1387716457174286340/pu/img/5NGizJXlWjX36H0j.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1387716457174286340/pu/img/5NGizJXlWjX36H0j.jpg",
+					"url": "https://t.co/xuA7nSFmzc",
+					"display_url": "pic.twitter.com/xuA7nSFmzc",
+					"expanded_url": "https://twitter.com/Kaliberinteract/status/1387716529144406017/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 30920,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1387716457174286340/pu/vid/1280x720/2CGrn1Ne_JZWzwdX.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1387716457174286340/pu/vid/640x360/sUzFowu9b7RWTyS7.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1387716457174286340/pu/pl/EWOTelUJdHgkmMHo.m3u8?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1387716457174286340/pu/vid/480x270/PuGrH87Zw7s471oj.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 10932162,
+			"id_str": "10932162",
+			"name": "Kaliber",
+			"screen_name": "Kaliberinteract",
+			"location": "Utrecht",
+			"description": "Kaliber. Digitaal bureau dat strategie, creativiteit en technologie combineert om mensen te activeren.",
+			"url": "http://t.co/0uBteIkGBt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/0uBteIkGBt",
+							"expanded_url": "http://www.kaliber.net",
+							"display_url": "kaliber.net",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1936,
+			"friends_count": 906,
+			"listed_count": 112,
+			"created_at": "Fri Dec 07 10:50:24 +0000 2007",
+			"favourites_count": 397,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3166,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1161285822311800834/ZPAmW_UZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1161285822311800834/ZPAmW_UZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/10932162/1612193476",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "663B12",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Gisteren dronken we het 1e terrasbiertje! En dat de bitterbal mogelijk geschrapt werd, konden we niet verkroppen. Daarom maakten we de Bitterbel: met 1 druk op de knop bestel je 'n portie. Een portie wat? Een portie Vocking Bitterballen natuurlijk.#bellenvoorballen @Vockingworst https://t.co/xuA7nSFmzc"
+	},
+	{
 		"created_at": "Thu Apr 29 06:04:39 +0000 2021",
 		"id": 1387649006516506600,
 		"id_str": "1387649006516506625",
