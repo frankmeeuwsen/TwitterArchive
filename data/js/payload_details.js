a147cc4b3bc411efa46cb87817d841616fb268f6
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-05-11 07:21:05 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73649,
-	"created_at": "Mon May 10 19:21:04 +0000 2021",
+	"tweets": 73651,
+	"created_at": "Tue May 11 05:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,319 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Tue May 11 05:09:14 +0000 2021",
+		"id": 1391983715044188200,
+		"id_str": "1391983715044188162",
+		"full_text": "@Oli4K @marc2503 Er moet nog iets in om het virus een persoon te maken. \n- Het virus staat om de hoek van het terras te wachten als we allemaal tegelijk gaan. \n- Je kunt het virus niet dwingen tijdens openingstijden binnen te blijven",
+		"truncated": false,
+		"display_text_range": [
+			17,
+			233
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Oli4K",
+					"name": "Olivier Slabbers",
+					"id": 6091182,
+					"id_str": "6091182",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"screen_name": "marc2503",
+					"name": "Marc",
+					"id": 15356625,
+					"id_str": "15356625",
+					"indices": [
+						7,
+						16
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1391871412302921700,
+		"in_reply_to_status_id_str": "1391871412302921738",
+		"in_reply_to_user_id": 6091182,
+		"in_reply_to_user_id_str": "6091182",
+		"in_reply_to_screen_name": "Oli4K",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8875,
+			"friends_count": 757,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7808,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64568,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@Oli4K @marc2503 Er moet nog iets in om het virus een persoon te maken. \n- Het virus staat om de hoek van het terras te wachten als we allemaal tegelijk gaan. \n- Je kunt het virus niet dwingen tijdens openingstijden binnen te blijven"
+	},
+	{
+		"created_at": "Tue May 11 04:51:01 +0000 2021",
+		"id": 1391892945419350000,
+		"id_str": "1391979132775542793",
+		"full_text": "From me and @dhtoomey https://t.co/zNLyWxpQEL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			21
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dhtoomey",
+					"name": "Dan Toomey ☕️",
+					"id": 774402860947677200,
+					"id_str": "774402860947677184",
+					"indices": [
+						12,
+						21
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1391892889106587600,
+					"id_str": "1391892889106587653",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/E1D_NUSXIAUmFcH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1D_NUSXIAUmFcH.jpg",
+					"url": "https://t.co/zNLyWxpQEL",
+					"display_url": "pic.twitter.com/zNLyWxpQEL",
+					"expanded_url": "https://twitter.com/Neal_Freyman/status/1391892945419350017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 668,
+							"h": 1138,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 399,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 668,
+							"h": 1138,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1391892889106587600,
+					"id_str": "1391892889106587653",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/E1D_NUSXIAUmFcH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1D_NUSXIAUmFcH.jpg",
+					"url": "https://t.co/zNLyWxpQEL",
+					"display_url": "pic.twitter.com/zNLyWxpQEL",
+					"expanded_url": "https://twitter.com/Neal_Freyman/status/1391892945419350017/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 668,
+							"h": 1138,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 399,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 668,
+							"h": 1138,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4827618077,
+			"id_str": "4827618077",
+			"name": "Neal Freyman ☕️",
+			"screen_name": "Neal_Freyman",
+			"location": "New York, NY",
+			"description": "Editor at @Morningbrew",
+			"url": "https://t.co/oRqPAiCoRq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oRqPAiCoRq",
+							"expanded_url": "http://www.morningbrew.com",
+							"display_url": "morningbrew.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5362,
+			"friends_count": 704,
+			"listed_count": 58,
+			"created_at": "Tue Jan 19 21:01:20 +0000 2016",
+			"favourites_count": 18179,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/789194990509363200/lrNDuxq__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/789194990509363200/lrNDuxq__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4827618077/1476995507",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 31,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "From me and @dhtoomey https://t.co/zNLyWxpQEL"
+	},
+	{
 		"created_at": "Mon May 10 18:46:29 +0000 2021",
 		"id": 1391826997006028800,
 		"id_str": "1391826997006028800",
