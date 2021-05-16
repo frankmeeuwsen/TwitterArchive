bced4b16e43e8c96a0aa6a037bf0fc9609a43dd3
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-05-16 17:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73674,
-	"created_at": "Sat May 15 22:21:04 +0000 2021",
+	"tweets": 73675,
+	"created_at": "Sun May 16 15:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sun May 16 15:13:00 +0000 2021",
+		"id": 1393860074842595300,
+		"id_str": "1393947597753176069",
+		"full_text": "Voor Utrechters: In de jaarbeurs lopen ze al vooruit en worden de jaren 62/63/64/65 gevaccineerd\nAls je bereid bent voor de vaccinatie naar de Jaarbeurs te komen, kun je bellen met 030-6305400 optie 4. Dan kun je een afspraak maken. @hayify",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			240
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hayify",
+					"name": "Hay Kranen",
+					"id": 6257562,
+					"id_str": "6257562",
+					"indices": [
+						233,
+						240
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 33863737,
+			"id_str": "33863737",
+			"name": "ineke ludikhuize 🍼",
+			"screen_name": "InekeL",
+			"location": "woont in Utrecht",
+			"description": "Denkt en praat over mantelzorg - schreef als ervaringsdeskundige 'Een warme jas' voor mantelzorgers - twittert over van alles sinds 21-04-2009 - houdt van 030",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 914,
+			"friends_count": 998,
+			"listed_count": 14,
+			"created_at": "Tue Apr 21 10:00:59 +0000 2009",
+			"favourites_count": 4775,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19036,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1371775628492554242/-CXsqY3F_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1371775628492554242/-CXsqY3F_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/33863737/1613820577",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "871685aa82f8a489",
+			"url": "https://api.twitter.com/1.1/geo/id/871685aa82f8a489.json",
+			"place_type": "city",
+			"name": "Utrecht",
+			"full_name": "Utrecht, Nederland",
+			"country_code": "NL",
+			"country": "Nederland",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							4.9702867,
+							52.0262429
+						],
+						[
+							5.1952468,
+							52.0262429
+						],
+						[
+							5.1952468,
+							52.142137
+						],
+						[
+							4.9702867,
+							52.142137
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 41,
+		"favorite_count": 33,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "nl",
+		"text": "Voor Utrechters: In de jaarbeurs lopen ze al vooruit en worden de jaren 62/63/64/65 gevaccineerd\nAls je bereid bent voor de vaccinatie naar de Jaarbeurs te komen, kun je bellen met 030-6305400 optie 4. Dan kun je een afspraak maken. @hayify"
+	},
+	{
 		"created_at": "Sat May 15 21:32:17 +0000 2021",
 		"id": 1393680662427799600,
 		"id_str": "1393680662427799556",
