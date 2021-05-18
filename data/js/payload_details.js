be9e55f25dc665a581c359697b51e79e1fda14b3
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-05-18 16:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73679,
-	"created_at": "Tue May 18 09:21:04 +0000 2021",
+	"tweets": 73680,
+	"created_at": "Tue May 18 14:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Tue May 18 14:08:25 +0000 2021",
+		"id": 1394656122888409000,
+		"id_str": "1394656122888409088",
+		"full_text": "In dit tempo van vaccineren zijn alle 5G-opstart-Bill-Gates-channeling grappen al gemaakt voor ik eens geprikt ben en ze kan maken. \n\nPolletiek en Halsema doen niks!!!!1!1!! https://t.co/bTqDv4gDqV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1394656115682693000,
+					"id_str": "1394656115682693129",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E1rQWRcX0Ak5Zj3.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E1rQWRcX0Ak5Zj3.jpg",
+					"url": "https://t.co/bTqDv4gDqV",
+					"display_url": "pic.twitter.com/bTqDv4gDqV",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1394656122888409088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 400,
+							"h": 304,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 304,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 304,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1394656115682693000,
+					"id_str": "1394656115682693129",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E1rQWRcX0Ak5Zj3.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E1rQWRcX0Ak5Zj3.jpg",
+					"url": "https://t.co/bTqDv4gDqV",
+					"display_url": "pic.twitter.com/bTqDv4gDqV",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1394656122888409088/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 400,
+							"h": 304,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 400,
+							"h": 304,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 304,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							25,
+							19
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/E1rQWRcX0Ak5Zj3.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8840,
+			"friends_count": 763,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7834,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64598,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "In dit tempo van vaccineren zijn alle 5G-opstart-Bill-Gates-channeling grappen al gemaakt voor ik eens geprikt ben en ze kan maken. \n\nPolletiek en Halsema doen niks!!!!1!1!! https://t.co/bTqDv4gDqV"
+	},
+	{
 		"created_at": "Tue May 18 09:08:24 +0000 2021",
 		"id": 1394580620936892400,
 		"id_str": "1394580620936892417",
