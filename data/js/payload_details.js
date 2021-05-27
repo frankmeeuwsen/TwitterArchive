1217898da79ab9af7fbc6eb1295dada29697e7f1
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-05-27 19:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73724,
-	"created_at": "Thu May 27 14:21:06 +0000 2021",
+	"tweets": 73725,
+	"created_at": "Thu May 27 17:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_05.js",
 		"year": 2021,
 		"var_name": "tweets_2021_05",
-		"tweet_count": 126,
+		"tweet_count": 127,
 		"month": 5
 	},
 	{
