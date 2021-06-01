ae599aeafced34da63de4462a2204a22aabc72d0
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-01 19:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73745,
-	"created_at": "Tue Jun 01 06:21:04 +0000 2021",
+	"tweets": 73746,
+	"created_at": "Tue Jun 01 17:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,261 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Tue Jun 01 17:17:26 +0000 2021",
+		"id": 1399777121329889300,
+		"id_str": "1399777121329889282",
+		"full_text": "Interessante launch! https://t.co/UMnMYZBSZ4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/UMnMYZBSZ4",
+					"expanded_url": "https://twitter.com/mep/status/1399772558942158856",
+					"display_url": "twitter.com/mep/status/139…",
+					"indices": [
+						21,
+						44
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8781,
+			"friends_count": 775,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7911,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64688,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1399772558942158800,
+		"quoted_status_id_str": "1399772558942158856",
+		"quoted_status_permalink": {
+			"url": "https://t.co/UMnMYZBSZ4",
+			"expanded": "https://twitter.com/mep/status/1399772558942158856",
+			"display": "twitter.com/mep/status/139…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jun 01 16:59:19 +0000 2021",
+			"id": 1399772558942158800,
+			"id_str": "1399772558942158856",
+			"full_text": "Excited for the launch of @tomorrow on Twitter – a new approach to local weather from @EricHolthaus and a diverse network of meteorologists &amp; climate journalists.\n\n🏙 16 newsletters via @Revue\n💰 Subscribers access experts\n⚙️ Writers split revenue https://t.co/Pc1fl1GDr7",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				249
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "tomorrow",
+						"name": "Tomorrow",
+						"id": 1396568392312791000,
+						"id_str": "1396568392312791048",
+						"indices": [
+							26,
+							35
+						]
+					},
+					{
+						"screen_name": "EricHolthaus",
+						"name": "Eric Holthaus",
+						"id": 290180065,
+						"id_str": "290180065",
+						"indices": [
+							86,
+							99
+						]
+					},
+					{
+						"screen_name": "revue",
+						"name": "Revue",
+						"id": 3001165942,
+						"id_str": "3001165942",
+						"indices": [
+							189,
+							195
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/Pc1fl1GDr7",
+						"expanded_url": "https://twitter.com/EricHolthaus/status/1399751523354165249",
+						"display_url": "twitter.com/EricHolthaus/s…",
+						"indices": [
+							250,
+							273
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 29127028,
+				"id_str": "29127028",
+				"name": "Mike Park",
+				"screen_name": "mep",
+				"location": "Iowa 🌽 & Brooklyn 🗽",
+				"description": "VP Product @Twitter focused on longform. Co-creator Twitter Amplify. Prior: music startup-er, options trader, unemployed drummer, miler @DukeTFXC.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 5037,
+				"friends_count": 2278,
+				"listed_count": 119,
+				"created_at": "Mon Apr 06 02:36:57 +0000 2009",
+				"favourites_count": 10560,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 5464,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "383838",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1386840781193887747/7dl3LAcH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1386840781193887747/7dl3LAcH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/29127028/1469070467",
+				"profile_link_color": "108EAD",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C9C9C9",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1399751523354165200,
+			"quoted_status_id_str": "1399751523354165249",
+			"quoted_status_permalink": {
+				"url": "https://t.co/Pc1fl1GDr7",
+				"expanded": "https://twitter.com/EricHolthaus/status/1399751523354165249",
+				"display": "twitter.com/EricHolthaus/s…"
+			},
+			"retweet_count": 5,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Interessante launch! https://t.co/UMnMYZBSZ4"
+	},
+	{
 		"created_at": "Tue Jun 01 05:34:12 +0000 2021",
 		"id": 1399585393142550500,
 		"id_str": "1399600143679299586",
