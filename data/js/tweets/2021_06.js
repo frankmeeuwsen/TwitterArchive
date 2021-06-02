16128557885b52421ae104b4442ea86c2ee9422b
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-02 19:21:06 +0200

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 02 17:13:03 +0000 2021",
+		"id": 1400138404214476800,
+		"id_str": "1400138404214476809",
+		"full_text": "@remkusdevries @benswrite @lesley_pizza @mattmedeiros What did I say? https://t.co/IoxBuEwx7F",
+		"truncated": false,
+		"display_text_range": [
+			54,
+			69
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "remkusdevries",
+					"name": "Remkus de Vries",
+					"id": 14264386,
+					"id_str": "14264386",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "benswrite",
+					"name": "Ben hi! Rebooting????",
+					"id": 4695860702,
+					"id_str": "4695860702",
+					"indices": [
+						15,
+						25
+					]
+				},
+				{
+					"screen_name": "lesley_pizza",
+					"name": "Lesley 🍕",
+					"id": 17828291,
+					"id_str": "17828291",
+					"indices": [
+						26,
+						39
+					]
+				},
+				{
+					"screen_name": "mattmedeiros",
+					"name": "Matt Medeiros🎙",
+					"id": 18791509,
+					"id_str": "18791509",
+					"indices": [
+						40,
+						53
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1400138396773847000,
+					"id_str": "1400138396773847045",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E25KdCAXEAUP26t.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E25KdCAXEAUP26t.jpg",
+					"url": "https://t.co/IoxBuEwx7F",
+					"display_url": "pic.twitter.com/IoxBuEwx7F",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1400138404214476809/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 426,
+							"h": 212,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 426,
+							"h": 212,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 426,
+							"h": 212,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1400138396773847000,
+					"id_str": "1400138396773847045",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/E25KdCAXEAUP26t.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/E25KdCAXEAUP26t.jpg",
+					"url": "https://t.co/IoxBuEwx7F",
+					"display_url": "pic.twitter.com/IoxBuEwx7F",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1400138404214476809/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 426,
+							"h": 212,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 426,
+							"h": 212,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 426,
+							"h": 212,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							213,
+							106
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/E25KdCAXEAUP26t.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1400125448428925000,
+		"in_reply_to_status_id_str": "1400125448428924934",
+		"in_reply_to_user_id": 14264386,
+		"in_reply_to_user_id_str": "14264386",
+		"in_reply_to_screen_name": "remkusdevries",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/6YB8i11ryl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6YB8i11ryl",
+							"expanded_url": "https://diggingthedigital.com/nieuwsbrief",
+							"display_url": "diggingthedigital.com/nieuwsbrief",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8783,
+			"friends_count": 776,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7913,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64698,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@remkusdevries @benswrite @lesley_pizza @mattmedeiros What did I say? https://t.co/IoxBuEwx7F"
+	},
+	{
 		"created_at": "Wed Jun 02 15:48:30 +0000 2021",
 		"id": 1400117126833266700,
 		"id_str": "1400117126833266689",
