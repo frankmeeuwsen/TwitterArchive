2aae16afd1986c5baf0c239c4de36887aa1704ed
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-13 21:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73815,
-	"created_at": "Sun Jun 13 06:21:04 +0000 2021",
+	"tweets": 73816,
+	"created_at": "Sun Jun 13 19:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,189 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sun Jun 13 18:49:33 +0000 2021",
+		"id": 1404045747641794600,
+		"id_str": "1404148954607108099",
+		"full_text": "We are the champions #EKhockey2021 https://t.co/S0hYuvB8Bf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "EKhockey2021",
+					"indices": [
+						21,
+						34
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1404045729451102200,
+					"id_str": "1404045729451102212",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/E3wsJuhWUAQeugD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3wsJuhWUAQeugD.jpg",
+					"url": "https://t.co/S0hYuvB8Bf",
+					"display_url": "pic.twitter.com/S0hYuvB8Bf",
+					"expanded_url": "https://twitter.com/Milkshake/status/1404045747641794560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1404045729451102200,
+					"id_str": "1404045729451102212",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/E3wsJuhWUAQeugD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3wsJuhWUAQeugD.jpg",
+					"url": "https://t.co/S0hYuvB8Bf",
+					"display_url": "pic.twitter.com/S0hYuvB8Bf",
+					"expanded_url": "https://twitter.com/Milkshake/status/1404045747641794560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 6995192,
+			"id_str": "6995192",
+			"name": "Erik Huizer",
+			"screen_name": "Milkshake",
+			"location": "030",
+			"description": "Proud father, CEO@GÉANT, Internet Hall of Fame, TEDx speaker, NTR, Media, Science, Education, Networking, NGO's, Music, Kampong & (Chocolate) Milkshake",
+			"url": "https://t.co/CMEfLqpwtA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CMEfLqpwtA",
+							"expanded_url": "http://huison.home.xs4all.nl/Huizer/",
+							"display_url": "huison.home.xs4all.nl/Huizer/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3091,
+			"friends_count": 1779,
+			"listed_count": 139,
+			"created_at": "Thu Jun 21 14:04:31 +0000 2007",
+			"favourites_count": 8679,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24300,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFDAB9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/674157210243432448/wPwlWMkI_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/674157210243432448/wPwlWMkI_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/6995192/1356175597",
+			"profile_link_color": "FF6600",
+			"profile_sidebar_border_color": "FF6600",
+			"profile_sidebar_fill_color": "EECDAB",
+			"profile_text_color": "FF704D",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are the champions #EKhockey2021 https://t.co/S0hYuvB8Bf"
+	},
+	{
 		"created_at": "Sun Jun 13 05:50:16 +0000 2021",
 		"id": 1403899468945166300,
 		"id_str": "1403952842797371394",
