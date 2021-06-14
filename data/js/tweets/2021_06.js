1c289561e05824fa69e5e7c7ccd8789bd19acd6a
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-14 21:21:05 +0200

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Mon Jun 14 19:13:49 +0000 2021",
+		"id": 1404517449710592000,
+		"id_str": "1404517449710592004",
+		"full_text": "@nicoline Snel, zet je 5G uit door je bovenarm tegen de koelkastmagneet te houden! En zet iets van Coldplay op, dan komt het vanzelf goed...",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nicoline",
+					"name": "nicoline",
+					"id": 9455772,
+					"id_str": "9455772",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1404514736054706200,
+		"in_reply_to_status_id_str": "1404514736054706193",
+		"in_reply_to_user_id": 9455772,
+		"in_reply_to_user_id_str": "9455772",
+		"in_reply_to_screen_name": "nicoline",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8767,
+			"friends_count": 788,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 7991,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64787,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@nicoline Snel, zet je 5G uit door je bovenarm tegen de koelkastmagneet te houden! En zet iets van Coldplay op, dan komt het vanzelf goed..."
+	},
+	{
 		"created_at": "Mon Jun 14 16:08:43 +0000 2021",
 		"id": 1404201199885299700,
 		"id_str": "1404470870337376256",
@@ -28,7 +145,7 @@ Grailbird.data.tweets_2021_06 =
 			"name": "Nadia Buiter #GeenDorHout #SolidairNaarNul",
 			"screen_name": "nadia_buiter",
 			"location": "",
-			"description": "Social psychology student with a passion for research & improving inclusion in society. #endometriosis #adenomyosis Pronouns: she/her they/them. 24 y/o 🏳️‍🌈",
+			"description": "Social psychology student with a passion for research & improving inclusion in society. #endometriosis #adenomyosis Pronouns: she/her they/them. 24 y/o 🏳️🌈",
 			"url": null,
 			"entities": {
 				"description": {
