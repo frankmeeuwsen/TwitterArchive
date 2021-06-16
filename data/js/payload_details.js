46ecb8bd548ba80246592bfa69364159c2d87d78
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-16 15:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73833,
-	"created_at": "Wed Jun 16 12:21:04 +0000 2021",
+	"tweets": 73834,
+	"created_at": "Wed Jun 16 13:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 16 13:07:55 +0000 2021",
+		"id": 1404885087276175400,
+		"id_str": "1405150143746351110",
+		"full_text": "I want to revive the original spirit of the older, pre-commercial internet—if not in form, then in function. A slower, more thoughtful, more creative space. You're invited:\nhttps://t.co/LW4IUm8pud",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			196
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LW4IUm8pud",
+					"expanded_url": "https://edwardsnowden.substack.com/p/lifting-the-mask",
+					"display_url": "edwardsnowden.substack.com/p/lifting-the-…",
+					"indices": [
+						173,
+						196
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2916305152,
+			"id_str": "2916305152",
+			"name": "Edward Snowden",
+			"screen_name": "Snowden",
+			"location": "",
+			"description": "I used to work for the government. Now I work for the public. President at @FreedomofPress.",
+			"url": "https://t.co/kPBEESUmlH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kPBEESUmlH",
+							"expanded_url": "https://edwardsnowden.substack.com",
+							"display_url": "edwardsnowden.substack.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4691701,
+			"friends_count": 1,
+			"listed_count": 21868,
+			"created_at": "Thu Dec 11 21:24:28 +0000 2014",
+			"favourites_count": 507,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5491,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/648888480974508032/66_cUYfj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/648888480974508032/66_cUYfj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2916305152/1443542022",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1127,
+		"favorite_count": 4431,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I want to revive the original spirit of the older, pre-commercial internet—if not in form, then in function. A slower, more thoughtful, more creative space. You're invited:\nhttps://t.co/LW4IUm8pud"
+	},
+	{
 		"created_at": "Wed Jun 16 12:13:54 +0000 2021",
 		"id": 1405136548929097700,
 		"id_str": "1405136548929097729",
