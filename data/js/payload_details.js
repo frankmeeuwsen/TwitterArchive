5c32b137dd54d1fe2eca5493703e4c8fad02f1ec
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-18 21:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73848,
-	"created_at": "Fri Jun 18 17:21:04 +0000 2021",
+	"tweets": 73849,
+	"created_at": "Fri Jun 18 19:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 18 18:47:52 +0000 2021",
+		"id": 1405952938724532200,
+		"id_str": "1405960473128878080",
+		"full_text": "Check het verschil in focus op https://t.co/xwThAWoknL en https://t.co/7OUTNKpBJF. Het maakt heel helder wat het verschil is tussen de twee.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xwThAWoknL",
+					"expanded_url": "https://about.twitter.com",
+					"display_url": "about.twitter.com",
+					"indices": [
+						31,
+						54
+					]
+				},
+				{
+					"url": "https://t.co/7OUTNKpBJF",
+					"expanded_url": "http://about.facebook.com",
+					"display_url": "about.facebook.com",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 11496552,
+			"id_str": "11496552",
+			"name": "Mike Verbruggen",
+			"screen_name": "mikeverbruggen",
+			"location": "Utrecht, Utrecht NL",
+			"description": "Creatief mens. Oprichter van @videokantoor",
+			"url": "https://t.co/LobKC4EHjV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LobKC4EHjV",
+							"expanded_url": "http://Videokantoor.com",
+							"display_url": "Videokantoor.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1926,
+			"friends_count": 1269,
+			"listed_count": 116,
+			"created_at": "Tue Dec 25 07:47:21 +0000 2007",
+			"favourites_count": 4046,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32909,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/821315684395270144/cZFOTRQu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/821315684395270144/cZFOTRQu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/11496552/1446639134",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "4F4F4F",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Check het verschil in focus op https://t.co/xwThAWoknL en https://t.co/7OUTNKpBJF. Het maakt heel helder wat het verschil is tussen de twee."
+	},
+	{
 		"created_at": "Fri Jun 18 17:06:09 +0000 2021",
 		"id": 1405934874243080200,
 		"id_str": "1405934874243080195",
