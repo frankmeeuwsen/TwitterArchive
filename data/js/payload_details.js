52e645c304ea2c3429eef11e716c46eaa4627e05
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-19 09:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73849,
-	"created_at": "Fri Jun 18 19:21:04 +0000 2021",
+	"tweets": 73850,
+	"created_at": "Sat Jun 19 07:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sat Jun 19 06:36:37 +0000 2021",
+		"id": 1405860658449682400,
+		"id_str": "1406138833612247040",
+		"full_text": "https://t.co/yUltzHfDmY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1405860655811420200,
+					"id_str": "1405860655811420166",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E4Ke0WFWYAYtthP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4Ke0WFWYAYtthP.jpg",
+					"url": "https://t.co/yUltzHfDmY",
+					"display_url": "pic.twitter.com/yUltzHfDmY",
+					"expanded_url": "https://twitter.com/80snewsscreens/status/1405860658449682438/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 807,
+							"h": 578,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 487,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 807,
+							"h": 578,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1405860655811420200,
+					"id_str": "1405860655811420166",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/E4Ke0WFWYAYtthP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4Ke0WFWYAYtthP.jpg",
+					"url": "https://t.co/yUltzHfDmY",
+					"display_url": "pic.twitter.com/yUltzHfDmY",
+					"expanded_url": "https://twitter.com/80snewsscreens/status/1405860658449682438/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 807,
+							"h": 578,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 487,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 807,
+							"h": 578,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1217811774600368000,
+			"id_str": "1217811774600368131",
+			"name": "80s News Screens",
+			"screen_name": "80snewsscreens",
+			"location": "",
+			"description": "Find us on Instagram @80snewsscreens. Screengrabs from slower news days. See also @90snewsscreens",
+			"url": "https://t.co/nHC8L4zT6J",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nHC8L4zT6J",
+							"expanded_url": "http://instagram.com/80snewsscreens",
+							"display_url": "instagram.com/80snewsscreens",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38667,
+			"friends_count": 195,
+			"listed_count": 106,
+			"created_at": "Thu Jan 16 14:12:16 +0000 2020",
+			"favourites_count": 99,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 693,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1364266482549596161/hFTwDIAx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1364266482549596161/hFTwDIAx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1217811774600368131/1614102606",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 96,
+		"favorite_count": 691,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/yUltzHfDmY"
+	},
+	{
 		"created_at": "Fri Jun 18 18:47:52 +0000 2021",
 		"id": 1405952938724532200,
 		"id_str": "1405960473128878080",
