cd1dc0becf934ddc588e166deed2e958b2a66387
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-06-21 22:21:05 +0200

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Mon Jun 21 20:03:34 +0000 2021",
+		"id": 1407058681766809600,
+		"id_str": "1407066684972843010",
+		"full_text": "25 Years of Evolution of Web Design Trends.\nUEFA EURO 1996 vs. UEFA EURO 2021.\n#Euro2021 #Euro1996 https://t.co/5etMPDBgST",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Euro2021",
+					"indices": [
+						79,
+						88
+					]
+				},
+				{
+					"text": "Euro1996",
+					"indices": [
+						89,
+						98
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1407058374525587500,
+					"id_str": "1407058374525587464",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/E4bgIv_WEAgZjLD.png",
+					"media_url_https": "https://pbs.twimg.com/media/E4bgIv_WEAgZjLD.png",
+					"url": "https://t.co/5etMPDBgST",
+					"display_url": "pic.twitter.com/5etMPDBgST",
+					"expanded_url": "https://twitter.com/WebDesignMuseum/status/1407058681766809602/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 830,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 830,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 588,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1407058374525587500,
+					"id_str": "1407058374525587464",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/E4bgIv_WEAgZjLD.png",
+					"media_url_https": "https://pbs.twimg.com/media/E4bgIv_WEAgZjLD.png",
+					"url": "https://t.co/5etMPDBgST",
+					"display_url": "pic.twitter.com/5etMPDBgST",
+					"expanded_url": "https://twitter.com/WebDesignMuseum/status/1407058681766809602/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 830,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 830,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 588,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1407058427487129600,
+					"id_str": "1407058427487129600",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/E4bgL1SXEAAXm4L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4bgL1SXEAAXm4L.jpg",
+					"url": "https://t.co/5etMPDBgST",
+					"display_url": "pic.twitter.com/5etMPDBgST",
+					"expanded_url": "https://twitter.com/WebDesignMuseum/status/1407058681766809602/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 235,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 133,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 402,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 738019344047845400,
+			"id_str": "738019344047845377",
+			"name": "Web Design Museum",
+			"screen_name": "WebDesignMuseum",
+			"location": "",
+			"description": "Web Design Museum exhibits over 2,000 carefully selected websites that show web design trends between the years 1991 and 2006.",
+			"url": "https://t.co/vyFBftWgRB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vyFBftWgRB",
+							"expanded_url": "http://www.webdesignmuseum.org",
+							"display_url": "webdesignmuseum.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8566,
+			"friends_count": 20,
+			"listed_count": 129,
+			"created_at": "Wed Jun 01 14:48:18 +0000 2016",
+			"favourites_count": 19748,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2382,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/860928105975885828/8XLNUZIa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/860928105975885828/8XLNUZIa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/738019344047845377/1541958150",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 14,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "25 Years of Evolution of Web Design Trends.\nUEFA EURO 1996 vs. UEFA EURO 2021.\n#Euro2021 #Euro1996 https://t.co/5etMPDBgST"
+	},
+	{
 		"created_at": "Mon Jun 21 17:40:03 +0000 2021",
 		"id": 1407030567309385700,
 		"id_str": "1407030567309385736",
