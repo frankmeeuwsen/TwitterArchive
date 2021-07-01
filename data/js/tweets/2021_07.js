b912baceb5b23e11532153dddf2352d829dce50e
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-07-01 08:21:05 +0200

    Added 1 tweets

---
+++ data/js/tweets/2021_07.js
@@ -0,0 +1,252 @@
+Grailbird.data.tweets_2021_07 = 
+[
+	{
+		"created_at": "Thu Jul 01 05:22:18 +0000 2021",
+		"id": 1410468785379741700,
+		"id_str": "1410468785379741698",
+		"full_text": "1. If you look at the big ones, yes. Dive into the niches &gt; 💎💎💎. One that pops up for me: https://t.co/JKuKQIdSW6 by @craigmod \n2. Blogs come in all shapes and sizes, esp the personal ones in, again, niches. So no. \n3. Twitter is on to something with @revue. We’ll see… https://t.co/ra45aZmrE0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "craigmod",
+					"name": "Craig Mod",
+					"id": 1835951,
+					"id_str": "1835951",
+					"indices": [
+						120,
+						129
+					]
+				},
+				{
+					"screen_name": "revue",
+					"name": "Revue",
+					"id": 3001165942,
+					"id_str": "3001165942",
+					"indices": [
+						254,
+						260
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JKuKQIdSW6",
+					"expanded_url": "https://craigmod.com/huh/",
+					"display_url": "craigmod.com/huh/",
+					"indices": [
+						93,
+						116
+					]
+				},
+				{
+					"url": "https://t.co/ra45aZmrE0",
+					"expanded_url": "https://twitter.com/travelinganna/status/1409943498749579265",
+					"display_url": "twitter.com/travelinganna/…",
+					"indices": [
+						273,
+						296
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8770,
+			"friends_count": 797,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8077,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64885,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1409943498749579300,
+		"quoted_status_id_str": "1409943498749579265",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ra45aZmrE0",
+			"expanded": "https://twitter.com/travelinganna/status/1409943498749579265",
+			"display": "twitter.com/travelinganna/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jun 29 18:35:00 +0000 2021",
+			"id": 1409943498749579300,
+			"id_str": "1409943498749579265",
+			"full_text": "1. All newsletters look like Revue now, including Bulletin. \n2. These are just blogs. \n3. No one has solved for signup and discoverability yet.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				143
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14532620,
+				"id_str": "14532620",
+				"name": "Annemarie Dooling",
+				"screen_name": "TravelingAnna",
+				"location": "Philadelphia, PA",
+				"description": "Nice Italian girl from Brooklyn. Community obsessive. Illusionist. WSJ strategy (ex Vox, HuffPost)",
+				"url": "https://t.co/dofcoTQjOK",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/dofcoTQjOK",
+								"expanded_url": "https://www.twitter.com/travelinganna",
+								"display_url": "twitter.com/travelinganna",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 29800,
+				"friends_count": 3756,
+				"listed_count": 1270,
+				"created_at": "Fri Apr 25 19:38:43 +0000 2008",
+				"favourites_count": 43231,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 23188,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1388621159588147205/dhLNAHZw_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1388621159588147205/dhLNAHZw_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14532620/1596941073",
+				"profile_link_color": "1C8FC4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "200709",
+				"profile_text_color": "6A4836",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 8,
+			"favorite_count": 47,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "1. If you look at the big ones, yes. Dive into the niches &gt; 💎💎💎. One that pops up for me: https://t.co/JKuKQIdSW6 by @craigmod \n2. Blogs come in all shapes and sizes, esp the personal ones in, again, niches. So no. \n3. Twitter is on to something with @revue. We’ll see… https://t.co/ra45aZmrE0"
+	}
+]
