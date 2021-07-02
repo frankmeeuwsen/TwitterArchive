4f267701aef76c6fb2d3270aaddb74d194340cc8
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-07-02 12:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73913,
-	"created_at": "Thu Jul 01 22:21:03 +0000 2021",
+	"tweets": 73914,
+	"created_at": "Fri Jul 02 10:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 02 09:26:30 +0000 2021",
+		"id": 1410607814557331500,
+		"id_str": "1410892627105439747",
+		"full_text": "Aw, fuck. Here we go again. Expanding the empire.\n\nhttps://t.co/Ke2jMPIwia",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ke2jMPIwia",
+					"expanded_url": "https://www.getrevue.co/profile/killedbygoogle/issues/killed-by-google-remembering-google-reader-8-years-later-319412",
+					"display_url": "getrevue.co/profile/killed…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1041175266133827600,
+			"id_str": "1041175266133827585",
+			"name": "Killed by Google 🔪",
+			"screen_name": "killedbygoogle",
+			"location": "The Morgue @ Googleplex",
+			"description": "Cheekily tracking Google's bullshit. Press and other assorted death threats? Throw a knife@killedbygoogle.com. (I await a C&D.😍) aka @codyogden (he/they)",
+			"url": "https://t.co/3vOD3Qslw1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3vOD3Qslw1",
+							"expanded_url": "https://killedbygoogle.com",
+							"display_url": "killedbygoogle.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23144,
+			"friends_count": 116,
+			"listed_count": 154,
+			"created_at": "Sun Sep 16 04:01:42 +0000 2018",
+			"favourites_count": 38854,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18588,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1376700150286934022/9OIytUTS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1376700150286934022/9OIytUTS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1041175266133827585/1602097815",
+			"profile_link_color": "FAFAFA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 55,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Aw, fuck. Here we go again. Expanding the empire.\n\nhttps://t.co/Ke2jMPIwia"
+	},
+	{
 		"created_at": "Thu Jul 01 21:25:51 +0000 2021",
 		"id": 1410711269628428300,
 		"id_str": "1410711269628428292",
