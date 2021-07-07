296d463ac012f3672271a80ddf60e7a43777b9c8
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-07-07 21:21:05 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73941,
-	"created_at": "Wed Jul 07 17:21:04 +0000 2021",
+	"tweets": 73943,
+	"created_at": "Wed Jul 07 19:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,354 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Wed Jul 07 19:20:11 +0000 2021",
+		"id": 1412853544751964200,
+		"id_str": "1412853972042457091",
+		"full_text": "Pearl Jam's Alive single was released on this day back on July 7th 1991 @PearlJam https://t.co/sKPHA59RGU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PearlJam",
+					"name": "Pearl Jam",
+					"id": 15155074,
+					"id_str": "15155074",
+					"indices": [
+						72,
+						81
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1412853243072467000,
+					"id_str": "1412853243072466944",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/E5t2ifkX0AAopMw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5t2ifkX0AAopMw.jpg",
+					"url": "https://t.co/sKPHA59RGU",
+					"display_url": "pic.twitter.com/sKPHA59RGU",
+					"expanded_url": "https://twitter.com/demetriosns/status/1412853544751964161/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1189,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 674,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1192,
+							"h": 1203,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1412853243072467000,
+					"id_str": "1412853243072466944",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/E5t2ifkX0AAopMw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5t2ifkX0AAopMw.jpg",
+					"url": "https://t.co/sKPHA59RGU",
+					"display_url": "pic.twitter.com/sKPHA59RGU",
+					"expanded_url": "https://twitter.com/demetriosns/status/1412853544751964161/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1189,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 674,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1192,
+							"h": 1203,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2445883256,
+			"id_str": "2445883256",
+			"name": "demetrios",
+			"screen_name": "demetriosns",
+			"location": " 🇨🇦",
+			"description": "#pearljam",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4666,
+			"friends_count": 4765,
+			"listed_count": 49,
+			"created_at": "Tue Apr 15 19:14:28 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 28227,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/964089175489351680/xBdqHMwd_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/964089175489351680/xBdqHMwd_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2445883256/1518692315",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Pearl Jam's Alive single was released on this day back on July 7th 1991 @PearlJam https://t.co/sKPHA59RGU"
+	},
+	{
+		"created_at": "Wed Jul 07 19:18:27 +0000 2021",
+		"id": 1412528687644295200,
+		"id_str": "1412853539379032067",
+		"full_text": "My Avengers ❤️ https://t.co/9Lcgrr0zb3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1412528683500327000,
+					"id_str": "1412528683500326915",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/E5pPWo8WUAMTTrb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5pPWo8WUAMTTrb.jpg",
+					"url": "https://t.co/9Lcgrr0zb3",
+					"display_url": "pic.twitter.com/9Lcgrr0zb3",
+					"expanded_url": "https://twitter.com/LiamGaughan98/status/1412528687644295168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 828,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 556,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1412528683500327000,
+					"id_str": "1412528683500326915",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/E5pPWo8WUAMTTrb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5pPWo8WUAMTTrb.jpg",
+					"url": "https://t.co/9Lcgrr0zb3",
+					"display_url": "pic.twitter.com/9Lcgrr0zb3",
+					"expanded_url": "https://twitter.com/LiamGaughan98/status/1412528687644295168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 828,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 556,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 522554582,
+			"id_str": "522554582",
+			"name": "Liam 🌹",
+			"screen_name": "LiamGaughan98",
+			"location": "Newcastle upon tyne",
+			"description": "Rude Ginger Bitch || he/him",
+			"url": "https://t.co/JeWC3whZCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JeWC3whZCo",
+							"expanded_url": "http://Instagram.com/liamgaughan",
+							"display_url": "Instagram.com/liamgaughan",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2074,
+			"friends_count": 993,
+			"listed_count": 5,
+			"created_at": "Mon Mar 12 20:23:36 +0000 2012",
+			"favourites_count": 66797,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 26219,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1385131608701906947/dlVOctt__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1385131608701906947/dlVOctt__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/522554582/1572349066",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16071,
+		"favorite_count": 119560,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My Avengers ❤️ https://t.co/9Lcgrr0zb3"
+	},
+	{
 		"created_at": "Wed Jul 07 17:17:10 +0000 2021",
 		"id": 1412823013389648000,
 		"id_str": "1412823013389647875",
