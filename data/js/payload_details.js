4a552cb9d8a167f25671130d36f76da1a0e4cba3
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-07-10 01:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73962,
-	"created_at": "Fri Jul 09 22:21:04 +0000 2021",
+	"tweets": 73963,
+	"created_at": "Fri Jul 09 23:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 09 22:43:54 +0000 2021",
+		"id": 1413621991609835500,
+		"id_str": "1413630015858696193",
+		"full_text": "Former music journalist dad joke meme coming in 3, 2, 1 https://t.co/mvhj6IsDVK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1413621988522795000,
+					"id_str": "1413621988522795010",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/E54xtXiVIAIp3KX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E54xtXiVIAIp3KX.jpg",
+					"url": "https://t.co/mvhj6IsDVK",
+					"display_url": "pic.twitter.com/mvhj6IsDVK",
+					"expanded_url": "https://twitter.com/guerrillacandy/status/1413621991609835520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 598,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1266,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1055,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1413621988522795000,
+					"id_str": "1413621988522795010",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/E54xtXiVIAIp3KX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E54xtXiVIAIp3KX.jpg",
+					"url": "https://t.co/mvhj6IsDVK",
+					"display_url": "pic.twitter.com/mvhj6IsDVK",
+					"expanded_url": "https://twitter.com/guerrillacandy/status/1413621991609835520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 598,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1266,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1055,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16439600,
+			"id_str": "16439600",
+			"name": "Travis Hay",
+			"screen_name": "guerrillacandy",
+			"location": "Seattle, WA",
+			"description": "Slowly butchering the riff to Smoke on the Water since 2021.",
+			"url": "https://t.co/8M94B4XVoo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8M94B4XVoo",
+							"expanded_url": "https://www.guerrillacandy.com",
+							"display_url": "guerrillacandy.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5883,
+			"friends_count": 4150,
+			"listed_count": 352,
+			"created_at": "Wed Sep 24 19:36:54 +0000 2008",
+			"favourites_count": 339,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 41264,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1324412005332123648/qwgyLwD7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1324412005332123648/qwgyLwD7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16439600/1484246837",
+			"profile_link_color": "FA0505",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "300930",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Former music journalist dad joke meme coming in 3, 2, 1 https://t.co/mvhj6IsDVK"
+	},
+	{
 		"created_at": "Fri Jul 09 21:50:45 +0000 2021",
 		"id": 1413616639782932500,
 		"id_str": "1413616639782932482",
