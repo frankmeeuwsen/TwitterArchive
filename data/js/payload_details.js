07ab96429d8e0475ceed4f64ec488acdf22135e0
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-07-29 10:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74000,
-	"created_at": "Sat Jul 24 07:21:04 +0000 2021",
+	"tweets": 74001,
+	"created_at": "Thu Jul 29 08:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 29 07:51:28 +0000 2021",
+		"id": 1420653184599695400,
+		"id_str": "1420653184599695361",
+		"full_text": "Wat kan Olympisch boogschieten spannend zijn. En wat een toffe uitrusting hebben de atleten!\n\n  * https://t.co/PLaAmvjnEZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1420653182695448600,
+					"id_str": "1420653182695448576",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/E7csiu-XIAA2SQa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7csiu-XIAA2SQa.jpg",
+					"url": "https://t.co/PLaAmvjnEZ",
+					"display_url": "pic.twitter.com/PLaAmvjnEZ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1420653184599695361/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 32,
+							"h": 32,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 32,
+							"h": 32,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 32,
+							"h": 32,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 32,
+							"h": 32,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1420653182695448600,
+					"id_str": "1420653182695448576",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/E7csiu-XIAA2SQa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7csiu-XIAA2SQa.jpg",
+					"url": "https://t.co/PLaAmvjnEZ",
+					"display_url": "pic.twitter.com/PLaAmvjnEZ",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1420653184599695361/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 32,
+							"h": 32,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 32,
+							"h": 32,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 32,
+							"h": 32,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 32,
+							"h": 32,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://brid.gy/\" rel=\"nofollow\">Bridgy</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8774,
+			"friends_count": 802,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8122,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65005,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Wat kan Olympisch boogschieten spannend zijn. En wat een toffe uitrusting hebben de atleten!\n\n  * https://t.co/PLaAmvjnEZ"
+	},
+	{
 		"created_at": "Sat Jul 24 07:00:28 +0000 2021",
 		"id": 1418828410063835100,
 		"id_str": "1418828410063835137",
