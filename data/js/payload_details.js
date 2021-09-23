bbc6b1cf2e122f7d58453fd09e30b67557e062ae
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-09-23 20:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74173,
-	"created_at": "Wed Sep 22 18:21:04 +0000 2021",
+	"tweets": 74174,
+	"created_at": "Thu Sep 23 18:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,258 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 23 17:32:06 +0000 2021",
+		"id": 1441093028664385500,
+		"id_str": "1441093028664385540",
+		"full_text": "@erwblo de digitale bakstenen via Twitter verifiëren! In je eigen Bitcoin-funded Space (wel je key terugvinden… 😉) https://t.co/oZis4nOlus",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "erwblo",
+					"name": "Erwin Blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oZis4nOlus",
+					"expanded_url": "https://twitter.com/duivestein/status/1441090966220206094",
+					"display_url": "twitter.com/duivestein/sta…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 713333,
+		"in_reply_to_user_id_str": "713333",
+		"in_reply_to_screen_name": "erwblo",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8763,
+			"friends_count": 669,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8303,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65225,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1441090966220206000,
+		"quoted_status_id_str": "1441090966220206094",
+		"quoted_status_permalink": {
+			"url": "https://t.co/oZis4nOlus",
+			"expanded": "https://twitter.com/duivestein/status/1441090966220206094",
+			"display": "twitter.com/duivestein/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Sep 23 17:23:55 +0000 2021",
+			"id": 1441090966220206000,
+			"id_str": "1441090966220206094",
+			"full_text": "Twitter accelerates again with Bitcoin tips, NFTs, recorded Spaces, creator fund and more https://t.co/5vrG6bGh9R via @techcrunch",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				129
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "TechCrunch",
+						"name": "TechCrunch",
+						"id": 816653,
+						"id_str": "816653",
+						"indices": [
+							118,
+							129
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/5vrG6bGh9R",
+						"expanded_url": "https://tcrn.ch/2XRyVHj",
+						"display_url": "tcrn.ch/2XRyVHj",
+						"indices": [
+							90,
+							113
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1489581,
+				"id_str": "1489581",
+				"name": "Sander Duivestein - Author of Real Fake book",
+				"screen_name": "duivestein",
+				"location": "Metaverse",
+				"description": "keynote speaker, internet culture, synthetic media, deepfake, Creator Economy, fandoms, crypto, NFT, CGI influencers, Gen Z, AI #RealFake #EchtNep +31625026020",
+				"url": "https://t.co/fo5PxlxOZd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/fo5PxlxOZd",
+								"expanded_url": "http://www.sanderduivestein.com",
+								"display_url": "sanderduivestein.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 7270,
+				"friends_count": 4610,
+				"listed_count": 554,
+				"created_at": "Mon Mar 19 10:49:33 +0000 2007",
+				"favourites_count": 9485,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 40057,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "DBD6D1",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1429749490987192322/jo1tKlpZ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1429749490987192322/jo1tKlpZ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1489581/1631619877",
+				"profile_link_color": "EB862D",
+				"profile_sidebar_border_color": "504120",
+				"profile_sidebar_fill_color": "746E67",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@erwblo de digitale bakstenen via Twitter verifiëren! In je eigen Bitcoin-funded Space (wel je key terugvinden… 😉) https://t.co/oZis4nOlus"
+	},
+	{
 		"created_at": "Wed Sep 22 17:43:20 +0000 2021",
 		"id": 1440733464353861600,
 		"id_str": "1440733464353861638",
