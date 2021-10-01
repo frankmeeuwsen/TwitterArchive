bca1cb4d7c786c40b1a2bf5db8e04d119ae9f793
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-10-01 21:21:05 +0200

    Added 1 tweets

--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,343 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 01 18:29:42 +0000 2021",
+		"id": 1444006627238162400,
+		"id_str": "1444006627238162434",
+		"full_text": "Kunst is geen wedstrijd. Ik ben wel benieuwd welke van de twee meer waardevast over tijd zal blijken. https://t.co/2JBkMjijeU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2JBkMjijeU",
+					"expanded_url": "https://twitter.com/bramk/status/1443993291553394690",
+					"display_url": "twitter.com/bramk/status/1…",
+					"indices": [
+						102,
+						125
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8763,
+			"friends_count": 663,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8383,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65278,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1443993291553394700,
+		"quoted_status_id_str": "1443993291553394690",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2JBkMjijeU",
+			"expanded": "https://twitter.com/bramk/status/1443993291553394690",
+			"display": "twitter.com/bramk/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Oct 01 17:36:43 +0000 2021",
+			"id": 1443993291553394700,
+			"id_str": "1443993291553394690",
+			"full_text": "So this original @veefriends NFT drawing by @garyvee  just sold for $190K at the @christiesinc auction and the next lot, A WARHOL SKETCH, sold for $45K.\n\nAnd we didn't even cross the chasm yet when it comes to #NFTs 🙌 Amazing https://t.co/YCXPZja31s",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				225
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "NFTs",
+						"indices": [
+							210,
+							215
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "veefriends",
+						"name": "VeeFriends",
+						"id": 1365372609903419400,
+						"id_str": "1365372609903419395",
+						"indices": [
+							17,
+							28
+						]
+					},
+					{
+						"screen_name": "garyvee",
+						"name": "Gary Vaynerchuk",
+						"id": 5768872,
+						"id_str": "5768872",
+						"indices": [
+							44,
+							52
+						]
+					},
+					{
+						"screen_name": "ChristiesInc",
+						"name": "Christie's",
+						"id": 18725415,
+						"id_str": "18725415",
+						"indices": [
+							81,
+							94
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1443992614055907300,
+						"id_str": "1443992614055907348",
+						"indices": [
+							226,
+							249
+						],
+						"media_url": "http://pbs.twimg.com/media/FAoXocMXEBQ8Hb3.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FAoXocMXEBQ8Hb3.jpg",
+						"url": "https://t.co/YCXPZja31s",
+						"display_url": "pic.twitter.com/YCXPZja31s",
+						"expanded_url": "https://twitter.com/bramk/status/1443993291553394690/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 518,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1250,
+								"h": 540,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 294,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1443992614055907300,
+						"id_str": "1443992614055907348",
+						"indices": [
+							226,
+							249
+						],
+						"media_url": "http://pbs.twimg.com/media/FAoXocMXEBQ8Hb3.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FAoXocMXEBQ8Hb3.jpg",
+						"url": "https://t.co/YCXPZja31s",
+						"display_url": "pic.twitter.com/YCXPZja31s",
+						"expanded_url": "https://twitter.com/bramk/status/1443993291553394690/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 518,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1250,
+								"h": 540,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 294,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 45139007,
+				"id_str": "45139007",
+				"name": "Bram Kanstein",
+				"screen_name": "bramk",
+				"location": "bram@hey.com",
+				"description": "Startups, #Bitcoin & NFTs | Teaching 600+ people how to validate their ideas with @nocodemvp’s 💡 Made + sold @startupstash ⚡️ The internet made me do it",
+				"url": "https://t.co/ppLz1XUSOC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ppLz1XUSOC",
+								"expanded_url": "http://nocodemvp.com",
+								"display_url": "nocodemvp.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 23033,
+				"friends_count": 3167,
+				"listed_count": 963,
+				"created_at": "Sat Jun 06 14:30:25 +0000 2009",
+				"favourites_count": 49088,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 27468,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1433884178815365128/KfgsybXL_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1433884178815365128/KfgsybXL_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/45139007/1566583338",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "525252",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Kunst is geen wedstrijd. Ik ben wel benieuwd welke van de twee meer waardevast over tijd zal blijken. https://t.co/2JBkMjijeU"
+	},
+	{
 		"created_at": "Fri Oct 01 17:25:13 +0000 2021",
 		"id": 1443811435524632600,
 		"id_str": "1443990398679101447",
