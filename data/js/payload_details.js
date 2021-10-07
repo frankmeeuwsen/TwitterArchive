15a29f3890d06664e9cbcc5c180d9578d46175bf
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-10-07 16:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74237,
-	"created_at": "Wed Oct 06 18:21:03 +0000 2021",
+	"tweets": 74238,
+	"created_at": "Thu Oct 07 14:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 07 14:14:59 +0000 2021",
+		"id": 1446116852816494600,
+		"id_str": "1446116852816494600",
+		"full_text": "Als @Boris mij ergens gaat volgen weet ik dat ik op het goede pad ben. (/cc @getmatterapp ) https://t.co/UR1K9rm5tF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Boris",
+					"name": "Boris Veldhuijzen van Zanten",
+					"id": 17463,
+					"id_str": "17463",
+					"indices": [
+						4,
+						10
+					]
+				},
+				{
+					"screen_name": "getmatterapp",
+					"name": "Matter",
+					"id": 1232479427981791200,
+					"id_str": "1232479427981791233",
+					"indices": [
+						76,
+						89
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1446116848802558000,
+					"id_str": "1446116848802557969",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FBGjnLSVcBEM40N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBGjnLSVcBEM40N.jpg",
+					"url": "https://t.co/UR1K9rm5tF",
+					"display_url": "pic.twitter.com/UR1K9rm5tF",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1446116852816494600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 174,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 174,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 143,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1446116848802558000,
+					"id_str": "1446116848802557969",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FBGjnLSVcBEM40N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBGjnLSVcBEM40N.jpg",
+					"url": "https://t.co/UR1K9rm5tF",
+					"display_url": "pic.twitter.com/UR1K9rm5tF",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1446116852816494600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 174,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 174,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 143,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8762,
+			"friends_count": 658,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8413,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65301,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Als @Boris mij ergens gaat volgen weet ik dat ik op het goede pad ben. (/cc @getmatterapp ) https://t.co/UR1K9rm5tF"
+	},
+	{
 		"created_at": "Wed Oct 06 17:57:38 +0000 2021",
 		"id": 1445810496570024000,
 		"id_str": "1445810496570023936",
