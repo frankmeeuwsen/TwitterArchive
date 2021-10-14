6d6126abac86c82afea1c34e477d86d297e3aa44
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-10-14 22:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74254,
-	"created_at": "Thu Oct 14 16:21:04 +0000 2021",
+	"tweets": 74255,
+	"created_at": "Thu Oct 14 20:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 14 19:21:58 +0000 2021",
+		"id": 1448730822283088000,
+		"id_str": "1448730822283087873",
+		"full_text": "Ik durf niet te klikken... https://t.co/ggKMHinTRq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ggKMHinTRq",
+					"expanded_url": "https://twitter.com/niekdekker/status/1448726365298102273",
+					"display_url": "twitter.com/niekdekker/sta…",
+					"indices": [
+						27,
+						50
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8760,
+			"friends_count": 658,
+			"listed_count": 547,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8427,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65319,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1448726365298102300,
+		"quoted_status_id_str": "1448726365298102273",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ggKMHinTRq",
+			"expanded": "https://twitter.com/niekdekker/status/1448726365298102273",
+			"display": "twitter.com/niekdekker/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Oct 14 19:04:16 +0000 2021",
+			"id": 1448726365298102300,
+			"id_str": "1448726365298102273",
+			"full_text": "how about some @mschf endless procedurally generated enya for your thursday evening?\n\n🧚🏻 https://t.co/iUyXdtJjUu",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				112
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "mschf",
+						"name": "MSCHF",
+						"id": 1187182810169262000,
+						"id_str": "1187182810169262080",
+						"indices": [
+							15,
+							21
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/iUyXdtJjUu",
+						"expanded_url": "https://endlessenya.mschfmag.com",
+						"display_url": "endlessenya.mschfmag.com",
+						"indices": [
+							89,
+							112
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 14399246,
+				"id_str": "14399246",
+				"name": "niekdekker !",
+				"screen_name": "niekdekker",
+				"location": "he/him",
+				"description": "into music, art & the internet. design @mschf 👺 @poolsuite 🌴",
+				"url": "https://t.co/W9GR0Nzl2A",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/W9GR0Nzl2A",
+								"expanded_url": "http://niekdekker.com",
+								"display_url": "niekdekker.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3364,
+				"friends_count": 837,
+				"listed_count": 59,
+				"created_at": "Tue Apr 15 18:08:44 +0000 2008",
+				"favourites_count": 3978,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3979,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/971432254131134464/E6UfPiPC_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/971432254131134464/E6UfPiPC_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/14399246/1520442647",
+				"profile_link_color": "8E88CF",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ik durf niet te klikken... https://t.co/ggKMHinTRq"
+	},
+	{
 		"created_at": "Thu Oct 14 16:00:32 +0000 2021",
 		"id": 1448680128968544300,
 		"id_str": "1448680128968544262",
