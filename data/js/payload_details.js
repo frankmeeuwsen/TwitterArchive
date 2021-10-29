810c30052890a4aebf4973583c0225ebb99a6475
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-10-29 23:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74317,
-	"created_at": "Fri Oct 29 20:21:04 +0000 2021",
+	"tweets": 74318,
+	"created_at": "Fri Oct 29 21:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,315 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 29 20:28:09 +0000 2021",
+		"id": 1454183292832469000,
+		"id_str": "1454183292832468997",
+		"full_text": "Wat was ook weer de killer use case van 06-lijnen, videotheken en internet-payments? https://t.co/2FNCYCWO38",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2FNCYCWO38",
+					"expanded_url": "https://twitter.com/juddlegum/status/1453844273489747972",
+					"display_url": "twitter.com/juddlegum/stat…",
+					"indices": [
+						85,
+						108
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8753,
+			"friends_count": 671,
+			"listed_count": 547,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8490,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65393,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1453844273489748000,
+		"quoted_status_id_str": "1453844273489747972",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2FNCYCWO38",
+			"expanded": "https://twitter.com/juddlegum/status/1453844273489747972",
+			"display": "twitter.com/juddlegum/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Oct 28 22:01:00 +0000 2021",
+			"id": 1453844273489748000,
+			"id_str": "1453844273489747972",
+			"full_text": "In an interview with @benthompson, Zuckerberg says a “killer use case” of the metaverse is wearing augmented reality glasses when you are interacting with people in real life that allow you to continue text messaging people without people in real life noticing https://t.co/YQVirb6ZOt",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				260
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "benthompson",
+						"name": "Ben Thompson",
+						"id": 40273,
+						"id_str": "40273",
+						"indices": [
+							21,
+							33
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1453844267743555600,
+						"id_str": "1453844267743555601",
+						"indices": [
+							261,
+							284
+						],
+						"media_url": "http://pbs.twimg.com/media/FC0XqKGXsBEHszF.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FC0XqKGXsBEHszF.jpg",
+						"url": "https://t.co/YQVirb6ZOt",
+						"display_url": "pic.twitter.com/YQVirb6ZOt",
+						"expanded_url": "https://twitter.com/JuddLegum/status/1453844273489747972/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 804,
+								"h": 1178,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 464,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 804,
+								"h": 1178,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1453844267743555600,
+						"id_str": "1453844267743555601",
+						"indices": [
+							261,
+							284
+						],
+						"media_url": "http://pbs.twimg.com/media/FC0XqKGXsBEHszF.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FC0XqKGXsBEHszF.jpg",
+						"url": "https://t.co/YQVirb6ZOt",
+						"display_url": "pic.twitter.com/YQVirb6ZOt",
+						"expanded_url": "https://twitter.com/JuddLegum/status/1453844273489747972/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 804,
+								"h": 1178,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 464,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 804,
+								"h": 1178,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15464697,
+				"id_str": "15464697",
+				"name": "Judd Legum",
+				"screen_name": "JuddLegum",
+				"location": "Washington, DC",
+				"description": "I write Popular Information, a newsletter about politics and power. \n\nSend tips | DMs open | judd@popular.info | jlegum@protonmail.com",
+				"url": "https://t.co/7lEYXJGXf5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7lEYXJGXf5",
+								"expanded_url": "http://Popular.info",
+								"display_url": "Popular.info",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 509924,
+				"friends_count": 1364,
+				"listed_count": 6291,
+				"created_at": "Thu Jul 17 05:37:26 +0000 2008",
+				"favourites_count": 4954,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 65533,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/636336373453053952/yv-YqI5w_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/636336373453053952/yv-YqI5w_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15464697/1631034503",
+				"profile_link_color": "202037",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 398,
+			"favorite_count": 1858,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Wat was ook weer de killer use case van 06-lijnen, videotheken en internet-payments? https://t.co/2FNCYCWO38"
+	},
+	{
 		"created_at": "Fri Oct 29 20:09:31 +0000 2021",
 		"id": 1453952749343686700,
 		"id_str": "1454178605152587783",
