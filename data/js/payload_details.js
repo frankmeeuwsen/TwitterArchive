c4886e1df3bc4462cd2efbf571dcdadbf47dc667
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-11-01 21:21:10 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74339,
-	"created_at": "Mon Nov 01 15:21:03 +0000 2021",
+	"tweets": 74340,
+	"created_at": "Mon Nov 01 20:21:09 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Mon Nov 01 19:43:25 +0000 2021",
+		"id": 1455259200876073000,
+		"id_str": "1455259200876072961",
+		"full_text": "Heerlijke maandagavond. Eerst een kickoff van @marcoderksen voor de online leesclub van Echt Nep (synthetic media/fakenews/metaverse/etc). Nu in een Discord livestream waar @Dutchcowboy en @Rhymo een crashcourse NFT-worlddomination gaan geven. https://t.co/HTmWTs6xFM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			243
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "marcoderksen",
+					"name": "Marco Derksen",
+					"id": 1252681,
+					"id_str": "1252681",
+					"indices": [
+						46,
+						59
+					]
+				},
+				{
+					"screen_name": "Dutchcowboy",
+					"name": "M. Lens-FitzGerald",
+					"id": 794801,
+					"id_str": "794801",
+					"indices": [
+						173,
+						185
+					]
+				},
+				{
+					"screen_name": "Rhymo",
+					"name": "Raimo van der Klein ॐ",
+					"id": 788863,
+					"id_str": "788863",
+					"indices": [
+						189,
+						195
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1455259170052186000,
+					"id_str": "1455259170052186119",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FDIegT8XIAcKY0f.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FDIegT8XIAcKY0f.jpg",
+					"url": "https://t.co/HTmWTs6xFM",
+					"display_url": "pic.twitter.com/HTmWTs6xFM",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1455259200876072961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 560,
+							"h": 316,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 560,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 316,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1455259170052186000,
+					"id_str": "1455259170052186119",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FDIegT8XIAcKY0f.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FDIegT8XIAcKY0f.jpg",
+					"url": "https://t.co/HTmWTs6xFM",
+					"display_url": "pic.twitter.com/HTmWTs6xFM",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1455259200876072961/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 560,
+							"h": 316,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 560,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 560,
+							"h": 316,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							140,
+							79
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FDIegT8XIAcKY0f.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8752,
+			"friends_count": 676,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8510,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65420,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Heerlijke maandagavond. Eerst een kickoff van @marcoderksen voor de online leesclub van Echt Nep (synthetic media/fakenews/metaverse/etc). Nu in een Discord livestream waar @Dutchcowboy en @Rhymo een crashcourse NFT-worlddomination gaan geven. https://t.co/HTmWTs6xFM"
+	},
+	{
 		"created_at": "Mon Nov 01 15:16:39 +0000 2021",
 		"id": 1455192066598838300,
 		"id_str": "1455192066598838280",
