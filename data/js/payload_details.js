42b5e849e91e47293852a19b2d76a9763b62d9b8
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-11-10 21:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74399,
-	"created_at": "Wed Nov 10 17:21:04 +0000 2021",
+	"tweets": 74400,
+	"created_at": "Wed Nov 10 20:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 10 19:24:05 +0000 2021",
+		"id": 1458515827829973000,
+		"id_str": "1458515827829972994",
+		"full_text": "Er was een tijd dat Linktree vooral Instagram, Twitter, nieuwsbrief en website liet zien. Nieuwe realiteit. De web3 linktree van @neurocolor https://t.co/jsoibukAPu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "neurocolor",
+					"name": "神経色(henpai)",
+					"id": 1024846771204370400,
+					"id_str": "1024846771204370433",
+					"indices": [
+						129,
+						140
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1458515818187268000,
+					"id_str": "1458515818187268106",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FD2waLpXIAoilT-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FD2waLpXIAoilT-.jpg",
+					"url": "https://t.co/jsoibukAPu",
+					"display_url": "pic.twitter.com/jsoibukAPu",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1458515827829972994/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1458515818187268000,
+					"id_str": "1458515818187268106",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FD2waLpXIAoilT-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FD2waLpXIAoilT-.jpg",
+					"url": "https://t.co/jsoibukAPu",
+					"display_url": "pic.twitter.com/jsoibukAPu",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1458515827829972994/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 828,
+							"h": 1792,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8751,
+			"friends_count": 700,
+			"listed_count": 547,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8579,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65496,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Er was een tijd dat Linktree vooral Instagram, Twitter, nieuwsbrief en website liet zien. Nieuwe realiteit. De web3 linktree van @neurocolor https://t.co/jsoibukAPu"
+	},
+	{
 		"created_at": "Wed Nov 10 17:14:10 +0000 2021",
 		"id": 1458483129832837000,
 		"id_str": "1458483129832837122",
@@ -222,7 +418,7 @@ Grailbird.data.tweets_2021_11 =
 				"name": "Sïstēr Märy Bäphømēt*",
 				"screen_name": "marybaphomet",
 				"location": "Up a fucking hill",
-				"description": "Satan, beer and heavy fucking metal baby. Multiple Ironman finisher. Yorkshire bird.\nProud 🏳️‍🌈🏳️‍⚧️ ally.  💙NHS nurse💙 *Not a real girl.",
+				"description": "Satan, beer and heavy fucking metal baby. Multiple Ironman finisher. Yorkshire bird.\nProud 🏳️🌈🏳️⚧️ ally.  💙NHS nurse💙 *Not a real girl.",
 				"url": "https://t.co/RxK3BdBJie",
 				"entities": {
 					"url": {
