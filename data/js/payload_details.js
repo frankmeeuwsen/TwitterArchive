13c6251fa00586af9c2bee107073f886d2126533
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-11-13 08:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74416,
-	"created_at": "Sat Nov 13 06:21:04 +0000 2021",
+	"tweets": 74417,
+	"created_at": "Sat Nov 13 07:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sat Nov 13 07:11:19 +0000 2021",
+		"id": 1459418582186659800,
+		"id_str": "1459418582186659840",
+		"full_text": "Vandaag is @AaronSwartzDay . Een dag om The Internet's Own Boy te herinneren. https://t.co/bNWIKfo4rO https://t.co/6vCdv4QFxX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AaronSwartzDay",
+					"name": "Aaron Swartz Day",
+					"id": 2565971588,
+					"id_str": "2565971588",
+					"indices": [
+						11,
+						26
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/bNWIKfo4rO",
+					"expanded_url": "https://www.aaronswartzday.org/",
+					"display_url": "aaronswartzday.org",
+					"indices": [
+						78,
+						101
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1459418500238295000,
+					"id_str": "1459418500238295040",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FEDlZMVWQAAI14C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEDlZMVWQAAI14C.jpg",
+					"url": "https://t.co/6vCdv4QFxX",
+					"display_url": "pic.twitter.com/6vCdv4QFxX",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1459418582186659840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 602,
+							"h": 301,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 602,
+							"h": 301,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 602,
+							"h": 301,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1459418500238295000,
+					"id_str": "1459418500238295040",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FEDlZMVWQAAI14C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEDlZMVWQAAI14C.jpg",
+					"url": "https://t.co/6vCdv4QFxX",
+					"display_url": "pic.twitter.com/6vCdv4QFxX",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1459418582186659840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 602,
+							"h": 301,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 602,
+							"h": 301,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 602,
+							"h": 301,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8753,
+			"friends_count": 704,
+			"listed_count": 547,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8597,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65521,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Vandaag is @AaronSwartzDay . Een dag om The Internet's Own Boy te herinneren. https://t.co/bNWIKfo4rO https://t.co/6vCdv4QFxX"
+	},
+	{
 		"created_at": "Sat Nov 13 06:16:58 +0000 2021",
 		"id": 1459404905400213500,
 		"id_str": "1459404905400213514",
