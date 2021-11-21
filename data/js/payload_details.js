78e9b80cfb6c1c978a665d769949d0891090f8dd
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-11-22 00:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74458,
-	"created_at": "Sun Nov 21 22:21:04 +0000 2021",
+	"tweets": 74459,
+	"created_at": "Sun Nov 21 23:21:05 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sun Nov 21 22:24:26 +0000 2021",
+		"id": 1462547478863962000,
+		"id_str": "1462547478863962114",
+		"full_text": "Dit zou toch echt een grappig @Lucky_TV filmpje kunnen zijn met pizza-bestellende politie. En luie bezorgers. https://t.co/M3ECZddp0r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Lucky_TV",
+					"name": "LuckyTV",
+					"id": 120853015,
+					"id_str": "120853015",
+					"indices": [
+						30,
+						39
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/M3ECZddp0r",
+					"expanded_url": "https://twitter.com/BrosHub/status/1462344621653168128",
+					"display_url": "twitter.com/BrosHub/status…",
+					"indices": [
+						110,
+						133
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8752,
+			"friends_count": 714,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8662,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65573,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1462344621653168000,
+		"quoted_status_id_str": "1462344621653168128",
+		"quoted_status_permalink": {
+			"url": "https://t.co/M3ECZddp0r",
+			"expanded": "https://twitter.com/BrosHub/status/1462344621653168128",
+			"display": "twitter.com/BrosHub/status…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Nov 21 08:58:21 +0000 2021",
+			"id": 1462344621653168000,
+			"id_str": "1462344621653168128",
+			"full_text": "Iets eerder\n\nhttps://t.co/5D0afLZGtT",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				36
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/5D0afLZGtT",
+						"expanded_url": "https://twitter.com/broshub/status/1462185278358097920?s=21",
+						"display_url": "twitter.com/broshub/status…",
+						"indices": [
+							13,
+							36
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1462192272469016600,
+			"in_reply_to_status_id_str": "1462192272469016579",
+			"in_reply_to_user_id": 1098334452,
+			"in_reply_to_user_id_str": "1098334452",
+			"in_reply_to_screen_name": "BrosHub",
+			"user": {
+				"id": 1098334452,
+				"id_str": "1098334452",
+				"name": "Bros Hub",
+				"screen_name": "BrosHub",
+				"location": "Netherlands",
+				"description": "#bitcoin",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 245,
+				"friends_count": 498,
+				"listed_count": 3,
+				"created_at": "Thu Jan 17 14:40:48 +0000 2013",
+				"favourites_count": 4011,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 677,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "171717",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1439364088166068224/K4SVDIXq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1439364088166068224/K4SVDIXq_normal.jpg",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1462185278358098000,
+			"quoted_status_id_str": "1462185278358097920",
+			"quoted_status_permalink": {
+				"url": "https://t.co/5D0afLZGtT",
+				"expanded": "https://twitter.com/broshub/status/1462185278358097920?s=21",
+				"display": "twitter.com/broshub/status…"
+			},
+			"retweet_count": 3,
+			"favorite_count": 12,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Dit zou toch echt een grappig @Lucky_TV filmpje kunnen zijn met pizza-bestellende politie. En luie bezorgers. https://t.co/M3ECZddp0r"
+	},
+	{
 		"created_at": "Sun Nov 21 21:55:01 +0000 2021",
 		"id": 1462540077725274000,
 		"id_str": "1462540077725274118",
