84cc9483bf8a25962df819cf4c5846450d54d30a
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-12-06 13:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74532,
-	"created_at": "Mon Dec 06 08:21:03 +0000 2021",
+	"tweets": 74533,
+	"created_at": "Mon Dec 06 12:21:03 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Mon Dec 06 11:33:22 +0000 2021",
+		"id": 1467785758945820700,
+		"id_str": "1467819452414799878",
+		"full_text": "Hartjes voor de social media manager van VRT. 🥰 https://t.co/6HEiDjALGx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1467785296972587000,
+					"id_str": "1467785296972587014",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/media/FF6e83CXoAYGoJx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FF6e83CXoAYGoJx.jpg",
+					"url": "https://t.co/6HEiDjALGx",
+					"display_url": "pic.twitter.com/6HEiDjALGx",
+					"expanded_url": "https://twitter.com/ArbiterOfTweets/status/1467785758945820674/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 684,
+							"h": 667,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 663,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 684,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1467785296972587000,
+					"id_str": "1467785296972587014",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/media/FF6e83CXoAYGoJx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FF6e83CXoAYGoJx.jpg",
+					"url": "https://t.co/6HEiDjALGx",
+					"display_url": "pic.twitter.com/6HEiDjALGx",
+					"expanded_url": "https://twitter.com/ArbiterOfTweets/status/1467785758945820674/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 684,
+							"h": 667,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 663,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 684,
+							"h": 667,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1467785296964161500,
+					"id_str": "1467785296964161539",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/media/FF6e83AXEAMIWhv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FF6e83AXEAMIWhv.jpg",
+					"url": "https://t.co/6HEiDjALGx",
+					"display_url": "pic.twitter.com/6HEiDjALGx",
+					"expanded_url": "https://twitter.com/ArbiterOfTweets/status/1467785758945820674/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 637,
+							"h": 679,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 637,
+							"h": 679,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 637,
+							"h": 679,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 838758870575165400,
+			"id_str": "838758870575165440",
+			"name": "Arbiter",
+			"screen_name": "ArbiterOfTweets",
+			"location": "",
+			"description": "Historicus | Correspondent @Knack | Check @vrtnws\nTweets voor eigen rekening. Rekening op naam van Rien Emmery.\nIets checken? Stuur me een bericht! 📨📬",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22022,
+			"friends_count": 823,
+			"listed_count": 94,
+			"created_at": "Mon Mar 06 14:31:12 +0000 2017",
+			"favourites_count": 3957,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13331,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1446344114291855365/IhXRRK9u_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1446344114291855365/IhXRRK9u_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/838758870575165440/1627157296",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 271,
+		"favorite_count": 3372,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Hartjes voor de social media manager van VRT. 🥰 https://t.co/6HEiDjALGx"
+	},
+	{
 		"created_at": "Mon Dec 06 07:49:35 +0000 2021",
 		"id": 1467763133267779600,
 		"id_str": "1467763133267779584",
