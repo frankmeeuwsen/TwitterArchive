ff27dadf763b98c4a35567a86eddaac2e608393d
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-12-09 23:21:05 +0100

    Added 1 tweets

--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 09 21:29:37 +0000 2021",
+		"id": 1469056664926052400,
+		"id_str": "1469056664926052359",
+		"full_text": "Ik kijk met verbazing naar #generatienext waar de nieuwste innovatie uit China wordt besproken. Kassaloos shoppen. Kijk in de winkel wat je wilt en bestel het direct via een app. Waar ken ik dat van... https://t.co/dUbveSQnx9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			201
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "generatienext",
+					"indices": [
+						27,
+						41
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1469056576208183300,
+					"id_str": "1469056576208183309",
+					"indices": [
+						202,
+						225
+					],
+					"media_url": "http://pbs.twimg.com/media/FGMjLDPXIA0kXGG.png",
+					"media_url_https": "https://pbs.twimg.com/media/FGMjLDPXIA0kXGG.png",
+					"url": "https://t.co/dUbveSQnx9",
+					"display_url": "pic.twitter.com/dUbveSQnx9",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1469056664926052359/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 846,
+							"h": 436,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 846,
+							"h": 436,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 350,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1469056576208183300,
+					"id_str": "1469056576208183309",
+					"indices": [
+						202,
+						225
+					],
+					"media_url": "http://pbs.twimg.com/media/FGMjLDPXIA0kXGG.png",
+					"media_url_https": "https://pbs.twimg.com/media/FGMjLDPXIA0kXGG.png",
+					"url": "https://t.co/dUbveSQnx9",
+					"display_url": "pic.twitter.com/dUbveSQnx9",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1469056664926052359/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 846,
+							"h": 436,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 846,
+							"h": 436,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 350,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8769,
+			"friends_count": 753,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8759,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65689,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ik kijk met verbazing naar #generatienext waar de nieuwste innovatie uit China wordt besproken. Kassaloos shoppen. Kijk in de winkel wat je wilt en bestel het direct via een app. Waar ken ik dat van... https://t.co/dUbveSQnx9"
+	},
+	{
 		"created_at": "Thu Dec 09 20:11:00 +0000 2021",
 		"id": 1469036881866338300,
 		"id_str": "1469036881866338304",
