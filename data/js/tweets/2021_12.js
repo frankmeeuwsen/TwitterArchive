7fad382fcb1efd686890bec46f5309eb4f14c409
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-12-18 15:21:05 +0100

    Added 1 tweets

--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,108 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sat Dec 18 14:05:31 +0000 2021",
+		"id": 1471519870973972500,
+		"id_str": "1472206393784549381",
+		"full_text": "For all asking, #NFTs are just digital goods. Your kids understand this because of Roblox or Minecraft. The sale of digital goods will be more important as we evolve to have more digital access to music, art and entertainment. This article explains a lot: https://t.co/upuC2xHO8j",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NFTs",
+					"indices": [
+						16,
+						21
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/upuC2xHO8j",
+					"expanded_url": "https://www.forbes.com/advisor/investing/nft-non-fungible-token/",
+					"display_url": "forbes.com/advisor/invest…",
+					"indices": [
+						256,
+						279
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1198406491,
+			"id_str": "1198406491",
+			"name": "Reese Witherspoon",
+			"screen_name": "ReeseW",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2965694,
+			"friends_count": 726,
+			"listed_count": 4340,
+			"created_at": "Tue Feb 19 19:22:38 +0000 2013",
+			"favourites_count": 9584,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6691,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1451187064712564742/VDR97Dj8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1451187064712564742/VDR97Dj8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1198406491/1527706157",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 386,
+		"favorite_count": 1804,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "For all asking, #NFTs are just digital goods. Your kids understand this because of Roblox or Minecraft. The sale of digital goods will be more important as we evolve to have more digital access to music, art and entertainment. This article explains a lot: https://t.co/upuC2xHO8j"
+	},
+	{
 		"created_at": "Sat Dec 18 12:43:28 +0000 2021",
 		"id": 1472185745565798400,
 		"id_str": "1472185745565798400",
