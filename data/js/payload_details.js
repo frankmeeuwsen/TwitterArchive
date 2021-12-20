bbe68ec161f4ff7fb54ac82b6d81113cc54ccd21
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-12-20 12:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74633,
-	"created_at": "Mon Dec 20 09:21:04 +0000 2021",
+	"tweets": 74634,
+	"created_at": "Mon Dec 20 11:21:05 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,304 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Mon Dec 20 11:10:44 +0000 2021",
+		"id": 1472887183866486800,
+		"id_str": "1472887183866486786",
+		"full_text": "Wind of Change van The Scorpions. Is dat nummer misschien toch nog ergens goed voor... https://t.co/98DqjtgaJ4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/98DqjtgaJ4",
+					"expanded_url": "https://twitter.com/copydimitri/status/1472879239057879043",
+					"display_url": "twitter.com/copydimitri/st…",
+					"indices": [
+						87,
+						110
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/VH3v4gLIpG @ForSubscribing Check ook de OPEN nieuwsbrief voor nieuwsgierige cybersurfer. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/VH3v4gLIpG",
+							"expanded_url": "http://Thanksforsubscribing.app/",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8767,
+			"friends_count": 771,
+			"listed_count": 547,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8855,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65791,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1472879239057879000,
+		"quoted_status_id_str": "1472879239057879043",
+		"quoted_status_permalink": {
+			"url": "https://t.co/98DqjtgaJ4",
+			"expanded": "https://twitter.com/copydimitri/status/1472879239057879043",
+			"display": "twitter.com/copydimitri/st…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Dec 20 10:39:10 +0000 2021",
+			"id": 1472879239057879000,
+			"id_str": "1472879239057879043",
+			"full_text": "It's my life - Dr Alban. Yep. https://t.co/6cfGNrbtno",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				29
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1472879109156094000,
+						"id_str": "1472879109156093952",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/FHC3vwJXwAAZ22p.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FHC3vwJXwAAZ22p.jpg",
+						"url": "https://t.co/6cfGNrbtno",
+						"display_url": "pic.twitter.com/6cfGNrbtno",
+						"expanded_url": "https://twitter.com/copydimitri/status/1472879239057879043/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 619,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 934,
+								"h": 850,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 934,
+								"h": 850,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1472879109156094000,
+						"id_str": "1472879109156093952",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/FHC3vwJXwAAZ22p.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FHC3vwJXwAAZ22p.jpg",
+						"url": "https://t.co/6cfGNrbtno",
+						"display_url": "pic.twitter.com/6cfGNrbtno",
+						"expanded_url": "https://twitter.com/copydimitri/status/1472879239057879043/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 619,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 934,
+								"h": 850,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 934,
+								"h": 850,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1434590236806365200,
+				"id_str": "1434590236806365186",
+				"name": "Copydimitri",
+				"screen_name": "copydimitri",
+				"location": "Groningen",
+				"description": "Weer terug. Dad. Podcaster. Wannabe #bushcrafter. Follow my journey from #dadbod to #fitdad. #Podcast: Verslaafd aan Gemak. Van papperige papa tot fitte vader.",
+				"url": "https://t.co/FAZRVZIzl2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/FAZRVZIzl2",
+								"expanded_url": "https://linktr.ee/Copydimitri",
+								"display_url": "linktr.ee/Copydimitri",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 53,
+				"friends_count": 106,
+				"listed_count": 0,
+				"created_at": "Sun Sep 05 18:54:27 +0000 2021",
+				"favourites_count": 362,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1048,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1434591111079677953/ky6ovxHm_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1434591111079677953/ky6ovxHm_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1434590236806365186/1630868403",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Wind of Change van The Scorpions. Is dat nummer misschien toch nog ergens goed voor... https://t.co/98DqjtgaJ4"
+	},
+	{
 		"created_at": "Mon Dec 20 08:51:02 +0000 2021",
 		"id": 1472852028326137900,
 		"id_str": "1472852028326137857",
