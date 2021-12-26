703ecbc154d2bfb687c9d8a9b3c1d7ded56a1f9b
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-12-26 16:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74685,
-	"created_at": "Sun Dec 26 12:21:04 +0000 2021",
+	"tweets": 74686,
+	"created_at": "Sun Dec 26 15:21:04 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sun Dec 26 14:44:58 +0000 2021",
+		"id": 1475115424199106600,
+		"id_str": "1475115424199106560",
+		"full_text": "@tinekepauw Het begin is er! Het eerste deurtje van het eerste kastje... https://t.co/Z74xPoK881",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tinekepauw",
+					"name": "Tineke Pauw",
+					"id": 19288444,
+					"id_str": "19288444",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1475115419044302800,
+					"id_str": "1475115419044302850",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/FHipqHrWUAIpIKr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHipqHrWUAIpIKr.jpg",
+					"url": "https://t.co/Z74xPoK881",
+					"display_url": "pic.twitter.com/Z74xPoK881",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1475115424199106560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1475115419044302800,
+					"id_str": "1475115419044302850",
+					"indices": [
+						73,
+						96
+					],
+					"media_url": "http://pbs.twimg.com/media/FHipqHrWUAIpIKr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FHipqHrWUAIpIKr.jpg",
+					"url": "https://t.co/Z74xPoK881",
+					"display_url": "pic.twitter.com/Z74xPoK881",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1475115424199106560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1475037182591131600,
+		"in_reply_to_status_id_str": "1475037182591131648",
+		"in_reply_to_user_id": 19288444,
+		"in_reply_to_user_id_str": "19288444",
+		"in_reply_to_screen_name": "tinekepauw",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8772,
+			"friends_count": 778,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8898,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65855,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@tinekepauw Het begin is er! Het eerste deurtje van het eerste kastje... https://t.co/Z74xPoK881"
+	},
+	{
 		"created_at": "Sun Dec 26 11:38:57 +0000 2021",
 		"id": 1475068612176195600,
 		"id_str": "1475068612176195596",
