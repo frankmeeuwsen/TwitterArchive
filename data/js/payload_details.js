baa4ab83f367841225daf58bfc6cc8cade095110
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-12-28 15:21:08 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74699,
-	"created_at": "Tue Dec 28 13:21:27 +0000 2021",
+	"tweets": 74700,
+	"created_at": "Tue Dec 28 14:21:07 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Tue Dec 28 13:23:12 +0000 2021",
+		"id": 1475819624830849000,
+		"id_str": "1475819624830849033",
+		"full_text": "@tuttel @rutgervz @erwblo Uit mijn tijd (N=1-opa spreekt) met zowel WestBrabantnet maar ook met Lifehacking is wat in Nederland werkt: Nederlandse betrouwbare bronnen en live bijeenkomsten. De Youtube pre-rolls om onwaarschijnlijk rijk te worden als je een cursus koopt, dragen daar niet aan bij.",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			296
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tuttel",
+					"name": "RunHanRun 🏃",
+					"id": 18478861,
+					"id_str": "18478861",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "rutgervz",
+					"name": "Rutger van Zuidam",
+					"id": 16866170,
+					"id_str": "16866170",
+					"indices": [
+						8,
+						17
+					]
+				},
+				{
+					"screen_name": "erwblo",
+					"name": "Erwin Blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						18,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1475818214542221300,
+		"in_reply_to_status_id_str": "1475818214542221314",
+		"in_reply_to_user_id": 18478861,
+		"in_reply_to_user_id_str": "18478861",
+		"in_reply_to_screen_name": "tuttel",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8769,
+			"friends_count": 779,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 8920,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 65871,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@tuttel @rutgervz @erwblo Uit mijn tijd (N=1-opa spreekt) met zowel WestBrabantnet maar ook met Lifehacking is wat in Nederland werkt: Nederlandse betrouwbare bronnen en live bijeenkomsten. De Youtube pre-rolls om onwaarschijnlijk rijk te worden als je een cursus koopt, dragen daar niet aan bij."
+	},
+	{
 		"created_at": "Tue Dec 28 13:20:21 +0000 2021",
 		"id": 1475818906124234800,
 		"id_str": "1475818906124234759",
