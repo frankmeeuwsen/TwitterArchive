abe3c8d5fa3f4d455e24c89e2850d83bf7272175
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-01-06 14:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74760,
-	"created_at": "Thu Jan 06 11:21:03 +0000 2022",
+	"tweets": 74761,
+	"created_at": "Thu Jan 06 13:21:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Thu Jan 06 13:13:44 +0000 2022",
+		"id": 1478486567723233300,
+		"id_str": "1479078734678401024",
+		"full_text": "Minimum Viable Product. https://t.co/2lQAAb88uq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1478486562296021000,
+					"id_str": "1478486562296020995",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/FISjse7XwAMvjq_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FISjse7XwAMvjq_.jpg",
+					"url": "https://t.co/2lQAAb88uq",
+					"display_url": "pic.twitter.com/2lQAAb88uq",
+					"expanded_url": "https://twitter.com/patriciomolina/status/1478486567723233282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 740,
+							"h": 740,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 740,
+							"h": 740,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1478486562296021000,
+					"id_str": "1478486562296020995",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/FISjse7XwAMvjq_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FISjse7XwAMvjq_.jpg",
+					"url": "https://t.co/2lQAAb88uq",
+					"display_url": "pic.twitter.com/2lQAAb88uq",
+					"expanded_url": "https://twitter.com/patriciomolina/status/1478486567723233282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 740,
+							"h": 740,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 740,
+							"h": 740,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14768048,
+			"id_str": "14768048",
+			"name": "Pato Molina",
+			"screen_name": "patriciomolina",
+			"location": "Buenos Aires, Argentina",
+			"description": "Emprendedor. Co-founder de Real Trends, la plataforma más popular para vender en Mercado Libre. Tech, startups, crypto, programación y Racing. #SpacesHost 🎙",
+			"url": "https://t.co/SuVpgDcqSl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SuVpgDcqSl",
+							"expanded_url": "https://patriciomolina.com/",
+							"display_url": "patriciomolina.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23884,
+			"friends_count": 568,
+			"listed_count": 168,
+			"created_at": "Wed May 14 02:21:02 +0000 2008",
+			"favourites_count": 40595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 13884,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1413223797327646720/_RdBZMLn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1413223797327646720/_RdBZMLn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14768048/1639410486",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15754,
+		"favorite_count": 58921,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Minimum Viable Product. https://t.co/2lQAAb88uq"
+	},
+	{
 		"created_at": "Thu Jan 06 11:06:50 +0000 2022",
 		"id": 1479046795787448300,
 		"id_str": "1479046795787448323",
