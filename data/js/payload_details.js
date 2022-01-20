33ba55819b53ffb0a9c82a1e939c20508a93006a
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-01-20 15:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74841,
-	"created_at": "Thu Jan 20 13:21:04 +0000 2022",
+	"tweets": 74842,
+	"created_at": "Thu Jan 20 14:21:05 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Thu Jan 20 14:00:52 +0000 2022",
+		"id": 1484164024606597000,
+		"id_str": "1484164024606597122",
+		"full_text": "@Luukros Vandaag deze clientjourney in mijn mail. Voor twee schroeven en 8 dwarsmoeren. Al lang blij dat er geen NPS bij zat... Het is echt absurd. \nPS: De hoogslaper is nog niet ingestort dus ze bevallen goed ;-) https://t.co/NnD5D0KqeS",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			213
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Luukros",
+					"name": "Luuk Ros ♻️",
+					"id": 22533801,
+					"id_str": "22533801",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1484163711279538200,
+					"id_str": "1484163711279538181",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/FJjPCC9XoAUH9_k.png",
+					"media_url_https": "https://pbs.twimg.com/media/FJjPCC9XoAUH9_k.png",
+					"url": "https://t.co/NnD5D0KqeS",
+					"display_url": "pic.twitter.com/NnD5D0KqeS",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1484164024606597122/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 787,
+							"h": 824,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 649,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 787,
+							"h": 824,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1484163711279538200,
+					"id_str": "1484163711279538181",
+					"indices": [
+						214,
+						237
+					],
+					"media_url": "http://pbs.twimg.com/media/FJjPCC9XoAUH9_k.png",
+					"media_url_https": "https://pbs.twimg.com/media/FJjPCC9XoAUH9_k.png",
+					"url": "https://t.co/NnD5D0KqeS",
+					"display_url": "pic.twitter.com/NnD5D0KqeS",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1484164024606597122/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 787,
+							"h": 824,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 649,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 787,
+							"h": 824,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1484153424304291800,
+		"in_reply_to_status_id_str": "1484153424304291841",
+		"in_reply_to_user_id": 22533801,
+		"in_reply_to_user_id_str": "22533801",
+		"in_reply_to_screen_name": "Luukros",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8753,
+			"friends_count": 801,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66051,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@Luukros Vandaag deze clientjourney in mijn mail. Voor twee schroeven en 8 dwarsmoeren. Al lang blij dat er geen NPS bij zat... Het is echt absurd. \nPS: De hoogslaper is nog niet ingestort dus ze bevallen goed ;-) https://t.co/NnD5D0KqeS"
+	},
+	{
 		"created_at": "Thu Jan 20 12:29:40 +0000 2022",
 		"id": 1484141072880128000,
 		"id_str": "1484141072880128004",
