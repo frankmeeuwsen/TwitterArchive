9e11fb12eb2494b6709c35caf9bb480dee2f0f54
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-01-30 20:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74874,
-	"created_at": "Sun Jan 30 15:21:05 +0000 2022",
+	"tweets": 74875,
+	"created_at": "Sun Jan 30 19:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 30 19:09:59 +0000 2022",
+		"id": 1487865694725316600,
+		"id_str": "1487865694725316612",
+		"full_text": "Eindelijk kijken we Dune. Het jaar in de film is 10191. \n\n8000 jaar in de toekomst. En ze lopen een ruimteschip uit met een doedelzakspeler. Die dingen zijn onverwoestbaar in de loop van de toekomst… https://t.co/5inMs8PjmE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1487865687855050800,
+					"id_str": "1487865687855050754",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FKX19cYXsAIFsND.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FKX19cYXsAIFsND.jpg",
+					"url": "https://t.co/5inMs8PjmE",
+					"display_url": "pic.twitter.com/5inMs8PjmE",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1487865694725316612/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 230,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 230,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 230,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1487865687855050800,
+					"id_str": "1487865687855050754",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FKX19cYXsAIFsND.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FKX19cYXsAIFsND.jpg",
+					"url": "https://t.co/5inMs8PjmE",
+					"display_url": "pic.twitter.com/5inMs8PjmE",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1487865694725316612/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 230,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 230,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 230,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							50,
+							23
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FKX19cYXsAIFsND.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8750,
+			"friends_count": 806,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9144,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66093,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Eindelijk kijken we Dune. Het jaar in de film is 10191. \n\n8000 jaar in de toekomst. En ze lopen een ruimteschip uit met een doedelzakspeler. Die dingen zijn onverwoestbaar in de loop van de toekomst… https://t.co/5inMs8PjmE"
+	},
+	{
 		"created_at": "Sun Jan 30 15:00:37 +0000 2022",
 		"id": 1487802937829175300,
 		"id_str": "1487802937829175301",
