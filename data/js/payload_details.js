d1fb6d214f3d14b6a28e39af2df8adf3df8a71f5
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-02-01 08:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74882,
-	"created_at": "Mon Jan 31 19:21:04 +0000 2022",
+	"tweets": 74883,
+	"created_at": "Tue Feb 01 07:21:04 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,109 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Tue Feb 01 07:05:16 +0000 2022",
+		"id": 1488288679944675300,
+		"id_str": "1488408090311540736",
+		"full_text": "Podcasting is an open platform, you don't need approval from Spotify, Apple, Facebook, Google or journalists to do a podcast. But that'll change quickly if it's seen that all podcasts emanate from big companies.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			211
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3839,
+			"id_str": "3839",
+			"name": "Dave The Podcaster",
+			"screen_name": "davewiner",
+			"location": "Woodstock, NY",
+			"description": "My new outliner is https://t.co/xgHmOV4Wuf -- it excels in blogging and scripting. All files are OPML and therefore ready to be used in other apps. Bing bong! 😀",
+			"url": "https://t.co/alwLFPBgDF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/alwLFPBgDF",
+							"expanded_url": "http://scripting.com/",
+							"display_url": "scripting.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/xgHmOV4Wuf",
+							"expanded_url": "http://drummer.scripting.com",
+							"display_url": "drummer.scripting.com",
+							"indices": [
+								19,
+								42
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 66754,
+			"friends_count": 3921,
+			"listed_count": 5255,
+			"created_at": "Sat Aug 05 23:04:08 +0000 2006",
+			"favourites_count": 18608,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 199154,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398806004528263168/2SXipvrt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398806004528263168/2SXipvrt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3839/1636295224",
+			"profile_link_color": "7F7F90",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E1E1DF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 22,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Podcasting is an open platform, you don't need approval from Spotify, Apple, Facebook, Google or journalists to do a podcast. But that'll change quickly if it's seen that all podcasts emanate from big companies."
+	}
+]
