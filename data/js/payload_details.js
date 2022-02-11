139a7eb79998eba976091c1ac7b918129996e59d
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-02-11 20:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74937,
-	"created_at": "Fri Feb 11 18:21:04 +0000 2022",
+	"tweets": 74938,
+	"created_at": "Fri Feb 11 19:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 11 19:02:54 +0000 2022",
+		"id": 1492172518047764500,
+		"id_str": "1492212568307421190",
+		"full_text": "https://t.co/sDLXQpuGQF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1492172513929052200,
+					"id_str": "1492172513929052167",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FLVC_txXwAcx85T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLVC_txXwAcx85T.jpg",
+					"url": "https://t.co/sDLXQpuGQF",
+					"display_url": "pic.twitter.com/sDLXQpuGQF",
+					"expanded_url": "https://twitter.com/arielsonline/status/1492172518047764487/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1492172513929052200,
+					"id_str": "1492172513929052167",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FLVC_txXwAcx85T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLVC_txXwAcx85T.jpg",
+					"url": "https://t.co/sDLXQpuGQF",
+					"display_url": "pic.twitter.com/sDLXQpuGQF",
+					"expanded_url": "https://twitter.com/arielsonline/status/1492172518047764487/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1380968208,
+			"id_str": "1380968208",
+			"name": "ariel",
+			"screen_name": "arielsonline",
+			"location": "she / her",
+			"description": "deleting derived data…",
+			"url": "https://t.co/WC4wn1Ap8b",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WC4wn1Ap8b",
+							"expanded_url": "http://ariel.photos",
+							"display_url": "ariel.photos",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6912,
+			"friends_count": 840,
+			"listed_count": 60,
+			"created_at": "Fri Apr 26 03:35:57 +0000 2013",
+			"favourites_count": 15013,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4398,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5ABB5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1471251723557609475/mYupLXA2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1471251723557609475/mYupLXA2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1380968208/1634166503",
+			"profile_link_color": "CC0000",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 52,
+		"favorite_count": 450,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/sDLXQpuGQF"
+	},
+	{
 		"created_at": "Fri Feb 11 18:09:15 +0000 2022",
 		"id": 1492199066381467600,
 		"id_str": "1492199066381467654",
