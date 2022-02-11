b577c2c903abd04e371e198c12ef19ea71f5b639
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-02-11 08:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74931,
-	"created_at": "Thu Feb 10 22:21:03 +0000 2022",
+	"tweets": 74932,
+	"created_at": "Fri Feb 11 07:21:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,323 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 11 07:10:02 +0000 2022",
+		"id": 1492033168697925600,
+		"id_str": "1492033168697925668",
+		"full_text": "Happy Ome Ed Luisterdag! https://t.co/XCNcxkP1OX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			24
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XCNcxkP1OX",
+					"expanded_url": "https://twitter.com/PearlJamHolland/status/1492015325830914049",
+					"display_url": "twitter.com/PearlJamHollan…",
+					"indices": [
+						25,
+						48
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8746,
+			"friends_count": 816,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9242,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66169,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1492015325830914000,
+		"quoted_status_id_str": "1492015325830914049",
+		"quoted_status_permalink": {
+			"url": "https://t.co/XCNcxkP1OX",
+			"expanded": "https://twitter.com/PearlJamHolland/status/1492015325830914049",
+			"display": "twitter.com/PearlJamHollan…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Feb 11 05:59:08 +0000 2022",
+			"id": 1492015325830914000,
+			"id_str": "1492015325830914049",
+			"full_text": "Eddie Vedder’s third solo album is out NOW! #earthling #eddievedder #pearljam \nhttps://t.co/bvPgynkFaa https://t.co/7Xb0w2MgHU",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				102
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "earthling",
+						"indices": [
+							44,
+							54
+						]
+					},
+					{
+						"text": "eddievedder",
+						"indices": [
+							55,
+							67
+						]
+					},
+					{
+						"text": "pearljam",
+						"indices": [
+							68,
+							77
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/bvPgynkFaa",
+						"expanded_url": "https://open.spotify.com/playlist/00KEt2wDMNqqyQbnnGN6bO?si=pr4b7NkGSKuhmMtbCnbruA",
+						"display_url": "open.spotify.com/playlist/00KEt…",
+						"indices": [
+							79,
+							102
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1492015320952938500,
+						"id_str": "1492015320952938514",
+						"indices": [
+							103,
+							126
+						],
+						"media_url": "http://pbs.twimg.com/media/FLS0B4TXIBITxo7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FLS0B4TXIBITxo7.jpg",
+						"url": "https://t.co/7Xb0w2MgHU",
+						"display_url": "pic.twitter.com/7Xb0w2MgHU",
+						"expanded_url": "https://twitter.com/PearlJamHolland/status/1492015325830914049/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 591,
+								"h": 578,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 591,
+								"h": 578,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 591,
+								"h": 578,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1492015320952938500,
+						"id_str": "1492015320952938514",
+						"indices": [
+							103,
+							126
+						],
+						"media_url": "http://pbs.twimg.com/media/FLS0B4TXIBITxo7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FLS0B4TXIBITxo7.jpg",
+						"url": "https://t.co/7Xb0w2MgHU",
+						"display_url": "pic.twitter.com/7Xb0w2MgHU",
+						"expanded_url": "https://twitter.com/PearlJamHolland/status/1492015325830914049/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 591,
+								"h": 578,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 591,
+								"h": 578,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 591,
+								"h": 578,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2933941318,
+				"id_str": "2933941318",
+				"name": "Pearl Jam Holland",
+				"screen_name": "PearlJamHolland",
+				"location": "",
+				"description": "Troubled souls unite! Dutch PJ-fanaccount. Follow for Pearl Jam news, videos, lyrics, facts & photos. Instagram: pearljamholland.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2742,
+				"friends_count": 269,
+				"listed_count": 28,
+				"created_at": "Sat Dec 20 17:16:58 +0000 2014",
+				"favourites_count": 11548,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4438,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/968918956860985346/u_FxXkMa_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/968918956860985346/u_FxXkMa_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2933941318/1581096105",
+				"profile_link_color": "DD2E44",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Happy Ome Ed Luisterdag! https://t.co/XCNcxkP1OX"
+	},
+	{
 		"created_at": "Thu Feb 10 21:28:48 +0000 2022",
 		"id": 1491886895634256000,
 		"id_str": "1491886895634255873",
