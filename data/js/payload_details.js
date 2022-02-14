c50635641d69d94bac46d9dff6cc49f007e04594
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-02-14 11:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74945,
-	"created_at": "Sun Feb 13 18:21:03 +0000 2022",
+	"tweets": 74946,
+	"created_at": "Mon Feb 14 10:21:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,10 +1,127 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 14 10:00:19 +0000 2022",
+		"id": 1493163184999514000,
+		"id_str": "1493163184999514116",
+		"full_text": "@TadekSolarz Nou vooral hoe de twee door elkaar worden gehaald zo lijkt het. Alsof elk scenario van het RIVM publiek moet worden geevalueerd als ware het een voorspelling.",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			171
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TadekSolarz",
+					"name": "Tadek Solarz",
+					"id": 26734588,
+					"id_str": "26734588",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1493160822327107600,
+		"in_reply_to_status_id_str": "1493160822327107592",
+		"in_reply_to_user_id": 26734588,
+		"in_reply_to_user_id_str": "26734588",
+		"in_reply_to_screen_name": "TadekSolarz",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8746,
+			"friends_count": 817,
+			"listed_count": 550,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9255,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66187,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1616318316",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@TadekSolarz Nou vooral hoe de twee door elkaar worden gehaald zo lijkt het. Alsof elk scenario van het RIVM publiek moet worden geevalueerd als ware het een voorspelling."
+	},
+	{
 		"created_at": "Sun Feb 13 17:42:53 +0000 2022",
 		"id": 1492917205578788900,
 		"id_str": "1492917205578788869",
-		"full_text": "Great interview with the founders of ⁦@grungepoprecord⁩ with clips from all the great bands on their label. \nThe grunge revival is coming. https://t.co/WXKOjFHmcm",
+		"full_text": "Great interview with the founders of @grungepoprecord with clips from all the great bands on their label. \nThe grunge revival is coming. https://t.co/WXKOjFHmcm",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -126,7 +243,7 @@ Grailbird.data.tweets_2022_02 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Great interview with the founders of ⁦@grungepoprecord⁩ with clips from all the great bands on their label. \nThe grunge revival is coming. https://t.co/WXKOjFHmcm"
+		"text": "Great interview with the founders of @grungepoprecord with clips from all the great bands on their label. \nThe grunge revival is coming. https://t.co/WXKOjFHmcm"
 	},
 	{
 		"created_at": "Sun Feb 13 14:30:12 +0000 2022",
