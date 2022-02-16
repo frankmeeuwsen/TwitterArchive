ae34266e2b68189c02ce5fbd74807d559b993357
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-02-16 20:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74959,
-	"created_at": "Wed Feb 16 17:21:04 +0000 2022",
+	"tweets": 74960,
+	"created_at": "Wed Feb 16 19:21:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,121 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Wed Feb 16 18:28:14 +0000 2022",
+		"id": 1493888387371221000,
+		"id_str": "1494015781755047938",
+		"full_text": "Watch New Video by Dutch Rockers Rockford”Walkin’ In” is the new single of Rockford https://t.co/GzT6busQTH\n #news",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "news",
+					"indices": [
+						109,
+						114
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GzT6busQTH",
+					"expanded_url": "https://buff.ly/3oSuvu3",
+					"display_url": "buff.ly/3oSuvu3",
+					"indices": [
+						84,
+						107
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3240537328,
+			"id_str": "3240537328",
+			"name": "Rock and Blog NEWS",
+			"screen_name": "RockAndBlogNEWS",
+			"location": "Edimburgh",
+			"description": "All about #rock #music, #hardrock and #heavymetal . News, album #reviews, tour information, #interviews ...",
+			"url": "https://t.co/88RImb0tVR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/88RImb0tVR",
+							"expanded_url": "https://rockandblog.news",
+							"display_url": "rockandblog.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1093,
+			"friends_count": 1362,
+			"listed_count": 16,
+			"created_at": "Thu May 07 14:40:23 +0000 2015",
+			"favourites_count": 416,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1393,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/596323896862887936/Qfy8KVKP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/596323896862887936/Qfy8KVKP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3240537328/1575310696",
+			"profile_link_color": "F5ABB5",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Watch New Video by Dutch Rockers Rockford”Walkin’ In” is the new single of Rockford https://t.co/GzT6busQTH\n #news"
+	},
+	{
 		"created_at": "Wed Feb 16 16:42:30 +0000 2022",
 		"id": 1493989173811978200,
 		"id_str": "1493989173811978252",
