0ff7a847837f4ec4da39b680f0f0362a7f156a0d
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-02-27 21:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75029,
-	"created_at": "Sun Feb 27 19:21:05 +0000 2022",
+	"tweets": 75030,
+	"created_at": "Sun Feb 27 20:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sun Feb 27 19:58:25 +0000 2022",
+		"id": 1498024741843701800,
+		"id_str": "1498024741843701763",
+		"full_text": "Zondagavond: de podcast voor @ndrgwrdrdldjs voorbereiden. Ik mag te gast zijn met 5 mede-Chris Cornell liefhebbers om 13 nummers uit zijn prachtige oeuvre van commentaar te voorzien. (Geen Black Hole Sun, alleen ~ondergewaardeerde~ parels!) https://t.co/22lomQSPVB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			240
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ndrgwrdrdldjs",
+					"name": "Ondrgwrdeerdeliedjes",
+					"id": 2274872545,
+					"id_str": "2274872545",
+					"indices": [
+						29,
+						43
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1498024707681144800,
+					"id_str": "1498024707681144838",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/FMoNiOYX0AY0t5J.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMoNiOYX0AY0t5J.jpg",
+					"url": "https://t.co/22lomQSPVB",
+					"display_url": "pic.twitter.com/22lomQSPVB",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1498024741843701763/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 676,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1498024707681144800,
+					"id_str": "1498024707681144838",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/FMoNiOYX0AY0t5J.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMoNiOYX0AY0t5J.jpg",
+					"url": "https://t.co/22lomQSPVB",
+					"display_url": "pic.twitter.com/22lomQSPVB",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1498024741843701763/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 676,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 827,
+							"h": 822,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8753,
+			"friends_count": 835,
+			"listed_count": 553,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9407,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66294,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Zondagavond: de podcast voor @ndrgwrdrdldjs voorbereiden. Ik mag te gast zijn met 5 mede-Chris Cornell liefhebbers om 13 nummers uit zijn prachtige oeuvre van commentaar te voorzien. (Geen Black Hole Sun, alleen ~ondergewaardeerde~ parels!) https://t.co/22lomQSPVB"
+	},
+	{
 		"created_at": "Sun Feb 27 18:23:27 +0000 2022",
 		"id": 1498000844867911700,
 		"id_str": "1498000844867911682",
