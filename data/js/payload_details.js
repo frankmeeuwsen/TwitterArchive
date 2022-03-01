106d45504dc39b5b7fadd5c2d7ae0c5b93d2308e
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-01 20:21:06 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75034,
-	"created_at": "Mon Feb 28 21:21:05 +0000 2022",
+	"tweets": 75035,
+	"created_at": "Tue Mar 01 19:21:04 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,222 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Tue Mar 01 18:25:33 +0000 2022",
+		"id": 1498726149408628700,
+		"id_str": "1498726149408628738",
+		"full_text": "Bregman-moe. Kent u die uitdrukking? https://t.co/OcYXV8l3Ov",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OcYXV8l3Ov",
+					"expanded_url": "https://twitter.com/rcbregman/status/1498673316315467795",
+					"display_url": "twitter.com/rcbregman/stat…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8754,
+			"friends_count": 835,
+			"listed_count": 553,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9415,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66299,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1498673316315467800,
+		"quoted_status_id_str": "1498673316315467795",
+		"quoted_status_permalink": {
+			"url": "https://t.co/OcYXV8l3Ov",
+			"expanded": "https://twitter.com/rcbregman/status/1498673316315467795",
+			"display": "twitter.com/rcbregman/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Mar 01 14:55:37 +0000 2022",
+			"id": 1498673316315467800,
+			"id_str": "1498673316315467795",
+			"full_text": "Short 🧵 on my feelings of deep unease about the current atmosphere, and the dizzying speed with which radical conclusions are being drawn. People who couldn’t find Ukraine on a map a week ago, are calling for a no fly-zone. 1/9",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				227
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 200514238,
+				"id_str": "200514238",
+				"name": "Rutger Bregman",
+				"screen_name": "rcbregman",
+				"location": "",
+				"description": "Historian. Author of UTOPIA FOR REALISTS (2017) and HUMANKIND (2020). @GivingWhatWeCan.",
+				"url": "https://t.co/gtTuspIbVz",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gtTuspIbVz",
+								"expanded_url": "https://www.rutgerbregman.com",
+								"display_url": "rutgerbregman.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 403590,
+				"friends_count": 287,
+				"listed_count": 2283,
+				"created_at": "Sat Oct 09 14:11:13 +0000 2010",
+				"favourites_count": 7730,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 2501,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1365285823416045571/i-JmI6Gd_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1365285823416045571/i-JmI6Gd_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/200514238/1637309584",
+				"profile_link_color": "DF5B57",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 712,
+			"favorite_count": 2944,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Bregman-moe. Kent u die uitdrukking? https://t.co/OcYXV8l3Ov"
+	}
+]
