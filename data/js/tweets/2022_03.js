3122407d3224a7d5b0fd9dacd314d31c3659faad
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-02 12:21:04 +0100

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 02 11:15:57 +0000 2022",
+		"id": 1498980422897832000,
+		"id_str": "1498980422897831943",
+		"full_text": "\"This Week in the 90's\" nieuwsbrief (@twit90s) on uncle Ed and Pearl Jam's Ten: \"When Eddie sings a word like “away”, it feels like listening to a lumberjack chainsaw down a sequoia.\" \n\nCan confirm. \n\nhttps://t.co/9JGGDAb1R9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			224
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "twit90s",
+					"name": "🎵 This Week in the 90s",
+					"id": 1450957225082736600,
+					"id_str": "1450957225082736640",
+					"indices": [
+						37,
+						45
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9JGGDAb1R9",
+					"expanded_url": "https://www.thisweekinthe90s.com/p/opus3-edward-barton-pearl-jam?token=eyJ1c2VyX2lkIjo3MTAxOTAwNCwiXyI6IjJPSDRQIiwiaWF0IjoxNjQ2MjE5NjE3LCJleHAiOjE2NDYyMjMyMTcsImlzcyI6InB1Yi0yODU2NTQiLCJzdWIiOiJwb3N0LXJlYWN0aW9uIn0.78siio2JhsTHSYpDC1ij4VBqRkexm7EGe9703a82OS8&s=r",
+					"display_url": "thisweekinthe90s.com/p/opus3-edward…",
+					"indices": [
+						201,
+						224
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8755,
+			"friends_count": 835,
+			"listed_count": 553,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9421,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66302,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"This Week in the 90's\" nieuwsbrief (@twit90s) on uncle Ed and Pearl Jam's Ten: \"When Eddie sings a word like “away”, it feels like listening to a lumberjack chainsaw down a sequoia.\" \n\nCan confirm. \n\nhttps://t.co/9JGGDAb1R9"
+	},
+	{
 		"created_at": "Wed Mar 02 07:59:56 +0000 2022",
 		"id": 1498931095307534300,
 		"id_str": "1498931095307534336",
