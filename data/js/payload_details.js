a5f4b03d38f8998869d293f7e5b633c3971e8807
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-02 09:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75036,
-	"created_at": "Wed Mar 02 07:21:03 +0000 2022",
+	"tweets": 75037,
+	"created_at": "Wed Mar 02 08:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,236 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 02 07:59:56 +0000 2022",
+		"id": 1498931095307534300,
+		"id_str": "1498931095307534336",
+		"full_text": "“[We] vinden dat elk land, dat na een periode van onderdrukking kiest voor vrijheid en dat wil markeren door plaats- en landnamen te gebruiken in de eigen taal, onze steun verdient”. \n\nKom er maar in Ljouwert/Liwwadden… https://t.co/2xVLWjxNOx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			219
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2xVLWjxNOx",
+					"expanded_url": "https://twitter.com/volkskrant/status/1498923560785506305",
+					"display_url": "twitter.com/volkskrant/sta…",
+					"indices": [
+						220,
+						243
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8755,
+			"friends_count": 835,
+			"listed_count": 553,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9420,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66301,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1498923560785506300,
+		"quoted_status_id_str": "1498923560785506305",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2xVLWjxNOx",
+			"expanded": "https://twitter.com/volkskrant/status/1498923560785506305",
+			"display": "twitter.com/volkskrant/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 02 07:30:00 +0000 2022",
+			"id": 1498923560785506300,
+			"id_str": "1498923560785506305",
+			"full_text": "De redactie van de Volkskrant kiest ervoor om voortaan de Oekraïense benaming van steden te gebruiken in plaats van namen die de Russische transcriptie volgen. Dit betekent dat Kiev voortaan als Kyiv wordt geschreven, en Charkov als Charkiv https://t.co/BQTdjzfBUw",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				264
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/BQTdjzfBUw",
+						"expanded_url": "https://www.volkskrant.nl/nieuws-achtergrond/de-volkskrant-hanteert-voortaan-oekraiense-namen-van-steden-kiev-wordt-kyiv~b5a04fc8/",
+						"display_url": "volkskrant.nl/nieuws-achterg…",
+						"indices": [
+							241,
+							264
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3103261,
+				"id_str": "3103261",
+				"name": "de Volkskrant",
+				"screen_name": "volkskrant",
+				"location": "Amsterdam",
+				"description": "Onafhankelijk, betrouwbaar, toegankelijk: volg de Volkskrant hier voor actualiteit, opinie, cultuur en meer. \nWebcare: @Webcare_VK",
+				"url": "https://t.co/IrIO6p6668",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/IrIO6p6668",
+								"expanded_url": "http://www.volkskrant.nl",
+								"display_url": "volkskrant.nl",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 814739,
+				"friends_count": 1481,
+				"listed_count": 4096,
+				"created_at": "Sun Apr 01 04:36:17 +0000 2007",
+				"favourites_count": 149,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 316957,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F7F7F7",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/914826679528361984/3F6_hd7T_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/914826679528361984/3F6_hd7T_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3103261/1638357322",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "003566",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 14,
+			"favorite_count": 88,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "“[We] vinden dat elk land, dat na een periode van onderdrukking kiest voor vrijheid en dat wil markeren door plaats- en landnamen te gebruiken in de eigen taal, onze steun verdient”. \n\nKom er maar in Ljouwert/Liwwadden… https://t.co/2xVLWjxNOx"
+	},
+	{
 		"created_at": "Wed Mar 02 07:18:23 +0000 2022",
 		"id": 1498920640669261800,
 		"id_str": "1498920640669261829",
