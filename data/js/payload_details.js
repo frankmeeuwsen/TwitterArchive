166bf0cc0c5f2c31d1b9240fcd90c931f5624bfc
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-10 07:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75068,
-	"created_at": "Wed Mar 09 21:21:03 +0000 2022",
+	"tweets": 75069,
+	"created_at": "Thu Mar 10 06:21:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,236 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 10 05:59:09 +0000 2022",
+		"id": 1501799800785850400,
+		"id_str": "1501799800785850369",
+		"full_text": "Mogen wij dit volgende week ook bij de Gemeenteraadsverkiezingen @NOS? https://t.co/QijyfWdkeS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NOS",
+					"name": "NOS",
+					"id": 7174972,
+					"id_str": "7174972",
+					"indices": [
+						65,
+						69
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QijyfWdkeS",
+					"expanded_url": "https://twitter.com/theapjournalist/status/1501588231715901440",
+					"display_url": "twitter.com/theapjournalis…",
+					"indices": [
+						71,
+						94
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8754,
+			"friends_count": 843,
+			"listed_count": 552,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9522,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66342,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1501588231715901400,
+		"quoted_status_id_str": "1501588231715901440",
+		"quoted_status_permalink": {
+			"url": "https://t.co/QijyfWdkeS",
+			"expanded": "https://twitter.com/theapjournalist/status/1501588231715901440",
+			"display": "twitter.com/theapjournalis…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 09 15:58:27 +0000 2022",
+			"id": 1501588231715901400,
+			"id_str": "1501588231715901440",
+			"full_text": "SOUTH KOREAN ELECTION GRAPHICS ARE NEXT LEVEL",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				45
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1543801099,
+				"id_str": "1543801099",
+				"name": "Andrew Peng",
+				"screen_name": "TheAPJournalist",
+				"location": "Soon: Washington, D.C.",
+				"description": "@TheYappie editor tracking Asian American politics + @aaja member • @OCANational comms • Past: @OfficeofEdTech, @AAAJ_AAJC, @NAACP • andrew.peng@theyappie.com",
+				"url": "https://t.co/2LzYOsloxc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/2LzYOsloxc",
+								"expanded_url": "http://bit.ly/theyappie",
+								"display_url": "bit.ly/theyappie",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10465,
+				"friends_count": 5291,
+				"listed_count": 344,
+				"created_at": "Mon Jun 24 17:53:31 +0000 2013",
+				"favourites_count": 49841,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 102902,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1477337899448213507/KhL_hSy0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477337899448213507/KhL_hSy0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1543801099/1564158111",
+				"profile_link_color": "0A8DFF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7347,
+			"favorite_count": 25072,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Mogen wij dit volgende week ook bij de Gemeenteraadsverkiezingen @NOS? https://t.co/QijyfWdkeS"
+	},
+	{
 		"created_at": "Wed Mar 09 21:17:44 +0000 2022",
 		"id": 1501668582937317400,
 		"id_str": "1501668582937317378",
