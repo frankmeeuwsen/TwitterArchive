5e39e978727754cb36d4a57ecfba46207f497e2f
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-20 22:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75103,
-	"created_at": "Sun Mar 20 20:21:04 +0000 2022",
+	"tweets": 75104,
+	"created_at": "Sun Mar 20 21:21:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,395 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sun Mar 20 21:20:37 +0000 2022",
+		"id": 1505655574276489200,
+		"id_str": "1505655574276489229",
+		"full_text": "Heb ik weer. https://t.co/mfYkpd8Ghg https://t.co/eLUEp87a7u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mfYkpd8Ghg",
+					"expanded_url": "https://twitter.com/anundyingfloppa/status/1505577571341287427",
+					"display_url": "twitter.com/anundyingflopp…",
+					"indices": [
+						13,
+						36
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1505655570363203600,
+					"id_str": "1505655570363203586",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FOUpw5GWQAICbqI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOUpw5GWQAICbqI.jpg",
+					"url": "https://t.co/eLUEp87a7u",
+					"display_url": "pic.twitter.com/eLUEp87a7u",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1505655574276489229/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 584,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 584,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1505655570363203600,
+					"id_str": "1505655570363203586",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FOUpw5GWQAICbqI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOUpw5GWQAICbqI.jpg",
+					"url": "https://t.co/eLUEp87a7u",
+					"display_url": "pic.twitter.com/eLUEp87a7u",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1505655574276489229/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 584,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 584,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8751,
+			"friends_count": 857,
+			"listed_count": 551,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9579,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66388,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1505577571341287400,
+		"quoted_status_id_str": "1505577571341287427",
+		"quoted_status_permalink": {
+			"url": "https://t.co/mfYkpd8Ghg",
+			"expanded": "https://twitter.com/anundyingfloppa/status/1505577571341287427",
+			"display": "twitter.com/anundyingflopp…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Mar 20 16:10:39 +0000 2022",
+			"id": 1505577571341287400,
+			"id_str": "1505577571341287427",
+			"full_text": "the place you screenshot is where you have to move (pt 2) https://t.co/FQdutyAvGL",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				57
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1505577435810697200,
+						"id_str": "1505577435810697217",
+						"indices": [
+							58,
+							81
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOTis3NXEAEVv2g.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOTis3NXEAEVv2g.jpg",
+						"url": "https://t.co/FQdutyAvGL",
+						"display_url": "pic.twitter.com/FQdutyAvGL",
+						"expanded_url": "https://twitter.com/anundyingfloppa/status/1505577571341287427/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1505577435810697200,
+						"id_str": "1505577435810697217",
+						"indices": [
+							58,
+							81
+						],
+						"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOTis3NXEAEVv2g.jpg",
+						"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOTis3NXEAEVv2g.jpg",
+						"url": "https://t.co/FQdutyAvGL",
+						"display_url": "pic.twitter.com/FQdutyAvGL",
+						"expanded_url": "https://twitter.com/anundyingfloppa/status/1505577571341287427/photo/1",
+						"type": "animated_gif",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								4,
+								3
+							],
+							"variants": [
+								{
+									"bitrate": 0,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/tweet_video/FOTis3NXEAEVv2g.mp4"
+								}
+							]
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1273825381162709000,
+				"id_str": "1273825381162708992",
+				"name": "flelf 💐🌻🍄🌳",
+				"screen_name": "anundyingfloppa",
+				"location": "Rutland, VT",
+				"description": "20 / mentally ill aspiring tardwife / very over-recovered anorexic",
+				"url": "https://t.co/Ed42LNgvwz",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Ed42LNgvwz",
+								"expanded_url": "http://curiouscat.qa/dafloof",
+								"display_url": "curiouscat.qa/dafloof",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 617,
+				"friends_count": 730,
+				"listed_count": 5,
+				"created_at": "Fri Jun 19 03:50:15 +0000 2020",
+				"favourites_count": 95289,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 25579,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1505001003963322370/g2X6NjYt_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1505001003963322370/g2X6NjYt_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273825381162708992/1647655176",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 69,
+			"favorite_count": 800,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Heb ik weer. https://t.co/mfYkpd8Ghg https://t.co/eLUEp87a7u"
+	},
+	{
 		"created_at": "Sun Mar 20 20:12:21 +0000 2022",
 		"id": 1505638394843521000,
 		"id_str": "1505638394843521029",
