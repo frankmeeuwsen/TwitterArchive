55e0901ed856280f0513dc65fd9183fd7b90fee0
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-24 22:21:05 +0100

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 24 21:00:01 +0000 2022",
+		"id": 1507099942451654700,
+		"id_str": "1507099942451654658",
+		"full_text": "#BreakfastClubDay\n\nOp de Criterion Bluray zijn een aantal verwijderde scenes van de film te vinden. En dus nu ook op Youtube.  https://t.co/y7gqKbKyaN Er zitten fantastische momenten bij met Carl die de toekomst van de kids voorspelt en Allison die een Prince album vindt. https://t.co/umGJUZS73Q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BreakfastClubDay",
+					"indices": [
+						0,
+						17
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/y7gqKbKyaN",
+					"expanded_url": "https://www.youtube.com/watch?v=AhBaPxg0O_M",
+					"display_url": "youtube.com/watch?v=AhBaPx…",
+					"indices": [
+						127,
+						150
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1506766920493805600,
+					"id_str": "1506766920493805578",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOkch-9XsAo0VtD.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOkch-9XsAo0VtD.jpg",
+					"url": "https://t.co/umGJUZS73Q",
+					"display_url": "pic.twitter.com/umGJUZS73Q",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1507099942451654658/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506766920493805600,
+					"id_str": "1506766920493805578",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOkch-9XsAo0VtD.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOkch-9XsAo0VtD.jpg",
+					"url": "https://t.co/umGJUZS73Q",
+					"display_url": "pic.twitter.com/umGJUZS73Q",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1507099942451654658/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							2,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FOkch-9XsAo0VtD.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8746,
+			"friends_count": 862,
+			"listed_count": 551,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9624,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66438,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "#BreakfastClubDay\n\nOp de Criterion Bluray zijn een aantal verwijderde scenes van de film te vinden. En dus nu ook op Youtube.  https://t.co/y7gqKbKyaN Er zitten fantastische momenten bij met Carl die de toekomst van de kids voorspelt en Allison die een Prince album vindt. https://t.co/umGJUZS73Q"
+	},
+	{
 		"created_at": "Thu Mar 24 20:00:01 +0000 2022",
 		"id": 1507084844915953700,
 		"id_str": "1507084844915953677",
