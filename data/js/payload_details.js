de1639aac15c37dc07acf582f9f32466871d982c
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-24 12:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75128,
-	"created_at": "Thu Mar 24 10:21:03 +0000 2022",
+	"tweets": 75129,
+	"created_at": "Thu Mar 24 11:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 24 11:00:02 +0000 2022",
+		"id": 1506948951508848600,
+		"id_str": "1506948951508848642",
+		"full_text": "#BreakfastClubDay\n\nDe mooiste zin komt van Allison (Ally Sheedy) als Brian (Anthony Michael Hall) zijn verhaal vertelt. \n\n\"When you grow up, your heart dies\"\n\nGunship maakt een nummer rondom de zin. De clip zit propvol 80s referenties. Heerlijk. https://t.co/PbVRAyUkiW https://t.co/no5HODRUn3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BreakfastClubDay",
+					"indices": [
+						0,
+						17
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PbVRAyUkiW",
+					"expanded_url": "https://youtu.be/ri9IefTuNzc",
+					"display_url": "youtu.be/ri9IefTuNzc",
+					"indices": [
+						246,
+						269
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1506778045910241300,
+					"id_str": "1506778045910241280",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1506778045910241280/pu/img/fL1n790ytyuHBLKL.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1506778045910241280/pu/img/fL1n790ytyuHBLKL.jpg",
+					"url": "https://t.co/no5HODRUn3",
+					"display_url": "pic.twitter.com/no5HODRUn3",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1506948951508848642/video/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 284,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 284,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 284,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506778045910241300,
+					"id_str": "1506778045910241280",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1506778045910241280/pu/img/fL1n790ytyuHBLKL.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1506778045910241280/pu/img/fL1n790ytyuHBLKL.jpg",
+					"url": "https://t.co/no5HODRUn3",
+					"display_url": "pic.twitter.com/no5HODRUn3",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1506948951508848642/video/1",
+					"type": "video",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 284,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 284,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 284,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							125,
+							71
+						],
+						"duration_millis": 2890,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1506778045910241280/pu/pl/H8MwAit3l0F7AaP9.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1506778045910241280/pu/vid/500x284/tmnUc9Dz-AMzXgOr.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8748,
+			"friends_count": 862,
+			"listed_count": 551,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9611,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66417,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "#BreakfastClubDay\n\nDe mooiste zin komt van Allison (Ally Sheedy) als Brian (Anthony Michael Hall) zijn verhaal vertelt. \n\n\"When you grow up, your heart dies\"\n\nGunship maakt een nummer rondom de zin. De clip zit propvol 80s referenties. Heerlijk. https://t.co/PbVRAyUkiW https://t.co/no5HODRUn3"
+	},
+	{
 		"created_at": "Thu Mar 24 09:24:15 +0000 2022",
 		"id": 1506924846973915100,
 		"id_str": "1506924846973915140",
