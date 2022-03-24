3252cd07e9df45e3a0aa66a4818aa9f633645b70
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-03-24 14:21:05 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75130,
-	"created_at": "Thu Mar 24 12:21:04 +0000 2022",
+	"tweets": 75131,
+	"created_at": "Thu Mar 24 13:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 24 13:00:01 +0000 2022",
+		"id": 1506979148358557700,
+		"id_str": "1506979148358557696",
+		"full_text": "#BreakfastClubDay\n\nJudd Nelson improviseert de mop terwijl hij over het plafond kruipt. Volgens de overlevering zou hij een grap vertellen met als clue dat hij binnenloopt en zegt \"forgot my pencil\". Maar niemand kon er een goede mop omheen bedenken. https://t.co/g82iB92QOn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			250
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BreakfastClubDay",
+					"indices": [
+						0,
+						17
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1506739963429326800,
+					"id_str": "1506739963429326854",
+					"indices": [
+						251,
+						274
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOkEA4EXEAYOw8R.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOkEA4EXEAYOw8R.jpg",
+					"url": "https://t.co/g82iB92QOn",
+					"display_url": "pic.twitter.com/g82iB92QOn",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1506979148358557696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 244,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 244,
+							"h": 200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 244,
+							"h": 200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506739963429326800,
+					"id_str": "1506739963429326854",
+					"indices": [
+						251,
+						274
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOkEA4EXEAYOw8R.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOkEA4EXEAYOw8R.jpg",
+					"url": "https://t.co/g82iB92QOn",
+					"display_url": "pic.twitter.com/g82iB92QOn",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1506979148358557696/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 244,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 244,
+							"h": 200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 244,
+							"h": 200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							61,
+							50
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FOkEA4EXEAYOw8R.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8748,
+			"friends_count": 862,
+			"listed_count": 551,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9613,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66420,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "#BreakfastClubDay\n\nJudd Nelson improviseert de mop terwijl hij over het plafond kruipt. Volgens de overlevering zou hij een grap vertellen met als clue dat hij binnenloopt en zegt \"forgot my pencil\". Maar niemand kon er een goede mop omheen bedenken. https://t.co/g82iB92QOn"
+	},
+	{
 		"created_at": "Thu Mar 24 12:07:00 +0000 2022",
 		"id": 1506965803345715200,
 		"id_str": "1506965803345715205",
