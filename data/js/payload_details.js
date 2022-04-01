9589517efb24ee5259112a203968bc976e9eff1b
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-04-01 08:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75179,
-	"created_at": "Thu Mar 31 20:21:04 +0000 2022",
+	"tweets": 75180,
+	"created_at": "Fri Apr 01 06:21:04 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_04.js
@@ -0,0 +1,327 @@
+Grailbird.data.tweets_2022_04 = 
+[
+	{
+		"created_at": "Fri Apr 01 05:43:59 +0000 2022",
+		"id": 1509768519440904200,
+		"id_str": "1509768519440904241",
+		"full_text": "Was ik er toch bijna ingetrapt...nog voor mijn eerste koffie. https://t.co/ih8t1Lgtl8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ih8t1Lgtl8",
+					"expanded_url": "https://twitter.com/PearlJamOnline/status/1509765602633822243",
+					"display_url": "twitter.com/PearlJamOnline…",
+					"indices": [
+						62,
+						85
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8750,
+			"friends_count": 864,
+			"listed_count": 551,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9678,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66482,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1509765602633822200,
+		"quoted_status_id_str": "1509765602633822243",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ih8t1Lgtl8",
+			"expanded": "https://twitter.com/PearlJamOnline/status/1509765602633822243",
+			"display": "twitter.com/PearlJamOnline…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Apr 01 05:32:24 +0000 2022",
+			"id": 1509765602633822200,
+			"id_str": "1509765602633822243",
+			"full_text": "BREAKING NEWS: @PearlJam announces massive “Lost Dogs” super deluxe edition! \n\n#pearljam #lostdogs https://t.co/w1vfut4N34",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				98
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "pearljam",
+						"indices": [
+							79,
+							88
+						]
+					},
+					{
+						"text": "lostdogs",
+						"indices": [
+							89,
+							98
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "PearlJam",
+						"name": "Pearl Jam 🇺🇦",
+						"id": 15155074,
+						"id_str": "15155074",
+						"indices": [
+							15,
+							24
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1509765597764235300,
+						"id_str": "1509765597764235305",
+						"indices": [
+							99,
+							122
+						],
+						"media_url": "http://pbs.twimg.com/media/FPPDz-TXsCkpwwv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FPPDz-TXsCkpwwv.jpg",
+						"url": "https://t.co/w1vfut4N34",
+						"display_url": "pic.twitter.com/w1vfut4N34",
+						"expanded_url": "https://twitter.com/PearlJamOnline/status/1509765602633822243/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1528,
+								"h": 1528,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1509765597764235300,
+						"id_str": "1509765597764235305",
+						"indices": [
+							99,
+							122
+						],
+						"media_url": "http://pbs.twimg.com/media/FPPDz-TXsCkpwwv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FPPDz-TXsCkpwwv.jpg",
+						"url": "https://t.co/w1vfut4N34",
+						"display_url": "pic.twitter.com/w1vfut4N34",
+						"expanded_url": "https://twitter.com/PearlJamOnline/status/1509765602633822243/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1528,
+								"h": 1528,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 27473819,
+				"id_str": "27473819",
+				"name": "PearlJamOnline.it",
+				"screen_name": "PearlJamOnline",
+				"location": "Reggio Emilia, Italy",
+				"description": "🇮🇹 @PearlJam italian fansite since 2001. Run by fans, for fans. Follow the strangest tribe...",
+				"url": "https://t.co/VwnPhGUTy6",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/VwnPhGUTy6",
+								"expanded_url": "http://pearljamonline.it",
+								"display_url": "pearljamonline.it",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 42107,
+				"friends_count": 2616,
+				"listed_count": 412,
+				"created_at": "Sun Mar 29 18:24:06 +0000 2009",
+				"favourites_count": 8530,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 24283,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "17160F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/976448147919646721/HxYSlD2O_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/976448147919646721/HxYSlD2O_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/27473819/1632325016",
+				"profile_link_color": "9D582E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EBE79F",
+				"profile_text_color": "0A080A",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 29,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Was ik er toch bijna ingetrapt...nog voor mijn eerste koffie. https://t.co/ih8t1Lgtl8"
+	}
+]
