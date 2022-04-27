3149c19b36849fc91393a5a0cb69623584d4a1b5
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-04-27 17:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75286,
-	"created_at": "Wed Apr 27 12:21:04 +0000 2022",
+	"tweets": 75287,
+	"created_at": "Wed Apr 27 15:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 27 14:37:33 +0000 2022",
+		"id": 1519324880285716500,
+		"id_str": "1519324880285716482",
+		"full_text": "@miekeroth @ernohannink @Stuxhost Dat kun jij.",
+		"truncated": false,
+		"display_text_range": [
+			34,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "miekeroth",
+					"name": "Mieke Roth",
+					"id": 259871223,
+					"id_str": "259871223",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "ernohannink",
+					"name": "Erno Hannink - Decide for Impact",
+					"id": 1477191,
+					"id_str": "1477191",
+					"indices": [
+						11,
+						23
+					]
+				},
+				{
+					"screen_name": "Stuxhost",
+					"name": "Stuxhost.com⚡️",
+					"id": 1345507602919272400,
+					"id_str": "1345507602919272450",
+					"indices": [
+						24,
+						33
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1519322518867398700,
+		"in_reply_to_status_id_str": "1519322518867398656",
+		"in_reply_to_user_id": 259871223,
+		"in_reply_to_user_id_str": "259871223",
+		"in_reply_to_screen_name": "miekeroth",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8743,
+			"friends_count": 880,
+			"listed_count": 550,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9792,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66610,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@miekeroth @ernohannink @Stuxhost Dat kun jij."
+	},
+	{
 		"created_at": "Wed Apr 27 11:35:53 +0000 2022",
 		"id": 1519269075565830100,
 		"id_str": "1519279161814175745",
