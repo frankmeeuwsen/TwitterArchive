b59fe441c09537ed92aec1a2741ae4f1d89208bd
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-05-13 23:21:04 +0200

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,257 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 13 21:17:46 +0000 2022",
+		"id": 1525223804552810500,
+		"id_str": "1525223804552810498",
+		"full_text": "Marsfabriekjes @wilbertleering !! https://t.co/wTZEQEtyNe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wilbertleering",
+					"name": "Wilbert Leering",
+					"id": 11021172,
+					"id_str": "11021172",
+					"indices": [
+						15,
+						30
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wTZEQEtyNe",
+					"expanded_url": "https://twitter.com/dvhn_nl/status/1525182476351442944",
+					"display_url": "twitter.com/dvhn_nl/status…",
+					"indices": [
+						34,
+						57
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8735,
+			"friends_count": 872,
+			"listed_count": 550,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9823,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66649,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1525182476351443000,
+		"quoted_status_id_str": "1525182476351442944",
+		"quoted_status_permalink": {
+			"url": "https://t.co/wTZEQEtyNe",
+			"expanded": "https://twitter.com/dvhn_nl/status/1525182476351442944",
+			"display": "twitter.com/dvhn_nl/status…"
+		},
+		"quoted_status": {
+			"created_at": "Fri May 13 18:33:33 +0000 2022",
+			"id": 1525182476351443000,
+			"id_str": "1525182476351442944",
+			"full_text": "Mysterieuze 'deur' ontdekt op planeet Mars https://t.co/SAMQfMjx2d",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				66
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/SAMQfMjx2d",
+						"expanded_url": "http://dlvr.it/SQKCJq",
+						"display_url": "dlvr.it/SQKCJq",
+						"indices": [
+							43,
+							66
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18549902,
+				"id_str": "18549902",
+				"name": "Dagblad van het Noorden",
+				"screen_name": "dvhn_nl",
+				"location": "Groningen",
+				"description": "Nieuws en de achtergronden uit Groningen en Drenthe van Dagblad van het Noorden: https://t.co/171v1Lrpct. Tips? mail naar: redactie@dvhn.nl",
+				"url": "https://t.co/171v1Lrpct",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/171v1Lrpct",
+								"expanded_url": "http://www.dvhn.nl",
+								"display_url": "dvhn.nl",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/171v1Lrpct",
+								"expanded_url": "http://www.dvhn.nl",
+								"display_url": "dvhn.nl",
+								"indices": [
+									81,
+									104
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 96561,
+				"friends_count": 759,
+				"listed_count": 602,
+				"created_at": "Fri Jan 02 10:34:12 +0000 2009",
+				"favourites_count": 1896,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 196348,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "0064AA",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1214162263323447296/3bm_u44g_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1214162263323447296/3bm_u44g_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18549902/1635621616",
+				"profile_link_color": "6BB3E2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "B8D6DB",
+				"profile_text_color": "001AC2",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "nl"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Marsfabriekjes @wilbertleering !! https://t.co/wTZEQEtyNe"
+	},
+	{
 		"created_at": "Fri May 13 18:38:19 +0000 2022",
 		"id": 1525183674949435400,
 		"id_str": "1525183674949435392",
