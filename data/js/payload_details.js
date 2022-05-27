5f77a4008b3671ddc6f6352d1fc239328d49ca41
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-05-27 12:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75375,
-	"created_at": "Thu May 26 10:21:03 +0000 2022",
+	"tweets": 75376,
+	"created_at": "Fri May 27 10:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 27 10:11:21 +0000 2022",
+		"id": 1530129522917613600,
+		"id_str": "1530129522917613569",
+		"full_text": "@martijn @Pathe Fijn dat je inmiddels bent geholpen en we bewijs hebben dat UX best een lastig dingetje is voor app-makers. \n\nMaar dé vraag blijft: naar welke film ga je?",
+		"truncated": false,
+		"display_text_range": [
+			16,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "martijn",
+					"name": "Martijn van Es",
+					"id": 1038,
+					"id_str": "1038",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "Pathe",
+					"name": "Pathé",
+					"id": 29402247,
+					"id_str": "29402247",
+					"indices": [
+						9,
+						15
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1530111704696340500,
+		"in_reply_to_status_id_str": "1530111704696340480",
+		"in_reply_to_user_id": 1038,
+		"in_reply_to_user_id_str": "1038",
+		"in_reply_to_screen_name": "martijn",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. \n\n#PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8738,
+			"friends_count": 868,
+			"listed_count": 550,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9878,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66721,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@martijn @Pathe Fijn dat je inmiddels bent geholpen en we bewijs hebben dat UX best een lastig dingetje is voor app-makers. \n\nMaar dé vraag blijft: naar welke film ga je?"
+	},
+	{
 		"created_at": "Thu May 26 10:18:21 +0000 2022",
 		"id": 1529768896462565400,
 		"id_str": "1529768896462565377",
