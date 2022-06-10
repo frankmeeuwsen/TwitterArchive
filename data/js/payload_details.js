3dcb979040741579d9cee159f55e2e91d58a853c
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-06-10 19:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75431,
-	"created_at": "Fri Jun 10 13:21:04 +0000 2022",
+	"tweets": 75432,
+	"created_at": "Fri Jun 10 17:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 10 16:57:02 +0000 2022",
+		"id": 1535305045901729800,
+		"id_str": "1535305045901729792",
+		"full_text": "Waitwhat? https://t.co/vRAc1UcpfB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vRAc1UcpfB",
+					"expanded_url": "https://twitter.com/getmatterapp/status/1535304397126873088",
+					"display_url": "twitter.com/getmatterapp/s…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8739,
+			"friends_count": 870,
+			"listed_count": 550,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 9967,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66788,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1535304397126873000,
+		"quoted_status_id_str": "1535304397126873088",
+		"quoted_status_permalink": {
+			"url": "https://t.co/vRAc1UcpfB",
+			"expanded": "https://twitter.com/getmatterapp/status/1535304397126873088",
+			"display": "twitter.com/getmatterapp/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jun 10 16:54:27 +0000 2022",
+			"id": 1535304397126873000,
+			"id_str": "1535304397126873088",
+			"full_text": "@DonniHonig Who knows what next week will bring ;)",
+			"truncated": false,
+			"display_text_range": [
+				12,
+				50
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "DonniHonig",
+						"name": "Donni Honig",
+						"id": 1206320693677379600,
+						"id_str": "1206320693677379586",
+						"indices": [
+							0,
+							11
+						]
+					}
+				],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": 1535304243472638000,
+			"in_reply_to_status_id_str": "1535304243472637954",
+			"in_reply_to_user_id": 1206320693677379600,
+			"in_reply_to_user_id_str": "1206320693677379586",
+			"in_reply_to_screen_name": "DonniHonig",
+			"user": {
+				"id": 1232479427981791200,
+				"id_str": "1232479427981791233",
+				"name": "Matter",
+				"screen_name": "getmatterapp",
+				"location": "",
+				"description": "Words are my matter. (Ursula K. Le Guin)\n\nWall of love: https://t.co/myCDXholgf",
+				"url": "https://t.co/HpaEubyYCZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/HpaEubyYCZ",
+								"expanded_url": "http://getmatter.com",
+								"display_url": "getmatter.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/myCDXholgf",
+								"expanded_url": "https://twitter.com/getmatterapp/timelines/1353843820639739906",
+								"display_url": "twitter.com/getmatterapp/t…",
+								"indices": [
+									56,
+									79
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 6631,
+				"friends_count": 5,
+				"listed_count": 184,
+				"created_at": "Wed Feb 26 01:36:21 +0000 2020",
+				"favourites_count": 2070,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1119,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1445088103979388936/LWkZZVSQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1445088103979388936/LWkZZVSQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1232479427981791233/1607026490",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Waitwhat? https://t.co/vRAc1UcpfB"
+	},
+	{
 		"created_at": "Fri Jun 10 12:46:52 +0000 2022",
 		"id": 1535227228006621200,
 		"id_str": "1535242092787777536",
