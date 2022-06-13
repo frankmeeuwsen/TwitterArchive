45e7c826bb0bf52ceba507714f78bd323166943e
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-06-13 07:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75445,
-	"created_at": "Sun Jun 12 21:21:03 +0000 2022",
+	"tweets": 75446,
+	"created_at": "Mon Jun 13 05:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Mon Jun 13 04:25:03 +0000 2022",
+		"id": 1536154422341881900,
+		"id_str": "1536202967400992768",
+		"full_text": "I Photoshop paddington into a movie or TV show until I forget: Day 461 https://t.co/bVJ0SCa5DL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1536154407250763800,
+					"id_str": "1536154407250763776",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FVGES2RVIAAuDl0.png",
+					"media_url_https": "https://pbs.twimg.com/media/FVGES2RVIAAuDl0.png",
+					"url": "https://t.co/bVJ0SCa5DL",
+					"display_url": "pic.twitter.com/bVJ0SCa5DL",
+					"expanded_url": "https://twitter.com/jaythechou/status/1536154422341881856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1536154407250763800,
+					"id_str": "1536154407250763776",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FVGES2RVIAAuDl0.png",
+					"media_url_https": "https://pbs.twimg.com/media/FVGES2RVIAAuDl0.png",
+					"url": "https://t.co/bVJ0SCa5DL",
+					"display_url": "pic.twitter.com/bVJ0SCa5DL",
+					"expanded_url": "https://twitter.com/jaythechou/status/1536154422341881856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1385246018619408400,
+			"id_str": "1385246018619408386",
+			"name": "Jaythechou",
+			"screen_name": "jaythechou",
+			"location": "",
+			"description": "And the Oscar goes to Paddington 3\nYou can find my other Paddington Photoshops at https://t.co/HPXhncHKz6",
+			"url": "https://t.co/cwDHL2Irqq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cwDHL2Irqq",
+							"expanded_url": "https://www.patreon.com/jaythechou",
+							"display_url": "patreon.com/jaythechou",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/HPXhncHKz6",
+							"expanded_url": "https://www.reddit.com/user/JaytheChou/",
+							"display_url": "reddit.com/user/JaytheCho…",
+							"indices": [
+								82,
+								105
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 266741,
+			"friends_count": 18,
+			"listed_count": 320,
+			"created_at": "Thu Apr 22 14:56:14 +0000 2021",
+			"favourites_count": 341,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1433702115805782020/sKCDbFXr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1433702115805782020/sKCDbFXr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1385246018619408386/1634625722",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1113,
+		"favorite_count": 12663,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I Photoshop paddington into a movie or TV show until I forget: Day 461 https://t.co/bVJ0SCa5DL"
+	},
+	{
 		"created_at": "Sun Jun 12 20:27:50 +0000 2022",
 		"id": 1536052394374713300,
 		"id_str": "1536082871097622528",
