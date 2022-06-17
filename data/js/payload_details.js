4ac4270a4b2882d33db4393afb3111133c45df05
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-06-17 20:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75464,
-	"created_at": "Fri Jun 17 16:21:04 +0000 2022",
+	"tweets": 75465,
+	"created_at": "Fri Jun 17 18:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Fri Jun 17 18:07:40 +0000 2022",
+		"id": 1537859538564616200,
+		"id_str": "1537859538564616193",
+		"full_text": "@MrsMartine “Volgens mij wil je dochter van de glijbaan”, zei de oudere vrouw tegen me, wijzend naar mijn 7-jarige zoon met lang haar. \n“Je bedoelt mijn zoon?”\n…\n…\n“Je zoon? Weet je het zeker?” https://t.co/afh9snIFb0",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			193
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MrsMartine",
+					"name": "Martine",
+					"id": 87220677,
+					"id_str": "87220677",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1537859531094610000,
+					"id_str": "1537859531094609920",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FVeTGHlX0AAir-a.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FVeTGHlX0AAir-a.jpg",
+					"url": "https://t.co/afh9snIFb0",
+					"display_url": "pic.twitter.com/afh9snIFb0",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1537859538564616193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 498,
+							"h": 230,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 230,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 230,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1537859531094610000,
+					"id_str": "1537859531094609920",
+					"indices": [
+						194,
+						217
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FVeTGHlX0AAir-a.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FVeTGHlX0AAir-a.jpg",
+					"url": "https://t.co/afh9snIFb0",
+					"display_url": "pic.twitter.com/afh9snIFb0",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1537859538564616193/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"small": {
+							"w": 498,
+							"h": 230,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 498,
+							"h": 230,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 498,
+							"h": 230,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							249,
+							115
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FVeTGHlX0AAir-a.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1537795168442667000,
+		"in_reply_to_status_id_str": "1537795168442667010",
+		"in_reply_to_user_id": 87220677,
+		"in_reply_to_user_id_str": "87220677",
+		"in_reply_to_screen_name": "MrsMartine",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8740,
+			"friends_count": 876,
+			"listed_count": 550,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10000,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66825,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1331927626672386049/wNJUeQtO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "@MrsMartine “Volgens mij wil je dochter van de glijbaan”, zei de oudere vrouw tegen me, wijzend naar mijn 7-jarige zoon met lang haar. \n“Je bedoelt mijn zoon?”\n…\n…\n“Je zoon? Weet je het zeker?” https://t.co/afh9snIFb0"
+	},
+	{
 		"created_at": "Fri Jun 17 14:45:06 +0000 2022",
 		"id": 1537808559425081300,
 		"id_str": "1537808559425081344",
