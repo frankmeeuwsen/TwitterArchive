0d954595c2e57aa056ab4adaec7d427a259be25d
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-07-11 15:21:04 +0200

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Mon Jul 11 11:34:57 +0000 2022",
+		"id": 1546159593780236300,
+		"id_str": "1546458015431380995",
+		"full_text": "“It’s Adam and Eve not Adam and Steve” well it’s also the Bible and not the Straightble.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14641347,
+			"id_str": "14641347",
+			"name": "Daniel Kibblesmith",
+			"screen_name": "kibblesmith",
+			"location": "Hollywood, U.S.A.",
+			"description": "Writer TV, Comic Books, Book Books. @Netflix, @Marvel, @DCComics, @ColbertLateShow, @ClickHole, #PrincessDinosaur 👑🦖 Married to @jenashleywright. Dad.",
+			"url": "https://t.co/CT6X4A04hn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CT6X4A04hn",
+							"expanded_url": "http://Kibblesmith.com",
+							"display_url": "Kibblesmith.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 105354,
+			"friends_count": 5250,
+			"listed_count": 1228,
+			"created_at": "Sat May 03 20:42:01 +0000 2008",
+			"favourites_count": 224294,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 104242,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EFEFEF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1516115908716081162/jH3sCTEl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1516115908716081162/jH3sCTEl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14641347/1652801243",
+			"profile_link_color": "ADADAD",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3561,
+		"favorite_count": 43033,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "“It’s Adam and Eve not Adam and Steve” well it’s also the Bible and not the Straightble."
+	},
+	{
 		"created_at": "Fri Jul 08 12:45:25 +0000 2022",
 		"id": 1545388584869535700,
 		"id_str": "1545388584869535752",
