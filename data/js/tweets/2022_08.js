471843a016cf4acc59cd165770a82f459e0e5d91
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-08-20 17:21:04 +0200

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 20 14:29:00 +0000 2022",
+		"id": 1560997333856387000,
+		"id_str": "1560997333856387073",
+		"full_text": "@steeph Eigenlijk ben jij voor Lowlands wat @rawi1981 voor @DeslimstemensNL is ;-)",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "steeph",
+					"name": "𝓢tephan Okhuijsen",
+					"id": 7692762,
+					"id_str": "7692762",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "rawi1981",
+					"name": "Ramon Wiendels",
+					"id": 137479449,
+					"id_str": "137479449",
+					"indices": [
+						44,
+						53
+					]
+				},
+				{
+					"screen_name": "DeslimstemensNL",
+					"name": "DeslimstemensNL",
+					"id": 702771146,
+					"id_str": "702771146",
+					"indices": [
+						59,
+						75
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1560542966443348000,
+		"in_reply_to_status_id_str": "1560542966443347969",
+		"in_reply_to_user_id": 7692762,
+		"in_reply_to_user_id_str": "7692762",
+		"in_reply_to_screen_name": "steeph",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8733,
+			"friends_count": 861,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10087,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 66975,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@steeph Eigenlijk ben jij voor Lowlands wat @rawi1981 voor @DeslimstemensNL is ;-)"
+	},
+	{
 		"created_at": "Sat Aug 20 14:00:30 +0000 2022",
 		"id": 1560990157641339000,
 		"id_str": "1560990157641338880",
