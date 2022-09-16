69540867cf8df80946de764b0f012ec4b27c5303
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-09-16 20:21:05 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75644,
-	"created_at": "Fri Sep 16 13:21:04 +0000 2022",
+	"tweets": 75646,
+	"created_at": "Fri Sep 16 18:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 16 17:40:21 +0000 2022",
+		"id": 1570829957999513600,
+		"id_str": "1570829957999513600",
+		"full_text": "@wilbertleering @liekebie Dat klinkt als in de buurt van hier. Biertjes bij Jan Primus!",
+		"truncated": false,
+		"display_text_range": [
+			26,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wilbertleering",
+					"name": "Wilbert Leering",
+					"id": 11021172,
+					"id_str": "11021172",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "liekebie",
+					"name": "Lieke",
+					"id": 49404029,
+					"id_str": "49404029",
+					"indices": [
+						16,
+						25
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1570732266145583000,
+		"in_reply_to_status_id_str": "1570732266145583106",
+		"in_reply_to_user_id": 11021172,
+		"in_reply_to_user_id_str": "11021172",
+		"in_reply_to_screen_name": "wilbertleering",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8733,
+			"friends_count": 876,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 67049,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@wilbertleering @liekebie Dat klinkt als in de buurt van hier. Biertjes bij Jan Primus!"
+	},
+	{
+		"created_at": "Fri Sep 16 17:38:24 +0000 2022",
+		"id": 1570729092508700700,
+		"id_str": "1570829466330595329",
+		"full_text": "We gaan na 18 gelukkige jaren ons huis verlaten. Het huis ligt midden in de Wittenvrouwenbuurt, grenzend aan het centrum. Zegt het voort. Delen wordt zeer gewaardeerd natuurlijk ❤️\n\nhttps://t.co/AvloCTWnN9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			205
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AvloCTWnN9",
+					"expanded_url": "https://www.funda.nl/koop/utrecht/huis-42959018-bouwstraat-50/",
+					"display_url": "funda.nl/koop/utrecht/h…",
+					"indices": [
+						182,
+						205
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 11021172,
+			"id_str": "11021172",
+			"name": "Wilbert Leering",
+			"screen_name": "wilbertleering",
+			"location": "Utrecht, The Netherlands",
+			"description": "Playlists (@perfectsnl & Apple Music NL), concepts, https://t.co/z7QSQgdRqK, tegenwindfietsen (@nktegenwind) en podcast (@fuckwijn)",
+			"url": "https://t.co/z7QSQgdRqK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/z7QSQgdRqK",
+							"expanded_url": "http://allheadsondeck.nl",
+							"display_url": "allheadsondeck.nl",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/z7QSQgdRqK",
+							"expanded_url": "http://allheadsondeck.nl",
+							"display_url": "allheadsondeck.nl",
+							"indices": [
+								52,
+								75
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1870,
+			"friends_count": 1241,
+			"listed_count": 34,
+			"created_at": "Mon Dec 10 15:51:11 +0000 2007",
+			"favourites_count": 4754,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29994,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550141449769029632/S5hRFCHi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550141449769029632/S5hRFCHi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/11021172/1603311739",
+			"profile_link_color": "0A0A0A",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 7,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "We gaan na 18 gelukkige jaren ons huis verlaten. Het huis ligt midden in de Wittenvrouwenbuurt, grenzend aan het centrum. Zegt het voort. Delen wordt zeer gewaardeerd natuurlijk ❤️\n\nhttps://t.co/AvloCTWnN9"
+	},
+	{
 		"created_at": "Fri Sep 16 13:01:48 +0000 2022",
 		"id": 1570759860261912600,
 		"id_str": "1570759860261912578",
