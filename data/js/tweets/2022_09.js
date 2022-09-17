f215614451c27c03652fbebcf70f386f0ee78074
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-09-17 10:21:04 +0200

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Sat Sep 17 07:37:18 +0000 2022",
+		"id": 1570901369707692000,
+		"id_str": "1571040586458894336",
+		"full_text": "https://t.co/S67UzE6fXT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1570901364527571000,
+					"id_str": "1570901364527570944",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/Fcz2d9HXoAAes5y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fcz2d9HXoAAes5y.jpg",
+					"url": "https://t.co/S67UzE6fXT",
+					"display_url": "pic.twitter.com/S67UzE6fXT",
+					"expanded_url": "https://twitter.com/weirddalle/status/1570901369707692035/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1170,
+							"h": 1409,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 996,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 565,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1570901364527571000,
+					"id_str": "1570901364527570944",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/Fcz2d9HXoAAes5y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fcz2d9HXoAAes5y.jpg",
+					"url": "https://t.co/S67UzE6fXT",
+					"display_url": "pic.twitter.com/S67UzE6fXT",
+					"expanded_url": "https://twitter.com/weirddalle/status/1570901369707692035/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1170,
+							"h": 1409,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 996,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 565,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1492677599390322700,
+			"id_str": "1492677599390322689",
+			"name": "Weird Dall-E Mini Generations",
+			"screen_name": "weirddalle",
+			"location": "dm for promo",
+			"description": "submit in subreddit (link in bio) | follow personal @huhwtfthe | create your own here: https://t.co/MaWctZIJ3N",
+			"url": "https://t.co/HG2mH9RQsH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HG2mH9RQsH",
+							"expanded_url": "https://bit.ly/3AKTmXD",
+							"display_url": "bit.ly/3AKTmXD",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/MaWctZIJ3N",
+							"expanded_url": "https://www.craiyon.com",
+							"display_url": "craiyon.com",
+							"indices": [
+								87,
+								110
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1124752,
+			"friends_count": 208,
+			"listed_count": 998,
+			"created_at": "Sun Feb 13 01:50:55 +0000 2022",
+			"favourites_count": 382,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1097,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1554789079979548675/KU0ZS_vJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1554789079979548675/KU0ZS_vJ_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2903,
+		"favorite_count": 42441,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/S67UzE6fXT"
+	},
+	{
 		"created_at": "Sat Sep 17 07:13:00 +0000 2022",
 		"id": 1571034468160540700,
 		"id_str": "1571034468160540675",
