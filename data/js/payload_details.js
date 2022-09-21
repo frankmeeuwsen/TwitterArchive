be3963d17f864c9d67e7ca25b59e180d0a23b527
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-09-21 12:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75664,
-	"created_at": "Wed Sep 21 09:21:04 +0000 2022",
+	"tweets": 75665,
+	"created_at": "Wed Sep 21 10:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,10 +1,147 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 21 09:22:35 +0000 2022",
+		"id": 1572516630000959500,
+		"id_str": "1572516630000959489",
+		"full_text": "@msteeman @boerdeboer @PVV Wat is bezoldiging?",
+		"truncated": false,
+		"display_text_range": [
+			27,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "msteeman",
+					"name": "Marcel Steeman",
+					"id": 19087381,
+					"id_str": "19087381",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "boerdeboer",
+					"name": "boerdeboer -",
+					"id": 11798112,
+					"id_str": "11798112",
+					"indices": [
+						10,
+						21
+					]
+				},
+				{
+					"screen_name": "PVV",
+					"name": "‏⁵¹¹ ᓚᓅȷﾕ ᓚᓅյ̇ᘓ",
+					"id": 14193927,
+					"id_str": "14193927",
+					"indices": [
+						22,
+						26
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1572514218116616200,
+		"in_reply_to_status_id_str": "1572514218116616193",
+		"in_reply_to_user_id": 19087381,
+		"in_reply_to_user_id_str": "19087381",
+		"in_reply_to_screen_name": "msteeman",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8730,
+			"friends_count": 876,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10153,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 67072,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@msteeman @boerdeboer @PVV Wat is bezoldiging?"
+	},
+	{
 		"created_at": "Wed Sep 21 08:50:25 +0000 2022",
 		"id": 1572508537145725000,
 		"id_str": "1572508537145724928",
-		"full_text": "Tof om te zien hoe al het werk van onze teams bij ⁦@Kaliberinteract⁩ nu samenkomt in deze nieuwe campagne voor KLM  https://t.co/edRExOIpwH",
+		"full_text": "Tof om te zien hoe al het werk van onze teams bij @Kaliberinteract nu samenkomt in deze nieuwe campagne voor KLM  https://t.co/edRExOIpwH",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -126,7 +263,7 @@ Grailbird.data.tweets_2022_09 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "nl",
-		"text": "Tof om te zien hoe al het werk van onze teams bij ⁦@Kaliberinteract⁩ nu samenkomt in deze nieuwe campagne voor KLM  https://t.co/edRExOIpwH"
+		"text": "Tof om te zien hoe al het werk van onze teams bij @Kaliberinteract nu samenkomt in deze nieuwe campagne voor KLM  https://t.co/edRExOIpwH"
 	},
 	{
 		"created_at": "Tue Sep 20 16:16:32 +0000 2022",
