599defc8a520170870fc2fac5d061f4498f2a2fc
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-09-28 21:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75678,
-	"created_at": "Wed Sep 28 16:21:04 +0000 2022",
+	"tweets": 75679,
+	"created_at": "Wed Sep 28 19:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,353 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 28 18:49:37 +0000 2022",
+		"id": 1575196046371668000,
+		"id_str": "1575196046371667969",
+		"full_text": "KITT to the Max. https://t.co/782KNkAWg3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			16
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/782KNkAWg3",
+					"expanded_url": "https://twitter.com/fleddymelculy/status/1575192699669544962",
+					"display_url": "twitter.com/fleddymelculy/…",
+					"indices": [
+						17,
+						40
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8726,
+			"friends_count": 882,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10184,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 67089,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1575192699669545000,
+		"quoted_status_id_str": "1575192699669544962",
+		"quoted_status_permalink": {
+			"url": "https://t.co/782KNkAWg3",
+			"expanded": "https://twitter.com/fleddymelculy/status/1575192699669544962",
+			"display": "twitter.com/fleddymelculy/…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 28 18:36:19 +0000 2022",
+			"id": 1575192699669545000,
+			"id_str": "1575192699669544962",
+			"full_text": "What if you mashup Sepultura’s ROOTS with the Knight Rider theme… #djfleddymelculy https://t.co/Ikmkj0XPvo",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				82
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "djfleddymelculy",
+						"indices": [
+							66,
+							82
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1575192604412678100,
+						"id_str": "1575192604412678149",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1575192604412678149/pu/img/EJ8m4E5A2XIhzZP6.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1575192604412678149/pu/img/EJ8m4E5A2XIhzZP6.jpg",
+						"url": "https://t.co/Ikmkj0XPvo",
+						"display_url": "pic.twitter.com/Ikmkj0XPvo",
+						"expanded_url": "https://twitter.com/fleddymelculy/status/1575192699669544962/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 540,
+								"h": 960,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 540,
+								"h": 960,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1575192604412678100,
+						"id_str": "1575192604412678149",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1575192604412678149/pu/img/EJ8m4E5A2XIhzZP6.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1575192604412678149/pu/img/EJ8m4E5A2XIhzZP6.jpg",
+						"url": "https://t.co/Ikmkj0XPvo",
+						"display_url": "pic.twitter.com/Ikmkj0XPvo",
+						"expanded_url": "https://twitter.com/fleddymelculy/status/1575192699669544962/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 540,
+								"h": 960,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 540,
+								"h": 960,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								9,
+								16
+							],
+							"duration_millis": 68216,
+							"variants": [
+								{
+									"bitrate": 950000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1575192604412678149/pu/vid/480x852/rstS-9-mrTswPqKq.mp4?tag=12"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1575192604412678149/pu/pl/EYSjXJrzxYGJDAgI.m3u8?tag=12&container=fmp4"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1575192604412678149/pu/vid/540x960/6xzDm8HEOmBswARY.mp4?tag=12"
+								},
+								{
+									"bitrate": 632000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1575192604412678149/pu/vid/320x568/4668JSHU5b1clp3S.mp4?tag=12"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 720695376517455900,
+				"id_str": "720695376517455872",
+				"name": "Fleddy Melculy",
+				"screen_name": "fleddymelculy",
+				"location": "Hel",
+				"description": "Ticketlinks Benelux clubtour @ https://t.co/L6r1jMTWJ1",
+				"url": "https://t.co/606Oad65NI",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/606Oad65NI",
+								"expanded_url": "http://www.fleddymelculy.com",
+								"display_url": "fleddymelculy.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/L6r1jMTWJ1",
+								"expanded_url": "http://www.fleddymelculy.com/tour",
+								"display_url": "fleddymelculy.com/tour",
+								"indices": [
+									31,
+									54
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 985,
+				"friends_count": 16,
+				"listed_count": 12,
+				"created_at": "Thu Apr 14 19:29:02 +0000 2016",
+				"favourites_count": 2001,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 3705,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1487178350598758400/pQR54sRd_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487178350598758400/pQR54sRd_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/720695376517455872/1571566290",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 10,
+			"favorite_count": 23,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "KITT to the Max. https://t.co/782KNkAWg3"
+	},
+	{
 		"created_at": "Wed Sep 28 16:01:47 +0000 2022",
 		"id": 1575153809642365000,
 		"id_str": "1575153809642364929",
