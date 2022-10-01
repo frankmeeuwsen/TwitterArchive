06f445ce5dbd08e1fd1486390d35f3c8cb6190e1
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-10-01 16:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75691,
-	"created_at": "Fri Sep 30 16:21:04 +0000 2022",
+	"tweets": 75692,
+	"created_at": "Sat Oct 01 14:21:04 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_10.js
@@ -0,0 +1,268 @@
+Grailbird.data.tweets_2022_10 = 
+[
+	{
+		"created_at": "Sat Oct 01 14:15:14 +0000 2022",
+		"id": 1576214158122815500,
+		"id_str": "1576214158122815489",
+		"full_text": "Prima plekje in Kreuzberg gevonden. Punkrock platenzaak met eigen pils. #kaliberlin https://t.co/cE6sw8MoOi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "kaliberlin",
+					"indices": [
+						72,
+						83
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1576214150715523000,
+					"id_str": "1576214150715523074",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_Waz9XwAIn5LM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_Waz9XwAIn5LM.jpg",
+					"url": "https://t.co/cE6sw8MoOi",
+					"display_url": "pic.twitter.com/cE6sw8MoOi",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1576214158122815489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1576214150715523000,
+					"id_str": "1576214150715523074",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_Waz9XwAIn5LM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_Waz9XwAIn5LM.jpg",
+					"url": "https://t.co/cE6sw8MoOi",
+					"display_url": "pic.twitter.com/cE6sw8MoOi",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1576214158122815489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1576214150748971000,
+					"id_str": "1576214150748971008",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_Wa0FWIAAc__A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_Wa0FWIAAc__A.jpg",
+					"url": "https://t.co/cE6sw8MoOi",
+					"display_url": "pic.twitter.com/cE6sw8MoOi",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1576214158122815489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1576214150744875000,
+					"id_str": "1576214150744875008",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_Wa0EXoAAAKJT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_Wa0EXoAAAKJT.jpg",
+					"url": "https://t.co/cE6sw8MoOi",
+					"display_url": "pic.twitter.com/cE6sw8MoOi",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1576214158122815489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8725,
+			"friends_count": 882,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10197,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 67104,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Prima plekje in Kreuzberg gevonden. Punkrock platenzaak met eigen pils. #kaliberlin https://t.co/cE6sw8MoOi"
+	}
+]
