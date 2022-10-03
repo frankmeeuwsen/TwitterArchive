c938d443a9fe9a0dd8fa825f84dd4ad3314b2dd8
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-10-03 21:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75692,
-	"created_at": "Sat Oct 01 14:21:04 +0000 2022",
+	"tweets": 75693,
+	"created_at": "Mon Oct 03 19:21:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,307 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Mon Oct 03 19:18:55 +0000 2022",
+		"id": 1577015359940759600,
+		"id_str": "1577015359940759553",
+		"full_text": "Foto's tijdens de demonstratie in Berlijn #MahsaAmini https://t.co/03Z8rOwvXj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MahsaAmini",
+					"indices": [
+						42,
+						53
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1577015240532836400,
+					"id_str": "1577015240532836352",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/FeKvAYFWQAAJLeo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeKvAYFWQAAJLeo.jpg",
+					"url": "https://t.co/03Z8rOwvXj",
+					"display_url": "pic.twitter.com/03Z8rOwvXj",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1577015359940759553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1577015240532836400,
+					"id_str": "1577015240532836352",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/FeKvAYFWQAAJLeo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeKvAYFWQAAJLeo.jpg",
+					"url": "https://t.co/03Z8rOwvXj",
+					"display_url": "pic.twitter.com/03Z8rOwvXj",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1577015359940759553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1577015240763506700,
+					"id_str": "1577015240763506694",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/FeKvAY8WAAYVw8a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeKvAY8WAAYVw8a.jpg",
+					"url": "https://t.co/03Z8rOwvXj",
+					"display_url": "pic.twitter.com/03Z8rOwvXj",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1577015359940759553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1577015241107439600,
+					"id_str": "1577015241107439626",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/FeKvAaOWAAoqC0Q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeKvAaOWAAoqC0Q.jpg",
+					"url": "https://t.co/03Z8rOwvXj",
+					"display_url": "pic.twitter.com/03Z8rOwvXj",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1577015359940759553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1577015241447178200,
+					"id_str": "1577015241447178255",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/FeKvAbfWAA8n31o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeKvAbfWAA8n31o.jpg",
+					"url": "https://t.co/03Z8rOwvXj",
+					"display_url": "pic.twitter.com/03Z8rOwvXj",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1577015359940759553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1577015344870609000,
+		"in_reply_to_status_id_str": "1577015344870608896",
+		"in_reply_to_user_id": 816203,
+		"in_reply_to_user_id_str": "816203",
+		"in_reply_to_screen_name": "frankmeeuwsen",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8725,
+			"friends_count": 882,
+			"listed_count": 549,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10197,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 67106,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Foto's tijdens de demonstratie in Berlijn #MahsaAmini https://t.co/03Z8rOwvXj"
+	},
+	{
 		"created_at": "Sat Oct 01 14:15:14 +0000 2022",
 		"id": 1576214158122815500,
 		"id_str": "1576214158122815489",
