c9bf6ef55c79fc7cdc58b0cd6f5fa6aac152bef4
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-10-07 16:21:04 +0200

    Added 1 tweets

--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Fri Oct 07 14:15:38 +0000 2022",
+		"id": 1578388583911772200,
+		"id_str": "1578388583911772160",
+		"full_text": "Ik weet niet meer met wie je de discussie had @erwblo maar de live re-issue van een oud Guns N' Roses nummer in mijn Release Radar knalt er wel lekker in.\n\nNou ja... tot Axl zijn strot open trekt. https://t.co/hyzfpgzpEP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			196
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "erwblo",
+					"name": "Erwin Blom",
+					"id": 713333,
+					"id_str": "713333",
+					"indices": [
+						46,
+						53
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1578388575292375000,
+					"id_str": "1578388575292375040",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FeeQC-vWQAAoO_D.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FeeQC-vWQAAoO_D.jpg",
+					"url": "https://t.co/hyzfpgzpEP",
+					"display_url": "pic.twitter.com/hyzfpgzpEP",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1578388583911772160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 244,
+							"h": 190,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 244,
+							"h": 190,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 244,
+							"h": 190,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1578388575292375000,
+					"id_str": "1578388575292375040",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FeeQC-vWQAAoO_D.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FeeQC-vWQAAoO_D.jpg",
+					"url": "https://t.co/hyzfpgzpEP",
+					"display_url": "pic.twitter.com/hyzfpgzpEP",
+					"expanded_url": "https://twitter.com/frankmeeuwsen/status/1578388583911772160/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 244,
+							"h": 190,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 244,
+							"h": 190,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 244,
+							"h": 190,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							122,
+							95
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FeeQC-vWQAAoO_D.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "Werk: @kaliberInteract Nieuwsbriefgeek: https://t.co/UXJIKcZN2m @ForSubscribing. #PearlJam",
+			"url": "https://t.co/qbBtWV0PBm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWV0PBm",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/UXJIKcZN2m",
+							"expanded_url": "http://Thanksforsubscribing.app",
+							"display_url": "Thanksforsubscribing.app",
+							"indices": [
+								40,
+								63
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8725,
+			"friends_count": 885,
+			"listed_count": 548,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10206,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 67115,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "nl",
+		"text": "Ik weet niet meer met wie je de discussie had @erwblo maar de live re-issue van een oud Guns N' Roses nummer in mijn Release Radar knalt er wel lekker in.\n\nNou ja... tot Axl zijn strot open trekt. https://t.co/hyzfpgzpEP"
+	},
+	{
 		"created_at": "Fri Oct 07 12:49:26 +0000 2022",
 		"id": 1578366894423232500,
 		"id_str": "1578366894423232513",
