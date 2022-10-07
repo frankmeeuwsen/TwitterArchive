84850a954e99eb88b511247b28783b5ab0cbc547
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-10-07 23:21:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75706,
-	"created_at": "Fri Oct 07 18:21:03 +0000 2022",
+	"tweets": 75707,
+	"created_at": "Fri Oct 07 21:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Fri Oct 07 20:38:37 +0000 2022",
+		"id": 1574473389716160500,
+		"id_str": "1578484965133541376",
+		"full_text": "Whoa - @SubstackInc is resurrecting RSS feeds!\n\nhttps://t.co/1wIcImLFiQ https://t.co/TrbFUsemQm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SubstackInc",
+					"name": "Substack",
+					"id": 877295296828522500,
+					"id_str": "877295296828522496",
+					"indices": [
+						7,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1wIcImLFiQ",
+					"expanded_url": "https://substack.com/inbox",
+					"display_url": "substack.com/inbox",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1574473222938136600,
+					"id_str": "1574473222938136595",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/FdmnDemXkBMcsm6.png",
+					"media_url_https": "https://pbs.twimg.com/media/FdmnDemXkBMcsm6.png",
+					"url": "https://t.co/TrbFUsemQm",
+					"display_url": "pic.twitter.com/TrbFUsemQm",
+					"expanded_url": "https://twitter.com/ScottMonty/status/1574473389716160513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 334,
+							"h": 772,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 334,
+							"h": 772,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 294,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1574473222938136600,
+					"id_str": "1574473222938136595",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/FdmnDemXkBMcsm6.png",
+					"media_url_https": "https://pbs.twimg.com/media/FdmnDemXkBMcsm6.png",
+					"url": "https://t.co/TrbFUsemQm",
+					"display_url": "pic.twitter.com/TrbFUsemQm",
+					"expanded_url": "https://twitter.com/ScottMonty/status/1574473389716160513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 334,
+							"h": 772,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 334,
+							"h": 772,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 294,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1035491,
+			"id_str": "1035491",
+			"name": "Scott Monty",
+			"screen_name": "ScottMonty",
+			"location": "All the links:",
+			"description": "I help people become better leaders & storytellers. Keynote speaker. Podcaster. Leadership | Integrity | Empathy⏳⏰Newsletter: https://t.co/7rFAZly01l",
+			"url": "https://t.co/wlWpdya2pR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wlWpdya2pR",
+							"expanded_url": "http://linktr.ee/scottmonty",
+							"display_url": "linktr.ee/scottmonty",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7rFAZly01l",
+							"expanded_url": "http://timelesstimely.com",
+							"display_url": "timelesstimely.com",
+							"indices": [
+								126,
+								149
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 104627,
+			"friends_count": 13056,
+			"listed_count": 7788,
+			"created_at": "Mon Mar 12 19:32:06 +0000 2007",
+			"favourites_count": 15106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 105855,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1532047079174021122/u9PvX-pU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1532047079174021122/u9PvX-pU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1035491/1660153073",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "ADADAE",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 32,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Whoa - @SubstackInc is resurrecting RSS feeds!\n\nhttps://t.co/1wIcImLFiQ https://t.co/TrbFUsemQm"
+	},
+	{
 		"created_at": "Fri Oct 07 18:19:22 +0000 2022",
 		"id": 1578449923434946600,
 		"id_str": "1578449923434946560",
@@ -448,7 +654,7 @@ Grailbird.data.tweets_2022_10 =
 		"user": {
 			"id": 130707728,
 			"id_str": "130707728",
-			"name": "Slightly Venomous Kirk 🏴‍☠️",
+			"name": "Slightly Venomous Kirk 🏴☠️",
 			"screen_name": "jamusp",
 			"location": "Finger Lakes, NY",
 			"description": "Unperfect follower of Yeshua; Constitutionalist, Perfect Husband, Father of two boys; 24 yr Service Technician; hopeful but not naive; mildly amusing to myself",
