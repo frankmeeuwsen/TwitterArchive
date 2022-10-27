37926513b055ecec77249fdde6508b86a9237c9f
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-10-27 23:21:05 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75772,
-	"created_at": "Thu Oct 27 20:21:04 +0000 2022",
+	"tweets": 75773,
+	"created_at": "Thu Oct 27 21:21:05 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,117 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Thu Oct 27 21:01:43 +0000 2022",
+		"id": 1585667940355121200,
+		"id_str": "1585738535482425345",
+		"full_text": "Veel kunstliefhebbers ineens, op Twitter. Als er op de kunstsector bezuinigd wordt, hoor je ze niet. Of dan is het een linkse hobby. \n#meisjemetdeparel #JustStopOil",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "meisjemetdeparel",
+					"indices": [
+						134,
+						151
+					]
+				},
+				{
+					"text": "JustStopOil",
+					"indices": [
+						152,
+						164
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1334788597384405000,
+			"id_str": "1334788597384404992",
+			"name": "Jamal Ouariachi",
+			"screen_name": "JamalOuariachi",
+			"location": "",
+			"description": "Schrijver. Nieuwe roman: Herfstdraad @Querido_nl. Columns voor Trouw, essays voor De Standaard, toneel voor @Theatertroep. Progressief, ondanks alles.",
+			"url": "https://t.co/b5EmI0VIS9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b5EmI0VIS9",
+							"expanded_url": "http://www.jamalouariachi.nl/",
+							"display_url": "jamalouariachi.nl",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4047,
+			"friends_count": 968,
+			"listed_count": 23,
+			"created_at": "Fri Dec 04 09:16:32 +0000 2020",
+			"favourites_count": 13011,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5318,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1442597236832018436/7HcVX6Wr_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1442597236832018436/7HcVX6Wr_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1334788597384404992/1632650753",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 43,
+		"favorite_count": 288,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "nl",
+		"text": "Veel kunstliefhebbers ineens, op Twitter. Als er op de kunstsector bezuinigd wordt, hoor je ze niet. Of dan is het een linkse hobby. \n#meisjemetdeparel #JustStopOil"
+	},
+	{
 		"created_at": "Thu Oct 27 19:35:52 +0000 2022",
 		"id": 1585633428803764200,
 		"id_str": "1585716930518056960",
