25eb13a91fadb9ad6a343d41d94a2b5a6dd31d08
Frank Meeuwsen <frank@incredibleadventure.nl>
2022-11-19 12:21:04 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75879,
-	"created_at": "Sat Nov 19 09:21:04 +0000 2022",
+	"tweets": 75880,
+	"created_at": "Sat Nov 19 11:21:04 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Sat Nov 19 11:07:23 +0000 2022",
+		"id": 1593923890400600000,
+		"id_str": "1593923890400600064",
+		"full_text": "@marcoderksen Ik vind het opvallend dat degenen die bij Twitter blijven werken, zo stil zijn. Maar ja, die moeten nu meters maken natuurlijk. Het idee van \"whaling and culling\" wat ik langs zag komen is zeker mogelijk. Ik hoop dat er niet teveel collateral damage bij zit...",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "marcoderksen",
+					"name": "Marco Derksen 💬",
+					"id": 1252681,
+					"id_str": "1252681",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1593923526074974200,
+		"in_reply_to_status_id_str": "1593923526074974208",
+		"in_reply_to_user_id": 1252681,
+		"in_reply_to_user_id_str": "1252681",
+		"in_reply_to_screen_name": "marcoderksen",
+		"user": {
+			"id": 816203,
+			"id_str": "816203",
+			"name": "Frank Meeuwsen",
+			"screen_name": "frankmeeuwsen",
+			"location": "Utrecht, the Netherlands",
+			"description": "✊Reclaim the Web! \n🦣 https://t.co/hNkdBWhhiT\n\n🔥 @kaliberInteract. \n\n❤️ #PearlJam\n\n🕹👾",
+			"url": "https://t.co/qbBtWViqsU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qbBtWViqsU",
+							"expanded_url": "https://diggingthedigital.com",
+							"display_url": "diggingthedigital.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/hNkdBWhhiT",
+							"expanded_url": "https://indieweb.social/@frank",
+							"display_url": "indieweb.social/@frank",
+							"indices": [
+								21,
+								44
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8680,
+			"friends_count": 837,
+			"listed_count": 541,
+			"created_at": "Tue Mar 06 22:38:45 +0000 2007",
+			"favourites_count": 10343,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 67334,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550881418934468609/ZLLck4yz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/816203/1645736184",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "nl",
+		"text": "@marcoderksen Ik vind het opvallend dat degenen die bij Twitter blijven werken, zo stil zijn. Maar ja, die moeten nu meters maken natuurlijk. Het idee van \"whaling and culling\" wat ik langs zag komen is zeker mogelijk. Ik hoop dat er niet teveel collateral damage bij zit..."
+	},
+	{
 		"created_at": "Sat Nov 19 09:02:09 +0000 2022",
 		"id": 1593892374811480000,
 		"id_str": "1593892374811480064",
